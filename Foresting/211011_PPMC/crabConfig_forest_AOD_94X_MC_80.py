0aeaeef8f61824adf89d0d984913faaebeb3e31e
mitaylor <mitay@mit.edu>
2021-10-12 14:47:44 -0400

    added pp foresting config files

---
+++ Foresting/211011_PPMC/crabConfig_forest_AOD_94X_MC_80.py
@@ -0,0 +1,29 @@
+# OutputBase = '/store/group/phys_heavyions/mitaylor'
+OutputBase = '/store/user/mitaylor/'
+DatasetName = '/QCDPhoton_pThat-80_TuneCP5_5p02TeV_pythia8/RunIIpp5Spring18DR-94X_mc2017_realistic_forppRef5TeV_v1-v1/AODSIM'
+Tag = 'QCDPhoton_pThat-80_TuneCP5_5p02TeV_pythia8_AOD_944_v1'
+
+from WMCore.Configuration import Configuration
+config = Configuration()
+
+config.section_("General")
+config.General.requestName = Tag
+
+config.section_("JobType")
+config.JobType.pluginName = 'Analysis'
+config.JobType.psetName = 'runConfig_forest_AOD_94X_MC.py'
+config.JobType.maxMemoryMB = 2500    # request high memory machines.
+config.JobType.maxJobRuntimeMin = 2750    # request longer runtime, ~48 hours.
+
+config.section_("Data")
+config.Data.inputDataset = DatasetName
+config.Data.inputDBS = 'global'
+config.Data.splitting = 'FileBased'
+config.Data.unitsPerJob = 1
+config.Data.publication = False
+config.Data.outputDatasetTag = Tag
+config.Data.outLFNDirBase = OutputBase + DatasetName + '/'
+
+config.section_("Site")
+# config.Site.storageSite = "T2_CH_CERN"
+config.Site.storageSite = 'T2_US_MIT'
