6771ebd2d4fc4179e0becde6ea70ae4520daf58e
mitaylor <mitay@mit.edu>
2021-10-24 22:23:30 -0400

    add cmssw cmsenv

--- MainAnalysis/condor/RunRegulate.sh
+++ MainAnalysis/condor/RunRegulate.sh
@@ -2,4 +2,10 @@
 
 input=$1
 
+WorkDir=`pwd`
+
+cd /home/submit/mitay/cmssw/CMSSW_10_3_3_patch1/src/
+eval `scramv1 runtime -sh`
+cd $WorkDir
+
 ../bin/regulate ../configs/regulate/ran/${type}.conf /mnt/submit-hi1/d00/higroup/mitay/photons/${type}.root
