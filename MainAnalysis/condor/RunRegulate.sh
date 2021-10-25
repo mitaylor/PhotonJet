c3a0b4385bd83a0fe7b44757e543013c9702168b
mitaylor <mitay@mit.edu>
2021-10-25 09:48:02 -0400

    change condor job output location

--- MainAnalysis/condor/RunRegulate.sh
+++ MainAnalysis/condor/RunRegulate.sh
@@ -8,4 +8,4 @@ cd /home/submit/mitay/cmssw/CMSSW_10_3_3_patch1/src/
 eval `scramv1 runtime -sh`
 cd $WorkDir
 
-../bin/regulate ../configs/regulate/ran/${type}.conf /mnt/submit-hi1/d00/higroup/mitay/photons/${type}.root
+../bin/regulate ../configs/regulate/ran/${type}.conf /mnt/submit-hi1/d00/higroup/mitay/condor/${type}.root
