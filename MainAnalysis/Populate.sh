05a4fb727cac87dd2d0a648f1aa01b914db77e11
mitaylor <mitay@mit.edu>
2021-10-26 16:38:12 -0400

    adding manipulate configs

--- MainAnalysis/Populate.sh
+++ MainAnalysis/Populate.sh
@@ -4,30 +4,17 @@ histogram() {
     tag=$1
 
     echo ./bin/populate configs/populate/populate_${tag}_raw.conf \
-        data/populate_${tag}_raw.root &
+        data/ran/populate_${tag}_raw.root &
     echo ./bin/populate configs/populate/populate_${tag}_bkg.conf \
-        data/populate_${tag}_bkg.root &
-}
-
-nominal() {
-    sample=$1
-
-    histogram ${sample}
-}
-
-systematic() {
-    sample=$1
-    syst=$2
-
-    histogram ${sample}_${syst}
+        data/ran/populate_${tag}_bkg.root &
 }
 
 samples=(pp aa pp_smear_0_10 pp_smear_10_30 pp_smear_30_50 pp_smear_50_90)
 
 for sample in ${samples[@]}; do
-    nominal $sample
+    histogram ${sample}
 
     for syst in jeu_down jeu_up wo_ele_rej qcd qcd_gen_iso; do
-        systematic $sample $syst
+        histogram ${sample}_${syst}
     done
 done
