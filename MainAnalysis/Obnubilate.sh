f1476b85e5ed2cc044654885fc3b04ffcb7dcc02
mitaylor <mitay@mit.edu>
2021-10-27 15:51:16 -0400

    adding obnubilate

--- MainAnalysis/Obnubilate.sh
+++ MainAnalysis/Obnubilate.sh
@@ -2,6 +2,7 @@
 
 samples=(pp aa pp_smear_0_10 pp_smear_10_30 pp_smear_30_50 pp_smear_50_90)
 
+for sample in ${samples[@]}; do
     # final systematic uncertainties
     ./bin/obnubilate configs/obnubilate/ran/obnubilate_${sample}.conf \
         data/ran/obnubilate_${sample}.root
