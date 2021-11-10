56ed5d425890c1e84cfa104a3c1684b82f60013b
mitaylor <mitay@mit.edu>
2021-11-10 18:45:33 -0500

    general: modify populate configs to include all events and modify the script to nohup everything

--- MainAnalysis/Populate.sh
+++ MainAnalysis/Populate.sh
@@ -4,9 +4,9 @@ histogram() {
     tag=$1
 
     nohup ./bin/populate configs/populate/ran/populate_${tag}_raw.conf \
-        data/ran/populate_${tag}_raw.root > log/populate_${tag}_raw_log.root 2>&1 &
+        data/ran/populate_${tag}_raw.root > log/populate_${tag}_raw_log.txt 2>&1 &
     nohup ./bin/populate configs/populate/ran/populate_${tag}_bkg.conf \
-        data/ran/populate_${tag}_bkg.root > log/populate_${tag}_bkg_log.root 2>&1 &
+        data/ran/populate_${tag}_bkg.root > log/populate_${tag}_bkg_log.txt 2>&1 &
 }
 
 samples=(pp aa pp_smear_0_10 pp_smear_10_30 pp_smear_30_50 pp_smear_50_90)
