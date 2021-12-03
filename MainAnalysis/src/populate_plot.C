403f2a6f4a6f99a653ee9e05100acb94019f3d97
mitaylor <mitay@mit.edu>
2021-12-03 14:34:38 -0500

    populate: update plotting to include all plots

--- MainAnalysis/src/populate_plot.C
+++ MainAnalysis/src/populate_plot.C
@@ -26,6 +26,19 @@
 using namespace std::literals::string_literals;
 using namespace std::placeholders;
 
+double get_max(TH1* h1, TH1* h2) {
+    auto max_1 = h1->GetMaximum();
+    auto max_2 = h2->GetMaximum();
+    auto max = (max_1 > max_2) ? max_1 : max_2;
+    auto max *= 1.2
+}
+
+double get_min(TH1* h1, TH1* h2) {
+    auto min_1 = h1->GetMinimum();
+    auto min_2 = h2->GetMinimum();
+    auto min = (min_1 < min_2) ? min_1 : min_2;
+    auto min = min > 0 ? 0 : min * 1.2;
+}
 
 int populate(char const* config) {
     auto conf = new configurer(config);
@@ -33,12 +46,18 @@ int populate(char const* config) {
     auto data = conf->get<std::string>("data");
     auto mc = conf->get<std::string>("mc");
     auto tag = conf->get<std::string>("tag");
-    std::string histname = tag + "_sub_pjet_es_f_dphi"s;
+    auto system = conf->get<std::string>("system");
+
+    auto rdphi = conf->get<std::vector<float>>("dphi_range");
+    auto rdr = conf->get<std::vector<float>>("dr_range");
 
     auto dpt = conf->get<std::vector<float>>("pt_diff");
     auto dhf = conf->get<std::vector<float>>("hf_diff");
     auto diso = conf->get<std::vector<float>>("iso_diff");
     auto djtmin = conf->get<std::vector<float>>("jtmin_diff");
+
+    /* convert to integral angle units (cast to double) */
+    convert_in_place_pi(rdphi);
 
     /* push back vectors for the photon isolation cone and min jet pT */
     diso.push_back(999999);
@@ -52,28 +71,39 @@ int populate(char const* config) {
 
     auto mindex = new multival(dpt, dhf, diso, djtmin);
     
+    /* load data */
     TFile* fd = new TFile(data.data(), "read");
-    auto data_pjet_es_f_dphi = new history<TH1F>(fd, histname);
+    auto data_pjet_es_f_dphi = new history<TH1F>(fd, tag + "_sub_pjet_es_f_dphi"s);
+    auto data_pjet_es_f_dr = new history<TH1F>(fd, tag + "_sub_pjet_es_f_dr"s);
+    auto data_pjet_dphi_deta = new history<TH1F>(fd, tag + "_sub_pjet_dphi_deta"s);
 
     TFile* fm = new TFile(mc.data(), "read");
-    auto mc_pjet_es_f_dphi = new history<TH1F>(fm, histname);
+    auto mc_pjet_es_f_dphi = new history<TH1F>(fm, tag + "_sub_pjet_es_f_dphi"s);
+    auto mc_pjet_es_f_dr = new history<TH1F>(fm, tag + "_sub_pjet_es_f_dr"s);
+    auto mc_pjet_dphi_deta = new history<TH1F>(fm, tag + "_sub_pjet_dphi_deta"s);
 
-
-    auto p = new pencil();
-    p->category("type", "data", "mc");
-
-    auto canvases = ipt->size() * ihf->size();
-    std::vector<paper*> c(canvases, nullptr);
+    /* plot info */
     std::vector<int64_t> shape { ijtmin->size(), iiso->size() };
 
     std::function<void(int64_t, float)> iso_info = [&](int64_t x, float pos) {
         info_text(x, pos, "R_{iso,#gamma} = %.1f", diso); };
-
     std::function<void(int64_t, float)> jt_info = [&](int64_t x, float pos) {
         info_text(x, pos, "Jet min p_{T} = %.0f", djtmin); };
-
     auto iso_jt_info = [&](int64_t index) {
         stack_text(index, 0.75, 0.04, shape, jt_info, iso_info); };
+
+    /* do plotting */
+    auto p = new pencil();
+    p->category("type", "data", "mc");
+
+    auto canvases = ipt->size() * ihf->size();
+    std::vector<paper*> c1(canvases, nullptr);
+    std::vector<paper*> c2(canvases, nullptr);
+    std::vector<paper*> c3(canvases, nullptr);
+    std::vector<paper*> c4(canvases, nullptr);
+
+    auto redraw_dphi_axis = [&](TH1* h, int64_t) {
+        transform_axis(h, [](int64_t val) -> float { return std::abs(revert_radian(val)); }); };
 
     for (int i = 0; i < ipt->size(); ++i) {
         for (int j = 0; j < ihf->size(); ++j) {
@@ -81,29 +111,63 @@ int populate(char const* config) {
             auto suffix = "_" + std::to_string(i) + "_" + std::to_string(j);
 
             auto index = mindex->index_for(x{i, j, 0, 0});
-            auto data_max = (*data_pjet_es_f_dphi)[index]->GetMaximum();
-            auto data_min = (*data_pjet_es_f_dphi)[index]->GetMinimum();
 
-            auto mc_max = (*mc_pjet_es_f_dphi)[index]->GetMaximum();
-            auto mc_min = (*mc_pjet_es_f_dphi)[index]->GetMinimum();
+            auto max_dphi = get_max((*data_pjet_es_f_dphi)[index], (*mc_pjet_es_f_dphi)[index]);
+            auto min_dphi = get_min((*data_pjet_es_f_dphi)[index], (*mc_pjet_es_f_dphi)[index]);
+            auto max_dr = get_max((*data_pjet_es_f_dr)[index], (*mc_pjet_es_f_dr)[index]);
+            auto min_dr = get_min((*data_pjet_es_f_dr)[index], (*mc_pjet_es_f_dr)[index]);
+            auto max_dpde = get_max((*data_pjet_dphi_deta)[index], (*mc_pjet_dphi_deta)[index]);
+            auto min_dpde = get_min((*data_pjet_dphi_deta)[index], (*mc_pjet_dphi_deta)[index]);
 
-            auto max = (data_max > mc_max) ? data_max : mc_max;
-            auto min = (data_min < mc_min) ? data_min : mc_min;
+            c1[canvas] = new paper(system + "_" + tag + "_es_dphi" + suffix, p);
+            c2[canvas] = new paper(system + "_" + tag + "_dr" + suffix, p);
+            c3[canvas] = new paper(system + "_" + tag + "_mc_dphi_deta" + suffix, p);
+            c4[canvas] = new paper(system + "_" + tag + "_data_dphi_deta" + suffix, p);
 
-            c[canvas] = new paper(tag + "_es_dphi" + suffix, p);
-            apply_style(c[canvas], "");
-            c[canvas]->divide(-1, iiso->size());
+            apply_style(c1[canvas], "");
+            apply_style(c2[canvas], "");
+            apply_style(c3[canvas], "");
+            apply_style(c4[canvas], "");
 
-            c[canvas]->accessory(iso_jt_info);
+            c1[canvas]->divide(-1, iiso->size());
+            c2[canvas]->divide(-1, iiso->size());
+            c3[canvas]->divide(-1, iiso->size());
+            c4[canvas]->divide(-1, iiso->size());
+
+            c1->accessory(std::bind(line_at, _1, 0.f, rdphi[0], rdphi[1]));
+            c2->accessory(std::bind(line_at, _1, 0.f, rdr[0], rdr[1]));
+
+            c1->jewellery(redraw_dphi_axis);
+
+            c1[canvas]->accessory(iso_jt_info);
+            c2[canvas]->accessory(iso_jt_info);
+            c3[canvas]->accessory(iso_jt_info);
+            c4[canvas]->accessory(iso_jt_info);
 
             for (int k = 0; k < iiso->size(); ++k) {
                 for (int l = 0; l < ijtmin->size(); ++l) {
                     index = mindex->index_for(x{i, j, k, l});
-                    (*data_pjet_es_f_dphi)[index]->SetAxisRange(min, max, "Y");
-                    (*mc_pjet_es_f_dphi)[index]->SetAxisRange(min, max, "Y");
 
-                    c[canvas]->add((*data_pjet_es_f_dphi)[index], "data");
-                    c[canvas]->stack((*mc_pjet_es_f_dphi)[index], "mc");
+                    (*data_pjet_es_f_dphi)[index]->SetAxisRange(min_dphi, max_dphi, "Y");
+                    (*mc_pjet_es_f_dphi)[index]->SetAxisRange(min_dphi, max_dphi, "Y");
+
+                    (*data_pjet_es_f_dr)[index]->SetAxisRange(min_dr, max_dr, "Y");
+                    (*mc_pjet_es_f_dr)[index]->SetAxisRange(min_dr, max_dr, "Y");
+
+                    (*data_pjet_dphi_deta)[index]->SetAxisRange(min_dpde, max_dpde, "Z");
+                    (*mc_pjet_dphi_deta)[index]->SetAxisRange(min_dpde, max_dpde, "Z");
+
+                    c1[canvas]->add((*data_pjet_es_f_dphi)[index], "data");
+                    c1[canvas]->stack((*mc_pjet_es_f_dphi)[index], "mc");
+
+                    c2[canvas]->add((*data_pjet_es_f_dr)[index], "data");
+                    c2[canvas]->stack((*mc_pjet_es_f_dr)[index], "mc");
+
+                    c3[canvas]->add((*mc_pjet_dphi_deta)[index]);
+                    c3[canvas]->adjust((*mc_pjet_dphi_deta)[index], "colz", "");
+
+                    c4[canvas]->add((*data_pjet_dphi_deta)[index]);
+                    c4[canvas]->adjust((*data_pjet_dphi_deta)[index], "colz", "");
                 }
             }
         }
@@ -112,7 +176,13 @@ int populate(char const* config) {
     p->set_binary("type");
     p->sketch();
 
-    for (auto canvas : c)
+    for (auto canvas : c1)
+        canvas->draw("pdf");
+    for (auto canvas : c2)
+        canvas->draw("pdf");
+    for (auto canvas : c3)
+        canvas->draw("pdf");
+    for (auto canvas : c4)
         canvas->draw("pdf");
 
     return 0;
