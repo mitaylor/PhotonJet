ebef0bb80cc9368a51a4f10b24c2c291d01df6d7
mitaylor <mitay@mit.edu>
2021-12-04 22:21:54 -0500

    populate: update plotting

--- MainAnalysis/src/populate_plot.C
+++ MainAnalysis/src/populate_plot.C
@@ -86,13 +86,13 @@ int populate(char const* config) {
     TFile* fd = new TFile(data.data(), "read");
     auto data_pjet_es_f_dphi = new history<TH1F>(fd, tag + "_sub_pjet_es_f_dphi"s);
     auto data_pjet_es_f_dr = new history<TH1F>(fd, tag + "_sub_pjet_f_dr"s);
-    auto data_pjet_dphi_deta = new history<TH1F>(fd, tag + "_sub_pjet_dphi_deta"s);
+    auto data_pjet_dphi_deta = new history<TH2F>(fd, tag + "_sub_pjet_dphi_deta"s);
     auto data_nevt = new history<TH1F>(fd, tag + "_nevt"s);
 
     TFile* fm = new TFile(mc.data(), "read");
     auto mc_pjet_es_f_dphi = new history<TH1F>(fm, tag + "_sub_pjet_es_f_dphi"s);
     auto mc_pjet_es_f_dr = new history<TH1F>(fm, tag + "_sub_pjet_f_dr"s);
-    auto mc_pjet_dphi_deta = new history<TH1F>(fm, tag + "_sub_pjet_dphi_deta"s);
+    auto mc_pjet_dphi_deta = new history<TH2F>(fm, tag + "_sub_pjet_dphi_deta"s);
     auto mc_nevt = new history<TH1F>(fm, tag + "_nevt"s);
 
     scale_TH2(data_pjet_dphi_deta, data_nevt);
