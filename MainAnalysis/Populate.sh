6fa8554bf47331733e0a36d286c4e5325babb6e2
mitaylor <mitay@mit.edu>
2021-12-10 16:43:16 -0500

    populate: update configs for jes on / off

--- MainAnalysis/Populate.sh
+++ MainAnalysis/Populate.sh
@@ -3,10 +3,10 @@
 histogram() {
     tag=$1
 
-    nohup ./bin/populate configs/populate/ran/populate_${tag}_raw.conf \
-        data/ran/populate_${tag}_raw.root > log/populate_${tag}_raw_log.txt 2>&1 &
-    nohup ./bin/populate configs/populate/ran/populate_${tag}_bkg.conf \
-        data/ran/populate_${tag}_bkg.root > log/populate_${tag}_bkg_log.txt 2>&1 &
+    nohup ./bin/populate configs/populate/ran_jes_on/populate_${tag}_raw.conf \
+        data/jes_on/populate_${tag}_raw.root > log/populate_${tag}_raw_jes_on_log.txt 2>&1 &
+    nohup ./bin/populate configs/populate/ran_jes_on/populate_${tag}_bkg.conf \
+        data/jes_on/populate_${tag}_bkg.root > log/populate_${tag}_bkg_jes_on_log.txt 2>&1 &
 }
 
 samples=(pp aa pp_smear_0_10 pp_smear_10_30 pp_smear_30_50 pp_smear_50_90)
