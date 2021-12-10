cb08663fb7ff9f6b4c3e26694d16617c254f86d2
mitaylor <mitay@mit.edu>
2021-12-10 18:45:01 -0500

    speculate: fix axes

--- MainAnalysis/src/speculate.C
+++ MainAnalysis/src/speculate.C
@@ -100,7 +100,8 @@ int speculate(char const* config, char const* output) {
 
     /* calculate efficiency */
     auto hframe = frame((*counts)[0]->GetXaxis(), (*counts)[0]->GetYaxis());
-    hframe->GetYaxis()->SetTitle("efficiency");
+    hframe->GetYaxis()->SetTitle("trigger efficiency");
+    hframe->GetXaxis()->SetTitle("photon p_{T}");
 
     auto eff = new TGraphAsymmErrors((*counts)[1], (*counts)[0],
         "c1=0.683 b(1,1) mode");
