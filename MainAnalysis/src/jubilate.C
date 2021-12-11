d15b6c5cda6f002c3bf4e8e0e10a094d8ae3ccb1
mitaylor <mitay@mit.edu>
2021-12-11 11:50:55 -0500

    jubilate: adding files

--- MainAnalysis/src/jubilate.C
+++ MainAnalysis/src/jubilate.C
@@ -38,7 +38,6 @@ int jubilate(char const* config, char const* output) {
     auto tag = conf->get<std::string>("tag");
 
     auto rdphi = conf->get<std::vector<float>>("dphi_range");
-    auto rx = conf->get<std::vector<float>>("x_range");
     auto rdr = conf->get<std::vector<float>>("dr_range");
 
     auto dpt = conf->get<std::vector<float>>("pt_diff");
@@ -60,12 +59,10 @@ int jubilate(char const* config, char const* output) {
 
     auto pjet_es_f_dphi = new history<TH1F>(f, "raw_pjet_es_f_dphi");
     auto pjet_wta_f_dphi = new history<TH1F>(f, "raw_pjet_wta_f_dphi");
-    auto pjet_f_x = new history<TH1F>(f, "raw_pjet_f_x");
     auto pjet_f_ddr = new history<TH1F>(f, "raw_pjet_f_ddr");
 
     auto mix_pjet_es_f_dphi = new history<TH1F>(f, "raw_mix_pjet_es_f_dphi");
     auto mix_pjet_wta_f_dphi = new history<TH1F>(f, "raw_mix_pjet_wta_f_dphi");
-    auto mix_pjet_f_x = new history<TH1F>(f, "raw_mix_pjet_f_x");
     auto mix_pjet_f_ddr = new history<TH1F>(f, "raw_mix_pjet_f_ddr");
 
     /* shrink to remove overflow photon pt bin */
@@ -78,19 +75,17 @@ int jubilate(char const* config, char const* output) {
     wrap(nevt);
     wrap(pjet_es_f_dphi);
     wrap(pjet_wta_f_dphi);
-    wrap(pjet_f_x);
     wrap(pjet_f_ddr);
     wrap(mix_pjet_es_f_dphi);
     wrap(mix_pjet_wta_f_dphi);
-    wrap(mix_pjet_f_x);
     wrap(mix_pjet_f_ddr);
 
-    /* scale by bin width */
-    scale_bin_width(
-        pjet_f_x,
-        pjet_f_ddr,
-        mix_pjet_f_x,
-        mix_pjet_f_ddr);
+    // /* scale by bin width */
+    // scale_bin_width(
+    //     pjet_f_x,
+    //     pjet_f_ddr,
+    //     mix_pjet_f_x,
+    //     mix_pjet_f_ddr);
 
     /* draw figures */
     auto redraw_dphi_axis = [&](TH1* h, int64_t) {
@@ -128,17 +123,11 @@ int jubilate(char const* config, char const* output) {
     c2->jewellery(redraw_dphi_axis);
     c2->divide(-1 , ihf->size());
 
-    auto c3 = new paper(tag + "_mixing_x_d_pthf", hb);
-    apply_style(c3, collisions, -0.1, 2.0);
-    c3->accessory(std::bind(line_at, _1, 0.f, rx[0], rx[1]));
+    auto c3 = new paper(tag + "_mixing_ddr_d_pthf", hb);
+    apply_style(c3, collisions, -1., 24.);
+    c3->accessory(std::bind(line_at, _1, 0.f, rdr[0], rdr[1]));
     c3->accessory(pthf_info);
     c3->divide(-1 , ihf->size());
-
-    auto c4 = new paper(tag + "_mixing_ddr_d_pthf", hb);
-    apply_style(c4, collisions, -1., 24.);
-    c4->accessory(std::bind(line_at, _1, 0.f, rdr[0], rdr[1]));
-    c4->accessory(pthf_info);
-    c4->divide(-1 , ihf->size());
 
     for (int64_t i = 0; i < nevt->size(); ++i) {
         c1->add((*pjet_es_f_dphi)[i], system, "raw");
@@ -147,16 +136,13 @@ int jubilate(char const* config, char const* output) {
         c2->add((*pjet_wta_f_dphi)[i], system, "raw");
         c2->stack((*mix_pjet_wta_f_dphi)[i], system, "mix");
 
-        c3->add((*pjet_f_x)[i], system, "raw");
-        c3->stack((*mix_pjet_f_x)[i], system, "mix");
-
-        c4->add((*pjet_f_ddr)[i], system, "raw");
-        c4->stack((*mix_pjet_f_ddr)[i], system, "mix");
+        c3->add((*pjet_f_ddr)[i], system, "raw");
+        c3->stack((*mix_pjet_f_ddr)[i], system, "mix");
     }
 
     hb->sketch();
 
-    for (auto const& c : { c1, c2, c3, c4 })
+    for (auto const& c : { c1, c2, c3 })
         c->draw("pdf");
 
     /* save output */
