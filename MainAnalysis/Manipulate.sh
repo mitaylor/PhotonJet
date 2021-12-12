2244252cec9f83016260619b7aa2eda6c79e227f
mitaylor <mitay@mit.edu>
2021-12-12 18:59:34 -0500

    run full analysis

--- MainAnalysis/Manipulate.sh
+++ MainAnalysis/Manipulate.sh
@@ -27,7 +27,7 @@ samples=(pp aa pp_smear_0_10 pp_smear_10_30 pp_smear_30_50 pp_smear_50_90)
 for sample in ${samples[@]}; do
     nominal $sample
 
-    for syst in jeu_down jeu_up wo_ele_rej qcd; do #qcd_gen_iso
+    for syst in jeu_down jeu_up wo_ele_rej qcd qcd_gen_iso; do
         systematic $sample $syst
     done
 done
