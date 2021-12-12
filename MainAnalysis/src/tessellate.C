72566da2cb72caafdd28d449b9969f4b7e132cb1
mitaylor <mitay@mit.edu>
2021-12-12 13:12:12 -0500

    tessellate: removing signal subtraction from background

--- MainAnalysis/src/tessellate.C
+++ MainAnalysis/src/tessellate.C
@@ -271,8 +271,8 @@ int tessellate(char const* config, char const* output) {
     });
 
     auto convert_index = [&](int64_t index) {
-        auto final = index + index / (dpt.size() - 1);
-        std::cout << index << " " << final << std::endl;
+        auto final = index + (index - 1) / (ipt.size() - 1);
+        // std::cout << index << " " << final << std::endl;
         return final;
     };
 
