055e8b67489ade992db2480591be5a22d0361f1b
Molly <Molly@Janets-MacBook-Air.local>
2021-12-23 13:15:03 -0500

    Foresting: updating PbPb HP miniAOD foresting

--- Foresting/211011_PbPbData/crabConfig_forest_miniAOD_112X_HP.py
+++ Foresting/211011_PbPbData/crabConfig_forest_miniAOD_112X_HP.py
@@ -1,7 +1,6 @@
-# OutputBase = '/store/group/phys_heavyions/mitaylor'
-OutputBase = '/store/user/pchou/'
+OutputBase = '/store/user/mitay/PhotonJet'
 DatasetName = '/HIHardProbes/HIRun2018A-PbPb18_MiniAODv1-v1/MINIAOD'
-Tag = 'HIHardProbes_HIRun2018A-PbPb18_MiniAODv1-v1'
+Tag = '20211223PbPbHP'
 
 from WMCore.Configuration import Configuration
 config = Configuration()
@@ -11,9 +10,9 @@ config.General.requestName = Tag
 
 config.section_("JobType")
 config.JobType.pluginName = 'Analysis'
-config.JobType.psetName = 'forest_miniAOD_112X_DATA.py'
-config.JobType.maxMemoryMB = 5000    # request high memory machines.
-config.JobType.maxJobRuntimeMin = 1500    # request longer runtime, ~48 hours. 2750
+config.JobType.psetName = 'runConfig_forest_miniAOD_112X_DATA_HP.py'
+config.JobType.maxMemoryMB = 5000    # request high memory machines
+config.JobType.maxJobRuntimeMin = 1500
 
 config.section_("Data")
 config.Data.inputDataset = DatasetName
@@ -23,7 +22,7 @@ config.Data.unitsPerJob = 1
 config.Data.publication = False
 config.Data.outputDatasetTag = Tag
 config.Data.outLFNDirBase = OutputBase + DatasetName
+config.Data.lumiMask = 'Cert_326381-327564_HI_PromptReco_Collisions18_JSON.txt'
 
 config.section_("Site")
-# config.Site.storageSite = "T2_CH_CERN"
 config.Site.storageSite = 'T2_US_MIT'
--- setup_PbPb.sh
+++ setup_PbPb.sh
@@ -1,3 +1,3 @@
-cd ~/cmssw/CMSSW_10_3_3_patch1/src/
+cd ~/cmssw/CMSSW_11_2_1_patch2/src/
 eval `scramv1 runtime -sh`
 cd ~/physics/PhotonJet2018/
