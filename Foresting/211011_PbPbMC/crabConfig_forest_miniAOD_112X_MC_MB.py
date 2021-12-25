113857f108a7de5b0907d9eb9fa7b33a971fd602
Molly <Molly@Janets-MacBook-Air.local>
2021-12-24 21:59:32 -0500

    Foresting: adding files to forest PbPb MB, HP, MC

---
+++ Foresting/211011_PbPbMC/crabConfig_forest_miniAOD_112X_MC_MB.py
@@ -0,0 +1,27 @@
+OutputBase = '/store/user/mitaylor/PhotonJet'
+DatasetName = '/MinBias_Hydjet_Drum5F_2018_5p02TeV/HINPbPbSpring21MiniAOD-NoPUmva98_112X_upgrade2018_realistic_HI_v9-v1/MINIAODSIM'
+Tag = '20211224PbPbMCMB'
+
+from WMCore.Configuration import Configuration
+config = Configuration()
+
+config.section_("General")
+config.General.requestName = Tag
+
+config.section_("JobType")
+config.JobType.pluginName = 'Analysis'
+config.JobType.psetName = 'runConfig_forest_miniAOD_112X_MC.py'
+config.JobType.maxMemoryMB = 2500
+config.JobType.maxJobRuntimeMin = 1500   
+
+config.section_("Data")
+config.Data.inputDataset = DatasetName
+config.Data.inputDBS = 'global'
+config.Data.splitting = 'FileBased'
+config.Data.unitsPerJob = 1
+config.Data.publication = False
+config.Data.outputDatasetTag = Tag
+config.Data.outLFNDirBase = OutputBase + DatasetName
+
+config.section_("Site")
+config.Site.storageSite = 'T2_US_MIT'
