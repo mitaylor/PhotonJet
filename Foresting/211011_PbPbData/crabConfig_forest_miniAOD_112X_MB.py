459ccafe0413e8053f90aec8a1d309f4fe9e7938
mitaylor <mitay@mit.edu>
2022-02-15 14:35:55 -0500

    Foresting: resubmitting the MB processing

--- Foresting/211011_PbPbData/crabConfig_forest_miniAOD_112X_MB.py
+++ Foresting/211011_PbPbData/crabConfig_forest_miniAOD_112X_MB.py
@@ -1,6 +1,6 @@
 OutputBase = '/store/user/mitaylor/'
 DatasetName = '/HIMinimumBias4/HIRun2018A-PbPb18_MiniAODv1-v1/MINIAOD'
-Tag = '20211223PbPbMB'
+Tag = '20220215PbPbMB'
 
 from WMCore.Configuration import Configuration
 config = Configuration()
