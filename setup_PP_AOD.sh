2d5f8dc791afa31e025fe2b424b4b1500b39d3a5
mitaylor <mitay@mit.edu>
2022-03-11 18:25:53 -0500

    Foresting: updating setup scripts and adding Foresting code for the extra PbPb MC (AOD)

---
+++ setup_PP_AOD.sh
@@ -0,0 +1,3 @@
+cd ~/cmssw/CMSSW_9_4_11/src/
+eval `scramv1 runtime -sh`
+cd ~/physics/PhotonJet2018/
---
+++ setup_PbPb_miniAOD.sh
@@ -0,0 +1,3 @@
+cd ~/cmssw/CMSSW_11_2_1_patch2/src/
+eval `scramv1 runtime -sh`
+cd ~/physics/PhotonJet2018/
