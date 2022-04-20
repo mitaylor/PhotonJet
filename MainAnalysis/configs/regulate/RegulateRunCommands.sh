032d61f31374168bbbe049d1ae7508370ffb2c99
mitaylor <mitay@mit.edu>
2022-04-20 09:29:36 -0400

    regulate: new mb data config

--- MainAnalysis/configs/regulate/RegulateRunCommands.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-types=`find . -type f -name "*.conf" -exec basename {} \;  | sed "s/\.conf//"`
-
-for type in $types
-	do
-		echo "nohup ./bin/regulate configs/regulate/new_jes_off/${type}.conf /mnt/submit03/d00/scratch/mitay/photons/${type}.root > log/${type}_jes_off_log.txt 2>&1 &"
-	done
