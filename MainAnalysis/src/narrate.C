17f62478b5a96bc5f66e291acae3283e295ca911
mitaylor <mitay@mit.edu>
2022-04-27 19:37:54 -0400

    narrate: adding ratio

--- MainAnalysis/src/narrate.C
+++ MainAnalysis/src/narrate.C
@@ -154,7 +154,7 @@ int narrate(char const* config, char const* output) {
             (*rho_mc)[index]->SetMaximum(10);
             (*rho_mc)[index]->SetMinimum(1E-7);
 
-            (*rho_ratio)[index]>Divide((*rho_data)[index], (*rho_mc)[index]);
+            (*rho_ratio)[index]->Divide((*rho_data)[index], (*rho_mc)[index]);
         }
     }
 
