ba78f8f8330334743dcd7e3a1cf1c6f252d20edf
mitaylor <mitay@mit.edu>
2022-05-02 01:54:09 -0400

    populate: update configs

--- MainAnalysis/src/populate.C
+++ MainAnalysis/src/populate.C
@@ -360,7 +360,6 @@ int populate(char const* config, char const* output) {
             auto bin = (*efficiency)[1]->FindBin(leading_pt);
             auto corr = (*efficiency)[0]->GetBinContent(bin) / (*efficiency)[1]->GetBinContent(bin);
             if (corr < 1) { std::cout << "error" << std::endl; return -1; }
-            std::cout << leading_pt << " " << bin << " " << corr << std::endl;
             weight *= corr;
         }
 
@@ -368,7 +367,6 @@ int populate(char const* config, char const* output) {
             auto avg_rho = get_avg_rho(pjt,-photon_eta_abs,photon_eta_abs);
             auto bin = (*rho_weighting)[index]->FindBin(avg_rho);
             auto corr = (*rho_weighting)[index]->GetBinContent(bin);
-            std::cout << avg_rho << " " << bin << " " << corr << std::endl;
             weight *= corr;
         }
 
