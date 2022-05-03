45240435124fee8762b86fa26eccf04833a576cb
mitaylor <mitay@mit.edu>
2022-05-02 21:59:09 -0400

    narrate: test distribution with photon selections

--- MainAnalysis/src/narrate_selected.C
+++ MainAnalysis/src/narrate_selected.C
@@ -121,6 +121,30 @@ int narrate(char const* config, char const* output) {
 
             t->GetEntry(i);
 
+            if (std::abs(pjt->vz) > 15) { continue; }
+
+            int64_t leading = -1;
+            float leading_pt = 0;
+            for (int64_t j = 0; j < pjt->nPho; ++j) {
+                if ((*pjt->phoEt)[j] <= 40) { continue; }
+                if (std::abs((*pjt->phoSCEta)[j]) >= eta_max[0]) { continue; }
+                if ((*pjt->phoHoverE)[j] > 0.119947) { continue; }
+
+                if ((*pjt->phoEt)[j] > leading_pt) {
+                    leading = j;
+                    leading_pt = (*pjt->phoEt)[j];
+                }
+            }
+
+            if (leading < 0) { continue; }
+            if ((*pjt->phoSigmaIEtaIEta_2012)[leading] > 0.010392) { continue; }
+            if (in_pho_failure_region(pjt, leading)) { continue; }
+
+            float isolation = (*pjt->pho_ecalClusterIsoR3)[leading]
+                + (*pjt->pho_hcalRechitIsoR3)[leading]
+                + (*pjt->pho_trackIsoR3PtCut20)[leading];
+            if (isolation > 2.099277) { continue; }
+
             for (size_t j = 0; j < eta_min.size(); ++j) {
                 for (size_t k = 0; k < dhf.size()-1; ++k) {
                     auto eta_x = static_cast<int64_t>(j);
