b5ed254ea9bfab6a6787bd74bf1039227b889388
mitaylor <mitay@mit.edu>
2022-05-09 18:25:40 +0200

    populate and beneficiate: add the acceptance weighting to populate and update the binning for beneficiate

--- MainAnalysis/src/beneficiate.C
+++ MainAnalysis/src/beneficiate.C
@@ -56,7 +56,7 @@ int beneficiate(char const* output) {
     double phi_min = -3.1416;
     double phi_max = 3.1416;
 
-    std::vector<float> dphi_range{ 0 0.0625 0.125 0.1875 0.25 0.3125 0.375 0.4375 0.5 0.5625 0.625 0.6875 0.75 0.8125 0.875 0.9375 1 };
+    std::vector<float> dphi_range{ 0, 0.0625, 0.125, 0.1875, 0.25, 0.3125, 0.375, 0.4375, 0.5, 0.5625, 0.625, 0.6875, 0.75, 0.8125, 0.875, 0.9375, 1 };
 
     auto ijeta = new interval("jetEta"s, dim_size, jet_eta_min, jet_eta_max);
     auto ipeta = new interval("phoEta"s, dim_size, pho_eta_min, pho_eta_max);
