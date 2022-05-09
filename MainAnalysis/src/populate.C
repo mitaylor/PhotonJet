a02b315e8bc03fb55917bab2b4f0b76f6d7efcc9
mitaylor <mitay@mit.edu>
2022-05-09 18:28:15 +0200

    populate and beneficiate: add the acceptance weighting to populate and update the binning for beneficiate

--- MainAnalysis/src/populate.C
+++ MainAnalysis/src/populate.C
@@ -92,7 +92,7 @@ void fill_axes(pjtree* pjt, int64_t pthf_x, float weight,
             auto dphi_x = idphi->index_for(revert_pi(photon_jet_dphi));
             auto bin = (*total)[dphi_x]->FindBin(jet_eta, photon_eta);
             corr = (*total)[dphi_x]->GetBinContent(bin) / (*acceptance)[dphi_x]->GetBinContent(bin);
-            if (corr < 1) { std::cout << "error" << std::endl; return -1; }
+            if (corr < 1) { std::cout << "error" << std::endl; }
         }
 
         /* fill histograms */
