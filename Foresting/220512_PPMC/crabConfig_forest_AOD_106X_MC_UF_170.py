e300aeff2c1ad200e6f10ca829eed865b6be1398
mitaylor <mitay@mit.edu>
2022-05-12 18:18:27 +0200

    Foresting: add PP UL MC

---
+++ Foresting/220512_PPMC/crabConfig_forest_AOD_106X_MC_UF_170.py
@@ -0,0 +1,27 @@
+OutputBase = '/store/user/mitaylor/PhotonJet'
+DatasetName = '/QCDPhoton_pThat-170_TuneCP5_5p02TeV-pythia8/RunIISummer20UL17pp5TeVRECO-106X_mc2017_realistic_forppRef5TeV_v3-v2/AODSIM'
+Tag = '20220512PPMUF170'
+
+from WMCore.Configuration import Configuration
+config = Configuration()
+
+config.section_("General")
+config.General.requestName = Tag
+
+config.section_("JobType")
+config.JobType.pluginName = 'Analysis'
+config.JobType.psetName = 'runConfig_forest_AOD_106X_MC.py'
+config.JobType.maxMemoryMB = 5000
+config.JobType.maxJobRuntimeMin = 2750
+
+config.section_("Data")
+config.Data.inputDataset = DatasetName
+config.Data.inputDBS = 'global'
+config.Data.splitting = 'FileBased'
+config.Data.unitsPerJob = 1
+config.Data.publication = False
+config.Data.outputDatasetTag = Tag
+config.Data.outLFNDirBase = OutputBase + DatasetName
+
+config.section_("Site")
+config.Site.storageSite = 'T2_US_MIT'
