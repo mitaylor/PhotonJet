f05f04da2dbcbbd9e65e1add69e613e597ff0780
mitaylor <mitay@mit.edu>
2022-05-16 09:29:28 +0200

    plot_qcd: plot the pthat spectrum for the PbPb qcd files to make sure it looks good

---
+++ MainAnalysis/src/plot_qcd.C
@@ -0,0 +1,65 @@
+#include "../include/lambdas.h"
+#include "../include/pjtree.h"
+#include "../include/specifics.h"
+
+#include "../git/config/include/configurer.h"
+
+#include "../git/history/include/interval.h"
+#include "../git/history/include/multival.h"
+#include "../git/history/include/memory.h"
+
+#include "../git/tricks-and-treats/include/overflow_angles.h"
+#include "../git/tricks-and-treats/include/trunk.h"
+
+#include "TFile.h"
+#include "TTree.h"
+#include "TH1.h"
+
+#include <memory>
+#include <string>
+#include <vector>
+
+using namespace std::literals::string_literals;
+using namespace std::placeholders;
+
+int populate(char const* config, char const* output) {
+    auto conf = new configurer(config);
+
+    auto files = conf->get<std::vector<std::string>>("files");
+
+    auto pthat_w = new TH1F("pthat_w", "pthat", 100, 0, 200);
+
+    /* manage memory manually */
+    TH1::AddDirectory(false);
+    TH1::SetDefaultSumw2();
+
+    /* load input */
+    for (auto const& file : files) {
+        TFile* f = new TFile(file.data(), "read");
+        TTree* t = (TTree*)f->Get("pj");
+        auto pjt = new pjtree(true, false, false, t, { 0, 0, 0, 0, 0, 0, 0 });
+
+        int64_t nentries = static_cast<int64_t>(t->GetEntries());
+        for (int64_t i = 0; i < nentries; ++i) {
+            if (i % frequency == 0) { printf("entry: %li/%li\n", i, nentries); }
+
+            t->GetEntry(i);
+
+            pthat_w->Fill(pjt->pthat, pjt->w);
+        }
+    }
+
+    pthat_w->SaveAs(output);
+
+    printf("destroying objects..\n");
+
+    return 0;
+}
+
+int main(int argc, char* argv[]) {
+    if (argc == 3)
+        return populate(argv[1], argv[2]);
+
+    printf("usage: %s [config] [output]\n", argv[0]);
+    return 1;
+}
