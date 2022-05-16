666ee6643a4c5d8d3c76b7693690b6f3aa76f5fd
mitaylor <mitay@mit.edu>
2022-05-16 11:55:11 +0200

    populate: update the rho weighting so it only takes one process

--- MainAnalysis/Populate.sh
+++ MainAnalysis/Populate.sh
@@ -7,22 +7,12 @@ histogram() {
     echo "nohup ./bin/populate configs/populate/new/jet_30/populate_${tag}_bkg.conf data/jet_30/populate_${tag}_bkg.root > log/populate_${tag}_bkg_jet_30_log.txt 2>&1 &"
 }
 
-samples=(pp)
+samples=(pp aa)
 
 for sample in ${samples[@]}; do
     histogram ${sample}
 
     for syst in wo_ele_rej qcd qcd_gen_iso; do
-        histogram ${sample}_${syst}
-    done
-done
-
-samples=(aa)
-
-for sample in ${samples[@]}; do
-    histogram ${sample}
-
-    for syst in wo_ele_rej qcd_0_10 qcd_10_30 qcd_30_50 qcd_50_90 qcd_gen_iso_0_10 qcd_gen_iso_10_30 qcd_gen_iso_30_50 qcd_gen_iso_50_90; do
         histogram ${sample}_${syst}
     done
 done
