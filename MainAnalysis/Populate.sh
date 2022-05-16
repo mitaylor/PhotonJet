c020233ef61904908ab1207886b02320c806fa3d
mitaylor <mitay@mit.edu>
2022-05-16 12:34:53 +0200

    populate: update the rho weighting so it only takes one process

--- MainAnalysis/Populate.sh
+++ MainAnalysis/Populate.sh
@@ -5,6 +5,8 @@ histogram() {
 
     echo "nohup ./bin/populate configs/populate/new/jet_30/populate_${tag}_raw.conf data/jet_30/populate_${tag}_raw.root > log/populate_${tag}_raw_jet_30_log.txt 2>&1 &"
     echo "nohup ./bin/populate configs/populate/new/jet_30/populate_${tag}_bkg.conf data/jet_30/populate_${tag}_bkg.root > log/populate_${tag}_bkg_jet_30_log.txt 2>&1 &"
+    echo "nohup ./bin/populate configs/populate/new/jet_20/populate_${tag}_raw.conf data/jet_20/populate_${tag}_raw.root > log/populate_${tag}_raw_jet_20_log.txt 2>&1 &"
+    echo "nohup ./bin/populate configs/populate/new/jet_20/populate_${tag}_bkg.conf data/jet_20/populate_${tag}_bkg.root > log/populate_${tag}_bkg_jet_20_log.txt 2>&1 &"
 }
 
 samples=(pp aa)
@@ -12,7 +14,7 @@ samples=(pp aa)
 for sample in ${samples[@]}; do
     histogram ${sample}
 
-    for syst in wo_ele_rej qcd qcd_gen_iso; do
+    for syst in wo_ele_rej qcd qcd_gen_iso cent_down cent_up; do
         histogram ${sample}_${syst}
     done
 done
