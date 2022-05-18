97b936675196fba05577a7e7fc2012147111f29b
mitaylor <mitay@mit.edu>
2022-05-18 12:34:35 +0200

    populate: update configs for pp smear

--- MainAnalysis/Populate.sh
+++ MainAnalysis/Populate.sh
@@ -3,18 +3,26 @@
 histogram() {
     tag=$1
 
-    echo "nohup ./bin/populate configs/populate/new/jet_30/populate_${tag}_raw.conf data/jet_30/populate_${tag}_raw.root > log/populate_${tag}_raw_jet_30_log.txt 2>&1 &"
-    echo "nohup ./bin/populate configs/populate/new/jet_30/populate_${tag}_bkg.conf data/jet_30/populate_${tag}_bkg.root > log/populate_${tag}_bkg_jet_30_log.txt 2>&1 &"
     echo "nohup ./bin/populate configs/populate/new/jet_20/populate_${tag}_raw.conf data/jet_20/populate_${tag}_raw.root > log/populate_${tag}_raw_jet_20_log.txt 2>&1 &"
     echo "nohup ./bin/populate configs/populate/new/jet_20/populate_${tag}_bkg.conf data/jet_20/populate_${tag}_bkg.root > log/populate_${tag}_bkg_jet_20_log.txt 2>&1 &"
 }
 
-samples=(pp aa)
+samples=(pp pp_smear_0_10 pp_smear_10_30 pp_smear_30_50 pp_smear_50_90 aa)
 
 for sample in ${samples[@]}; do
     histogram ${sample}
 
-    for syst in wo_ele_rej qcd qcd_gen_iso cent_down cent_up; do
+    for syst in wo_ele_rej qcd qcd_gen_iso; do
+        histogram ${sample}_${syst}
+    done
+done
+
+samples=(aa)
+
+for sample in ${samples[@]}; do
+    histogram ${sample}
+
+    for syst in cent_down cent_up; do
         histogram ${sample}_${syst}
     done
 done
--- MainAnalysis/Vacillate.sh
+++ MainAnalysis/Vacillate.sh
@@ -6,7 +6,7 @@ histogram() {
     echo "nohup ./bin/vacillate_dr configs/vacillate_dr/new/jet_20/vacillate_${tag}.conf data/jet_20/vacillate_${tag}.root > log/vacillate_${tag}_jet_20_log.txt 2>&1 &"
 }
 
-samples=(pp_smear_0_10 pp_smear_10_30 pp_smear_30_50 pp_smear_50_90 aa)
+samples=(pp pp_smear_0_10 pp_smear_10_30 pp_smear_30_50 pp_smear_50_90 aa)
 
 for sample in ${samples[@]}; do
     histogram ${sample}
@@ -19,8 +19,6 @@ done
 samples=(pp)
 
 for sample in ${samples[@]}; do
-    histogram ${sample}
-
     for syst in front back; do
         histogram ${sample}_${syst}
     done
