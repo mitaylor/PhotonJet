8721b5e630ef3489cba2f6fc760f50c6ab3e21f5
mitaylor <mitay@mit.edu>
2022-05-19 21:22:43 +0200

    obnubilate: add interpolation

--- MainAnalysis/src/obnubilate.C
+++ MainAnalysis/src/obnubilate.C
@@ -121,13 +121,13 @@ int obnubilate(char const* config, char const* output) {
 
             for (int64_t i = 0; i < batch->size(); ++i) {
                 for (int64_t j = 0; j < (*batch)[i]->GetNbinsX(); ++j) {
-                    if (j == 2) {
+                    if (j == 1) {
                         auto value = (*batch)[i]->GetBinContent(j) - (*batch)[i]->GetBinContent(j + 2);
                         value *= 1/3;
                         value += (*batch)[i]->GetBinContent(j + 2);
                         (*batch)[i]->SetBinContent(j + 1, value);
                     }
-                    if (j == 3) {
+                    if (j == 2) {
                         auto value = (*batch)[i]->GetBinContent(j) - (*batch)[i]->GetBinContent(j + 2);
                         value *= 2/3;
                         value += (*batch)[i]->GetBinContent(j + 2);
