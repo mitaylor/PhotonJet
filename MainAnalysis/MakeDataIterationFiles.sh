a53b3a24625a9f32e667039b77f314ef18cba2c1
mitaylor <mitay@mit.edu>
2022-05-20 15:17:33 +0200

    make files including all iteration information for the study on number of iterations

---
+++ MainAnalysis/MakeDataIterationFiles.sh
@@ -0,0 +1,40 @@
+#!/usr/bin/env bash
+
+arithmetic() {
+    tag=$1
+
+    ./bin/get_data_iteration_study configs/quantitate/quantitate_${tag}.conf \
+        data/jet_20/data_iteration_${tag}.root
+}
+
+nominal() {
+    sample=$1
+
+    arithmetic ${sample}
+}
+
+systematic() {
+    sample=$1
+    syst=$2
+
+    arithmetic ${sample}_${syst}
+}
+
+# samples=(pp aa pp_smear_0_10 pp_smear_10_30 pp_smear_30_50 pp_smear_50_90)
+samples=(pp aa)
+
+for sample in ${samples[@]}; do
+    nominal $sample
+
+    for syst in wo_ele_rej qcd qcd_gen_iso loose_purity tight_purity jeu_up jeu_down jer_up; do
+        systematic $sample $syst
+    done
+done
+
+samples=(aa)
+
+for sample in ${samples[@]}; do
+    for syst in cent_up cent_down; do
+        systematic $sample $syst
+    done
+done
