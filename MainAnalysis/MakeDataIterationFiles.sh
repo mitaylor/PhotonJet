ce714853bdce1325f36b8a98443763dbc3dc9d0a
mitaylor <mitay@mit.edu>
2022-05-20 18:44:23 +0200

    get_data_iteration: add uncertainty and update pp config files

--- MainAnalysis/MakeDataIterationFiles.sh
+++ MainAnalysis/MakeDataIterationFiles.sh
@@ -21,7 +21,7 @@ systematic() {
 }
 
 # samples=(pp aa pp_smear_0_10 pp_smear_10_30 pp_smear_30_50 pp_smear_50_90)
-samples=(pp aa)
+samples=(aa pp)
 
 for sample in ${samples[@]}; do
     nominal $sample
