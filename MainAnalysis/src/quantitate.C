5c5aa6d863fb29d03be697480d092c94983b115c
mitaylor <mitay@mit.edu>
2022-05-20 22:20:28 +0200

    obnubilate: update ranges

--- MainAnalysis/src/quantitate.C
+++ MainAnalysis/src/quantitate.C
@@ -236,11 +236,11 @@ int quantitate(char const* config, char const* output) {
         std::cout << refold_name << std::endl;
 
         auto HUnfoldedBayes = (TH1F*) fafters[j]->Get(unfold_name.data());
-        auto MUnfolded = fafters[j]->Get(matrix_name.data());
+        auto MUnfolded = (TH2F*) fafters[j]->Get(matrix_name.data());
         auto HRefolded = (TH1F*) fafters[j]->Get(refold_name.data());
 
         std::cout << HUnfoldedBayes->GetName() << std::endl;
-        std::cout << MUnfolded[0]->GetName() << std::endl;
+        std::cout << MUnfolded->GetName() << std::endl;
         std::cout << HRefolded->GetName() << std::endl;
 
         (*unfolded)[j] = HUnfoldedBayes;
