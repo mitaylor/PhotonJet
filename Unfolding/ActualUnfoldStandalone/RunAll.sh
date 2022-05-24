c95730480de1c26ad5c7925f8caeb4336cfb158c
mitaylor <mitay@mit.edu>
2022-05-24 14:04:24 -0400

    Unfolding: add run script for the actual unfolding

--- Unfolding/ActualUnfoldStandalone/RunAll.sh
+++ Unfolding/ActualUnfoldStandalone/RunAll.sh
@@ -4,8 +4,7 @@ Samples=(PPNominal PPWithoutEleRej PPTightPurity PPLoosePurity PPQCD\
 				PPQCDGenIso PPJEUUp PPJEUDown PPJERUp)
 
 for Sample in ${Samples[@]}; do
-    echo "./Execute --Input Input/${Sample}0.root --Output Output/${Sample}0.root \
-        --Prior MC --DoBayes true --DoSVD false --DoInvert true --DoTUnfold false --DoFit true"
+    echo "${Sample}0"
     ./Execute --Input Input/${Sample}0.root --Output Output/${Sample}0.root \
         --Prior MC --DoBayes true --DoSVD false --DoInvert true --DoTUnfold false --DoFit true
 done
@@ -14,20 +13,16 @@ Samples=(AANominal AAWithoutEleRej AATightPurity AALoosePurity AAQCD\
 				AAQCDGenIso AAJEUUp AAJEUDown AAJERUp AACentUp AACentDown)
 
 for Sample in ${Samples[@]}; do
-    echo "./Execute --Input Input/${Sample}0.root --Output Output/${Sample}0.root \
-        --Prior MC --DoBayes true --DoSVD false --DoInvert true --DoTUnfold false --DoFit true"
+    echo "${Sample}0"
     ./Execute --Input Input/${Sample}0.root --Output Output/${Sample}0.root \
         --Prior MC --DoBayes true --DoSVD false --DoInvert true --DoTUnfold false --DoFit true
-    echo "./Execute --Input Input/${Sample}1.root --Output Output/${Sample}1.root \
-        --Prior MC --DoBayes true --DoSVD false --DoInvert true --DoTUnfold false --DoFit true"
+    echo "${Sample}1"
     ./Execute --Input Input/${Sample}1.root --Output Output/${Sample}1.root \
         --Prior MC --DoBayes true --DoSVD false --DoInvert true --DoTUnfold false --DoFit true
-    echo "./Execute --Input Input/${Sample}2.root --Output Output/${Sample}2.root \
-        --Prior MC --DoBayes true --DoSVD false --DoInvert true --DoTUnfold false --DoFit true"
+    echo "${Sample}2"
     ./Execute --Input Input/${Sample}2.root --Output Output/${Sample}2.root \
         --Prior MC --DoBayes true --DoSVD false --DoInvert true --DoTUnfold false --DoFit true
-    echo "./Execute --Input Input/${Sample}3.root --Output Output/${Sample}3.root \
-        --Prior MC --DoBayes true --DoSVD false --DoInvert true --DoTUnfold false --DoFit true"
+    echo "${Sample}3"
     ./Execute --Input Input/${Sample}3.root --Output Output/${Sample}3.root \
         --Prior MC --DoBayes true --DoSVD false --DoInvert true --DoTUnfold false --DoFit true
 done
