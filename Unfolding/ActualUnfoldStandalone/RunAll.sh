109123d436d1a6e55cd6e42a2f0fabc12fe0b9da
mitaylor <mitay@mit.edu>
2022-05-24 14:03:25 -0400

    Unfolding: add run script for the actual unfolding

--- Unfolding/ActualUnfoldStandalone/RunAll.sh
+++ Unfolding/ActualUnfoldStandalone/RunAll.sh
@@ -4,6 +4,8 @@ Samples=(PPNominal PPWithoutEleRej PPTightPurity PPLoosePurity PPQCD\
 				PPQCDGenIso PPJEUUp PPJEUDown PPJERUp)
 
 for Sample in ${Samples[@]}; do
+    echo "./Execute --Input Input/${Sample}0.root --Output Output/${Sample}0.root \
+        --Prior MC --DoBayes true --DoSVD false --DoInvert true --DoTUnfold false --DoFit true"
     ./Execute --Input Input/${Sample}0.root --Output Output/${Sample}0.root \
         --Prior MC --DoBayes true --DoSVD false --DoInvert true --DoTUnfold false --DoFit true
 done
@@ -12,12 +14,20 @@ Samples=(AANominal AAWithoutEleRej AATightPurity AALoosePurity AAQCD\
 				AAQCDGenIso AAJEUUp AAJEUDown AAJERUp AACentUp AACentDown)
 
 for Sample in ${Samples[@]}; do
+    echo "./Execute --Input Input/${Sample}0.root --Output Output/${Sample}0.root \
+        --Prior MC --DoBayes true --DoSVD false --DoInvert true --DoTUnfold false --DoFit true"
     ./Execute --Input Input/${Sample}0.root --Output Output/${Sample}0.root \
         --Prior MC --DoBayes true --DoSVD false --DoInvert true --DoTUnfold false --DoFit true
+    echo "./Execute --Input Input/${Sample}1.root --Output Output/${Sample}1.root \
+        --Prior MC --DoBayes true --DoSVD false --DoInvert true --DoTUnfold false --DoFit true"
     ./Execute --Input Input/${Sample}1.root --Output Output/${Sample}1.root \
         --Prior MC --DoBayes true --DoSVD false --DoInvert true --DoTUnfold false --DoFit true
+    echo "./Execute --Input Input/${Sample}2.root --Output Output/${Sample}2.root \
+        --Prior MC --DoBayes true --DoSVD false --DoInvert true --DoTUnfold false --DoFit true"
     ./Execute --Input Input/${Sample}2.root --Output Output/${Sample}2.root \
         --Prior MC --DoBayes true --DoSVD false --DoInvert true --DoTUnfold false --DoFit true
+    echo "./Execute --Input Input/${Sample}3.root --Output Output/${Sample}3.root \
+        --Prior MC --DoBayes true --DoSVD false --DoInvert true --DoTUnfold false --DoFit true"
     ./Execute --Input Input/${Sample}3.root --Output Output/${Sample}3.root \
         --Prior MC --DoBayes true --DoSVD false --DoInvert true --DoTUnfold false --DoFit true
 done
