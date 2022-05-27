9a8809aececf9754c4297751287cf1efa0b350ff
mitaylor <mitay@mit.edu>
2022-05-27 18:25:01 -0400

    obubilate: add print outs

--- MainAnalysis/src/obnubilate.C
+++ MainAnalysis/src/obnubilate.C
@@ -133,7 +133,7 @@ int obnubilate(char const* config, char const* output) {
                     if (percentage > max) { max = percentage; }
                 }
 
-                printf("%i: %.2f-%.2f%%\n ", (int) i, min, max);
+                printf("%i: %.1f-%.1f%%\n ", (int) i, min, max);
 
                 // std::cout << i << ": " << min << "-" << max << "%" << std::endl;
             }
