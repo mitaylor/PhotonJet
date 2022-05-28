e661c1ab33061139327a75cd207ac21f2497f5df
mitaylor <mitay@mit.edu>
2022-05-28 01:07:17 -0400

    Accumulate: update run script

--- MainAnalysis/Accumulate.sh
+++ MainAnalysis/Accumulate.sh
@@ -3,8 +3,8 @@
 arithmetic() {
     tag=$1
 
-    ./bin/accumulate configs/accumulate/new/pho_60/accumulate_${tag}.conf \
-        data/pho_60/accumulate_${tag}.root
+    ./bin/accumulate configs/accumulate/new/jet_20/accumulate_${tag}.conf \
+        data/jet_20/accumulate_${tag}.root
 }
 
 nominal() {
@@ -23,7 +23,7 @@ systematic() {
 }
 
 # samples=(pp aa pp_smear_0_10 pp_smear_10_30 pp_smear_30_50 pp_smear_50_90)
-samples=(pp aa)
+samples=(pp_smear_0_10 pp_smear_10_30 pp_smear_30_50 pp_smear_50_90)
 
 for sample in ${samples[@]}; do
     nominal $sample
