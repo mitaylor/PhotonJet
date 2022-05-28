8e8070df02cdcf01f7eb3655def3d1d0dcb7b5ec
mitaylor <mitay@mit.edu>
2022-05-28 02:09:18 -0400

    plot_response_matrices: add configs for pp smear

--- MainAnalysis/Vacillate.sh
+++ MainAnalysis/Vacillate.sh
@@ -3,7 +3,7 @@
 histogram() {
     tag=$1
 
-    echo "nohup ./bin/vacillate_dr configs/vacillate_dr/new/jet_20_small/vacillate_${tag}.conf data/jet_20/vacillate_${tag}.root > log/vacillate_small_${tag}_jet_20_log.txt 2>&1 &"
+    echo "nohup ./bin/vacillate_dr configs/vacillate_dr/new/jet_20/vacillate_${tag}.conf data/jet_20/vacillate_${tag}.root > log/vacillate_${tag}_jet_20_log.txt 2>&1 &"
 }
 
 # samples=(pp_smear_0_10 pp_smear_10_30 pp_smear_30_50 pp_smear_50_90 pp aa)
