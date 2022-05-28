8d520db4da51a13d0b0b9f9e9f392f9e32ab9b68
mitaylor <mitay@mit.edu>
2022-05-27 23:15:47 -0400

    distillate: check the dj resolution after smearing

--- MainAnalysis/src/fabulate_angle_smeared.C
+++ MainAnalysis/src/fabulate_angle_smeared.C
@@ -171,7 +171,7 @@ int fabulate(char const* config, char const* output) {
                 auto change = rng->Exp(res(aa_c, aa_s, aa_n, reco_pt)) - rng->Exp(res(pp_c, pp_s, pp_n, reco_pt));
                 // auto sign = (rng->Integer(2) == 0) ? -1 : 1;                
                 // auto adj = rdr + change * sign;
-                auto adj = rdr + change;
+                auto adj = rdr + change/2;
 
                 // std::cout << dr_x << " " << reco_pt << " " << res_diff << " " << change << std::endl;
 
