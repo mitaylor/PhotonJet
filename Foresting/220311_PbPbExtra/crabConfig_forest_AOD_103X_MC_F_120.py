0b4e3ced179f79336b68443ffebbcb9afb2010cd
mitaylor <mitay@mit.edu>
2022-05-30 14:50:13 -0400

    Foresting: updating PbPb AOD configs to use the latest JEC, creating new configs

--- Foresting/220311_PbPbExtra/crabConfig_forest_AOD_103X_MC_F_120.py
+++ Foresting/220311_PbPbExtra/crabConfig_forest_AOD_103X_MC_F_120.py
@@ -1,6 +1,6 @@
 OutputBase = '/store/user/mitaylor/PhotonJet'
 DatasetName = '/QCDPhoton_pThat-120_Filter30GeV_TuneCP5_HydjetDrumMB_5p02TeV_Pythia8/HINPbPbAutumn18DR-FixL1CaloGT_103X_upgrade2018_realistic_HI_v13_ext1-v2/AODSIM'
-Tag = '20220311PbPbMC30F120'
+Tag = '20220530PbPbMC30F120'
 
 from WMCore.Configuration import Configuration
 config = Configuration()
@@ -11,8 +11,8 @@ config.General.requestName = Tag
 config.section_("JobType")
 config.JobType.pluginName = 'Analysis'
 config.JobType.psetName = 'runConfig_forest_AOD_103X_MC.py'
-config.JobType.maxMemoryMB = 2500
-config.JobType.maxJobRuntimeMin = 1500   
+config.JobType.maxMemoryMB = 5000
+config.JobType.maxJobRuntimeMin = 2750   
 
 config.section_("Data")
 config.Data.inputDataset = DatasetName
