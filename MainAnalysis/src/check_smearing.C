bcf07ed703ae4f3fe90b74385a0300ca13bcc647
mitaylor <mitay@mit.edu>
2022-05-31 19:36:13 -0400

    check_smearing: check that pp data has been smeared by the right amount

--- MainAnalysis/src/check_smearing.C
+++ MainAnalysis/src/check_smearing.C
@@ -46,10 +46,11 @@ int congratulate(char const* config, char const* output) {
     auto ihf = new interval(dhf);
     auto idr = new interval(ddr);
 
-    auto mdrhf = new multival(*idr, *ihf);
+    auto mdrhf = new multival(ddr, dhf);
+    auto mdrhf = new multival(ddr_short, dhf);
 
     std::vector<float>::const_iterator first = ddr.begin();
-    std::vector<float>::const_iterator last = ddr.begin() + idr->index_for(0.2) + 1;
+    std::vector<float>::const_iterator last = ddr.begin() + idr->index_for(0.2) + 2;
     std::vector<float> ddr_short(first, last);
 
     /* manage memory manually */
@@ -84,7 +85,7 @@ int congratulate(char const* config, char const* output) {
         info_text(x, pos, "%i - %i%%", dcent, true); };
 
     auto drhf_info = [&](int64_t index) {
-        stack_text(index, 0.75, 0.04, hists[0], dr_info, hf_info); };
+        stack_text(index, 0.75, 0.04, mdrhf_short, dr_info, hf_info); };
 
     auto collisions = "#sqrt{s_{NN}} = 5.02 TeV"s;
 
