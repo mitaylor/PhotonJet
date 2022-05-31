4f3ed6878c52152e088c1be89178f2a6be23f91d
mitaylor <mitay@mit.edu>
2022-05-31 18:59:30 -0400

    check_smearing: check that pp data has been smeared by the right amount

--- MainAnalysis/CheckSmearing.sh
+++ MainAnalysis/CheckSmearing.sh
@@ -3,7 +3,7 @@
 nominal() {
     tag=$1
 
-    ./bin/fabulate_angle configs/fabulate/new/fabulate_${tag}_test.conf \
+    ./bin/fabulate_angle_smeared configs/fabulate/new/fabulate_${tag}_test.conf \
         data/fabulate_${tag}_smear_test.root
     ./bin/distillate_angle configs/distillate/new/distillate_${tag}_dj_smear_test.conf \
         data/distillate_${tag}_dj_smear_test.root
