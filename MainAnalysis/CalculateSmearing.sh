9e2b87567d788763c7bec281e6074e0e7b7549f4
mitaylor <mitay@mit.edu>
2022-05-31 15:34:52 -0400

    smearing: update populate, vacillate, and fabulate to change the smear binning and correct bug in populate

--- MainAnalysis/CalculateSmearing.sh
+++ MainAnalysis/CalculateSmearing.sh
@@ -1,22 +1,16 @@
 #!/usr/bin/env bash
 
 nominal() {
+    tag=$1
+
     ./bin/fabulate_angle configs/fabulate/new/fabulate_${tag}_angle.conf \
         data/fabulate_${tag}_angle.root
     ./bin/distillate_angle configs/distillate/new/distillate_${tag}_angle_dj.conf \
         data/distillate_${tag}_angle.root
 }
 
-systematic() {
-    sample=$1
-    syst=$2
-
-    arithmetic ${sample}_${syst}
-}
-
 samples=(pp aa pp_smear_0_10 pp_smear_10_30 pp_smear_30_50 pp_smear_50_90)
 
 for sample in ${samples[@]}; do
     nominal $sample
-
 done
