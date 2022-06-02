149b13e7116a0e507fdde40877e157971c9d82b5
mitaylor <mitay@mit.edu>
2022-06-02 06:04:12 -0400

    VaryResponseMatrix: make script

---
+++ MainAnalysis/VaryResponseMatrix.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+samples=(pp aa pp_smear_0_10 pp_smear_10_30 pp_smear_30_50 pp_smear_50_90)
+
+for sample in ${samples[@]}; do
+    ./bin/vary_response_matrix configs/vary_response_matrix/vary_response_matrix_${sample}.conf \
+        data/jet_20/vacillate_${sample}_stat_response.root
+done
