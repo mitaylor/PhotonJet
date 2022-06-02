9d05384dfd3828f89e74f61598765f8eee325754
mitaylor <mitay@mit.edu>
2022-06-02 02:20:37 -0400

    PlotResponse: update config location

--- MainAnalysis/PlotResponse.sh
+++ MainAnalysis/PlotResponse.sh
@@ -3,8 +3,8 @@
 arithmetic() {
     tag=$1
 
-    echo "./bin/plot_response_matrices configs/test/plot_response_matrices/plot_response_matrices_${tag}.conf"
-    ./bin/plot_response_matrices configs/test/plot_response_matrices/plot_response_matrices_${tag}.conf
+    echo "./bin/plot_response_matrices configs/plot_response_matrices/plot_response_matrices_${tag}.conf"
+    ./bin/plot_response_matrices configs/plot_response_matrices/plot_response_matrices_${tag}.conf
 }
 
 nominal() {
