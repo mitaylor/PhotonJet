9eeee29e3745eec758a37376d22b766e3854efd1
mitaylor <mitay@mit.edu>
2022-06-02 04:54:40 -0400

    update scripts for fine binning

--- MainAnalysis/Accumulate.sh
+++ MainAnalysis/Accumulate.sh
@@ -3,16 +3,14 @@
 arithmetic() {
     tag=$1
 
-    ./bin/accumulate configs/accumulate/new/jet_20/accumulate_${tag}.conf \
-        data/jet_20/accumulate_${tag}.root
+    ./bin/accumulate configs/accumulate/new/jet_20_fine/accumulate_${tag}.conf \
+        data/jet_20_fine/accumulate_${tag}.root
 }
 
 nominal() {
     sample=$1
 
     arithmetic ${sample}
-    arithmetic ${sample}_loose_purity
-    arithmetic ${sample}_tight_purity
 }
 
 systematic() {
@@ -22,21 +20,21 @@ systematic() {
     arithmetic ${sample}_${syst}
 }
 
-# samples=(pp aa pp_smear_0_10 pp_smear_10_30 pp_smear_30_50 pp_smear_50_90)
-samples=(pp_smear_0_10 pp_smear_10_30 pp_smear_30_50 pp_smear_50_90)
+samples=(pp aa pp_smear_0_10 pp_smear_10_30 pp_smear_30_50 pp_smear_50_90)
+# samples=(pp_smear_0_10 pp_smear_10_30 pp_smear_30_50 pp_smear_50_90)
 
 for sample in ${samples[@]}; do
     nominal $sample
 
-    for syst in wo_ele_rej qcd qcd_gen_iso; do
+    for syst in qcd; do
         systematic $sample $syst
     done
 done
 
 samples=(aa)
 
-for sample in ${samples[@]}; do
-    for syst in cent_up cent_down; do
-        systematic $sample $syst
-    done
-done
+# for sample in ${samples[@]}; do
+#     for syst in cent_up cent_down; do
+#         systematic $sample $syst
+#     done
+# done
