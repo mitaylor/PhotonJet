844452c8f7adfbeb8cac88991130f86fd7adf050
mitaylor <mitay@mit.edu>
2022-06-03 07:37:46 -0400

    ratio: create first draft of the ratio plot

--- MainAnalysis/src/ratio.C
+++ MainAnalysis/src/ratio.C
@@ -185,19 +185,19 @@ int ratio(char const* config, char const* output) {
                 links[h] = (*syst)[index]; });
         }, hists, systs, unfolded_qcds, truth_reco_isos);
 
-        /* take the ratio */std::cout << __LINE__ << std::endl;
-        for (int64_t i = 0; i < hists[0]->size(); ++i) {std::cout << __LINE__ << std::endl;
-            for (int64_t j = 1; j <= (*hists[0])[i]->GetNbinsX(); ++j) {std::cout << __LINE__ << std::endl;
-                auto aa_hist = (*hists[0])[i];std::cout << __LINE__ << std::endl;
-                auto pp_hist = (*hists[1])[i];std::cout << __LINE__ << std::endl;
+        /* take the ratio */
+        for (int64_t i = 0; i < hists[0]->size(); ++i) {
+            for (int64_t j = 1; j <= (*hists[0])[i]->GetNbinsX(); ++j) { std::cout << j << std::endl;
+                auto aa_hist = (*hists[0])[i];
+                auto pp_hist = (*hists[1])[i];
 
                 double aa_val = aa_hist->GetBinContent(j);
-                double aa_err = aa_hist->GetBinError(j);std::cout << __LINE__ << std::endl;
+                double aa_err = aa_hist->GetBinError(j);
                 double aa_syst_err = links[aa_hist]->GetBinContent(j);std::cout << __LINE__ << std::endl;
-                auto aa_err_scale = aa_err/aa_val;
-                auto aa_syst_err_scale = aa_syst_err/aa_val;
+                auto aa_err_scale = aa_err/aa_val;std::cout << __LINE__ << std::endl;
+                auto aa_syst_err_scale = aa_syst_err/aa_val;std::cout << __LINE__ << std::endl;
 
-                double pp_val = pp_hist->GetBinContent(j);
+                double pp_val = pp_hist->GetBinContent(j);std::cout << __LINE__ << std::endl;
                 double pp_err = pp_hist->GetBinError(j);std::cout << __LINE__ << std::endl;
                 double pp_syst_err = links[pp_hist]->GetBinContent(j);std::cout << __LINE__ << std::endl;
                 auto pp_err_scale = pp_err/pp_val;
@@ -215,7 +215,7 @@ int ratio(char const* config, char const* output) {
                 aa_hist->SetBinError(j, aa_err);
 
                 pp_hist->SetBinContent(j, 1);
-                pp_hist->SetBinError(j, pp_err);std::cout << __LINE__ << std::endl;
+                pp_hist->SetBinError(j, pp_err);
             }
         }
 
