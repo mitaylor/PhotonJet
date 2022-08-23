66e7b9d0a8d47e8200df068f444a14dfaba91902
mitaylor <mitay@mit.edu>
2022-08-23 07:07:26 -0400

    paper: plotting update for heading text

--- MainAnalysis/git/paper-and-pencil/src/paper.C
+++ MainAnalysis/git/paper-and-pencil/src/paper.C
@@ -91,7 +91,7 @@ void paper::draw(std::string const& ext) {
         canvas->cd();
         auto ledge = base + 0.15 * dx; std::cout << ledge << std::endl;
         auto redge = 1 - 0.1 * dx; std::cout << redge << std::endl;
-        auto bedge = dy * _rows; std::cout << bedge << std::endl;
+        auto bedge = 1 - 0.9 * dy; std::cout << bedge << std::endl;
         auto heading = new TPad("P0","",ledge,bedge,redge,1);
         heading->SetFillStyle(4000);
         heading->Draw();
