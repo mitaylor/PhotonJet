bc355ef0e1b8810de80d282da6832f29ccb528e5
mitaylor <mitay@mit.edu>
2022-08-25 18:19:32 -0400

    hf_shift: check hf vs rho distributions in hp and mb MC

--- MainAnalysis/src/hf_shift.C
+++ MainAnalysis/src/hf_shift.C
@@ -164,7 +164,7 @@ int hf_shift(char const* config, char const* output) {
     auto c3 = new paper("comp_rho_v_hf", hb);
     apply_style(c3, cms, system_tag);
     c3->add((*mb_rh_p)[0], "Hydjet");
-    c3->stack((*hp_rh_p)[0], "Hydjet");
+    c3->stack((*hp_rh_p)[0], "Pythia+Hydjet");
 
     hb->sketch();
     c1->draw("pdf");
