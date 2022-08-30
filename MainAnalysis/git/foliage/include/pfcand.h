e609b945dc2acaf690005e0cb7ff4a4876dd3da0
mitaylor <mitay@mit.edu>
2022-08-30 01:16:30 -0400

    pfcand: update to include eta information

--- MainAnalysis/git/foliage/include/pfcand.h
+++ MainAnalysis/git/foliage/include/pfcand.h
@@ -11,6 +11,8 @@
 #define B_VEC_PF_RECO(ACTION, ...)                                         \
     ACTION(sv<int>,         pfId,                       ## __VA_ARGS__)    \
     ACTION(sv<float>,       pfPt,                       ## __VA_ARGS__)    \
+    ACTION(sv<float>,       pfEta,                       ## __VA_ARGS__)    \
+    ACTION(sv<float>,       pfPhi,                       ## __VA_ARGS__)    \
 
 class pfcand : tree {
   public:
