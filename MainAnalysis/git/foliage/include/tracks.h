5826c6fa08901c1235c2076de0a5210cb18bdba9
mitaylor <mitay@mit.edu>
2022-08-30 00:30:03 -0400

    pjtree: add nTrk information to PP skims

--- MainAnalysis/git/foliage/include/tracks.h
+++ MainAnalysis/git/foliage/include/tracks.h
@@ -6,34 +6,34 @@
 
 #include "TTree.h"
 
-#define B_ALL_TRK_RECO(ACTION, ...)                                         \
-    B_VAL_TRK_RECO(ACTION,                              ## __VA_ARGS__)     \
-    B_ARR_TRK_RECO(ACTION,                              ## __VA_ARGS__)     \
+// #define B_ALL_TRK_RECO(ACTION, ...)                                         \
+//     B_VAL_TRK_RECO(ACTION,                              ## __VA_ARGS__)     \
+//     B_ARR_TRK_RECO(ACTION,                              ## __VA_ARGS__)     \
 
 #define B_VAL_TRK_RECO(ACTION, ...)                                         \
-    ACTION(int,             nTrk,                       ## __VA_ARGS__)     \
     ACTION(int,             nVtx,                       ## __VA_ARGS__)     \
+//     ACTION(int,             nTrk,                       ## __VA_ARGS__)     \
 
-#define B_ARR_TRK_RECO(ACTION, ...)                                         \
-    ACTION(float,           trkPt,                      ## __VA_ARGS__)     \
-    ACTION(float,           trkPtError,                 ## __VA_ARGS__)     \
-    ACTION(uint8_t,         trkNHit,                    ## __VA_ARGS__)     \
-    ACTION(uint8_t,         trkNlayer,                  ## __VA_ARGS__)     \
-    ACTION(float,           trkEta,                     ## __VA_ARGS__)     \
-    ACTION(float,           trkPhi,                     ## __VA_ARGS__)     \
-    ACTION(int32_t,         trkCharge,                  ## __VA_ARGS__)     \
-    ACTION(bool,            highPurity,                 ## __VA_ARGS__)     \
-    ACTION(float,           trkChi2,                    ## __VA_ARGS__)     \
-    ACTION(uint8_t,         trkNdof,                    ## __VA_ARGS__)     \
-    ACTION(float,           trkDxy1,                    ## __VA_ARGS__)     \
-    ACTION(float,           trkDxyError1,               ## __VA_ARGS__)     \
-    ACTION(float,           trkDz1,                     ## __VA_ARGS__)     \
-    ACTION(float,           trkDzError1,                ## __VA_ARGS__)     \
+// #define B_ARR_TRK_RECO(ACTION, ...)                                         \
+//     ACTION(float,           trkPt,                      ## __VA_ARGS__)     \
+//     ACTION(float,           trkPtError,                 ## __VA_ARGS__)     \
+//     ACTION(uint8_t,         trkNHit,                    ## __VA_ARGS__)     \
+//     ACTION(uint8_t,         trkNlayer,                  ## __VA_ARGS__)     \
+//     ACTION(float,           trkEta,                     ## __VA_ARGS__)     \
+//     ACTION(float,           trkPhi,                     ## __VA_ARGS__)     \
+//     ACTION(int32_t,         trkCharge,                  ## __VA_ARGS__)     \
+//     ACTION(bool,            highPurity,                 ## __VA_ARGS__)     \
+//     ACTION(float,           trkChi2,                    ## __VA_ARGS__)     \
+//     ACTION(uint8_t,         trkNdof,                    ## __VA_ARGS__)     \
+//     ACTION(float,           trkDxy1,                    ## __VA_ARGS__)     \
+//     ACTION(float,           trkDxyError1,               ## __VA_ARGS__)     \
+//     ACTION(float,           trkDz1,                     ## __VA_ARGS__)     \
+//     ACTION(float,           trkDzError1,                ## __VA_ARGS__)     \
 
 class tracks : tree {
   public:
     tracks() = default;
-    tracks(int64_t size);
+    // tracks(int64_t size);
     tracks(tracks const&) = delete;
     tracks& operator=(tracks const&) = delete;
     ~tracks() = default;
@@ -41,7 +41,7 @@ class tracks : tree {
     void read(TTree* t);
 
     B_VAL_TRK_RECO(DECLVAL)
-    B_ARR_TRK_RECO(DECLPTR)
+    // B_ARR_TRK_RECO(DECLPTR)
 
   private:
     int64_t _size;
--- MainAnalysis/git/foliage/src/tracks.C
+++ MainAnalysis/git/foliage/src/tracks.C
@@ -1,12 +1,12 @@
 #include "../include/tracks.h"
 
-tracks::tracks(int64_t size)
-    : _size(size) { }
+// tracks::tracks(int64_t size)
+//     : _size(size) { }
 
 void tracks::read(TTree* t) {
     B_VAL_TRK_RECO(SETZERO)
-    B_ARR_TRK_RECO(ALLOCARR, _size)
+    // B_ARR_TRK_RECO(ALLOCARR, _size)
 
     B_VAL_TRK_RECO(SETVALADDR, t)
-    B_ARR_TRK_RECO(SETPTRADDR, t)
+    // B_ARR_TRK_RECO(SETPTRADDR, t)
 }
