93819eff2ff2f9a880187aa573588084d2ef9ca8
mitaylor <mitay@mit.edu>
2022-08-30 07:42:09 -0400

    hf_shift: update to use Ncoll

--- MainAnalysis/src/hf_shift.C
+++ MainAnalysis/src/hf_shift.C
@@ -42,21 +42,35 @@ int hf_shift(char const* config, char const* output) {
 
     TH1::SetDefaultSumw2();
     
+    /* define histograms */
     auto irho = new interval("#rho"s, 100, 0, 400);
-    auto ihf = new interval("HF Energy"s, 150, 0, 6000);
+    auto ihf = new interval("HF Energy"s, 100, 0, 7000);
+    auto in = new interval("Ncoll"s, 100, 0, 2100);
 
-    auto mrh = new multival(*irho, *ihf);
+    auto mhn = new multival(*ihf, *in);
+    auto mrn = new multival(*irho, *in);
 
-    auto frh = std::bind(&multival::book<TH2F>, mrh, _1, _2, _3);
-    auto frhp = [&](int64_t, std::string const& name, std::string const& label) {
-        return new TProfile(name.data(), (";#rho;HF Energy;"s + label).data(), 100, 0, 400, 0, 7000, "LE"); };
+    auto fhn = std::bind(&multival::book<TH2F>, mhn, _1, _2, _3); // hf:ncoll
+    auto frn = std::bind(&multival::book<TH2F>, mrn, _1, _2, _3); // rho:ncoll
 
-    auto hp_rh = new history<TH2F>("hp_rh"s, "Pythia+Hydjet", frh, 1);
-    auto mb_rh = new history<TH2F>("mb_rh"s, "Hydjet", frh, 1);
+    auto hp_hn = new history<TH2F>("hp_hn"s, "Pythia+Hydjet", fhn, 1);
+    auto mb_hn = new history<TH2F>("mb_hn"s, "Hydjet", fhn, 1);
 
-    auto hp_rh_p = new history<TProfile>("hp_rh_p"s, "Pythia+Hydjet", frhp, 1);
-    auto mb_rh_p = new history<TProfile>("mb_rh_p"s, "Hydjet", frhp, 1);
+    auto hp_rn = new history<TH2F>("hp_rn"s, "Pythia+Hydjet", frn, 1);
+    auto mb_rn = new history<TH2F>("mb_rn"s, "Hydjet", frn, 1);
 
+    auto fhnp = [&](int64_t, std::string const& name, std::string const& label) {
+        return new TProfile(name.data(), (";Ncoll;HF Energy;"s + label).data(), 100, 0, 2100, 0, 7000, "LE"); };
+    auto frnp = [&](int64_t, std::string const& name, std::string const& label) {
+        return new TProfile(name.data(), (";Ncoll;#rho;"s + label).data(), 100, 0, 2100, 0, 400, "LE"); };
+
+    auto hp_hn_p = new history<TProfile>("hp_hn_p"s, "Pythia+Hydjet", fhnp, 1);
+    auto mb_hn_p = new history<TProfile>("mb_hn_p"s, "Hydjet", fhnp, 1);
+
+    auto hp_rn_p = new history<TProfile>("hp_rn_p"s, "Pythia+Hydjet", frnp, 1);
+    auto mb_rn_p = new history<TProfile>("mb_rn_p"s, "Hydjet", frnp, 1);
+
+    /* read input files */
     TFile* hp_f = new TFile(hp_input.data(), "read");
     TTree* hp_t = (TTree*) hp_f->Get("pj");
     auto hp_pjt = new pjtree(false, false, true, hp_t, { 1, 1, 1, 1, 1, 0, 1, 0, 0 });
@@ -65,7 +79,11 @@ int hf_shift(char const* config, char const* output) {
     TTree* mb_t = (TTree*) mb_f->Get("pj");
     auto mb_pjt = new pjtree(false, false, true, mb_t, { 1, 1, 1, 1, 1, 0, 1, 0, 0 });
 
+    double hp_avg_hf = 0;
+    double hp_avg_rho = 0;
+
     int64_t nentries = static_cast<int64_t>(hp_t->GetEntries());
+    nentries = nentries > 10000000 ? 10000000 : nentries;
 
     for (int64_t i = 0; i < nentries; ++i) {
         if (i % 100000 == 0)
@@ -125,8 +143,15 @@ int hf_shift(char const* config, char const* output) {
         if (electron) { continue; }
 
         auto avg_rho = get_avg_rho(hp_pjt, -photon_eta_abs, photon_eta_abs);
-        (*hp_rh)[0]->Fill(avg_rho, hp_pjt->hiHF, hp_pjt->w);
-        (*hp_rh_p)[0]->Fill(avg_rho, hp_pjt->hiHF, hp_pjt->w);
+
+        hp_avg_hf += hp_pjt->hiHF;
+        hp_avg_rho += avg_rho;
+
+        (*hp_hn)[0]->Fill(hp_pjt->hiHF, hp_pjt->Ncoll, hp_pjt->w);
+        (*hp_rn)[0]->Fill(avg_rho, hp_pjt->Ncoll, hp_pjt->w);
+
+        (*hp_hn_p)[0]->Fill(hp_pjt->Ncoll, hp_pjt->hiHF, hp_pjt->w);
+        (*hp_rn_p)[0]->Fill(hp_pjt->Ncoll, avg_rho, hp_pjt->w);
     }
 
     nentries = static_cast<int64_t>(mb_t->GetEntries());
@@ -136,49 +161,115 @@ int hf_shift(char const* config, char const* output) {
             printf("entry: %li/%li\n", i, nentries);
 
         mb_t->GetEntry(i);
+
         auto avg_rho = get_avg_rho(mb_pjt, -photon_eta_abs, photon_eta_abs);
-        (*mb_rh)[0]->Fill(avg_rho, mb_pjt->hiHF, mb_pjt->w);
-        (*mb_rh_p)[0]->Fill(avg_rho, mb_pjt->hiHF, mb_pjt->w);
+
+        mb_avg_hf += mb_pjt->hiHF;
+        mb_avg_rho += avg_rho;
+
+        (*mb_hn)[0]->Fill(mb_pjt->hiHF, mb_pjt->Ncoll, mb_pjt->w);
+        (*mb_rn)[0]->Fill(avg_rho, mb_pjt->Ncoll, mb_pjt->w);
+
+        (*mb_hn_p)[0]->Fill(mb_pjt->Ncoll, mb_pjt->hiHF, mb_pjt->w);
+        (*mb_rn_p)[0]->Fill(mb_pjt->Ncoll, avg_rho, mb_pjt->w);
     }
 
+    /* subtract distributions */
+    auto diff_hn_p = (TH1*) (*hp_hn_p)[0]->Clone();
+    auto diff_rn_p = (TH1*) (*hp_rn_p)[0]->Clone();
+
+    diff_hn_p->SetNameTitle("diff_hn_p", ";;Orange - Purple");
+    diff_rn_p->SetNameTitle("diff_rn_p", ";;Orange - Purple");
+
+    if (!(diff_hn_p->GetSumw2N() > 0)) diff_hn_p->Sumw2(true);
+    if (!(diff_rn_p->GetSumw2N() > 0)) diff_rn_p->Sumw2(true);
+
+    diff_hn_p->Add((*mb_hn_p)[0], -1);
+    diff_rn_p->Add((*mb_rn_p)[0], -1);
+
+    diff_hn_p->SetMaximum(300);
+    diff_hn_p->SetMinimum(-300);
+    diff_rn_p->SetMaximum(10);
+    diff_rn_p->SetMinimum(-10);
+
+    std::cout << "HF difference: " << hp_avg_hf - mb_avg_hf << std::endl;
+    std::cout << "Rho difference: " << hp_avg_rho - mb_avg_rho << std::endl;
+
     /* draw rho distributions */
+    /* draw distributions */
     auto system_tag = "PbPb #sqrt{s_{NN}} = 5.02 TeV"s; 
     auto cms = "#bf{#scale[1.4]{CMS}} #it{#scale[1.2]{Simulation}}"s;
     // cms += "         p_{T}^{#gamma} > 40 GeV";
 
     auto hb = new pencil();
     hb->category("type", "Pythia+Hydjet", "Hydjet"); 
-    
-    auto c1 = new paper("pythia_hydjet_rho_v_hf", hb); 
-    apply_style(c1, cms, system_tag);
-    c1->set(paper::flags::logz);
-    c1->add((*hp_rh)[0], "Pythia+Hydjet");
-    c1->adjust((*hp_rh)[0], "col", "");
 
-    auto c2 = new paper("hydjet_rho_v_hf", hb);
-    apply_style(c2, cms, system_tag);
-    c2->set(paper::flags::logz);
-    c2->add((*mb_rh)[0], "Hydjet");
-    c2->adjust((*mb_rh)[0], "col", "");
-
-    auto c3 = new paper("comp_rho_v_hf", hb);
+    auto c3 = new paper("pythia_hydjet_hf_v_npart", hb); 
     apply_style(c3, cms, system_tag);
-    (*mb_rh_p)[0]->SetMarkerSize(0.1);
-    (*hp_rh_p)[0]->SetMarkerSize(0.1);
-    c3->add((*mb_rh_p)[0], "Hydjet");
-    c3->stack((*hp_rh_p)[0], "Pythia+Hydjet");
+    c3->set(paper::flags::logz);
+    c3->add((*hp_hn)[0], "Pythia+Hydjet");
+    c3->adjust((*hp_hn)[0], "col", "");
+
+    auto c4 = new paper("hydjet_hf_v_npart", hb);
+    apply_style(c4, cms, system_tag);
+    c4->set(paper::flags::logz);
+    c4->add((*mb_hn)[0], "Hydjet");
+    c4->adjust((*mb_hn)[0], "col", "");
+
+    auto c5 = new paper("pythia_hydjet_rho_v_npart", hb); 
+    apply_style(c5, cms, system_tag);
+    c5->set(paper::flags::logz);
+    c5->add((*hp_rn)[0], "Pythia+Hydjet");
+    c5->adjust((*hp_rn)[0], "col", "");
+
+    auto c6 = new paper("hydjet_rho_v_npart", hb);
+    apply_style(c6, cms, system_tag);
+    c6->set(paper::flags::logz);
+    c6->add((*mb_rn)[0], "Hydjet");
+    c6->adjust((*mb_rn)[0], "col", "");
+
+    auto c8 = new paper("comp_hf_v_npart", hb);
+    c8->divide(1, -1);
+    apply_style(c8, cms, system_tag);
+    c8->add((*mb_hn_p)[0], "Hydjet");
+    c8->stack((*hp_hn_p)[0], "Pythia+Hydjet");
+    c8->add(diff_hn_p);
+
+    auto c9 = new paper("comp_rho_v_npart", hb);
+    c9->divide(1, -1);
+    apply_style(c9, cms, system_tag);
+    c9->add((*mb_rn_p)[0], "Hydjet");
+    c9->stack((*hp_rn_p)[0], "Pythia+Hydjet");
+    c9->add(diff_rn_p);
+
+    auto hp_style = [](TH1* h) {
+        h->SetMarkerStyle(34);
+        h->SetMarkerColor(95);
+        h->SetMarkerSize(0.60);
+    };
+
+    auto mb_style = [](TH1* h) {
+        h->SetMarkerStyle(34);
+        h->SetMarkerColor(51);
+        h->SetMarkerSize(0.60);
+    };
+
+    hb->style("Hydjet", mb_style);
+    hb->style("Pythia+Hydjet", hp_style);
 
     hb->sketch();
-    c1->draw("pdf");
-    c2->draw("pdf");
     c3->draw("pdf");
+    c4->draw("pdf");
+    c5->draw("pdf");
+    c6->draw("pdf");
+    c8->draw("pdf");
+    c9->draw("pdf");
 
-    /* save output */
     in(output, [&]() {
-        mb_rh->save();
-        hp_rh->save();
-        mb_rh_p->save();
-        hp_rh_p->save();
+        hp_hn->save();
+        hp_rn->save();
+        mb_hn->save();
+        mb_rn->save();
     });
 
     return 0;
