fede3c94d644204bf2f1adbc2d56076034af8477
mitaylor <mitay@mit.edu>
2022-09-01 06:49:08 -0400

    hf_shift: debugging

--- MainAnalysis/src/hf_shift.C
+++ MainAnalysis/src/hf_shift.C
@@ -182,6 +182,10 @@ int hf_shift(char const* config, char const* output) {
     auto diff_hn_p = (TH1*) (*hp_hn_p)[0]->Clone();
     auto diff_rn_p = (TH1*) (*hp_rn_p)[0]->Clone();
 
+    for (int i = 1; i <= diff_hn_p->GetNBins(); ++i) {
+        cout << diff_hn_p->GetBinContent(i) << endl;
+    }
+
     diff_rn_p->Scale(nentries / nphotons);
     diff_hn_p->Scale(nentries / nphotons);
 
