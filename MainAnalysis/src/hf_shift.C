87b36236644b2794f15e2ae278599ee9e1d3c26e
mitaylor <mitay@mit.edu>
2022-09-06 07:54:01 -0400

    hf_shift: compare the extra MC with the AOD skim

--- MainAnalysis/src/hf_shift.C
+++ MainAnalysis/src/hf_shift.C
@@ -83,7 +83,7 @@ int hf_shift(char const* config, char const* output) {
     double hp_avg_rho = 0;
 
     int64_t nentries = static_cast<int64_t>(hp_t->GetEntries());
-    nentries = nentries > 10000000 ? 10000000 : nentries;
+    // nentries = nentries > 10000000 ? 10000000 : nentries;
     double nphotons = 0;
 
     for (int64_t i = 0; i < nentries; ++i) {
@@ -152,7 +152,7 @@ int hf_shift(char const* config, char const* output) {
     }
 
     nentries = static_cast<int64_t>(mb_t->GetEntries());
-    nentries = nentries > 10000000 ? 10000000 : nentries;
+    // nentries = nentries > 10000000 ? 10000000 : nentries;
     double nmb = 0;
 
     double mb_avg_hf = 0;
