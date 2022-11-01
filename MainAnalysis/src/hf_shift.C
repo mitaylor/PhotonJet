a069993a254d1f9a660aeef319050d17d15facad
mitaylor <mitay@mit.edu>
2022-11-01 05:29:00 -0400

    hf_shift: add constant fit

--- MainAnalysis/src/hf_shift.C
+++ MainAnalysis/src/hf_shift.C
@@ -83,7 +83,7 @@ int hf_shift(char const* config, char const* output) {
     auto mb_pjt = new pjtree(true, false, true, mb_t, { 1, 1, 1, 1, 1, 0, 1, 1, 0 });
 
     int64_t nentries = static_cast<int64_t>(hp_t->GetEntries());
-    // nentries = nentries > 100000 ? 100000 : nentries;
+    nentries = nentries > 100000 ? 100000 : nentries;
     double nphotons = 0;
 
     for (int64_t i = 0; i < nentries; ++i) {
@@ -157,7 +157,7 @@ int hf_shift(char const* config, char const* output) {
     }
 
     nentries = static_cast<int64_t>(mb_t->GetEntries());
-    // nentries = nentries > 100000 ? 100000 : nentries;
+    nentries = nentries > 100000 ? 100000 : nentries;
     double nmb = 0;
 
     for (int64_t i = 0; i < nentries; ++i) {
