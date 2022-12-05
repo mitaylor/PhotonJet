22c241bf7bf377d12d16932923a6205c7f1f0bc4
mitaylor <mitay@mit.edu>
2022-12-05 13:25:15 -0500

    Foresting: redo PbPb data forests to get back files which disappeared

--- Foresting/211011_PbPbData/crabConfig_forest_miniAOD_112X_HP.py
+++ Foresting/211011_PbPbData/crabConfig_forest_miniAOD_112X_HP.py
@@ -1,6 +1,6 @@
 OutputBase = '/store/user/mitaylor/PhotonJet'
 DatasetName = '/HIHardProbes/HIRun2018A-PbPb18_MiniAODv1-v1/MINIAOD'
-Tag = '20211223PbPbHP'
+Tag = '20221205PbPbHP'
 
 from WMCore.Configuration import Configuration
 config = Configuration()
