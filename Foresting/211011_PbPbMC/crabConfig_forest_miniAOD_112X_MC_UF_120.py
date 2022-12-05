2502f4f9029f804172874c7d07fea1abcaedc1e8
mitaylor <mitay@mit.edu>
2022-12-05 13:23:49 -0500

    Foresting: redo PbPb QCD MC forests to get back files which disappeared

--- Foresting/211011_PbPbMC/crabConfig_forest_miniAOD_112X_MC_UF_120.py
+++ Foresting/211011_PbPbMC/crabConfig_forest_miniAOD_112X_MC_UF_120.py
@@ -1,6 +1,6 @@
 OutputBase = '/store/user/mitaylor/PhotonJet'
 DatasetName = '/QCDPhoton_pThat-120_TuneCP5_HydjetDrumMB_5p02TeV_Pythia8/HINPbPbSpring21MiniAOD-mva98_112X_upgrade2018_realistic_HI_v9-v1/MINIAODSIM'
-Tag = '20211224PbPbMCUF120'
+Tag = '20221205PbPbMCUF120'
 
 from WMCore.Configuration import Configuration
 config = Configuration()
--- Foresting/211011_PbPbMC/crabConfig_forest_miniAOD_112X_MC_UF_50.py
+++ Foresting/211011_PbPbMC/crabConfig_forest_miniAOD_112X_MC_UF_50.py
@@ -1,6 +1,6 @@
 OutputBase = '/store/user/mitaylor/PhotonJet'
 DatasetName = '/QCDPhoton_pThat-50_TuneCP5_HydjetDrumMB_5p02TeV_Pythia8/HINPbPbSpring21MiniAOD-mva98_112X_upgrade2018_realistic_HI_v9-v1/MINIAODSIM'
-Tag = '20211224PbPbMCUF50'
+Tag = '20221205PbPbMCUF50'
 
 from WMCore.Configuration import Configuration
 config = Configuration()
