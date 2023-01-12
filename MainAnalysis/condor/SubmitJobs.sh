b2d54615cbfd6bc990c47c704beb71095d273ebe
mitaylor <mitay@mit.edu>
2023-01-12 03:33:59 -0500

    Condor: last try

--- MainAnalysis/condor/SubmitJobs.sh
+++ MainAnalysis/condor/SubmitJobs.sh
@@ -1,32 +1,31 @@
 #!/usr/bin/env bash
 
+get_number { declare local s="${1%%.*}"; echo "${s##*[!0-9]}"; }
+
 folder=${1}
 config_fragment=${2}
 output_tag=${3}
-n_files=${4}
 
 mkdir -p ${output_tag}
 cd ${output_tag}
 
 # split the input files so there are no more than 30 files per job
-find ${folder} -type f > ${output_tag}
-split -l ${n_files} -d -a 3 ${output_tag} ${output_tag}
-files=`find . -type f -name "${output_tag}???"`
+files=`find ${folder} -type f -printf '%f\n' > ${output_tag}`
 
 # create all of the config files
 for file in ${files}; do
-    sed -i '1 i\ std::vector<std::string> files =' ${file}
-    sed -i 's/$/ \\/' ${file} # add \ after every line
-    sed -i '${s/\\$//;p;x}' ${file} # delete the final \
-
-    echo "$(cat ${config_fragment})" >> ${file} # add the rest of the configuration file
+    index=$(get_number ${file})
+    echo 'std::vector<std::string> files = \' > ${output_tag}_{index}.conf
+    echo "${file}" >> ${output_tag}_{index}.conf
+    echo "" >> ${output_tag}_{index}.conf
+    echo "$(cat ${config_fragment})" >> ${output_tag}_{index}.conf # add the rest of the configuration file
 done
 
-find . -type f -name "${output_tag}???" -printf '%f\n' > "${output_tag}.list"
+find . -type f -name "${output_tag}_*" -printf '%f\n' > "${output_tag}.list"
 sed -i '=' "${output_tag}.list"
 sed -i 'N; s/\n/, /' "${output_tag}.list"
 
 echo "$(cat ${output_tag}.list)"
 
-cat ../SubmitCondor.condor | sed "s/__MASTER__/${output_tag}/g" > SubmitCondor_${output_tag}.condor
-condor_submit SubmitCondor_${output_tag}.condor
+# cat ../SubmitCondor.condor | sed "s/__MASTER__/${output_tag}/g" > SubmitCondor_${output_tag}.condor
+# condor_submit SubmitCondor_${output_tag}.condor
---
+++ MainAnalysis/condor/SubmitJobs_Split.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+
+folder=${1}
+config_fragment=${2}
+output_tag=${3}
+n_files=${4}
+
+mkdir -p ${output_tag}
+cd ${output_tag}
+
+# split the input files so there are no more than 30 files per job
+find ${folder} -type f > ${output_tag}
+split -l ${n_files} -d -a 3 ${output_tag} ${output_tag}
+files=`find . -type f -name "${output_tag}???"`
+
+# create all of the config files
+for file in ${files}; do
+    sed -i '1 i\ std::vector<std::string> files =' ${file}
+    sed -i 's/$/ \\/' ${file} # add \ after every line
+    sed -i '${s/\\$//;p;x}' ${file} # delete the final \
+
+    echo "$(cat ${config_fragment})" >> ${file} # add the rest of the configuration file
+done
+
+find . -type f -name "${output_tag}???" -printf '%f\n' > "${output_tag}.list"
+sed -i '=' "${output_tag}.list"
+sed -i 'N; s/\n/, /' "${output_tag}.list"
+
+echo "$(cat ${output_tag}.list)"
+
+cat ../SubmitCondor.condor | sed "s/__MASTER__/${output_tag}/g" > SubmitCondor_${output_tag}.condor
+condor_submit SubmitCondor_${output_tag}.condor
