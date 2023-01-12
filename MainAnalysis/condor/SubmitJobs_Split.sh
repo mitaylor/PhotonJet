68055094a15c48f7e9b22625f04a35ec1325a101
mitaylor <mitay@mit.edu>
2023-01-12 04:10:06 -0500

    Condor: last try

--- MainAnalysis/condor/SubmitJobs_Split.sh
+++ MainAnalysis/condor/SubmitJobs_Split.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
 
+get_number () { 
+    s="${1%%.*}"
+    echo "${s##*[!0-9]}"
+}
+
 folder=${1}
 config_fragment=${2}
 output_tag=${3}
@@ -7,6 +12,7 @@ n_files=${4}
 
 mkdir -p ${output_tag}
 cd ${output_tag}
+rm *
 
 # split the input files so there are no more than 30 files per job
 find ${folder} -type f > ${output_tag}
@@ -15,18 +21,20 @@ files=`find . -type f -name "${output_tag}???"`
 
 # create all of the config files
 for file in ${files}; do
+    index=$(get_number ${file})
+
     sed -i '1 i\ std::vector<std::string> files =' ${file}
     sed -i 's/$/ \\/' ${file} # add \ after every line
     sed -i '${s/\\$//;p;x}' ${file} # delete the final \
-
     echo "$(cat ${config_fragment})" >> ${file} # add the rest of the configuration file
+
+    echo "${index}, ${file}_${index}.conf" >> "${output_tag}.list"
 done
 
-find . -type f -name "${output_tag}???" -printf '%f\n' > "${output_tag}.list"
-sed -i '=' "${output_tag}.list"
-sed -i 'N; s/\n/, /' "${output_tag}.list"
 
 echo "$(cat ${output_tag}.list)"
 
-cat ../SubmitCondor.condor | sed "s/__MASTER__/${output_tag}/g" > SubmitCondor_${output_tag}.condor
-condor_submit SubmitCondor_${output_tag}.condor
+# sed -e '$ ! s/$/,/g' lstfiles.txt | tr -d '\n'
+
+# cat ../SubmitCondor.condor | sed "s/__MASTER__/${output_tag}/g" > SubmitCondor_${output_tag}.condor
+# condor_submit SubmitCondor_${output_tag}.condor
