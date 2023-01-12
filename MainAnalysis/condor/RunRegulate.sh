f05a46b5b7a8871f8017ce4b2053ecc022cdfc94
mitaylor <mitay@mit.edu>
2023-01-12 01:22:59 -0500

    Condor: adding ability to submit Condor jobs to skim datasets

--- MainAnalysis/condor/RunRegulate.sh
+++ MainAnalysis/condor/RunRegulate.sh
@@ -5,8 +5,19 @@ output=$2
 
 WorkDir=`pwd`
 
-cd /afs/cern.ch/user/m/mitaylor/cmssw/CMSSW_10_3_3_patch1/src/
+# cd /afs/cern.ch/user/m/mitaylor/cmssw/CMSSW_10_3_3_patch1/src/
+# eval `scramv1 runtime -sh`
+# cd $WorkDir
+
+source /cvmfs/cms.cern.ch/cmsset_default.sh
+export SCRAM_ARCH=slc6_amd64_gcc700
+
+if [ -r CMSSW_10_3_3_patch1/src ] ; then
+ echo release CMSSW_10_3_3_patch1 already exists
+else
+scram p CMSSW CMSSW_10_3_3_patch1
+fi
+cd CMSSW_10_3_3_patch1/src
 eval `scramv1 runtime -sh`
-cd $WorkDir
 
 regulate $file $output
