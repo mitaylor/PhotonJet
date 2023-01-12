f7840580f8290a18c07e8b6d58e5fb43c3017804
mitaylor <mitay@mit.edu>
2023-01-12 03:40:58 -0500

    Condor: last try

--- MainAnalysis/condor/SubmitJobs.sh
+++ MainAnalysis/condor/SubmitJobs.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
-# get_number () { 
-#     s="${1%%.*}"
-#     echo "${s##*[!0-9]}"
-# }
+get_number () { 
+    s="${1%%.*}"
+    echo "${s##*[!0-9]}"
+}
 
 folder=${1}
 config_fragment=${2}
@@ -13,14 +13,12 @@ mkdir -p ${output_tag}
 cd ${output_tag}
 
 # split the input files so there are no more than 30 files per job
-files=`find ${folder} -type f -printf '%f\n' > ${output_tag}`
-echo $files
+# files=`find ${folder} -type f -printf '%f\n' > ${output_tag}`
+files=`find ${folder} -type f -printf '%f\n'
 
 # create all of the config files
 for file in ${files}; do
-    # index=$(get_number ${file})
-    index=2
-    echo $index
+    index=$(get_number ${file})
     echo 'std::vector<std::string> files = \' > ${output_tag}_{index}.conf
     echo "${file}" >> ${output_tag}_{index}.conf
     echo "" >> ${output_tag}_{index}.conf
