5648d5ec178401ccea5a571ef6191b5f7653bc09
mitaylor <mitay@mit.edu>
2023-01-12 01:13:19 -0500

    Condor: adding ability to submit Condor jobs to skim datasets

--- MainAnalysis/condor/RunRegulate.sh
+++ MainAnalysis/condor/RunRegulate.sh
@@ -3,4 +3,10 @@
 file=$1
 output=$2
 
-/home/submit/mitay/physics/PhotonJet2018/MainAnalysis/bin/regulate $file $output
+WorkDir=`pwd`
+
+cd /afs/cern.ch/user/m/mitaylor/cmssw/CMSSW_10_3_3_patch1/src/
+eval `scramv1 runtime -sh`
+cd $WorkDir
+
+regulate $file $output
