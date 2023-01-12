7769061ee1b896660b1f19a94190427bb63b5865
mitaylor <mitay@mit.edu>
2023-01-11 23:42:14 -0500

    Condor: adding ability to submit Condor jobs to skim datasets

--- MainAnalysis/condor/CreateInputs.sh
+++ MainAnalysis/condor/CreateInputs.sh
@@ -7,7 +7,7 @@ output_tag=${3}
 # split the input files so there are no more than 30 files per job
 find ${folder} -type f > ${output_tag}
 split -l 30 -d -a 3 ${output_tag} ${output_tag}
-files=`find . -type f -iname ${output_tag}???`
+files=`find . -type f -name ${output_tag}???`
 
 echo $files
 
@@ -20,7 +20,5 @@ for file in ${files}; do
     echo "$(cat ${config_fragment})" >> ${file}.py # add the rest of the configuration file
 done
 
-find . -type f -iname ${output_tag}??? > "${output_tag}.list"
+find . -type f -name ${output_tag}??? > "${output_tag}.list"
 sed '=' "${output_tag}.list" | sed -i 'N; s/\n/, /'
-
-echo "$(cat ${config_fragment})"
