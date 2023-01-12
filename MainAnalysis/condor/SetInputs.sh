7c078ec363ad20753b023c890b2508a3ec032d28
mitaylor <mitay@mit.edu>
2023-01-12 02:13:41 -0500

    Condor: adding ability to submit Condor jobs to skim datasets

--- MainAnalysis/condor/SetInputs.sh
+++ MainAnalysis/condor/SetInputs.sh
@@ -3,5 +3,5 @@
 # regulate_aa_data
 ./SubmitJobs.sh \
 "/mnt/T2_US_MIT/hadoop/cms/store/user/mitaylor/PhotonJet/HIHardProbes/HIRun2018A-PbPb18_MiniAODv1-v1/MINIAOD/HIHardProbes/20221205PbPbHP/221205_183819/" \
-../configs/regulate/preapproval-parallel/regulate_aa_data.conf \
+../../configs/regulate/preapproval-parallel/regulate_aa_data.conf \
 "regulate_aa_data" 30
