ed38714536da4bc38555f5fe798fba1dced7dae2
mitaylor <mitay@mit.edu>
2023-01-12 22:43:59 -0500

    Condor: adding configurations for PbPb miniAOD MC filtered

--- MainAnalysis/condor/SubmitJobs.sh
+++ MainAnalysis/condor/SubmitJobs.sh
@@ -8,31 +8,33 @@ get_number () {
 folder=${1}
 config_fragment=${2}
 output_tag=${3}
+n_files=${4}
 
 mkdir -p ${output_tag}
 cd ${output_tag}
 rm *
 
 # split the input files so there are no more than 30 files per job
-# files=`find ${folder} -type f -printf '%f\n' > ${output_tag}`
-files=`find ${folder} -type f -printf '%f\n'`
+find ${folder} -type f > ${output_tag}
+sed -i 's/^.*\/store/root:\/\/xrootd.cmsaf.mit.edu\/\/store/' ${output_tag}
+split -l ${n_files} -d -a 3 ${output_tag} ${output_tag}_
+files=`find . -type f -name "${output_tag}_[0-9][0-9][0-9]" -printf '%f\n'`
 
 # create all of the config files
 for file in ${files}; do
+    mv ${file} ${file}.conf
     index=$(get_number ${file})
-    echo 'std::vector<std::string> files = \' > ${output_tag}_${index}.conf
-    echo "${file}" >> ${output_tag}_${index}.conf
-    echo "" >> ${output_tag}_${index}.conf
-    echo "$(cat ${config_fragment})" >> ${output_tag}_${index}.conf # add the rest of the configuration file
 
-    echo "${index}, ${output_tag}_${index}.conf" >> "${output_tag}.list"
+    sed -i '1 i\ std::vector<std::string> files =' ${file}.conf
+    sed -i 's/$/ \\/' ${file}.conf # add \ after every line
+    sed -i '${s/\\$//;p;x}' ${file}.conf # delete the final \
+    echo "$(cat ${config_fragment})" >> ${file}.conf # add the rest of the configuration file
+
+    echo "${index}, ${file}.conf" >> "${output_tag}.list"
 done
 
-# find . -type f -name "${output_tag}_*" -printf '%f\n' > "${output_tag}.list"
-# sed -i '=' "${output_tag}.list"
-# sed -i 'N; s/\n/, /' "${output_tag}.list"
 
 echo "$(cat ${output_tag}.list)"
 
-# cat ../SubmitCondor.condor | sed "s/__MASTER__/${output_tag}/g" > SubmitCondor_${output_tag}.condor
-# condor_submit SubmitCondor_${output_tag}.condor
+cat ../SubmitCondor_Split.condor | sed "s/__MASTER__/${output_tag}/g" > SubmitCondor_${output_tag}.condor
+condor_submit SubmitCondor_${output_tag}.condor
