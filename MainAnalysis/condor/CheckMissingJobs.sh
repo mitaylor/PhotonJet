4b6fa32798c3cdeecd2def42792ece446d2afc77
mitaylor <mitay@mit.edu>
2023-01-19 07:13:03 -0500

    condor: convenience script to eliminate outdated files

--- MainAnalysis/condor/CheckMissingJobs.sh
+++ MainAnalysis/condor/CheckMissingJobs.sh
@@ -1,10 +1,15 @@
 #!/usr/bin/env bash
 
-confs=$(ls */*.conf | awk -F / '{print $2}' | sed 's/conf/root/g' | sed 's/^/\/data\/submit\/mitay\/photons\/condor\//')
+# confs=$(ls */*.conf | awk -F / '{print $2}' | sed 's/conf/root/g' | sed 's/^/\/data\/submit\/mitay\/photons\/condor\//')
+ls */*.conf | awk -F / '{print $2}' | sed 's/conf/root/g' | sed 's/^/\/data\/submit\/mitay\/photons\/condor\//' > temp.txt
 
 cp /tmp/x509up_u168456 x509up_u168456
 
-for conf in ${confs}; do
-    echo $conf
-    # bash -c '[[ ! -e ${conf} ]] && echo "${conf}"'
-done
+xargs -I % bash -c '[[ ! -e $1 ]] && echo "$1"' _ % < temp.txt > missing.txt
+
+rm temp.txt
+
+# for conf in ${confs}; do
+#     # echo $conf
+#     bash -c '[[ ! -e ${conf} ]] && echo "${conf}"'
+# done
