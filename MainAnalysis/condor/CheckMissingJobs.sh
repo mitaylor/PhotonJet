71b62d977dcb6276203d55f08931a3cb2ebc3a5e
mitaylor <mitay@mit.edu>
2023-01-19 07:31:55 -0500

    condor: convenience script to eliminate outdated files

--- MainAnalysis/condor/CheckMissingJobs.sh
+++ MainAnalysis/condor/CheckMissingJobs.sh
@@ -2,12 +2,17 @@
 
 # confs=$(ls */*.conf | awk -F / '{print $2}' | sed 's/conf/root/g' | sed 's/^/\/data\/submit\/mitay\/photons\/condor\//')
 ls */*.conf | awk -F / '{print $2}' | sed 's/conf/root/g' | sed 's/^/\/data\/submit\/mitay\/photons\/condor\//' > temp.txt
+xargs -I % bash -c '[[ ! -e $1 ]] && echo "$1"' _ % < temp.txt > missing.txt
+rm temp.txt
+
+numbers=$(cat missing.txt | awk -F / '{print $7}' | awk -F . '{print $1}' | awk -F _ '{print $NF}')
+folders=$(cat missing.txt | awk -F / '{print $7}' | awk -F . '{print $1}' | sed -r 's/(.*)\_.*/\1/')
 
 cp /tmp/x509up_u168456 x509up_u168456
 
-xargs -I % bash -c '[[ ! -e $1 ]] && echo "$1"' _ % < temp.txt > missing.txt
-
-rm temp.txt
+for number in $numbers; do
+    echo $number
+done
 
 # for conf in ${confs}; do
 #     # echo $conf
