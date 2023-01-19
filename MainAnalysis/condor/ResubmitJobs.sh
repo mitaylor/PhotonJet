5f69f0331238aa371472dd5cf0642c97fbabb396
mitaylor <mitay@mit.edu>
2023-01-18 22:58:48 -0500

    condor: write script to resubmit jobs that match a criteria

--- MainAnalysis/condor/ResubmitJobs.sh
+++ MainAnalysis/condor/ResubmitJobs.sh
@@ -2,7 +2,7 @@
 
 list=${1}
 
-folders=$(awk -F '/' '{print $2}' ${list})
+folders=($(awk -F '/' '{print $2}' ${list}))
 numbers=($(awk -F '.' '{print $4}' ${list}))
 
 cp /tmp/x509up_u168456 x509up_u168456
@@ -10,19 +10,18 @@ cp /tmp/x509up_u168456 x509up_u168456
 old_folder=""
 
 for i in "${!folders[@]}"; do
-    echo $i
-    # if [ ${folders[i]} -eq ${old_folder} ]
-    # then
-    #     echo "${numbers[i]}, ${folders[i]}_${numbers[i]}.conf" # >> "${folders[i]}/${folders[i]}.list"
-    # else
-    #     # echo ${old_folder}
-    #     echo "${numbers[i]}, ${folders[i]}_${numbers[i]}.conf" #> "${folders[i]}/${folders[i]}.list"
-    # fi
+    if [ ${folders[i]} -eq ${old_folder} ]
+    then
+        echo "${numbers[i]}, ${folders[i]}_${numbers[i]}.conf" # >> "${folders[i]}/${folders[i]}.list"
+    else
+        # echo ${old_folder}
+        echo "${numbers[i]}, ${folders[i]}_${numbers[i]}.conf" #> "${folders[i]}/${folders[i]}.list"
+    fi
 
-    # old_folder=${folders[i]}
+    old_folder=${folders[i]}
 done
 
-# echo ${old_folder}
+echo ${old_folder}
 
 
 # get_number () { 
