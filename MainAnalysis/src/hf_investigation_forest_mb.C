9ac4837767c7988b390e0bad3bcd1e0df6a58d38
mitaylor <mitay@mit.edu>
2023-02-01 15:53:29 -0500

    hf_investigation: try adding new plots

--- MainAnalysis/src/hf_investigation_forest_mb.C
+++ MainAnalysis/src/hf_investigation_forest_mb.C
@@ -113,25 +113,27 @@ int Compare(char const* config, char const* output) {
     TTreeReaderValue<std::vector<float>> pfPhi(pfReader, "pfPhi");
 
     /* create histograms */
-    int max_hf = 70000;
-    int max_avg_hf = 100; //2500;
-
-    auto ipt = new interval(dpt);
-    auto ihf = new interval("PF HF"s, 20, 0, max_avg_hf);
+    auto iphoton = new interval(dpt);
+    auto ienergy = new interval("HF Energy Sum"s, 20, 0, 2500);
+    auto ipt = new interval("HF Pt Sum"s, 20, 0, 150);
     auto ipthat = new interval("pthat"s, 200, 0, 200);
-    auto igen = new interval("Gen Pt", 20, 0, 100);
+    auto ieta = new interval("Eta", 20, -5, 5);
 
-    auto fhf = std::bind(&interval::book<TH1F>, ihf, _1, _2, _3);
+    auto fpt = std::bind(&interval::book<TH1F>, ipt, _1, _2, _3);
+    auto fenergy = std::bind(&interval::book<TH1F>, ienergy, _1, _2, _3);
     auto fpthat = std::bind(&interval::book<TH1F>, ipthat, _1, _2, _3);
     auto fnpu = [&](int64_t, std::string const& name, std::string const& label) {
-        return new TProfile(name.data(), (";Ncoll;HF Energy;"s + label).data(), 18, 0, 18, 0, max_hf, "LE"); };
-    auto fgen = std::bind(&interval::book<TH1F>, igen, _1, _2, _3);
+        return new TProfile(name.data(), (";Ncoll;HF Energy;"s + label).data(), 18, 0, 18, 0, 70000, "LE"); };
+    auto feta = std::bind(&interval::book<TH1F>, ieta, _1, _2, _3);
 
-    auto h_hf_pf_selected = new history<TH1F>("h_hf_pf_selected"s, "", fhf, ipt->size());
-    auto h_npu_hf_pf_selected = new history<TProfile>("h_npu_hf_pf_selected"s, "", fnpu, 1);
+    auto h_hf_pf_energy_selected = new history<TH1F>("h_hf_pf_energy_selected"s, "", fenergy, iphoton->size());
+    auto h_hf_pf_pt_selected = new history<TH1F>("h_hf_pf_pt_selected"s, "", fpt, iphoton->size());
+    auto h_hf_pf_eta_selected = new history<TH1F>("h_hf_pf_eta_selected"s, "", feta, 1);
+    auto h_npu_hf_pf_energy_selected = new history<TProfile>("h_npu_hf_pf_energy_selected"s, "", fnpu, 1);
     auto h_pthat_selected = new history<TH1F>("h_pthat_selected"s, "", fpthat, 1);
-    auto h_hf_gen_selected = new history<TH1F>("h_hf_gen_selected"s, "", fgen, 1);
-    auto h_hf_gen_selected_subid0 = new history<TH1F>("h_hf_gen_selected_subid0"s, "", fgen, 1);
+    auto h_hf_gen_energy_selected = new history<TH1F>("h_hf_gen_energy_selected"s, "", fenergy, 1);
+    auto h_hf_gen_pt_selected = new history<TH1F>("h_hf_gen_pt_selected"s, "", fpt, 1);
+    auto h_hf_gen_eta_selected = new history<TH1F>("h_hf_gen_eta_selected"s, "", feta, 1);
 
     /* manage memory manually */
     TH1::AddDirectory(false);
@@ -147,93 +149,71 @@ int Compare(char const* config, char const* output) {
 
         if (std::abs(*vz) > 15) { continue; }
 
-        float gen_sum = 0;
-        float gen_sum_subid0 = 0;
+        float gen_energy_sum = 0;
+        float gen_energy_sum_subid0 = 0;
+        float gen_pt_sum = 0;
+        float gen_pt_sum_subid0 = 0;
 
-        for (size_t j = 0; j < pt->size(); ++j) {
-            if (std::abs((*eta)[j]) > 3 && std::abs((*eta)[j]) < 5  && (*pt)[j] > 0.4) {
-                gen_sum += (*pt)[j];
-
-                if ((*sube)[j] == 0) {
-                    gen_sum_subid0 += (*pt)[j];
+        if (*ncoll == 0) { 
+            for (size_t j = 0; j < pt->size(); ++j) {
+                if (std::abs((*eta)[j]) > 3 && std::abs((*eta)[j]) < 5  && (*pt)[j] > 0.4) {
+                    gen_energy_sum += (*pt)[j] * std::cosh((*eta)[j]);
+                    gen_pt_sum += (*pt)[j];
                 }
-            }
         }
 
         auto pt_x = ipt->index_for(200);
 
-        float pf_sum = 0;
+        float pf_energy_sum = 0;
+        float pf_pt_sum = 0;
 
         for (size_t j = 0; j < pfPt->size(); ++j) {
+            (*h_hf_pf_eta_selected)[0]->Fill((*pfEta)[j], *weight);
+
             if ((*pfId)[j] >= 6) {
-                pf_sum += (*pfPt)[j];
+                pf_energy_sum += (*pfEnergy)[j];
+                pf_pt_sum += (*pfPt)[j];
             }
         }
 
         if (*ncoll == 0) { 
-            (*h_hf_pf_selected)[pt_x]->Fill(pf_sum, *weight);
+            (*h_hf_pf_energy_selected)[pt_x]->Fill(pf_energy_sum, *weight);
+            (*h_hf_pf_pt_selected)[pt_x]->Fill(pf_pt_sum, *weight);
             (*h_pthat_selected)[0]->Fill(*pthat, *weight);
-            (*h_hf_gen_selected)[0]->Fill(gen_sum, *weight);
-            (*h_hf_gen_selected_subid0)[0]->Fill(gen_sum_subid0, *weight);
+            (*h_hf_gen_energy_selected)[0]->Fill(gen_energy_sum, *weight);
+            (*h_hf_gen_pt_selected)[0]->Fill(gen_pt_sum, *weight);
+            for (size_t j = 0; j < pt->size(); ++j) {
+                if ((*pt)[j] > 0.4) {
+                    (*h_hf_gen_eta_selected)[0]->Fill((*eta)[j], *weight);
+                }
+            }
         }
 
-        (*h_npu_hf_pf_selected)[0]->Fill(*ncoll, pf_sum, *weight);
+        (*h_npu_hf_pf_energy_selected)[0]->Fill(*ncoll, pf_energy_sum, *weight);
     }
 
     /* save histograms */
     in(output, [&]() {
-        h_hf_pf_selected->save(tag);
-        h_npu_hf_pf_selected->save(tag);
+        h_hf_pf_energy_selected->save(tag);
+        h_hf_pf_pt_selected->save(tag);
+        h_hf_pf_eta_selected->save(tag);
+        h_npu_hf_pf_energy_selected->save(tag);
+        h_pthat->save(tag);
         h_pthat_selected->save(tag);
-        h_hf_gen_selected->save(tag);
-        h_hf_gen_selected_subid0->save(tag);
+        h_hf_gen_energy_selected->save(tag);
+        h_hf_gen_pt_selected->save(tag);
+        h_hf_gen_eta_selected->save(tag);
     });
 
     /* plot histograms */
     auto pt_info = [&](int64_t index) {
         info_text(index, 0.75, "%.0f < p_{T}^{#gamma} < %.0f", dpt, false); };
 
-    auto mean_info_selected_pthat = [&](int64_t index) {
+    auto mean_info = [&](history<TH1F>* h, int64_t index) {
         char buffer[128] = { '\0' };
         sprintf(buffer, "mean: %.3f +- %.3f",
-            (*h_pthat_selected)[index - 1]->GetMean(1),
-            (*h_pthat_selected)[index - 1]->GetMeanError(1));
-
-        TLatex* text = new TLatex();
-        text->SetTextFont(43);
-        text->SetTextSize(12);
-        text->DrawLatexNDC(0.54, 0.75, buffer);
-    };
-
-    auto mean_info_pu = [&](int64_t index) {
-        char buffer[128] = { '\0' };
-        sprintf(buffer, "mean: %.3f +- %.3f",
-            (*h_hf_pf_selected)[index - 1]->GetMean(1),
-            (*h_hf_pf_selected)[index - 1]->GetMeanError(1));
-
-        TLatex* text = new TLatex();
-        text->SetTextFont(43);
-        text->SetTextSize(12);
-        text->DrawLatexNDC(0.54, 0.75, buffer);
-    };
-
-    auto mean_info_gen_selected = [&](int64_t index) {
-        char buffer[128] = { '\0' };
-        sprintf(buffer, "mean: %.3f +- %.3f",
-            (*h_hf_gen_selected)[index - 1]->GetMean(1),
-            (*h_hf_gen_selected)[index - 1]->GetMeanError(1));
-
-        TLatex* text = new TLatex();
-        text->SetTextFont(43);
-        text->SetTextSize(12);
-        text->DrawLatexNDC(0.54, 0.75, buffer);
-    };
-
-    auto mean_info_gen_selected_subid0 = [&](int64_t index) {
-        char buffer[128] = { '\0' };
-        sprintf(buffer, "mean: %.3f +- %.3f",
-            (*h_hf_gen_selected_subid0)[index - 1]->GetMean(1),
-            (*h_hf_gen_selected_subid0)[index - 1]->GetMeanError(1));
+            (*h)[index - 1]->GetMean(1),
+            (*h)[index - 1]->GetMeanError(1));
 
         TLatex* text = new TLatex();
         text->SetTextFont(43);
@@ -245,8 +225,8 @@ int Compare(char const* config, char const* output) {
         float avg_gain = 0;
 
         for (int i = 1; i < 6; ++i) {
-            avg_gain += (*h_npu_hf_pf_selected)[index - 1]->GetBinContent(i+1) 
-                - (*h_npu_hf_pf_selected)[index - 1]->GetBinContent(i);
+            avg_gain += (*h_npu_hf_pf_energy_selected)[index - 1]->GetBinContent(i+1) 
+                - (*h_npu_hf_pf_energy_selected)[index - 1]->GetBinContent(i);
         }
 
         avg_gain /= 5;
@@ -264,44 +244,70 @@ int Compare(char const* config, char const* output) {
     auto hb = new pencil();
     hb->category("type", "PbPb MC", "PP MC");
 
-    auto c2 = new paper(tag + "_" + pthat_tag + "_selected_pf_hf_sum", hb);
+    auto c2 = new paper(tag + "_" + pthat_tag + "_selected_pthat", hb);
     apply_style(c2, "", "#sqrt{s} = 5.02 TeV"s);
-    c2->accessory(pt_info);
-    c2->accessory(mean_info_pu);
-    c2->divide(ipt->size(), -1);
-    for (int64_t j = 0; j < ipt->size(); ++j) {
-        c2->add((*h_hf_pf_selected)[j], "PbPb MC");
-    }
+    c2->accessory(std::bind(mean_info, h_pthat_selected, _1));
+    c2->add((*h_pthat_selected)[0], "PbPb MC");
     hb->sketch();
     c2->draw("pdf");
 
-    auto c3 = new paper(tag + "_" + pthat_tag + "_selected_pthat", hb);
+    auto c3 = new paper(tag + "_" + pthat_tag + "_selected_pf_hf_energy", hb);
     apply_style(c3, "", "#sqrt{s} = 5.02 TeV"s);
-    c3->accessory(mean_info_selected_pthat);
-    c3->add((*h_pthat_selected)[0], "PbPb MC");
+    c3->accessory(pt_info);
+    c3->accessory(std::bind(mean_info, h_hf_pf_energy_selected, _1));
+    c3->divide(iphoton->size(), -1);
+    for (int64_t j = 0; j < iphoton->size(); ++j) {
+        c3->add((*h_hf_pf_energy_selected)[j], "PbPb MC");
+    }
     hb->sketch();
     c3->draw("pdf");
 
-    auto c4 = new paper(tag + "_" + pthat_tag + "_selected_pf_hf_vs_pu", hb);
+    auto c30 = new paper(tag + "_" + pthat_tag + "_selected_pf_eta", hb);
+    apply_style(c30, "", "#sqrt{s} = 5.02 TeV"s);
+    c30->accessory(pt_info);
+    c30->accessory(std::bind(mean_info, h_hf_pf_eta_selected, _1));
+    c30->add((*h_hf_pf_eta_selected)[0], "PbPb MC");
+    hb->sketch();
+    c30->draw("pdf");
+
+    auto c31 = new paper(tag + "_" + pthat_tag + "_selected_pf_hf_pt", hb);
+    apply_style(c31, "", "#sqrt{s} = 5.02 TeV"s);
+    c31->accessory(pt_info);
+    c31->accessory(std::bind(mean_info, h_hf_pf_pt_selected, _1));
+    c31->divide(iphoton->size(), -1);
+    for (int64_t j = 0; j < iphoton->size(); ++j) {
+        c31->add((*h_hf_pf_pt_selected)[j], "PbPb MC");
+    }
+    hb->sketch();
+    c31->draw("pdf");
+
+    auto c4 = new paper(tag + "_" + pthat_tag + "_selected_pf_hf_energy_vs_pu", hb);
     apply_style(c4, "", "#sqrt{s} = 5.02 TeV"s);
     c4->accessory(avg_incremental_gain);
-    c4->add((*h_npu_hf_pf_selected)[0], "PbPb MC");
+    c4->add((*h_npu_hf_pf_energy_selected)[0], "PbPb MC");
     hb->sketch();
     c4->draw("pdf");
 
-    auto c5 = new paper(tag + "_" + pthat_tag + "_selected_gen_hf_sum", hb);
+    auto c5 = new paper(tag + "_" + pthat_tag + "_selected_gen_hf_energy", hb);
     apply_style(c5, "", "#sqrt{s} = 5.02 TeV"s);
-    c5->accessory(mean_info_gen_selected);
-    c5->add((*h_hf_gen_selected)[0], "PbPb MC");
+    c5->accessory(std::bind(mean_info, h_hf_gen_energy_selected, _1));
+    c5->add((*h_hf_gen_energy_selected)[0], "PbPb MC");
     hb->sketch();
     c5->draw("pdf");
 
-    auto c6 = new paper(tag + "_" + pthat_tag + "_selected_gen_hf_sum_subid0", hb);
-    apply_style(c6, "", "#sqrt{s} = 5.02 TeV"s);
-    c6->accessory(mean_info_gen_selected_subid0);
-    c6->add((*h_hf_gen_selected_subid0)[0], "PbPb MC");
+    auto c50 = new paper(tag + "_" + pthat_tag + "_selected_gen_eta", hb);
+    apply_style(c50, "", "#sqrt{s} = 5.02 TeV"s);
+    c50->accessory(std::bind(mean_info, h_hf_gen_eta_selected, _1));
+    c50->add((*h_hf_gen_eta_selected)[0], "PbPb MC");
     hb->sketch();
-    c6->draw("pdf");
+    c50->draw("pdf");
+
+    auto c51 = new paper(tag + "_" + pthat_tag + "_selected_gen_hf_pt", hb);
+    apply_style(c51, "", "#sqrt{s} = 5.02 TeV"s);
+    c51->accessory(std::bind(mean_info, h_hf_gen_pt_selected, _1));
+    c51->add((*h_hf_gen_pt_selected)[0], "PbPb MC");
+    hb->sketch();
+    c51->draw("pdf");
 
     printf("destroying objects..\n");
 
