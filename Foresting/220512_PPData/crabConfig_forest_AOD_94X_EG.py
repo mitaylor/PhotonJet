58fbd40b7f8b686216d95ed1b733f69fa8c310ea
mitaylor <mitay@mit.edu>
2023-02-04 01:50:34 -0500

    Foresting: redo on CERN

--- Foresting/220512_PPData/crabConfig_forest_AOD_94X_EG.py
+++ Foresting/220512_PPData/crabConfig_forest_AOD_94X_EG.py
@@ -23,7 +23,6 @@ config.Data.unitsPerJob = 5
 config.Data.publication = False
 config.Data.outputDatasetTag = Tag
 config.Data.outLFNDirBase = OutputBase + DatasetName
-config.Data.lumiMask = '/afs/cern.ch/cms/CAF/CMSCOMM/COMM_DQM/certification/Collisions17/5TeV/ReReco/Cert_306546-306826_5TeV_EOY2017ReReco_Collisions17_JSON.txt'
 
 config.section_("Site")
 # config.Site.storageSite = 'T2_US_MIT'
