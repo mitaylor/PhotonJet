34beefaab8a4a309269ac939dcdc01edf6e6aa00
mitaylor <mitay@mit.edu>
2023-02-11 18:22:56 -0500

    condor: try individual jobs and copying inputs

--- MainAnalysis/condor/RunRegulate.sh
+++ MainAnalysis/condor/RunRegulate.sh
@@ -6,20 +6,20 @@ input=$3
 
 WorkDir=`pwd`
 
-# export X509_USER_PROXY=${WorkDir}/x509up_u168456
+export X509_USER_PROXY=${WorkDir}/x509up_u168456
 
-# source /cvmfs/cms.cern.ch/cmsset_default.sh
-# export SCRAM_ARCH=slc7_amd64_gcc700
+source /cvmfs/cms.cern.ch/cmsset_default.sh
+export SCRAM_ARCH=slc7_amd64_gcc700
 
-# if [ -r CMSSW_10_3_3_patch1/src ] ; then
-#  echo release CMSSW_10_3_3_patch1 already exists
-# else
-# scram p CMSSW CMSSW_10_3_3_patch1
-# fi
-# cd CMSSW_10_3_3_patch1/src
-# eval `scramv1 runtime -sh`
+if [ -r CMSSW_10_3_3_patch1/src ] ; then
+ echo release CMSSW_10_3_3_patch1 already exists
+else
+scram p CMSSW CMSSW_10_3_3_patch1
+fi
+cd CMSSW_10_3_3_patch1/src
+eval `scramv1 runtime -sh`
 
-# cd $WorkDir
+cd $WorkDir
 
 echo ${input} > ${output}
 
