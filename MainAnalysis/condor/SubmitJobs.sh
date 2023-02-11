30f4dfd690de471b2146c46ac8591504713785ec
mitaylor <mitay@mit.edu>
2023-02-11 16:57:21 -0500

    condor: try individual jobs and copying inputs

--- MainAnalysis/condor/SubmitJobs.sh
+++ MainAnalysis/condor/SubmitJobs.sh
@@ -24,7 +24,7 @@ sed -i 's/^.*\/store/root:\/\/xrootd.cmsaf.mit.edu\/\/store/' ${output_tag}
 
 # files=$(cat ${output_tag})
 
-awk -F OFS=", " "[_.]" '{print $(NF-1), $0}' ${output_tag} > ${output_tag}.list
+awk -F "[_.]" -v OFS=", " '{print $(NF-1), $0}' ${output_tag} > ${output_tag}.list
 
 # # create all of the config files
 # for file in ${files}; do
