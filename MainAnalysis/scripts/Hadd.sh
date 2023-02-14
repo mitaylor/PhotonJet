150bde5f5d076baea42287935ea7614984efe65d
mitaylor <mitay@mit.edu>
2023-02-14 02:40:56 -0500

    scripts: try making script to do hadd

--- MainAnalysis/scripts/Hadd.sh
+++ MainAnalysis/scripts/Hadd.sh
@@ -11,19 +11,19 @@ then
 
 	for ((i=0 ; i<$limit ; i++)); 
     do
-        index_1=`expr $i * 2`
-        index_2=`expr $i * 2 + 1`
+        index_1=`expr $i /* 2`
+        index_2=`expr $i /* 2 + 1`
         # hadd /data/submit/mitay/photons/${dataset}_$i.root ${files[index_1]} ${files[index_2]}
     done
 else
-    limit=`expr $n - 1 / 2`
+    limit=`expr $n / 2`
 
     echo $limit
 
 	for ((i=0 ; i<$limit ; i++)); 
     do
-        index_1=`expr $i * 2`
-        index_2=`expr $i * 2 + 1`
+        index_1=`expr $i /* 2`
+        index_2=`expr $i /* 2 + 1`
 
         echo "$index_1 $index_2"
         # hadd /data/submit/mitay/photons/${dataset}_$i.root ${files[index_1]} ${files[index_2]}
