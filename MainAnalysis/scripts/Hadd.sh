68c5d7777eba7d3f7ef0ffed4dd47e2c5a120ef5
mitaylor <mitay@mit.edu>
2023-02-14 02:46:41 -0500

    scripts: try making script to do hadd

--- MainAnalysis/scripts/Hadd.sh
+++ MainAnalysis/scripts/Hadd.sh
@@ -3,21 +3,24 @@
 dataset=regulate_aa_qcd_extra_80_F
 
 files=$(ls /data/submit/mitay/photons/condor/${dataset}*)
-n=$(ls /data/submit/mitay/photons/condor/${dataset}* | wc -l)
+
+hadd /data/submit/mitay/photons/${dataset}.root $files
+
+# n=$(ls /data/submit/mitay/photons/condor/${dataset}* | wc -l)
 
 
-limit=`expr $n / 2`
+# limit=`expr $n / 2`
 
-for ((i=0 ; i<$limit ; i++)); 
-do
-    index_1=`expr $i \* 2`
-    index_2=`expr $i \* 2 + 1`
-    echo "${files[index_1]} ${files[index_2]}"
-    echo hadd /data/submit/mitay/photons/${dataset}_$i.root ${files[index_1]} ${files[index_2]}
-    hadd /data/submit/mitay/photons/${dataset}_$i.root ${files[index_1]} ${files[index_2]}
-done
+# for ((i=0 ; i<$limit ; i++)); 
+# do
+#     index_1=`expr $i \* 2`
+#     index_2=`expr $i \* 2 + 1`
+#     echo "${files[index_1]} ${files[index_2]}"
+#     echo hadd /data/submit/mitay/photons/${dataset}_$i.root ${files[index_1]} ${files[index_2]}
+#     hadd /data/submit/mitay/photons/${dataset}_$i.root ${files[index_1]} ${files[index_2]}
+# done
 
-if [ `expr $n % 2` == 0 ]
-    index=`expr $n - 1`
-    cp ${files[index]} /data/submit/mitay/photons/${dataset}_$limit.root
-fi
+# if [ `expr $n % 2` == 0 ]
+#     index=`expr $n - 1`
+#     cp ${files[index]} /data/submit/mitay/photons/${dataset}_$limit.root
+# fi
