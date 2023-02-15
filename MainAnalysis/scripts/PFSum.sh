f926aa11e4068335a806b5a1e68914f7c82adfd0
mitaylor <mitay@mit.edu>
2023-02-15 09:47:29 -0500

    pf_sum: create script to do MB Data faster

--- MainAnalysis/scripts/PFSum.sh
+++ MainAnalysis/scripts/PFSum.sh
@@ -1,17 +1,19 @@
 #!/usr/bin/env bash
 
-# ls /data/submit/mitay/photons/condor/regulate_aa_mb_data_???.root > files.txt
-# awk -F "[_.]" '{print $(NF-1)}' files.txt > index.txt
+n=$1
 
-# files=($(cat files.txt))
-# index=($(cat index.txt))
+ls /data/submit/mitay/photons/condor/regulate_aa_mb_data_${n}??.root > files.txt
+awk -F "[_.]" '{print $(NF-1)}' files.txt > index.txt
 
-# for i in ${!files[@]}; do
-#     ../bin/pf_sum ${files[i]} /data/submit/mitay/photons/condor/regulate_aa_mb_data_sum_${index[i]}.root
-# done
+files=($(cat files.txt))
+index=($(cat index.txt))
 
-# rm files.txt
-# rm index.txt
+rm files.txt
+rm index.txt
+
+for i in ${!files[@]}; do
+    ../bin/pf_sum ${files[i]} /data/submit/mitay/photons/condor/regulate_aa_mb_data_sum_${index[i]}.root
+done
 
 # ls /data/submit/mitay/photons/condor/regulate_aa_mb_hydjet_??.root > files1.txt
 
