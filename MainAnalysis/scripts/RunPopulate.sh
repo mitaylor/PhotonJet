bb19d08ed31dc5ffc3eb3f35bd69c52523f88297
mitaylor <mitay@mit.edu>
2023-02-15 19:37:59 -0500

    populate: add parallelization

---
+++ MainAnalysis/scripts/RunPopulate.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+tag=$1
+n=$2
+
+ls /data/submit/mitay/photons/condor/regulate_aa_data_* > files
+split -n l/7 --numeric-suffixes=0 -a 2 files files_
+awk -F "[_.]" '{print $(NF-1)}' files_${n} > index_${n}
+
+files=($(cat files_${n}))
+index=($(cat index_${n}))
+
+rm files*
+rm index*
+
+for i in ${!files[@]}; do
+    cat ../configs/populate/preapproval-parallel/${tag}_template.conf  | sed "s/__INPUT__/${files[i]}/g" > nohup/${tag}_${index[i]}.conf
+    ../bin/populate_test nohup/${tag}_${index[i]}.conf /data/submit/mitay/populate/${tag}_${index[i]}.root
+done
