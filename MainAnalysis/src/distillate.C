68777c614e8c673b7a931db333f990e8a5cfc8ad
mitaylor <mitay@mit.edu>
2023-02-16 01:04:50 -0500

    distillate: try to fix plotting issue

--- MainAnalysis/src/distillate.C
+++ MainAnalysis/src/distillate.C
@@ -187,7 +187,7 @@ int distillate(char const* config, char const* output) {
         l->SetTextFont(43);
         l->SetTextSize(13);
         l->DrawLatexNDC(0.135, pos, buffer);
-        sprintf(buffer, "%" PRId64 "\n", x);
+        x++;
     };
 
     auto pthf_info = [&](int64_t index) {
