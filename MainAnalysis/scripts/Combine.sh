2b41dc25b44b7211fc1f718def6e56b334843450
mitaylor <mitay@mit.edu>
2023-02-17 08:16:22 -0500

    populate: fix normalization

--- MainAnalysis/scripts/Combine.sh
+++ MainAnalysis/scripts/Combine.sh
@@ -4,11 +4,11 @@ histogram() {
     tag=$1
 
     echo "./bin/combine_populate configs/combine_populate/combine_${tag}.conf data/preapproval/populate_${tag}.root > log/combine_populate_${tag}_log.txt"
-    
+
     ./bin/combine_populate configs/combine_populate/combine_${tag}.conf data/preapproval/populate_${tag}.root > log/combine_populate_${tag}_log.txt
 }
 
-samples=(pp_raw pp_bkg pp_qcd_raw, pp_qcd_bkg, pp_qcd_gen_iso_raw, pp_qcd_gen_iso_bkg, pp_wo_ele_rej_raw, pp_wo_ele_rej_bkg)
+samples=(pp_raw pp_bkg pp_qcd_raw pp_qcd_bkg pp_qcd_gen_iso_raw pp_qcd_gen_iso_bkg pp_wo_ele_rej_raw pp_wo_ele_rej_bkg)
 
 for sample in ${samples[@]}; do
     histogram ${sample}
