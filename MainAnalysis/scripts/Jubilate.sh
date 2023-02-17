643b88c7a3a1d6fab09593ec03ca7f3edecfc7b9
mitaylor <mitay@mit.edu>
2023-02-17 07:22:43 -0500

    jubilate: update

--- MainAnalysis/scripts/Jubilate.sh
+++ MainAnalysis/scripts/Jubilate.sh
@@ -6,7 +6,7 @@ histogram() {
     ./bin/jubilate configs/jubilate/preapproval/jubilate_${tag}_raw.conf data/preapproval/jubilate_${tag}_raw.root > log/jubilate_${tag}_raw_log.txt
 }
 
-samples=(pp pp_qcd aa aa_qcd) # add aa_qcd_original at some point
+samples=(pp pp_qcd aa aa_qcd aa_original) # add aa_qcd_original at some point
 
 for sample in ${samples[@]}; do
     histogram ${sample}
