c0dbc5dfa923c378f565dc78af0ca520c6da53dc
mitaylor <mitay@mit.edu>
2023-02-18 15:20:24 -0500

    jubilate: update to plot jet pt as well

--- MainAnalysis/src/jubilate.C
+++ MainAnalysis/src/jubilate.C
@@ -40,6 +40,7 @@ int jubilate(char const* config, char const* output) {
 
     auto rdphi = conf->get<std::vector<float>>("dphi_range");
     auto rdr = conf->get<std::vector<float>>("dr_range");
+    auto rjpt = conf->get<std::vector<float>>("jpt_range");
 
     auto dpt = conf->get<std::vector<float>>("pt_diff");
     auto dhf = conf->get<std::vector<float>>("hf_diff");
@@ -64,9 +65,11 @@ int jubilate(char const* config, char const* output) {
     history<TH1F>* pjet_es_f_dphi;
     history<TH1F>* pjet_wta_f_dphi;
     history<TH1F>* pjet_f_dr;
+    history<TH1F>* pjet_f_jpt;
     history<TH1F>* mix_pjet_es_f_dphi;
     history<TH1F>* mix_pjet_wta_f_dphi;
     history<TH1F>* mix_pjet_f_dr;
+    history<TH1F>* mix_pjet_f_jpt;
 
     if (!background) {
         nevt = new history<TH1F>(f, "raw_nevt");
@@ -74,20 +77,24 @@ int jubilate(char const* config, char const* output) {
         pjet_es_f_dphi = new history<TH1F>(f, "raw_pjet_es_f_dphi");
         pjet_wta_f_dphi = new history<TH1F>(f, "raw_pjet_wta_f_dphi");
         pjet_f_dr = new history<TH1F>(f, "raw_pjet_f_dr");
+        pjet_f_jpt = new history<TH1F>(f, "raw_pjet_f_jpt");
 
         mix_pjet_es_f_dphi = new history<TH1F>(f, "raw_mix_pjet_es_f_dphi");
         mix_pjet_wta_f_dphi = new history<TH1F>(f, "raw_mix_pjet_wta_f_dphi");
         mix_pjet_f_dr = new history<TH1F>(f, "raw_mix_pjet_f_dr");
+        mix_pjet_f_jpt = new history<TH1F>(f, "raw_mix_pjet_f_jpt");
     } else {
         nevt = new history<TH1F>(f, "bkg_nevt");
 
         pjet_es_f_dphi = new history<TH1F>(f, "bkg_pjet_es_f_dphi");
         pjet_wta_f_dphi = new history<TH1F>(f, "bkg_pjet_wta_f_dphi");
         pjet_f_dr = new history<TH1F>(f, "bkg_pjet_f_dr");
+        pjet_f_jpt = new history<TH1F>(f, "bkg_pjet_f_jpt");
 
         mix_pjet_es_f_dphi = new history<TH1F>(f, "bkg_mix_pjet_es_f_dphi");
         mix_pjet_wta_f_dphi = new history<TH1F>(f, "bkg_mix_pjet_wta_f_dphi");
         mix_pjet_f_dr = new history<TH1F>(f, "bkg_mix_pjet_f_dr");
+        mix_pjet_f_jpt = new history<TH1F>(f, "bkg_mix_pjet_f_jpt");
     }
 
     /* shrink to remove overflow photon pt bin */
@@ -101,9 +108,11 @@ int jubilate(char const* config, char const* output) {
     wrap(pjet_es_f_dphi);
     wrap(pjet_wta_f_dphi);
     wrap(pjet_f_dr);
+    wrap(pjet_f_jpt);
     wrap(mix_pjet_es_f_dphi);
     wrap(mix_pjet_wta_f_dphi);
     wrap(mix_pjet_f_dr);
+    wrap(mix_pjet_f_jpt);
 
 // /* scale by bin width */
 // scale_bin_width(
@@ -158,6 +167,12 @@ int jubilate(char const* config, char const* output) {
     c3->accessory(pthf_info);
     c3->divide(-1 , ihf->size());
 
+    auto c4 = new paper(tag + "_mixing_jpt_d_pthf", hb);
+    apply_style(c4, cms, system_tag, -1., 24.);
+    c4->accessory(std::bind(line_at, _1, 0.f, rjpt[0], rpjt[1]));
+    c4->accessory(pthf_info);
+    c4->divide(-1 , ihf->size());
+
     for (int64_t i = 0; i < nevt->size(); ++i) {
         c1->add((*pjet_es_f_dphi)[i], system, "raw");
         c1->stack((*mix_pjet_es_f_dphi)[i], system, "mix");
@@ -167,6 +182,9 @@ int jubilate(char const* config, char const* output) {
 
         c3->add((*pjet_f_dr)[i], system, "raw");
         c3->stack((*mix_pjet_f_dr)[i], system, "mix");
+
+        c4->add((*pjet_f_jpt)[i], system, "raw");
+        c4->stack((*mix_pjet_f_jpt)[i], system, "mix");
     }
 
     hb->sketch();
