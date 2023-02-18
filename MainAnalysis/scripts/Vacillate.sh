cb0538a36b320609df10505160ad7d03327e5a1b
mitaylor <mitay@mit.edu>
2023-02-18 12:13:18 -0500

    vacillate: update script

--- MainAnalysis/scripts/Vacillate.sh
+++ MainAnalysis/scripts/Vacillate.sh
@@ -3,16 +3,16 @@
 histogram() {
     tag=$1
 
-    echo "nohup ./bin/vacillate_dr configs/vacillate_dr/new/jet_20_fine/vacillate_${tag}.conf data/jet_20_fine/vacillate_${tag}.root > log/vacillate_${tag}_jet_20_fine_log.txt 2>&1 &"
+    echo "nohup ./bin/vacillate_dr configs/vacillate_dr/preapproval/vacillate_${tag}.conf data/preapproval/vacillate_${tag}.root > log/vacillate_${tag}_log.txt 2>&1 &"
 }
 
 # samples=(pp_smear_0_10 pp_smear_10_30 pp_smear_30_50 pp_smear_50_90 pp aa)
-samples=(pp aa pp_smear_0_10 pp_smear_10_30 pp_smear_30_50 pp_smear_50_90)
+samples=(pp)
 
 for sample in ${samples[@]}; do
     histogram ${sample}
 
-    for syst in qcd; do
+    for syst in qcd back front qcd_gen_iso jer_up jeu_up jeu_down wo_ele_rej; do
         histogram ${sample}_${syst}
     done
 done
