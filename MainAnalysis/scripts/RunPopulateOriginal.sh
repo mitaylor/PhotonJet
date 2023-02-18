1d6eeac76bb57b28548a64e1cf3db75b58f085ee
mitaylor <mitay@mit.edu>
2023-02-18 17:13:34 -0500

    populate: run comparisons qcd

--- MainAnalysis/scripts/RunPopulateOriginal.sh
+++ MainAnalysis/scripts/RunPopulateOriginal.sh
@@ -3,7 +3,8 @@
 tag=$1
 n=$2
 
-ls /data/submit/mitay/photons/condor/regulate_aa_data_* > files
+# ls /data/submit/mitay/photons/condor/regulate_aa_data_* > files
+ls /data/submit/mitay/photons/regulate_aa_qcd_weighted_* > files
 split -n l/7 --numeric-suffixes=0 -a 1 files files_
 awk -F "[_.]" '{print $(NF-1)}' files_${n} > index_${n}
 
