8be27bcc601eb26beb7fb47170fc14c6b3c5750a
mitaylor <mitay@mit.edu>
2023-02-18 22:44:16 -0500

    fabulate: calculate pp smear

--- MainAnalysis/scripts/Fabulate.sh
+++ MainAnalysis/scripts/Fabulate.sh
@@ -7,7 +7,7 @@ histogram() {
     ./bin/fabulate_angle configs/fabulate/preapproval/fabulate_${tag}_angle.conf data/preapproval/fabulate_${tag}_angle.root
 }
 
-samples=(aa pp pp_smear_0_10 pp_smear_10_30 pp_smear_30_50 pp_smear_50_90)
+samples=(pp_smear_0_10 pp_smear_10_30 pp_smear_30_50 pp_smear_50_90)
 
 for sample in ${samples[@]}; do
     histogram ${sample}
