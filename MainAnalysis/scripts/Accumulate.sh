41e9e10823f26c9c75d08bd51e9d9f7e5a55fc16
mitaylor <mitay@mit.edu>
2023-02-20 05:10:33 -0500

    scripts: update for new run through

--- MainAnalysis/scripts/Accumulate.sh
+++ MainAnalysis/scripts/Accumulate.sh
@@ -20,26 +20,26 @@ systematic() {
     arithmetic ${sample}_${syst}
 }
 
-# samples=(pp aa pp_smear_0_10 pp_smear_10_30 pp_smear_30_50 pp_smear_50_90)
-
-# for sample in ${samples[@]}; do
-#     nominal $sample
-
-#     for syst in qcd qcd_gen_iso wo_ele_rej; do
-#         systematic $sample $syst
-#     done
-# done
-
-# samples=(aa)
-
-# for sample in ${samples[@]}; do
-#     for syst in cent_up cent_down; do
-#         systematic $sample $syst
-#     done
-# done
-
-samples=(aa_mebs aa_qcd_mebs)
+samples=(pp aa pp_smear_0_10 pp_smear_10_30 pp_smear_30_50 pp_smear_50_90)
 
 for sample in ${samples[@]}; do
     nominal $sample
+
+    for syst in qcd qcd_gen_iso wo_ele_rej; do
+        systematic $sample $syst
+    done
 done
+
+samples=(aa)
+
+for sample in ${samples[@]}; do
+    for syst in cent_up cent_down; do
+        systematic $sample $syst
+    done
+done
+
+# samples=(aa_mebs aa_qcd_mebs)
+
+# for sample in ${samples[@]}; do
+#     nominal $sample
+# done
