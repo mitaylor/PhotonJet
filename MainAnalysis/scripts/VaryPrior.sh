a361e88587624b69fa33e840389476c89db1a58d
mitaylor <mitay@mit.edu>
2023-02-20 12:20:17 -0500

    vary_prior: run now that unfolding is done

--- MainAnalysis/scripts/VaryPrior.sh
+++ MainAnalysis/scripts/VaryPrior.sh
@@ -4,7 +4,7 @@ samples=(pp aa pp_smear_0_10 pp_smear_10_30 pp_smear_30_50 pp_smear_50_90)
 
 for sample in ${samples[@]}; do
     ./bin/vary_prior configs/vary_prior/vary_prior_${sample}_up.conf \
-        data/jet_20/vacillate_${sample}_prior_up.root
+        data/preapproval/vacillate_${sample}_prior_up.root
     ./bin/vary_prior configs/vary_prior/vary_prior_${sample}_down.conf \
-        data/jet_20/vacillate_${sample}_prior_down.root
+        data/preapproval/vacillate_${sample}_prior_down.root
 done
