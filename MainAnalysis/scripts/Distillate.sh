81e8fc123e6f8aa0d361aafcf618b0f91a67685c
mitaylor <mitay@mit.edu>
2023-02-20 00:02:31 -0500

    distillate: update pp smear

--- MainAnalysis/scripts/Distillate.sh
+++ MainAnalysis/scripts/Distillate.sh
@@ -7,7 +7,7 @@ histogram() {
     ./bin/distillate_angle configs/distillate/preapproval/distillate_${tag}_angle_dj.conf data/preapproval/distillate_${tag}_angle_dj.root
 }
 
-samples=(pp pp_smear_0_10 pp_smear_10_30 pp_smear_30_50 pp_smear_50_90)
+samples=(pp_smear_0_10 pp_smear_10_30 pp_smear_30_50 pp_smear_50_90)
 
 for sample in ${samples[@]}; do
     histogram ${sample}
