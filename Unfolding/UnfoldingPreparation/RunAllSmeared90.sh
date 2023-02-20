03a3421c432c07ba1c2d541891cbb32702c12187
mitaylor <mitay@mit.edu>
2023-02-20 05:37:22 -0500

    Unfolding: update with new inputs

--- Unfolding/UnfoldingPreparation/RunAllSmeared90.sh
+++ Unfolding/UnfoldingPreparation/RunAllSmeared90.sh
@@ -8,7 +8,7 @@
     --ResponseTruth pp_smear_50_90_g_0 \
     --ResponseReco pp_smear_50_90_r_0 \
     --PrimaryRecoBins 0,0.01,0.02,0.03,0.04,0.05,0.06,0.07,0.08,0.09,0.105,0.12,0.135,0.15,0.18,0.21,0.3 \
-    --BinningRecoBins 20,30,35,40,45,50,55,60,70,80,90,100,120,150,200,250 \
+    --BinningRecoBins 20,30,35,40,45,50,55,60,70,80,90,100,120,150,200 \
     --PrimaryGenBins 0,0.015,0.03,0.045,0.06,0.08,0.1,0.12,0.15,0.2,0.3 \
     --BinningGenBins 20,30,40,50,60,70,80,100,120,200 \
     --Output Output/PPSmear90Nominal0.root
@@ -21,7 +21,7 @@
     --ResponseTruth pp_smear_50_90_g_0 \
     --ResponseReco pp_smear_50_90_r_0 \
     --PrimaryRecoBins 0,0.01,0.02,0.03,0.04,0.05,0.06,0.07,0.08,0.09,0.105,0.12,0.135,0.15,0.18,0.21,0.3 \
-    --BinningRecoBins 20,30,35,40,45,50,55,60,70,80,90,100,120,150,200,250 \
+    --BinningRecoBins 20,30,35,40,45,50,55,60,70,80,90,100,120,150,200 \
     --PrimaryGenBins 0,0.015,0.03,0.045,0.06,0.08,0.1,0.12,0.15,0.2,0.3 \
     --BinningGenBins 20,30,40,50,60,70,80,100,120,200 \
     --Output Output/PPSmear90WithoutEleRej0.root
@@ -34,7 +34,7 @@
     --ResponseTruth pp_smear_50_90_g_0 \
     --ResponseReco pp_smear_50_90_r_0 \
     --PrimaryRecoBins 0,0.01,0.02,0.03,0.04,0.05,0.06,0.07,0.08,0.09,0.105,0.12,0.135,0.15,0.18,0.21,0.3 \
-    --BinningRecoBins 20,30,35,40,45,50,55,60,70,80,90,100,120,150,200,250 \
+    --BinningRecoBins 20,30,35,40,45,50,55,60,70,80,90,100,120,150,200 \
     --PrimaryGenBins 0,0.015,0.03,0.045,0.06,0.08,0.1,0.12,0.15,0.2,0.3 \
     --BinningGenBins 20,30,40,50,60,70,80,100,120,200 \
     --Output Output/PPSmear90TightPurity0.root
@@ -47,7 +47,7 @@
     --ResponseTruth pp_smear_50_90_g_0 \
     --ResponseReco pp_smear_50_90_r_0 \
     --PrimaryRecoBins 0,0.01,0.02,0.03,0.04,0.05,0.06,0.07,0.08,0.09,0.105,0.12,0.135,0.15,0.18,0.21,0.3 \
-    --BinningRecoBins 20,30,35,40,45,50,55,60,70,80,90,100,120,150,200,250 \
+    --BinningRecoBins 20,30,35,40,45,50,55,60,70,80,90,100,120,150,200 \
     --PrimaryGenBins 0,0.015,0.03,0.045,0.06,0.08,0.1,0.12,0.15,0.2,0.3 \
     --BinningGenBins 20,30,40,50,60,70,80,100,120,200 \
     --Output Output/PPSmear90LoosePurity0.root
@@ -60,7 +60,7 @@
     --ResponseTruth pp_smear_50_90_g_0 \
     --ResponseReco pp_smear_50_90_r_0 \
     --PrimaryRecoBins 0,0.01,0.02,0.03,0.04,0.05,0.06,0.07,0.08,0.09,0.105,0.12,0.135,0.15,0.18,0.21,0.3 \
-    --BinningRecoBins 20,30,35,40,45,50,55,60,70,80,90,100,120,150,200,250 \
+    --BinningRecoBins 20,30,35,40,45,50,55,60,70,80,90,100,120,150,200 \
     --PrimaryGenBins 0,0.015,0.03,0.045,0.06,0.08,0.1,0.12,0.15,0.2,0.3 \
     --BinningGenBins 20,30,40,50,60,70,80,100,120,200 \
     --Output Output/PPSmear90QCD0.root
@@ -73,7 +73,7 @@
     --ResponseTruth pp_smear_50_90_g_0 \
     --ResponseReco pp_smear_50_90_r_0 \
     --PrimaryRecoBins 0,0.01,0.02,0.03,0.04,0.05,0.06,0.07,0.08,0.09,0.105,0.12,0.135,0.15,0.18,0.21,0.3 \
-    --BinningRecoBins 20,30,35,40,45,50,55,60,70,80,90,100,120,150,200,250 \
+    --BinningRecoBins 20,30,35,40,45,50,55,60,70,80,90,100,120,150,200 \
     --PrimaryGenBins 0,0.015,0.03,0.045,0.06,0.08,0.1,0.12,0.15,0.2,0.3 \
     --BinningGenBins 20,30,40,50,60,70,80,100,120,200 \
     --Output Output/PPSmear90QCDGenIso0.root
@@ -86,7 +86,7 @@
     --ResponseTruth pp_smear_50_90_g_0 \
     --ResponseReco pp_smear_50_90_r_0 \
     --PrimaryRecoBins 0,0.01,0.02,0.03,0.04,0.05,0.06,0.07,0.08,0.09,0.105,0.12,0.135,0.15,0.18,0.21,0.3 \
-    --BinningRecoBins 20,30,35,40,45,50,55,60,70,80,90,100,120,150,200,250 \
+    --BinningRecoBins 20,30,35,40,45,50,55,60,70,80,90,100,120,150,200 \
     --PrimaryGenBins 0,0.015,0.03,0.045,0.06,0.08,0.1,0.12,0.15,0.2,0.3 \
     --BinningGenBins 20,30,40,50,60,70,80,100,120,200 \
     --Output Output/PPSmear90JEUUp0.root
@@ -99,7 +99,7 @@
     --ResponseTruth pp_smear_50_90_g_0 \
     --ResponseReco pp_smear_50_90_r_0 \
     --PrimaryRecoBins 0,0.01,0.02,0.03,0.04,0.05,0.06,0.07,0.08,0.09,0.105,0.12,0.135,0.15,0.18,0.21,0.3 \
-    --BinningRecoBins 20,30,35,40,45,50,55,60,70,80,90,100,120,150,200,250 \
+    --BinningRecoBins 20,30,35,40,45,50,55,60,70,80,90,100,120,150,200 \
     --PrimaryGenBins 0,0.015,0.03,0.045,0.06,0.08,0.1,0.12,0.15,0.2,0.3 \
     --BinningGenBins 20,30,40,50,60,70,80,100,120,200 \
     --Output Output/PPSmear90JEUDown0.root
@@ -112,7 +112,7 @@
     --ResponseTruth pp_smear_50_90_g_0 \
     --ResponseReco pp_smear_50_90_r_0 \
     --PrimaryRecoBins 0,0.01,0.02,0.03,0.04,0.05,0.06,0.07,0.08,0.09,0.105,0.12,0.135,0.15,0.18,0.21,0.3 \
-    --BinningRecoBins 20,30,35,40,45,50,55,60,70,80,90,100,120,150,200,250 \
+    --BinningRecoBins 20,30,35,40,45,50,55,60,70,80,90,100,120,150,200 \
     --PrimaryGenBins 0,0.015,0.03,0.045,0.06,0.08,0.1,0.12,0.15,0.2,0.3 \
     --BinningGenBins 20,30,40,50,60,70,80,100,120,200 \
     --Output Output/PPSmear90JERUp0.root
