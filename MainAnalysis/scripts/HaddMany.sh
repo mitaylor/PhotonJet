1d23f7537ddd037ae2d9f6d68cc7582a6b2842ef
mitaylor <mitay@mit.edu>
2023-02-20 04:04:34 -0500

    accumulate: update with plotting changes

--- MainAnalysis/scripts/HaddMany.sh
+++ MainAnalysis/scripts/HaddMany.sh
@@ -3,8 +3,8 @@
 dataset=$1
 n=$2
 
-# ls /data/submit/mitay/photons/condor/${dataset}* > ${dataset}
-# split -l ${n} --numeric-suffixes=0 -a 3 ${dataset} ${dataset}_
+ls /data/submit/mitay/photons/condor/${dataset}* > ${dataset}
+split -l ${n} --numeric-suffixes=0 -a 3 ${dataset} ${dataset}_
 
 files=($(ls ${dataset}_*))
 
@@ -14,4 +14,4 @@ for i in 1; do
     echo "hadd /data/submit/mitay/photons/${dataset}_$i.root $list"
 done
 
-rm ${dataset}*
+# rm ${dataset}*
