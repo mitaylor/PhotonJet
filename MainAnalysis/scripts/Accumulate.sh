69c235b14c7eff7ba68af5741e53b12f6f244424
mitaylor <mitay@mit.edu>
2023-02-20 05:39:36 -0500

    scripts: update

--- MainAnalysis/scripts/Accumulate.sh
+++ MainAnalysis/scripts/Accumulate.sh
@@ -25,7 +25,7 @@ samples=(pp aa pp_smear_0_10 pp_smear_10_30 pp_smear_30_50 pp_smear_50_90)
 for sample in ${samples[@]}; do
     nominal $sample
 
-    for syst in qcd qcd_gen_iso wo_ele_rej; do
+    for syst in qcd qcd_gen_iso wo_ele_rej tight_purity loose_purity; do
         systematic $sample $syst
     done
 done
