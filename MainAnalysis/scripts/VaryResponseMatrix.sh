d161a85e393687a9300a06eb023a4187f2e73706
mitaylor <mitay@mit.edu>
2023-02-20 05:06:47 -0500

    vary_response_matrix: run on new matrices

--- MainAnalysis/scripts/VaryResponseMatrix.sh
+++ MainAnalysis/scripts/VaryResponseMatrix.sh
@@ -3,6 +3,6 @@
 samples=(pp aa pp_smear_0_10 pp_smear_10_30 pp_smear_30_50 pp_smear_50_90)
 
 for sample in ${samples[@]}; do
-    ./bin/vary_response_matrix configs/preapproval/vary_response_matrix_${sample}.conf \
+    ./bin/vary_response_matrix configs/vary_response_matrix_${sample}.conf \
         data/preapproval/vacillate_${sample}_stat_response.root
 done
