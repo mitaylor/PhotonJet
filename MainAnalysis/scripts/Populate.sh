5ecc29b089251f3e54fedc1cf9268e01f06c4715
mitaylor <mitay@mit.edu>
2023-02-20 01:28:10 -0500

    populate, fabulate: update for pp smear data

--- MainAnalysis/scripts/Populate.sh
+++ MainAnalysis/scripts/Populate.sh
@@ -3,11 +3,11 @@
 histogram() {
     tag=$1
 
-    echo "nohup ./bin/populate configs/populate/new/jet_20_fine/populate_${tag}_raw.conf data/jet_20_fine/populate_${tag}_raw.root > log/populate_${tag}_raw_jet_20_fine_log.txt 2>&1 &"
-    echo "nohup ./bin/populate configs/populate/new/jet_20_fine/populate_${tag}_bkg.conf data/jet_20_fine/populate_${tag}_bkg.root > log/populate_${tag}_bkg_jet_20_fine_log.txt 2>&1 &"
+    echo "nohup ./bin/populate configs/populate/preapproval/populate_${tag}_raw.conf /data/submit/mitay/populate/populate_${tag}_raw.root > log/populate_${tag}_raw_log.txt 2>&1 &"
+    echo "nohup ./bin/populate configs/populate/preapproval/populate_${tag}_bkg.conf /data/submit/mitay/populate/populate_${tag}_bkg.root > log/populate_${tag}_bkg_log.txt 2>&1 &"
 }
 
-samples=(pp pp_smear_0_10 pp_smear_10_30 pp_smear_30_50 pp_smear_50_90 aa)
+samples=(pp_smear_0_10 pp_smear_10_30 pp_smear_30_50 pp_smear_50_90)
 # samples=(pp_smear_0_10 pp_smear_10_30 pp_smear_30_50 pp_smear_50_90)
 
 for sample in ${samples[@]}; do
@@ -18,10 +18,10 @@ for sample in ${samples[@]}; do
     done
 done
 
-samples=(aa)
+# samples=(aa)
 
-for sample in ${samples[@]}; do
-    for syst in cent_down cent_up; do
-        histogram ${sample}_${syst}
-    done
-done
+# for sample in ${samples[@]}; do
+#     for syst in cent_down cent_up; do
+#         histogram ${sample}_${syst}
+#     done
+# done
