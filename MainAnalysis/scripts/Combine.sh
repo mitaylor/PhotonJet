fcd4af8b88d5962e8205969c952994702ff72523
mitaylor <mitay@mit.edu>
2023-02-20 02:02:01 -0500

    combine_populate: bug fix

--- MainAnalysis/scripts/Combine.sh
+++ MainAnalysis/scripts/Combine.sh
@@ -14,38 +14,38 @@ histogram() {
 #     histogram ${sample}
 # done
 
-samples=(pp_smear_0_10_raw pp_smear_0_10_bkg pp_smear_0_10_qcd_raw pp_smear_0_10_qcd_bkg pp_smear_0_10_qcd_gen_iso_raw pp_smear_0_10_qcd_gen_iso_bkg pp_smear_0_10_wo_ele_rej_raw pp_smear_0_10_wo_ele_rej_bkg)
-
-for sample in ${samples[@]}; do
-    histogram ${sample}
-done
-
-samples=(pp_smear_10_30_raw pp_smear_10_30_bkg pp_smear_10_30_qcd_raw pp_smear_10_30_qcd_bkg pp_smear_10_30_qcd_gen_iso_raw pp_smear_10_30_qcd_gen_iso_bkg pp_smear_10_30_wo_ele_rej_raw pp_smear_10_30_wo_ele_rej_bkg)
-
-for sample in ${samples[@]}; do
-    histogram ${sample}
-done
-
-samples=(pp_smear_30_50_raw pp_smear_30_50_bkg pp_smear_30_50_qcd_raw pp_smear_30_50_qcd_bkg pp_smear_30_50_qcd_gen_iso_raw pp_smear_30_50_qcd_gen_iso_bkg pp_smear_30_50_wo_ele_rej_raw pp_smear_30_50_wo_ele_rej_bkg)
-
-for sample in ${samples[@]}; do
-    histogram ${sample}
-done
-
-samples=(pp_smear_50_90_raw pp_smear_50_90_bkg pp_smear_50_90_qcd_raw pp_smear_50_90_qcd_bkg pp_smear_50_90_qcd_gen_iso_raw pp_smear_50_90_qcd_gen_iso_bkg pp_smear_50_90_wo_ele_rej_raw pp_smear_50_90_wo_ele_rej_bkg)
-
-for sample in ${samples[@]}; do
-    histogram ${sample}
-done
-
-# samples=(aa_raw aa_bkg aa_qcd_raw aa_qcd_bkg aa_qcd_gen_iso_raw aa_qcd_gen_iso_bkg aa_wo_ele_rej_raw aa_wo_ele_rej_bkg aa_cent_down_raw aa_cent_down_bkg aa_cent_up_raw aa_cent_up_bkg)
+# samples=(pp_smear_0_10_raw pp_smear_0_10_bkg pp_smear_0_10_qcd_raw pp_smear_0_10_qcd_bkg pp_smear_0_10_qcd_gen_iso_raw pp_smear_0_10_qcd_gen_iso_bkg pp_smear_0_10_wo_ele_rej_raw pp_smear_0_10_wo_ele_rej_bkg)
 
 # for sample in ${samples[@]}; do
 #     histogram ${sample}
 # done
 
-samples=(aa_mebs_raw aa_mebs_bkg aa_qcd_mebs_raw aa_qcd_mebs_bkg)
+# samples=(pp_smear_10_30_raw pp_smear_10_30_bkg pp_smear_10_30_qcd_raw pp_smear_10_30_qcd_bkg pp_smear_10_30_qcd_gen_iso_raw pp_smear_10_30_qcd_gen_iso_bkg pp_smear_10_30_wo_ele_rej_raw pp_smear_10_30_wo_ele_rej_bkg)
+
+# for sample in ${samples[@]}; do
+#     histogram ${sample}
+# done
+
+# samples=(pp_smear_30_50_raw pp_smear_30_50_bkg pp_smear_30_50_qcd_raw pp_smear_30_50_qcd_bkg pp_smear_30_50_qcd_gen_iso_raw pp_smear_30_50_qcd_gen_iso_bkg pp_smear_30_50_wo_ele_rej_raw pp_smear_30_50_wo_ele_rej_bkg)
+
+# for sample in ${samples[@]}; do
+#     histogram ${sample}
+# done
+
+# samples=(pp_smear_50_90_raw pp_smear_50_90_bkg pp_smear_50_90_qcd_raw pp_smear_50_90_qcd_bkg pp_smear_50_90_qcd_gen_iso_raw pp_smear_50_90_qcd_gen_iso_bkg pp_smear_50_90_wo_ele_rej_raw pp_smear_50_90_wo_ele_rej_bkg)
+
+# for sample in ${samples[@]}; do
+#     histogram ${sample}
+# done
+
+samples=(aa_raw aa_bkg aa_qcd_raw aa_qcd_bkg aa_qcd_gen_iso_raw aa_qcd_gen_iso_bkg aa_wo_ele_rej_raw aa_wo_ele_rej_bkg aa_cent_down_raw aa_cent_down_bkg aa_cent_up_raw aa_cent_up_bkg)
 
 for sample in ${samples[@]}; do
     histogram ${sample}
 done
+
+# samples=(aa_mebs_raw aa_mebs_bkg aa_qcd_mebs_raw aa_qcd_mebs_bkg)
+
+# for sample in ${samples[@]}; do
+#     histogram ${sample}
+# done
