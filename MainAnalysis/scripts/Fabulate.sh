5125e4245a4ce5961a00ac2c67307fb070a52a06
mitaylor <mitay@mit.edu>
2023-02-20 02:20:30 -0500

    distillate: run test

--- MainAnalysis/scripts/Fabulate.sh
+++ MainAnalysis/scripts/Fabulate.sh
@@ -3,11 +3,11 @@
 histogram() {
     tag=$1
     echo $tag
-    # ./bin/fabulate configs/fabulate/preapproval/fabulate_${tag}.conf data/preapproval/fabulate_${tag}.root
+    ./bin/fabulate configs/fabulate/preapproval/fabulate_${tag}.conf data/preapproval/fabulate_${tag}.root
     ./bin/fabulate_angle configs/fabulate/preapproval/fabulate_${tag}_angle.conf data/preapproval/fabulate_${tag}_angle.root
 }
 
-samples=(pp_smear_0_10_test pp_smear_10_30_test pp_smear_30_50_test pp_smear_50_90_test)
+samples=(pp_smear_0_10 pp_smear_10_30 pp_smear_30_50 pp_smear_50_90)
 
 for sample in ${samples[@]}; do
     histogram ${sample}
