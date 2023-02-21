bf76a8bbd7c886bcbd9147897e90d62e219915b0
mitaylor <mitay@mit.edu>
2023-02-21 16:54:14 -0500

    Hadd: update

--- MainAnalysis/scripts/HaddMany.sh
+++ MainAnalysis/scripts/HaddMany.sh
@@ -8,8 +8,8 @@ split -l ${n} --numeric-suffixes=0 -a 3 ${dataset} ${dataset}_
 
 files=($(ls ${dataset}_*))
 
-for i in 1; do
-# for i in ${!files[@]}; do
+# for i in 1; do
+for i in ${!files[@]}; do
     list=$(cat ${files[i]})
     hadd /data/submit/mitay/photons/${dataset}_$i.root $list
 done
