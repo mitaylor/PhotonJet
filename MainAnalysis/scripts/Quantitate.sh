a267deafcebb4558d6cd12641a11ef275ac8da32
mitaylor <mitay@mit.edu>
2023-02-21 08:37:26 -0500

    obnubilate: temporarily remove prior up down

--- MainAnalysis/scripts/Quantitate.sh
+++ MainAnalysis/scripts/Quantitate.sh
@@ -33,7 +33,7 @@ samples=(pp aa pp_smear_0_10 pp_smear_10_30 pp_smear_30_50 pp_smear_50_90)
 for sample in ${samples[@]}; do
     nominal $sample
 
-    for syst in wo_ele_rej qcd qcd_gen_iso loose_purity tight_purity jeu_up jeu_down jer_up stat_matrix prior_down prior_up; do # prior_down prior_up
+    for syst in wo_ele_rej qcd qcd_gen_iso loose_purity tight_purity jeu_up jeu_down jer_up stat_matrix; do # prior_down prior_up
         systematic $sample $syst
     done
 done
