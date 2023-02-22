ff0c44b3a8ead97bbf870b0eed3566a2821d8b3f
mitaylor <mitay@mit.edu>
2023-02-22 13:48:46 -0500

    Accumulate: redo certian ones

--- MainAnalysis/scripts/Accumulate.sh
+++ MainAnalysis/scripts/Accumulate.sh
@@ -20,25 +20,25 @@ systematic() {
     arithmetic ${sample}_${syst}
 }
 
-samples=(pp aa pp_smear_0_10 pp_smear_10_30 pp_smear_30_50 pp_smear_50_90)
+# samples=(pp aa pp_smear_0_10 pp_smear_10_30 pp_smear_30_50 pp_smear_50_90)
 
-for sample in ${samples[@]}; do
-    nominal $sample
+# for sample in ${samples[@]}; do
+#     nominal $sample
 
-    for syst in qcd qcd_gen_iso wo_ele_rej tight_purity loose_purity; do
-        systematic $sample $syst
-    done
-done
+#     for syst in qcd qcd_gen_iso wo_ele_rej tight_purity loose_purity; do
+#         systematic $sample $syst
+#     done
+# done
 
-samples=(aa)
+# samples=(aa)
 
-for sample in ${samples[@]}; do
-    for syst in cent_up cent_down; do
-        systematic $sample $syst
-    done
-done
+# for sample in ${samples[@]}; do
+#     for syst in cent_up cent_down; do
+#         systematic $sample $syst
+#     done
+# done
 
-samples=(aa_mebs aa_qcd_mebs)
+samples=(aa aa_mebs aa_qcd_mebs)
 
 for sample in ${samples[@]}; do
     nominal $sample
