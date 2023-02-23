67c1abf8cb82f74176774547cd2e2ca6712214e7
mitaylor <mitay@mit.edu>
2023-02-22 20:31:05 -0500

    obnubilate: try new smearing method

--- MainAnalysis/src/obnubilate.C
+++ MainAnalysis/src/obnubilate.C
@@ -159,28 +159,41 @@ int obnubilate(char const* config, char const* output) {
 
             batch->apply(square_);
 
-            // for (int64_t i = 0; i < batch->size(); ++i) {
-            //     for (int64_t j = 0; j < (*batch)[i]->GetNbinsX(); ++j) {
-            //         if (j == 2) {
-            //             double value = std::abs((*batch)[i]->GetBinContent(j) - (*batch)[i]->GetBinContent(j + 4));
-            //             value = value * 3 / 4;
-            //             value += std::min((*batch)[i]->GetBinContent(j), (*batch)[i]->GetBinContent(j + 4));
-            //             (*batch)[i]->SetBinContent(j + 1, value);
-            //         }
-            //         if (j == 3) {
-            //             double value = std::abs((*batch)[i]->GetBinContent(j - 1) - (*batch)[i]->GetBinContent(j + 3));
-            //             value = value * 1 / 2;
-            //             value += std::min((*batch)[i]->GetBinContent(j - 1), (*batch)[i]->GetBinContent(j + 3));
-            //             (*batch)[i]->SetBinContent(j + 1, value);
-            //         }
-            //         if (j == 4) {
-            //             double value = std::abs((*batch)[i]->GetBinContent(j - 2) - (*batch)[i]->GetBinContent(j + 2));
-            //             value = value * 1 / 4;
-            //             value += std::min((*batch)[i]->GetBinContent(j - 2), (*batch)[i]->GetBinContent(j + 2));
-            //             (*batch)[i]->SetBinContent(j + 1, value);
-            //         }
-            //     }
-            // }
+            /* apply smoothing */
+            for (int64_t i = 0; i < batch->size(); ++i) {
+                for (int64_t j = 0; j < (*batch)[i]->GetNbinsX(); ++j) {
+                    if (j == 1) {
+                        double value = (std::abs((*batch)[i]->GetBinContent(j) + (*batch)[i]->GetBinContent(j + 1))) / 2;
+                        (*batch)[i]->SetBinContent(j, value);
+                    }
+                    else if (j == (*batch)[i]->GetNbinsX()) {
+                        double value = (std::abs((*batch)[i]->GetBinContent(j) + (*batch)[i]->GetBinContent(j - 1))) / 2;
+                        (*batch)[i]->SetBinContent(j, value);
+                    } 
+                    else {
+                        double value = (std::abs((*batch)[i]->GetBinContent(j) + (*batch)[i]->GetBinContent(j - 1) + (*batch)[i]->GetBinContent(j + 1))) / 2;
+                        (*batch)[i]->SetBinContent(j, value);
+                    }
+                    // if (j == 2) {
+                    //     double value = std::abs((*batch)[i]->GetBinContent(j) - (*batch)[i]->GetBinContent(j + 4));
+                    //     value = value * 3 / 4;
+                    //     value += std::min((*batch)[i]->GetBinContent(j), (*batch)[i]->GetBinContent(j + 4));
+                    //     (*batch)[i]->SetBinContent(j + 1, value);
+                    // }
+                    // if (j == 3) {
+                    //     double value = std::abs((*batch)[i]->GetBinContent(j - 1) - (*batch)[i]->GetBinContent(j + 3));
+                    //     value = value * 1 / 2;
+                    //     value += std::min((*batch)[i]->GetBinContent(j - 1), (*batch)[i]->GetBinContent(j + 3));
+                    //     (*batch)[i]->SetBinContent(j + 1, value);
+                    // }
+                    // if (j == 4) {
+                    //     double value = std::abs((*batch)[i]->GetBinContent(j - 2) - (*batch)[i]->GetBinContent(j + 2));
+                    //     value = value * 1 / 4;
+                    //     value += std::min((*batch)[i]->GetBinContent(j - 2), (*batch)[i]->GetBinContent(j + 2));
+                    //     (*batch)[i]->SetBinContent(j + 1, value);
+                    // }
+                }
+            }
         }
 
         zip([&](auto const& batch, auto group) {
