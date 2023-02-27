f82f4767820209bcbb464cdb9638b895e50e6622
mitaylor <mitay@mit.edu>
2023-02-27 11:08:24 -0500

    condor: resubmit aa data

--- MainAnalysis/condor/SubmitJobs.sh
+++ MainAnalysis/condor/SubmitJobs.sh
@@ -14,12 +14,12 @@ cd ${output_tag}
 # split the input files so there are no more than 30 files per job
 # find ${folder} -type f > ${output_tag}
 # sed -i 's/^.*\/store/root:\/\/xrootd.cmsaf.mit.edu\/\/store/' ${output_tag}
-sed -i 's/^.*\/store/root:\/\/eoscms.cern.ch\/\/store/' ${output_tag}
+# sed -i 's/^.*\/store/root:\/\/eoscms.cern.ch\/\/store/' ${output_tag}
 
 # split -l 1 --numeric-suffixes=0 -a 3 ${output_tag} ${output_tag}_
 # files=`find . -type f -name "${output_tag}_[0-9][0-9][0-9]" -printf '%f\n'`
 
-awk -F "[_.]" -v OFS=", " '{print $(NF-1), $0}' ${output_tag} > ${output_tag}.list
+# awk -F "[_.]" -v OFS=", " '{print $(NF-1), $0}' ${output_tag} > ${output_tag}.list
 
 cat ../SubmitCondor.condor | sed "s/__MASTER__/${output_tag}/g" > SubmitCondor.condor
 sed -i "s/__CONFIG__/${config_fragment}/g" SubmitCondor.condor
