f4a1d4047f2188df5969f822fea139d1d069eeb1
mitaylor <mitay@mit.edu>
2023-02-28 16:48:45 -0500

    obnubilate: try adjusting smoothing

--- MainAnalysis/scripts/Quantitate.sh
+++ MainAnalysis/scripts/Quantitate.sh
@@ -28,26 +28,26 @@ systematic() {
     arithmetic ${sample}_${syst}
 }
 
-# samples=(pp aa pp_smear_0_10 pp_smear_10_30 pp_smear_30_50 pp_smear_50_90)
-
-# for sample in ${samples[@]}; do
-#     nominal $sample
-
-#     for syst in wo_ele_rej qcd qcd_gen_iso loose_purity tight_purity jeu_up jeu_down jer_up stat_matrix prior_down prior_up; do # prior_down prior_up
-#         systematic $sample $syst
-#     done
-# done
-
-samples=(aa)
-
-# for sample in ${samples[@]}; do
-#     for syst in cent_up cent_down mebs qcd_mebs qcd_all; do
-#         systematic $sample $syst
-#     done
-# done
+samples=(pp aa pp_smear_0_10 pp_smear_10_30 pp_smear_30_50 pp_smear_50_90)
 
 for sample in ${samples[@]}; do
-    for syst in mebs qcd_mebs; do
+    nominal $sample
+
+    for syst in wo_ele_rej qcd qcd_gen_iso loose_purity tight_purity jeu_up jeu_down jer_up stat_matrix prior_down prior_up; do # prior_down prior_up
         systematic $sample $syst
     done
 done
+
+samples=(aa)
+
+for sample in ${samples[@]}; do
+    for syst in cent_up cent_down; do
+        systematic $sample $syst
+    done
+done
+
+# for sample in ${samples[@]}; do
+#     for syst in mebs qcd_mebs qcd_all; do
+#         systematic $sample $syst
+#     done
+# done
--- MainAnalysis/src/obnubilate.C
+++ MainAnalysis/src/obnubilate.C
@@ -161,8 +161,8 @@ int obnubilate(char const* config, char const* output) {
 
             /* apply smoothing */
             for (int64_t i = 0; i < batch->size(); ++i) {
-                for (int64_t j = 1; j <= (*batch)[i]->GetNbinsX(); ++j) {
-                // for (int64_t j = (*batch)[i]->GetNbinsX(); j > 0; --j) {
+                for (int64_t j = 1; j <= 6; ++j) {
+                // for (int64_t j = 1; j <= (*batch)[i]->GetNbinsX(); ++j) {
                     if (j == 1) {
                         double value = (std::abs((*batch)[i]->GetBinContent(j) + (*batch)[i]->GetBinContent(j + 1))) / 2;
                         (*batch)[i]->SetBinContent(j, value);
