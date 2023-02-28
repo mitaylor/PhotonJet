a3d9951f2d1eea7ea67a77672e671a42cf65efe7
mitaylor <mitay@mit.edu>
2023-02-28 13:09:17 -0500

    compare_before_unfolding: first try

---
+++ MainAnalysis/src/compare_before_unfolding.C
@@ -0,0 +1,201 @@
+#include "../include/lambdas.h"
+
+#include "../git/config/include/configurer.h"
+
+#include "../git/history/include/interval.h"
+#include "../git/history/include/multival.h"
+#include "../git/history/include/history.h"
+
+#include "../git/paper-and-pencil/include/paper.h"
+#include "../git/paper-and-pencil/include/pencil.h"
+
+#include "../git/tricks-and-treats/include/overflow_angles.h"
+#include "../git/tricks-and-treats/include/trunk.h"
+#include "../git/tricks-and-treats/include/zip.h"
+
+#include "TFile.h"
+#include "TH1.h"
+#include "TH2.h"
+
+using namespace std::literals::string_literals;
+using namespace std::placeholders;
+
+template <typename... T>
+void title(std::function<void(TH1*)> f, T*&... args) {
+    (void)(int [sizeof...(T)]) { (args->apply(f), 0)... };
+}
+
+template <typename... T>
+void normalise_to_unity(T*&... args) {
+    (void)(int [sizeof...(T)]) { (args->apply([](TH1* obj) {
+        obj->Scale(1. / obj->Integral("width")); }), 0)... };
+}
+
+template <typename T>
+T* null(int64_t, std::string const&, std::string const&) {
+    return nullptr;
+}
+
+TH2F* variance(TH1* flat, multival const* m) {
+    auto cov = new TH2F("cov", "", m->size(), 0, m->size(),
+        m->size(), 0, m->size());
+
+    for (int64_t i = 0; i < m->size(); ++i) {
+        auto err = flat->GetBinError(i + 1);
+        cov->SetBinContent(i + 1, i + 1, err * err);
+    }
+
+    return cov;
+}
+
+template <std::size_t N>
+TH1F* fold(TH1* flat, TH2* covariance, multival const* m, int64_t axis,
+           std::array<int64_t, N> const& offsets) {
+    auto name = std::string(flat->GetName()) + "_fold" + std::to_string(axis);
+    auto hfold = m->axis(axis).book<TH1F, 2>(0, name, "",
+        { offsets[axis << 1], offsets[(axis << 1) + 1] });
+
+    auto shape = m->shape();
+
+    auto front = std::vector<int64_t>(m->dims(), 0);
+    auto back = std::vector<int64_t>(m->dims(), 0);
+    for (int64_t i = 0; i < m->dims(); ++i) {
+        front[i] = offsets[i << 1];
+        back[i] = shape[i] - offsets[(i << 1) + 1];
+    }
+
+    auto size = back[axis] - front[axis];
+    auto list = new std::vector<int64_t>[size];
+
+    for (int64_t i = 0; i < m->size(); ++i) {
+        auto indices = m->indices_for(i);
+
+        bool flag = false;
+        zip([&](int64_t index, int64_t f, int64_t b) {
+            flag = flag || index < f || index >= b;
+        }, indices, front, back);
+        if (flag) { continue; }
+
+        auto index = indices[axis] - front[axis];
+        hfold->SetBinContent(index + 1, hfold->GetBinContent(index + 1)
+            + flat->GetBinContent(i + 1));
+
+        list[index].push_back(i);
+    }
+
+    auto cov = covariance ? (TH2F*)covariance->Clone() : variance(flat, m);
+
+    for (int64_t i = 0; i < size; ++i) {
+        auto indices = list[i];
+        int64_t count = indices.size();
+
+        auto error = 0.;
+        for (int64_t j = 0; j < count; ++j) {
+            auto j_x = indices[j] + 1;
+            for (int64_t k = 0; k < count; ++k) {
+                auto k_x = indices[k] + 1;
+                error = error + cov->GetBinContent(j_x, k_x);
+            }
+        }
+
+        hfold->SetBinError(i + 1, std::sqrt(error));
+    }
+
+    delete [] list;
+    delete cov;
+
+    hfold->Scale(1., "width");
+
+    return hfold;
+}
+
+int data_mc_comparison(char const* config, const char* output) {
+    auto conf = new configurer(config);
+
+    auto input_new = conf->get<std::string>("input_new");
+    auto input_old = conf->get<std::string>("input_old");
+
+    auto dpt = conf->get<std::vector<float>>("pt_diff");
+    auto dhf = conf->get<std::vector<float>>("hf_diff");
+    auto dcent = conf->get<std::vector<int32_t>>("cent_diff");
+
+    /* create intervals and multivals */
+    auto ihf = new interval(dhf);
+
+    /* load history objects */
+    TFile* f_new = new TFile(input_new.data(), "read");
+    TFile* f_old = new TFile(input_old.data(), "read");
+
+    auto h_new_dr = new history<TH1F>(f_new, "aa_nominal_s_pure_raw_sub_pjet_u_dr_sum0_side0");
+    h_new_dr->rename("aa_new_dr");
+    auto h_old_dr = old history<TH1F>(f_old, "aa_nominal_s_pure_raw_sub_pjet_u_dr_sum0_side0");
+    h_old_dr->rename("aa_old_dr");
+    auto h_new_jtpt = new history<TH1F>(f_new, "aa_nominal_s_pure_raw_sub_pjet_u_dr_sum0_side1");
+    h_new_jtpt->rename("aa_new_jtpt");
+    auto h_old_jtpt = old history<TH1F>(f_old, "aa_nominal_s_pure_raw_sub_pjet_u_dr_sum0_side1");
+    h_old_jtpt->rename("aa_old_jtpt");
+
+    normalise_to_unity(h_new_dr, h_old_dr, h_new_jtpt, h_old_jtpt);
+
+    
+    /* set up figures */
+    auto system_tag = "  #sqrt{s_{NN}} = 5.02 TeV, 1.69 nb^{-1}"s;
+    auto cms = "#bf{#scale[1.4]{CMS}} #it{#scale[1.2]{Preliminary}}"s;
+
+    auto hf_info = [&](int64_t index) {
+        info_text(index, 0.75, "Cent. %i - %i%%", dcent, true); };
+
+    auto kinematics = [&](int64_t index) {
+        if (index > 0) {
+            TLatex* l = new TLatex();
+            l->SetTextAlign(11);
+            l->SetTextFont(43);
+            l->SetTextSize(13);
+            l->DrawLatexNDC(0.135, 0.71, "40 < p_{T}^{#gamma} < 200, |#eta^{#gamma}| < 1.44");
+            l->DrawLatexNDC(0.135, 0.67, "anti-k_{T} R = 0.3, 30 < p_{T}^{jet} < 120, |#eta^{jet}| < 1.6");
+        }
+    };
+
+    auto hb = new pencil();
+    hb->category("type", "new", "old");
+
+    hb->alias("new", "Current Result Before Unfolding");
+    hb->alias("old", "Previous Result Before Unfolding");
+
+    auto p1 = new paper("accumulate_aa_dr_comparison", hb);
+    p1->divide(ihf->size(), -1);
+    p1->accessory(hf_info);
+    p1->accessory(kinematics);
+    apply_style(p1, cms, system_tag);
+    
+    h_new_dr->apply([&](TH1* h) { p1->add(h, "new"); });
+    h_old_dr->apply([&](TH1* h, int64_t index) { p1->stack(index + 1, h, "old"); });
+
+    auto p2 = new paper("accumulate_aa_jtpt_comparison", hb);
+    p2->divide(ihf->size(), -1);
+    p2->accessory(hf_info);
+    p2->accessory(kinematics);
+    apply_style(p2, cms, system_tag);
+    
+    h_new_jtpt->apply([&](TH1* h) { p2->add(h, "new"); });
+    h_old_jtpt->apply([&](TH1* h, int64_t index) { p2->stack(index + 1, h, "nominal_previous"); });
+
+    hb->sketch();
+
+    p1->draw("pdf");
+    p2->draw("pdf");
+
+    in(output, [&]() {
+        h_new_dr->save();
+    });
+
+    return 0;
+}
+
+int main(int argc, char* argv[]) {
+    if (argc == 3)
+        return data_mc_comparison(argv[1], argv[2]);
+
+    printf("usage: %s [config] [output]\n", argv[0]);
+    return 1;
+}
