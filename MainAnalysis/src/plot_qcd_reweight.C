67e864b16d03dac34a9c5ab05cd0da4ea30a7326
mitaylor <mitay@mit.edu>
2023-03-06 05:11:24 -0500

    plot_qcd: try reweighting the pthat spectrum with only filtered datasets

---
+++ MainAnalysis/src/plot_qcd_reweight.C
@@ -0,0 +1,87 @@
+#include "../include/lambdas.h"
+#include "../include/pjtree.h"
+#include "../include/specifics.h"
+
+#include "../git/config/include/configurer.h"
+
+#include "../git/history/include/interval.h"
+#include "../git/history/include/multival.h"
+#include "../git/history/include/memory.h"
+
+#include "../git/tricks-and-treats/include/overflow_angles.h"
+#include "../git/tricks-and-treats/include/trunk.h"
+
+#include "TFile.h"
+#include "TTree.h"
+#include "TH1.h"
+
+#include <memory>
+#include <string>
+#include <vector>
+
+using namespace std::literals::string_literals;
+using namespace std::placeholders;
+
+float weight_for(std::vector<int32_t> const& divisions,
+                 std::vector<float> const& weights, float value) {
+    int64_t index = -1;
+    for (auto edge : divisions)
+        if (value > edge)
+            ++index;
+
+    return weights[index];
+}
+
+int plot_qcd(char const* config, char const* output) {
+    auto conf = new configurer(config);
+
+    auto files = conf->get<std::vector<std::string>>("files");
+    auto pthat = conf->get<std::vector<int32_t>>("pthat");
+    auto pthatw = conf->get<std::vector<float>>("pthatw");
+    auto vzw = conf->get<std::vector<float>>("vzw");
+
+    auto pthat_w = new TH1F("pthat_w", "pthat", 100, 0, 200);
+
+    /* manage memory manually */
+    TH1::AddDirectory(false);
+    TH1::SetDefaultSumw2();
+
+    TF1* fweight = new TF1("fweight", "(gaus(0))/(gaus(3))");
+    if (mc_branches && apply_weights) {
+        fweight->SetParameters(vzw[0], vzw[1], vzw[2], vzw[3], vzw[4], vzw[5]); 
+    }
+
+    /* load input */
+    for (auto const& file : files) {
+        TFile* f = new TFile(file.data(), "read");
+        TTree* t = (TTree*)f->Get("pj");
+        auto pjt = new pjtree(true, false, false, t, { 1, 0, 0, 0, 0, 0, 0 });
+
+        int64_t nentries = static_cast<int64_t>(t->GetEntries());
+        for (int64_t i = 0; i < nentries; ++i) {
+            if (i % 100000 == 0) { printf("entry: %li/%li\n", i, nentries); }
+
+            t->GetEntry(i);
+
+            float weight = fweight->Eval(pjt->vz) * weight_for(pthat, pthatw, pjt->pthat) * pjt->weight;
+
+            pthat_w->Fill(pjt->pthat, weight);
+        }
+    }
+
+    pthat_w->Scale(1. / pthat_w->Integral());
+
+    pthat_w->SaveAs(output);
+
+    printf("destroying objects..\n");
+
+    return 0;
+}
+
+int main(int argc, char* argv[]) {
+    if (argc == 3)
+        return plot_qcd(argv[1], argv[2]);
+
+    printf("usage: %s [config] [output]\n", argv[0]);
+    return 1;
+}
