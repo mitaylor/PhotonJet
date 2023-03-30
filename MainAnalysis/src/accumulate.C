927cdcc6af841009d8c9f8d76c91e4df74c747bf
mitaylor <mitay@mit.edu>
2023-03-30 16:13:37 -0400

    accumulate: update to use global config

--- MainAnalysis/src/accumulate.C
+++ MainAnalysis/src/accumulate.C
@@ -175,7 +175,7 @@ int accumulate(char const* config, char const* selections, char const* output) {
     cms += "                                                                   ";
     cms += "                                                                   ";
     cms += "anti-k_{T} R = 0.3, p_{T}^{jet} > "s + to_text(jet_pt_min) + " GeV, |#eta^{jet}| < "s + to_text(jet_eta_abs) + ", ";
-    cms += ", p_{T}^{#gamma} > "s + photon_pt_min + " GeV, |#eta^{#gamma}| < "s + to_text(photon_eta_abs) + ", #Delta#phi_{j#gamma} > "s + to_text(dphi_min_numerator) + "#pi/"s + to_text(dphi_min_denominator);
+    cms += ", p_{T}^{#gamma} > "s + to_text(photon_pt_min) + " GeV, |#eta^{#gamma}| < "s + to_text(photon_eta_abs) + ", #Delta#phi_{j#gamma} > "s + to_text(dphi_min_numerator) + "#pi/"s + to_text(dphi_min_denominator);
 
     std::function<void(int64_t, float)> pt_info = [&](int64_t x, float pos) {
         info_text(x, pos, "%.0f < p_{T}^{#gamma} < %.0f", dpt, false); };
