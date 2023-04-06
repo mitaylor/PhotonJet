9b8a5b1d688cb4a42f522cd69fca3fef856bba69
mitaylor <mitay@mit.edu>
2023-04-06 04:56:08 -0400

    congratulate: remove weighting

--- MainAnalysis/src/ratio.C
+++ MainAnalysis/src/ratio.C
@@ -42,10 +42,6 @@ int ratio(char const* config, char const* output) {
     auto figures = conf->get<std::vector<std::string>>("figures");
     auto prefix = conf->get<std::string>("prefix");
 
-    auto corrections = conf->get<std::vector<std::string>>("corrections");
-    auto truth_reco_iso_labels = conf->get<std::vector<std::string>>("truth_reco_iso_labels");
-    auto qcd_after_labels = conf->get<std::vector<std::string>>("qcd_after_labels");
-
     auto xmins = conf->get<std::vector<float>>("xmin");
     auto xmaxs = conf->get<std::vector<float>>("xmax");
     auto ymins = conf->get<std::vector<float>>("ymin");
@@ -70,15 +66,6 @@ int ratio(char const* config, char const* output) {
     zip([&](auto& file, auto const& input) {
         file = new TFile(input.data(), "read");
     }, files, inputs);
-
-    std::vector<history<TH1F>*> truth_reco_isos(5, nullptr);
-    std::vector<history<TH1F>*> unfolded_qcds(5, nullptr);
-
-    zip([&](auto& truth_reco_iso, auto& unfolded_qcd, auto const correction, auto const& truth_reco_iso_label, auto const& qcd_after_label) {
-            auto file = new TFile(correction.data(), "read");
-            unfolded_qcd = new history<TH1F>(file, qcd_after_label);
-            truth_reco_iso = new history<TH1F>(file, truth_reco_iso_label);
-    }, truth_reco_isos, unfolded_qcds, corrections, truth_reco_iso_labels, qcd_after_labels);
 
     /* load histograms */
     std::vector<std::string> base_stubs(5);
@@ -134,46 +121,10 @@ int ratio(char const* config, char const* output) {
 
         /* link histograms, uncertainties */
         std::unordered_map<TH1*, TH1*> links;
-        zip([&](auto hist, auto syst, auto unfolded_qcd, auto truth_reco_iso) {
-            hist->apply([&](TH1* h, int64_t index) {
-                for (int64_t i = 1; i <= h->GetNbinsX(); ++i) {
-                    double val = h->GetBinContent(i);
-                    double err = h->GetBinError(i);
-                    double correction = (*truth_reco_iso)[index]->GetBinContent(i);
-
-                    if ((*unfolded_qcd)[index]->GetBinContent(i) > 0.001) {
-                        correction /= (*unfolded_qcd)[index]->GetBinContent(i);
-                    } else {
-                        correction = 1;
-                    }
-
-                    correction = 1;
- 
-                    h->SetBinContent(i, val*correction);
-                    h->SetBinError(i, err*correction);
-                }});
-            syst->apply([&](TH1* h, int64_t index) {
-                for (int64_t i = 1; i <= h->GetNbinsX(); ++i) {
-                    double val = h->GetBinContent(i);
-                    double err = h->GetBinError(i);
-                    double correction = (*truth_reco_iso)[index]->GetBinContent(i);
-
-                    if ((*unfolded_qcd)[index]->GetBinContent(i) > 0.001) {
-                        correction /= (*unfolded_qcd)[index]->GetBinContent(i);
-                    } else {
-                        correction = 1;
-                    }
-
-                    correction = 1;
-
-                    h->SetBinContent(i, val*correction);
-                    h->SetBinError(i, err*correction);
-                }});
-
-            /* scale everything by the truth gen iso vs reco iso difference */
+        zip([&](auto hist, auto syst) {
             hist->apply([&](TH1* h, int64_t index) {
                 links[h] = (*syst)[index]; });
-        }, hists, systs, unfolded_qcds, truth_reco_isos);
+        }, hists, systs);
 
         /* take the ratio */
         for (int64_t i = 0; i < hists[0]->size(); ++i) {
