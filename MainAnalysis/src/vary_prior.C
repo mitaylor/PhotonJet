a6cadf7a980cae083d9a97698aaa00e304e00a1e
mitaylor <mitay@mit.edu>
2023-04-10 21:10:43 -0400

    vary_prior: update to use global configs

--- MainAnalysis/src/vary_prior.C
+++ MainAnalysis/src/vary_prior.C
@@ -20,7 +20,7 @@
 using namespace std::literals::string_literals;
 using namespace std::placeholders;
 
-int data_mc_comparison(char const* config, const char* output) {
+int vary_prior(char const* config, char const* selections, const char* output) {
     auto conf = new configurer(config);
 
     auto input_data = conf->get<std::string>("input_data");
@@ -35,24 +35,22 @@ int data_mc_comparison(char const* config, const char* output) {
 
     auto tag = conf->get<std::string>("tag");
 
-    auto rdr = conf->get<std::vector<float>>("drg_range");
-    auto rpt = conf->get<std::vector<float>>("ptg_range");
+    auto sel = new configurer(selections);
 
-    auto dpt = conf->get<std::vector<float>>("pt_diff");
-    auto dhf = conf->get<std::vector<float>>("hf_diff");
-    auto dcent = conf->get<std::vector<int32_t>>("cent_diff");
+    auto set = sel->get<std::string>("set");
+    auto base = sel->get<std::string>("base");
+
+    auto rdr = sel->get<std::vector<float>>("drg_range");
+    auto rpt = sel->get<std::vector<float>>("ptg_range");
 
     /* create intervals and multivals */
     auto ipt= new interval(rpt);
     auto idr = new interval(rdr);
     auto mg = new multival(rdr, rpt);
 
-    // auto ihf = new interval(dhf);
-    // auto mpthf = new multival(dpt, dhf);
-
     /* load history objects */
-    TFile* fdata = new TFile(input_data.data(), "read");
-    TFile* ftruth = new TFile(input_truth.data(), "read");
+    TFile* fdata = new TFile((base + input_data).data(), "read");
+    TFile* ftruth = new TFile((base + input_truth).data(), "read");
 
     auto h_data = new history<TH1F>(fdata, tag + "_"s + data_label);
     auto h_truth_g = new history<TH1F>(ftruth, tag + "_"s + truth_label_g);
@@ -89,40 +87,13 @@ int data_mc_comparison(char const* config, const char* output) {
         h_truth_c->save();
     });
 
-    // /* set up figures */
-    // auto collisions = "#sqrt{s_{NN}} = 5.02 TeV"s;
-
-    // std::function<void(int64_t, float)> pt_info = [&](int64_t x, float pos) {
-    //     info_text(x, pos, "%.0f < p_{T}^{#gamma} < %.0f", dpt, false); };
-
-    // std::function<void(int64_t, float)> hf_info = [&](int64_t x, float pos) {
-    //     info_text(x, pos, "%i - %i%%", dcent, true); };
-
-    // auto pthf_info = [&](int64_t index) {
-    //     stack_text(index, 0.75, 0.04, mpthf, pt_info, hf_info); };
-
-    // auto hb = new pencil();
-    // hb->category("type", "nominal", "vary_up", "vary_down");
-
-    // /* (1) unfolded MC vs reco truth dr */
-    // auto p1 = new paper(tag + "_dj_unfolded_mc_vs_truth_reco_iso", hb);
-    // p1->divide(ihf->size(), -1);
-    // p1->accessory(pthf_info);
-    // apply_style(p1, collisions, -2., 27.);
-    // p1->accessory(std::bind(line_at, _1, 0.f, rdr[0], rdr[1]));
-    
-    // h_r_qcd_after->apply([&](TH1* h) { p1->add(h, "qcd_after"); });
-    // h_r_truth_reco_iso->apply([&](TH1* h, int64_t index) { p1->stack(index + 1, h, "truth_reco_iso"); });
-
-    // p1->draw("pdf");
-
     return 0;
 }
 
 int main(int argc, char* argv[]) {
-    if (argc == 3)
-        return data_mc_comparison(argv[1], argv[2]);
+    if (argc == 4)
+        return vary_prior(argv[1], argv[2], argv[3]);
 
-    printf("usage: %s [config] [output]\n", argv[0]);
+    printf("usage: %s [config] [selections] [output]\n", argv[0]);
     return 1;
 }
