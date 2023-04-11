ea21da8652ae44452e04bb864573dd6a7ab063df
mitaylor <mitay@mit.edu>
2023-04-11 10:47:26 -0400

    Unfolding: unfold MEBS

--- MainAnalysis/scripts/Manipulate.sh
+++ MainAnalysis/scripts/Manipulate.sh
@@ -14,22 +14,30 @@ run_aa() {
     ./bin/manipulate configs/manipulate/preapproval/manipulate_${tag}.conf configs/analysis/${set}/analysis_aa.conf data/preapproval/${set}/manipulate_${tag}.root
 }
 
-samples=(pp pp_smear_0_10 pp_smear_10_30 pp_smear_30_50 pp_smear_50_90)
+# samples=(pp pp_smear_0_10 pp_smear_10_30 pp_smear_30_50 pp_smear_50_90)
+
+# for sample in ${samples[@]}; do
+#     run_pp $sample
+
+#     for syst in qcd qcd_gen_iso wo_ele_rej tight_purity loose_purity; do
+#         run_pp ${sample}_${syst}
+#     done
+# done
+
+# samples=(aa)
+
+# for sample in ${samples[@]}; do
+#     run_aa $sample
+
+#     for syst in qcd qcd_gen_iso wo_ele_rej tight_purity loose_purity cent_up cent_down; do
+#         run_aa ${sample}_${syst}
+#     done
+# done
+
+# samples=(aa)
 
 for sample in ${samples[@]}; do
-    run_pp $sample
-
-    for syst in qcd qcd_gen_iso wo_ele_rej tight_purity loose_purity; do
-        run_pp ${sample}_${syst}
-    done
-done
-
-samples=(aa)
-
-for sample in ${samples[@]}; do
-    run_aa $sample
-
-    for syst in qcd qcd_gen_iso wo_ele_rej tight_purity loose_purity cent_up cent_down; do
-        run_pp ${sample}_${syst}
+    for syst in mebs qcd_mebs; do
+        run_aa ${sample}_${syst}
     done
 done
