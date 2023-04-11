6b2f4752c1f4f76f6fe464b1f64fa02457f34080
mitaylor <mitay@mit.edu>
2023-04-11 11:53:43 -0400

    estimate_hf: update to use global configs

--- MainAnalysis/scripts/EstimateHF.sh
+++ MainAnalysis/scripts/EstimateHF.sh
@@ -5,7 +5,7 @@ set=$1
 run_pp() {
     tag=$1
 
-    ./bin/estimate_hf configs/estimate_hf/estimate_hf_${tag}.conf configs/analysis/${set}/analysis_pp.conf data/preapproval/${set}/estimate_hf_${tag}.root
+    ./bin/estimate_hf configs/estimate_hf/estimate_hf_${tag}.conf configs/analysis/${set}/analysis_pp.conf data/preapproval/${set}/estimate_hf_${tag}.root > log/${set}_estimate_hf_${tag}.txt 2>&1 &
 }
 
 samples=(pp_data pp_qcd)
