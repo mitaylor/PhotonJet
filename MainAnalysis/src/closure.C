3489a35009cd3688aa80c55eabb93fd66f5d49a9
mitaylor <mitay@mit.edu>
2023-04-11 07:33:07 -0400

    closure: change to global config

--- MainAnalysis/src/closure.C
+++ MainAnalysis/src/closure.C
@@ -1,4 +1,5 @@
 #include "../include/lambdas.h"
+#include "../include/text.h"
 
 #include "../git/config/include/configurer.h"
 
@@ -67,8 +68,17 @@ int closure(char const* config, char const* selections, const char* output) {
 
     auto heavyion = sel->get<bool>("heavyion");
 
-    auto rdr = sel->get<std::vector<float>>("dr_bounds");
-    auto rpt = sel->get<std::vector<float>>("jet_pt_bounds");
+    auto const dphi_min_numerator = sel->get<float>("dphi_min_numerator");
+    auto const dphi_min_denominator = sel->get<float>("dphi_min_denominator");
+
+    auto const jet_pt_min = sel->get<float>("jet_pt_min");
+    auto const jet_eta_abs = sel->get<float>("jet_eta_abs");
+
+    auto const photon_eta_abs = sel->get<float>("photon_eta_abs");
+
+    auto bpho_pt = sel->get<std::vector<float>>("photon_pt_bounds");
+    auto bdr = sel->get<std::vector<float>>("dr_bounds");
+    auto bjet_pt = sel->get<std::vector<float>>("jet_pt_bounds");
 
     /* create intervals and multivals */
     auto ihf = new interval(dhf);
@@ -145,12 +155,16 @@ int closure(char const* config, char const* selections, const char* output) {
 
     auto kinematics = [&](int64_t index) {
         if (index > 0) {
+            auto photon_selections = to_text(bpho_pt[0]) + " < p_{T}^{#gamma} < "s + to_text(bpho_pt[1]) + " GeV, |#eta^{#gamma}| < "s + to_text(photon_eta_abs)  + 
+                ", #Delta#phi_{j#gamma} > " + to_text(dphi_min_numerator) + "#pi/"s + to_text(dphi_min_denominator);
+            auto jet_selections = "anti-k_{T} R = 0.3, " + to_text(bjet_pt[0]) + "p_{T}^{jet} < "s + to_text(bjet_pt[1]) + " GeV, |#eta^{jet}| < "s + to_text(jet_eta_abs);
+
             TLatex* l = new TLatex();
             l->SetTextAlign(31);
             l->SetTextFont(43);
             l->SetTextSize(13);
-            l->DrawLatexNDC(0.865, 0.71, "40 < p_{T}^{#gamma} < 200, |#eta^{#gamma}| < 1.44");
-            l->DrawLatexNDC(0.865, 0.67, "anti-k_{T} R = 0.3, 30 < p_{T}^{jet} < 120, |#eta^{jet}| < 1.6");
+            l->DrawLatexNDC(0.865, 0.71, photon_selections.data());
+            l->DrawLatexNDC(0.865, 0.65, photon_selections.data());
         }
     };
 
@@ -171,113 +185,113 @@ int closure(char const* config, char const* selections, const char* output) {
     hb->alias("matrix", "Response Matrix Reco");
 
     /* (1) unfolded MC vs reco truth dr */
-    auto p1 = new paper(label + "_dj_unfolded_mc_vs_truth_reco_iso", hb);
+    auto p1 = new paper(set + "_" + label + "_dj_unfolded_mc_vs_truth_reco_iso", hb);
     p1->divide(ihf->size(), -1);
     p1->accessory(hf_info);
     p1->accessory(kinematics);
     apply_style(p1, cms, system_tag, -2., 27.);
-    p1->accessory(std::bind(line_at, _1, 0.f, rdr[0], rdr[1]));
+    p1->accessory(std::bind(line_at, _1, 0.f, bdr[0], bdr[1]));
     
     h_r_qcd_after->apply([&](TH1* h) { p1->add(h, "qcd_after"); });
     h_r_truth_reco_iso->apply([&](TH1* h, int64_t index) { p1->stack(index + 1, h, "truth_reco_iso"); });
     
     /* (2) unfolded MC vs reco truth jtpt */
-    auto p2 = new paper(label + "_jtpt_unfolded_mc_vs_truth_reco_iso", hb);
+    auto p2 = new paper(set + "_" + label + "_jtpt_unfolded_mc_vs_truth_reco_iso", hb);
     p2->divide(ihf->size(), -1);
     p2->accessory(hf_info);
     p2->accessory(kinematics);
     apply_style(p2, cms, system_tag, -0.001, 0.04);
-    p2->accessory(std::bind(line_at, _1, 0.f, rpt[0], rpt[1]));
+    p2->accessory(std::bind(line_at, _1, 0.f, bjet_pt[0], bjet_pt[1]));
     
     h_j_qcd_after->apply([&](TH1* h) { p2->add(h, "qcd_after"); });
     h_j_truth_reco_iso->apply([&](TH1* h, int64_t index) { p2->stack(index + 1, h, "truth_reco_iso"); });
 
     /* (3) data vs refolded data */
-    auto p3 = new paper(label + "_refolding_test", hb);
+    auto p3 = new paper(set + "_" + label + "_refolding_test", hb);
     p3->divide(ihf->size(), -1);
     p3->accessory(hf_info);
     p3->accessory(kinematics);
     apply_style(p3, cms, system_tag, -2., 27.);
-    p3->accessory(std::bind(line_at, _1, 0.f, rdr[0], rdr[1]));
+    p3->accessory(std::bind(line_at, _1, 0.f, bdr[0], bdr[1]));
 
     h_r_data_before->apply([&](TH1* h) { p3->add(h, "data_before"); });
     h_r_data_circle->apply([&](TH1* h, int64_t index) { p3->stack(index + 1, h, "data_circle"); });
 
     /* (5) matched vs unmatched dr */
-    auto p5 = new paper(label + "_dj_matched_unmatched", hb);
+    auto p5 = new paper(set + "_" + label + "_dj_matched_unmatched", hb);
     p5->divide(ihf->size(), -1);
     p5->accessory(hf_info);
     p5->accessory(kinematics);
     apply_style(p5, cms, system_tag, -2., 27.);
-    p5->accessory(std::bind(line_at, _1, 0.f, rdr[0], rdr[1]));
+    p5->accessory(std::bind(line_at, _1, 0.f, bdr[0], bdr[1]));
     
     h_r_reco_reco_iso_matched->apply([&](TH1* h) { p5->add(h, "reco_matched"); });
     h_r_reco_reco_iso_unmatched->apply([&](TH1* h, int64_t index) { p5->stack(index + 1, h, "reco_unmatched"); });
 
     /* (6) matched vs unmatched jtpt */
-    auto p6 = new paper(label + "_jtpt_matched_unmatched", hb);
+    auto p6 = new paper(set + "_" + label + "_jtpt_matched_unmatched", hb);
     p6->divide(ihf->size(), -1);
     p6->accessory(hf_info);
     p6->accessory(kinematics);
     apply_style(p6, cms, system_tag, -0.001, 0.07);
-    p6->accessory(std::bind(line_at, _1, 0.f, rpt[0], rpt[1]));
+    p6->accessory(std::bind(line_at, _1, 0.f, bjet_pt[0], bjet_pt[1]));
     
     h_j_reco_reco_iso_matched->apply([&](TH1* h) { p6->add(h, "reco_matched"); });
     h_j_reco_reco_iso_unmatched->apply([&](TH1* h, int64_t index) { p6->stack(index + 1, h, "reco_unmatched"); });
 
     /* (7) MC vs reco truth dr */
-    auto p7 = new paper(label + "_dj_mc_vs_reco", hb);
+    auto p7 = new paper(set + "_" + label + "_dj_mc_vs_reco", hb);
     p7->divide(ihf->size(), -1);
     p7->accessory(hf_info);
     p7->accessory(kinematics);
     apply_style(p7, cms, system_tag, -2., 27.);
-    p7->accessory(std::bind(line_at, _1, 0.f, rdr[0], rdr[1]));
+    p7->accessory(std::bind(line_at, _1, 0.f, bdr[0], bdr[1]));
     
     h_r_reco_reco_iso_matched->apply([&](TH1* h) { p7->add(h, "reco_matched"); });
     h_r_qcd_before->apply([&](TH1* h, int64_t index) { p7->stack(index + 1, h, "qcd_before"); });
     h_r_r->apply([&](TH1* h, int64_t index) { p7->stack(index + 1, h, "matrix"); });
 
     /* (8) MC vs reco truth jtpt */
-    auto p8 = new paper(label + "_jtpt_mc_vs_reco", hb);
+    auto p8 = new paper(set + "_" + label + "_jtpt_mc_vs_reco", hb);
     p8->divide(ihf->size(), -1);
     p8->accessory(hf_info);
     p8->accessory(kinematics);
     apply_style(p8, cms, system_tag, -0.001, 0.04);
-    p8->accessory(std::bind(line_at, _1, 0.f, rpt[0], rpt[1]));
+    p8->accessory(std::bind(line_at, _1, 0.f, bjet_pt[0], bjet_pt[1]));
     
     h_j_reco_reco_iso_matched->apply([&](TH1* h) { p8->add(h, "reco_matched"); });
     h_j_qcd_before->apply([&](TH1* h, int64_t index) { p8->stack(index + 1, h, "qcd_before"); });
     h_j_r->apply([&](TH1* h, int64_t index) { p8->stack(index + 1, h, "matrix"); });
 
     /* (9) qcd vs refolded qcd */
-    auto p9 = new paper(label + "_dj_qcd_refolding_test", hb);
+    auto p9 = new paper(set + "_" + label + "_dj_qcd_refolding_test", hb);
     p9->divide(ihf->size(), -1);
     p9->accessory(hf_info);
     p9->accessory(kinematics);
     apply_style(p9, cms, system_tag, -2., 27.);
-    p9->accessory(std::bind(line_at, _1, 0.f, rdr[0], rdr[1]));
+    p9->accessory(std::bind(line_at, _1, 0.f, bdr[0], bdr[1]));
 
     h_r_qcd_before->apply([&](TH1* h) { p9->add(h, "qcd_before"); });
     h_r_qcd_circle->apply([&](TH1* h, int64_t index) { p9->stack(index + 1, h, "qcd_circle"); });
 
     /* (10) qcd vs refolded qcd */
-    auto p10 = new paper(label + "_jtpt_qcd_refolding_test", hb);
+    auto p10 = new paper(set + "_" + label + "_jtpt_qcd_refolding_test", hb);
     p10->divide(ihf->size(), -1);
     p10->accessory(hf_info);
     p10->accessory(kinematics);
     apply_style(p10, cms, system_tag, -0.001, 0.04);
-    p10->accessory(std::bind(line_at, _1, 0.f, rpt[0], rpt[1]));
+    p10->accessory(std::bind(line_at, _1, 0.f, bjet_pt[0], bjet_pt[1]));
 
     h_j_qcd_before->apply([&](TH1* h) { p10->add(h, "qcd_before"); });
     h_j_qcd_circle->apply([&](TH1* h, int64_t index) { p10->stack(index + 1, h, "qcd_circle"); });
 
     /* (11) truth reco iso vs truth gen iso */
-    auto p11 = new paper(label + "_dj_reco_iso_vs_gen_iso", hb);
+    auto p11 = new paper(set + "_" + label + "_dj_reco_iso_vs_gen_iso", hb);
     p11->divide(ihf->size(), -1);
     p11->accessory(hf_info);
     p11->accessory(kinematics);
     apply_style(p11, cms, system_tag, -2., 27.);
-    p11->accessory(std::bind(line_at, _1, 0.f, rdr[0], rdr[1]));
+    p11->accessory(std::bind(line_at, _1, 0.f, bdr[0], bdr[1]));
     
     h_r_truth_gen_iso->apply([&](TH1* h) { h->GetXaxis()->SetTitle("#deltaj"); });
     h_r_truth_reco_iso->apply([&](TH1* h) { h->GetXaxis()->SetTitle("#deltaj"); });
@@ -285,12 +299,12 @@ int closure(char const* config, char const* selections, const char* output) {
     h_r_truth_reco_iso->apply([&](TH1* h, int64_t index) { p11->stack(index + 1, h, "truth_reco_iso"); });
 
     /* (12) truth reco iso vs truth gen iso */
-    auto p12 = new paper(label + "_jtpt_reco_iso_vs_gen_iso", hb);
+    auto p12 = new paper(set + "_" + label + "_jtpt_reco_iso_vs_gen_iso", hb);
     p12->divide(ihf->size(), -1);
     p12->accessory(hf_info);
     p12->accessory(kinematics);
     apply_style(p12, cms, system_tag, -0.001, 0.04);
-    p12->accessory(std::bind(line_at, _1, 0.f, rpt[0], rpt[1]));
+    p12->accessory(std::bind(line_at, _1, 0.f, bjet_pt[0], bjet_pt[1]));
     
     h_j_truth_gen_iso->apply([&](TH1* h) { h->GetXaxis()->SetTitle("dp_{T}^{j}"); });
     h_j_truth_reco_iso->apply([&](TH1* h) { h->GetXaxis()->SetTitle("dp_{T}^{j}"); });
