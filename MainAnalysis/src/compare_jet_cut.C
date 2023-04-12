a1583363c554bdedf557c617da1a2f5d0bb28afd
mitaylor <mitay@mit.edu>
2023-04-12 04:27:41 -0400

    jet_cut: test new plots

--- MainAnalysis/src/compare_jet_cut.C
+++ MainAnalysis/src/compare_jet_cut.C
@@ -44,40 +44,41 @@ int narrate(char const* config, char const* selections) {
     auto rptg = sel->get<std::vector<float>>("ptg_range");
 
     auto osg = sel->get<std::vector<int64_t>>("osg");
-
+std::cout << __LINE__ << std::endl;
     /* load history objects */
     auto ihf = new interval(dhf);
-
+std::cout << __LINE__ << std::endl;
     TFile* f_aa = new TFile((base + input_aa).data(), "read");
     TFile* f_pp = new TFile((base + input_pp).data(), "read"); 
-
+std::cout << __LINE__ << std::endl;
     std::vector<int64_t> cut = {1, 2, 3, 4, 5};
-
+std::cout << __LINE__ << std::endl;
     std::vector<history<TH1F>*> aa_unfolded_fold0(cut.size(), nullptr);
     std::vector<history<TH1F>*> aa_unfolded_fold1(cut.size(), nullptr);
     std::vector<history<TH1F>*> pp_unfolded_fold0(cut.size(), nullptr);
     std::vector<history<TH1F>*> pp_unfolded_fold1(cut.size(), nullptr);
-
+    std::cout << __LINE__ << std::endl;
     for (int64_t i = 0; i < (int64_t) cut.size(); ++i) { 
-        aa_unfolded_fold0[i] = new history<TH1F>(f_aa, "aa_" + label + std::to_string(i) + "_fold0");
-        aa_unfolded_fold1[i] = new history<TH1F>(f_aa, "aa_" + label + std::to_string(i) + "_fold1");
-        pp_unfolded_fold0[i] = new history<TH1F>(f_pp, "pp_" + label + std::to_string(i) + "_fold0");
-        pp_unfolded_fold1[i] = new history<TH1F>(f_pp, "pp_" + label + std::to_string(i) + "_fold1");
+        std::cout << __LINE__ << std::endl;
+        aa_unfolded_fold0[i] = new history<TH1F>(f_aa, "aa_"s + label + std::to_string(i) + "_fold0"s);
+        aa_unfolded_fold1[i] = new history<TH1F>(f_aa, "aa_"s + label + std::to_string(i) + "_fold1"s);
+        pp_unfolded_fold0[i] = new history<TH1F>(f_pp, "pp_"s + label + std::to_string(i) + "_fold0"s);
+        pp_unfolded_fold1[i] = new history<TH1F>(f_pp, "pp_"s + label + std::to_string(i) + "_fold1"s);
     }
-
+std::cout << __LINE__ << std::endl;
     auto hb = new pencil();
     hb->category("system", "pp", "aa");
-
+std::cout << __LINE__ << std::endl;
     hb->alias("aa", "PbPb");
-
+std::cout << __LINE__ << std::endl;
     auto p1 = new paper(set + "_compare_jet_cut_dr", hb);
     apply_style(p1, "#bf{#scale[1.4]{CMS}}     #sqrt{s_{NN}} = 5.02 TeV"s, "PbPb 1.69 nb^{-1}, pp 302 pb^{-1}"s, -2, 27);
     p1->divide(ihf->size(), -1);
-
+std::cout << __LINE__ << std::endl;
     auto p2 = new paper(set + "_compare_jet_cut_jpt", hb);
     apply_style(p2, "#bf{#scale[1.4]{CMS}}     #sqrt{s_{NN}} = 5.02 TeV"s, "PbPb 1.69 nb^{-1}, pp 302 pb^{-1}"s, 0, 0.03);
     p2->divide(ihf->size(), -1);
-
+std::cout << __LINE__ << std::endl;
     for (int64_t i = 0; i < (int64_t) cut.size(); ++i) { 
         aa_unfolded_fold0[i]->apply([&](TH1* h) { p1->add(h, "aa"); });
         aa_unfolded_fold1[i]->apply([&](TH1* h) { p2->add(h, "aa"); });
