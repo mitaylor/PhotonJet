65b0790340ddce5f2045d087e17c4e19f42736ab
mitaylor <mitay@mit.edu>
2023-04-12 07:03:28 -0400

    ComparePhotonPtSpectrum: run all comparisons

--- MainAnalysis/scripts/ComparePhotonPtSpectrum.sh
+++ MainAnalysis/scripts/ComparePhotonPtSpectrum.sh
@@ -8,7 +8,7 @@ run_pp() {
     ./bin/compare_photon_pt_spectrum_accumulate configs/test/compare_photon_pt_spectrum_accumulate_pp.conf data/preapproval/${set}/compare_photon_pt_spectrum_accumulate_pp.root
     ./bin/compare_photon_pt_spectrum_populate configs/test/compare_photon_pt_spectrum_populate_pp.conf data/preapproval/${set}/compare_photon_pt_spectrum_populate_pp.root
     ./bin/compare_photon_pt_spectrum configs/test/compare_photon_pt_spectrum_pp_nominal.conf data/preapproval/${set}/compare_photon_pt_spectrum_pp_nominal.root
-    ./bin/compare_photon_pt_spectrum configs/test/compare_photon_pt_spectrum_pp_no_eff.conf data/preapproval/${set}/compare_photon_pt_spectrum_pp_no_eff.root
+    # ./bin/compare_photon_pt_spectrum configs/test/compare_photon_pt_spectrum_pp_no_eff.conf data/preapproval/${set}/compare_photon_pt_spectrum_pp_no_eff.root
 }
 
 run_aa() {
@@ -17,7 +17,7 @@ run_aa() {
     ./bin/compare_photon_pt_spectrum_accumulate configs/test/compare_photon_pt_spectrum_accumulate_aa.conf data/preapproval/${set}/compare_photon_pt_spectrum_accumulate_aa.root
     ./bin/compare_photon_pt_spectrum_populate configs/test/compare_photon_pt_spectrum_populate_aa.conf data/preapproval/${set}/compare_photon_pt_spectrum_populate_aa.root
     ./bin/compare_photon_pt_spectrum configs/test/compare_photon_pt_spectrum_aa_nominal.conf data/preapproval/${set}/compare_photon_pt_spectrum_aa_nominal.root
-    ./bin/compare_photon_pt_spectrum configs/test/compare_photon_pt_spectrum_aa_no_eff.conf data/preapproval/${set}/compare_photon_pt_spectrum_aa_no_eff.root
+    # ./bin/compare_photon_pt_spectrum configs/test/compare_photon_pt_spectrum_aa_no_eff.conf data/preapproval/${set}/compare_photon_pt_spectrum_aa_no_eff.root
 }
 
 samples=(pp)
