274e5ea01b1c777b05046c96ee5a66cd398339c8
mitaylor <mitay@mit.edu>
2023-04-12 07:01:34 -0400

    ComparePhotonPtSpectrum: run all comparisons

--- MainAnalysis/scripts/ComparePhotonPtSpectrum.sh
+++ MainAnalysis/scripts/ComparePhotonPtSpectrum.sh
@@ -5,19 +5,19 @@ set=$1
 run_pp() {
     tag=$1
 
-    ./bin/compare_photon_pt_spectrum_accumulate configs/test/compare_photon_pt_spectrum_accumulate_pp.conf configs/analysis/${set}/analysis_pp.conf data/preapproval/${set}/compare_photon_pt_spectrum_accumulate_pp.root
-    ./bin/compare_photon_pt_spectrum_populate configs/test/compare_photon_pt_spectrum_populate_pp.conf configs/analysis/${set}/analysis_pp.conf data/preapproval/${set}/compare_photon_pt_spectrum_populate_pp.root
-    ./bin/compare_photon_pt_spectrum configs/test/compare_photon_pt_spectrum_pp_nominal.conf configs/analysis/${set}/analysis_pp.conf data/preapproval/${set}/compare_photon_pt_spectrum_pp_nominal.root
-    ./bin/compare_photon_pt_spectrum configs/test/compare_photon_pt_spectrum_pp_no_eff.conf configs/analysis/${set}/analysis_pp.conf data/preapproval/${set}/compare_photon_pt_spectrum_pp_no_eff.root
+    ./bin/compare_photon_pt_spectrum_accumulate configs/test/compare_photon_pt_spectrum_accumulate_pp.conf data/preapproval/${set}/compare_photon_pt_spectrum_accumulate_pp.root
+    ./bin/compare_photon_pt_spectrum_populate configs/test/compare_photon_pt_spectrum_populate_pp.conf data/preapproval/${set}/compare_photon_pt_spectrum_populate_pp.root
+    ./bin/compare_photon_pt_spectrum configs/test/compare_photon_pt_spectrum_pp_nominal.conf data/preapproval/${set}/compare_photon_pt_spectrum_pp_nominal.root
+    ./bin/compare_photon_pt_spectrum configs/test/compare_photon_pt_spectrum_pp_no_eff.conf data/preapproval/${set}/compare_photon_pt_spectrum_pp_no_eff.root
 }
 
 run_aa() {
     tag=$1
 
-    ./bin/compare_photon_pt_spectrum_accumulate configs/test/compare_photon_pt_spectrum_accumulate_aa.conf configs/analysis/${set}/analysis_aa.conf data/preapproval/${set}/compare_photon_pt_spectrum_accumulate_aa.root
-    ./bin/compare_photon_pt_spectrum_populate configs/test/compare_photon_pt_spectrum_populate_aa.conf configs/analysis/${set}/analysis_aa.conf data/preapproval/${set}/compare_photon_pt_spectrum_populate_aa.root
-    ./bin/compare_photon_pt_spectrum configs/test/compare_photon_pt_spectrum_aa_nominal.conf configs/analysis/${set}/analysis_aa.conf data/preapproval/${set}/compare_photon_pt_spectrum_aa_nominal.root
-    ./bin/compare_photon_pt_spectrum configs/test/compare_photon_pt_spectrum_aa_no_eff.conf configs/analysis/${set}/analysis_aa.conf data/preapproval/${set}/compare_photon_pt_spectrum_aa_no_eff.root
+    ./bin/compare_photon_pt_spectrum_accumulate configs/test/compare_photon_pt_spectrum_accumulate_aa.conf data/preapproval/${set}/compare_photon_pt_spectrum_accumulate_aa.root
+    ./bin/compare_photon_pt_spectrum_populate configs/test/compare_photon_pt_spectrum_populate_aa.conf data/preapproval/${set}/compare_photon_pt_spectrum_populate_aa.root
+    ./bin/compare_photon_pt_spectrum configs/test/compare_photon_pt_spectrum_aa_nominal.conf data/preapproval/${set}/compare_photon_pt_spectrum_aa_nominal.root
+    ./bin/compare_photon_pt_spectrum configs/test/compare_photon_pt_spectrum_aa_no_eff.conf data/preapproval/${set}/compare_photon_pt_spectrum_aa_no_eff.root
 }
 
 samples=(pp)
