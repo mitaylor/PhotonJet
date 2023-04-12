dce9929df0bcf1646b2518544f542babecc0e7b9
mitaylor <mitay@mit.edu>
2023-04-12 02:43:05 -0400

    photon pt spectrum: update configs

--- MainAnalysis/scripts/PhotonPtSpectrum.sh
+++ MainAnalysis/scripts/PhotonPtSpectrum.sh
@@ -5,13 +5,13 @@ set=$1
 run_pp() {
     tag=$1
 
-    ./bin/photon_pt_spectrum configs/photon_pt_spectrum/preapproval/photon_pt_spectrum_${tag}.conf configs/analysis/${set}/analysis_pp.conf data/preapproval/${set}/photon_pt_spectrum_${tag}.root > log/${set}_photon_pt_spectrum_${tag}.txt 2>&1 &
+    ./bin/photon_pt_spectrum configs/photon_pt_spectrum/photon_pt_spectrum_${tag}.conf configs/analysis/${set}/analysis_pp.conf data/preapproval/${set}/photon_pt_spectrum_${tag}.root > log/${set}_photon_pt_spectrum_${tag}.txt 2>&1 &
 }
 
 run_aa() {
     tag=$1
 
-    ./bin/photon_pt_spectrum configs/photon_pt_spectrum/preapproval/photon_pt_spectrum_${tag}.conf configs/analysis/${set}/analysis_aa.conf data/preapproval/${set}/photon_pt_spectrum_${tag}.root > log/${set}_photon_pt_spectrum_${tag}.txt 2>&1 &
+    ./bin/photon_pt_spectrum configs/photon_pt_spectrum/photon_pt_spectrum_${tag}.conf configs/analysis/${set}/analysis_aa.conf data/preapproval/${set}/photon_pt_spectrum_${tag}.root > log/${set}_photon_pt_spectrum_${tag}.txt 2>&1 &
 }
 
 samples=(pp_data_no_eff pp_data pp_qcd)
