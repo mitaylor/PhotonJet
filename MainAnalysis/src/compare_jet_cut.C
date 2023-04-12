e2d81805aba72ab283c3af434a543fe6cd0192d9
mitaylor <mitay@mit.edu>
2023-04-12 04:18:33 -0400

    jet_cut: test new plots

--- MainAnalysis/src/compare_jet_cut.C
+++ MainAnalysis/src/compare_jet_cut.C
@@ -27,17 +27,11 @@ using namespace std::placeholders;
 int narrate(char const* config, char const* selections, char const* output) {
     auto conf = new configurer(config);
 
-    auto data = conf->get<std::vector<std::string>>("data");
-    auto files = conf->get<std::vector<std::string>>("files");
+    auto input_aa = conf->get<std::string>("input_aa");
+    auto input_pp = conf->get<std::string>("input_pp");
 
-    auto system = conf->get<std::string>("system");
-    auto tag = conf->get<std::string>("tag");
+    auto label = conf->get<std::string>("label");
 
-    auto eta_min = conf->get<std::vector<double>>("eta_min");
-    auto eta_max = conf->get<std::vector<double>>("eta_max");
-    auto bound_string = conf->get<std::vector<std::string>>("bound_string");
-    
-    auto rho_range = conf->get<std::vector<double>>("rho_range");
     auto dhf = conf->get<std::vector<float>>("hf_diff");
     auto dcent = conf->get<std::vector<int32_t>>("cent_diff");
 
@@ -45,228 +39,49 @@ int narrate(char const* config, char const* selections, char const* output) {
     auto sel = new configurer(selections);
 
     auto set = sel->get<std::string>("set");
+    auto base = sel->get<std::string>("base");
 
-    auto const photon_pt_min = sel->get<float>("photon_pt_min");
-    auto const hovere_max = sel->get<float>("hovere_max");
-    auto const see_min = sel->get<float>("see_min");
-    auto const see_max = sel->get<float>("see_max");
-    auto const iso_max = sel->get<float>("iso_max");
+    auto rptg = sel->get<std::vector<float>>("ptg_range");
 
-    TH1::SetDefaultSumw2();
+    auto osg = sel->get<std::vector<int64_t>>("osg");
+
+    /* load history objects */
+    TFile* f_aa = new TFile((base + input_aa).data(), "read");
+    TFile* f_pp = new TFile((base + input_pp).data(), "read"); 
+
+    std::vector<int64_t> cut = {1, 2, 3, 4, 5};
+
+    std::vector<history<TH1F>*> aa_unfolded_fold0(cut.size(), nullptr);
+    std::vector<history<TH1F>*> aa_unfolded_fold1(cut.size(), nullptr);
+    std::vector<history<TH1F>*> pp_unfolded_fold0(cut.size(), nullptr);
+    std::vector<history<TH1F>*> pp_unfolded_fold1(cut.size(), nullptr);
+
+    for (int64_t i = 0; i < (int64_t) cut.size(); ++i) { 
+        aa_unfolded_fold0[i] = new history<TH1F>(f_aa, "aa_" + label + std::to_string(i) + "_fold0");
+        aa_unfolded_fold1[i] = new history<TH1F>(f_aa, "aa_" + label + std::to_string(i) + "_fold1");
+        pp_unfolded_fold0[i] = new history<TH1F>(f_pp, "pp_" + label + std::to_string(i) + "_fold0");
+        pp_unfolded_fold1[i] = new history<TH1F>(f_pp, "pp_" + label + std::to_string(i) + "_fold1");
+    }
+
+    auto p1 = new paper(set + "_compare_jet_cut_dr", hb);
+    apply_style(p1, "#bf{#scale[1.4]{CMS}}     #sqrt{s_{NN}} = 5.02 TeV"s, "PbPb 1.69 nb^{-1}, pp 302 pb^{-1}"s, -2, 27);
+    p1->divide(ihf->size(), -1);
+
+    auto p2 = new paper(set + "_compare_jet_cut_jpt", hb);
+    apply_style(p2, "#bf{#scale[1.4]{CMS}}     #sqrt{s_{NN}} = 5.02 TeV"s, "PbPb 1.69 nb^{-1}, pp 302 pb^{-1}"s, 0, 0.03);
+    p2->divide(ihf->size(), -1);
+
+    for (int64_t i = 0; i < (int64_t) cut.size(); ++i) { 
+        aa_unfolded_fold0[i]->apply( p1->add(h, "aa"); )
+        aa_unfolded_fold1[i]->apply( p2->add(h, "aa"); )
+        pp_unfolded_fold0[i]->apply( p1->stack(h, "pp"); )
+        pp_unfolded_fold1[i]->apply( p2->stack(h, "pp"); )
+    }
     
-    auto ihf = new interval(dhf);
-    auto irho = new interval("#rho"s, rho_range[0], rho_range[1], rho_range[2]);
-    auto frho = std::bind(&interval::book<TH1F>, irho, _1, _2, _3);
+    hb->sketch();
 
-    auto dim_1_size = static_cast<int64_t>(eta_min.size());
-    auto dim_2_size = static_cast<int64_t>(dhf.size()-1);
-
-    auto rho_data = new history<TH1F>("rho_data"s, "", frho, dim_1_size, dim_2_size);
-    auto rho_mc = new history<TH1F>("rho_mc"s, "", frho, dim_1_size, dim_2_size);
-    auto rho_ratio = new history<TH1F>("rho_ratio"s, "", frho, dim_1_size, dim_2_size);
-
-
-    for (auto const& file : data) {
-        std::cout << file << std::endl;
-
-        TFile* f = new TFile(file.data(), "read");
-        TTree* t = (TTree*)f->Get("pj");
-        auto pjt = new pjtree(false, false, true, t, { 1, 0, 1, 1, 0, 0, 1, 0, 0 });
-
-        int64_t nentries = static_cast<int64_t>(t->GetEntries());
-        // int64_t nentries = 10000;
-
-        for (int64_t i = 0; i < nentries-1; ++i) {
-            if (i % 100000 == 0)
-                printf("entry: %li/%li\n", i, nentries);
-
-            t->GetEntry(i);
-            auto hf_x = ihf->index_for(pjt->hiHF);
-
-            if (hf_x < 0) continue;
-            if (hf_x == ihf->size()) continue;
-
-            if (std::abs(pjt->vz) > 15) { continue; }
-
-            int64_t leading = -1;
-            float leading_pt = 0;
-            for (int64_t j = 0; j < pjt->nPho; ++j) {
-                if ((*pjt->phoEt)[j] <= 30) { continue; }
-                if (std::abs((*pjt->phoSCEta)[j]) >= eta_max[0]) { continue; }
-                if ((*pjt->phoHoverE)[j] > hovere_max) { continue; }
-
-                auto pho_et = (*pjt->phoEtErNew)[j];
-
-                if (pho_et < photon_pt_min) { continue; }
-
-                if (pho_et > leading_pt) {
-                    leading = j;
-                    leading_pt = pho_et;
-                }
-            }
-
-            if (leading < 0) { continue; }
-
-            if ((*pjt->phoSigmaIEtaIEta_2012)[leading] > see_max
-                    || (*pjt->phoSigmaIEtaIEta_2012)[leading] < see_min)
-                continue;
-
-            if (in_pho_failure_region(pjt, leading)) { continue; }
-
-            float isolation = (*pjt->pho_ecalClusterIsoR3)[leading]
-                    + (*pjt->pho_hcalRechitIsoR3)[leading]
-                    + (*pjt->pho_trackIsoR3PtCut20)[leading];
-            if (isolation > iso_max) { continue; }
-
-            for (size_t j = 0; j < eta_min.size(); ++j) {
-                auto eta_x = static_cast<int64_t>(j);
-                auto avg_rho = get_avg_rho(pjt, eta_min[j], eta_max[j]);
-
-                (*rho_data)[rho_data->index_for(x{eta_x,hf_x})]->Fill(avg_rho);
-            }
-        }
-    }
-
-    for (auto const& file : files) {
-        std::cout << file << std::endl;
-
-        TFile* f = new TFile(file.data(), "read");
-        TTree* t = (TTree*)f->Get("pj");
-        auto pjt = new pjtree(false, false, true, t, { 1, 0, 1, 1, 0, 0, 1, 0, 0 });
-
-        int64_t nentries = static_cast<int64_t>(t->GetEntries());
-        // nentries = 10000;
-
-        for (int64_t i = 0; i < nentries-1; ++i) {
-            if (i % 100000 == 0)
-                printf("entry: %li/%li\n", i, nentries);
-
-            t->GetEntry(i);
-
-            if (std::abs(pjt->vz) > 15) { continue; }
-
-            int64_t leading = -1;
-            float leading_pt = 0;
-            for (int64_t j = 0; j < pjt->nPho; ++j) {
-                if ((*pjt->phoEt)[j] <= 30) { continue; }
-                if (std::abs((*pjt->phoSCEta)[j]) >= eta_max[0]) { continue; }
-                if ((*pjt->phoHoverE)[j] > hovere_max) { continue; }
-
-                auto pho_et = (*pjt->phoEtErNew)[j];
-
-                if (pho_et < photon_pt_min) { continue; }
-
-                if (pho_et > leading_pt) {
-                    leading = j;
-                    leading_pt = pho_et;
-                }
-            }
-
-            if (leading < 0) { continue; }
-
-            if ((*pjt->phoSigmaIEtaIEta_2012)[leading] > see_max
-                    || (*pjt->phoSigmaIEtaIEta_2012)[leading] < see_min)
-                continue;
-
-            if (in_pho_failure_region(pjt, leading)) { continue; }
-
-            float isolation = (*pjt->pho_ecalClusterIsoR3)[leading]
-                    + (*pjt->pho_hcalRechitIsoR3)[leading]
-                    + (*pjt->pho_trackIsoR3PtCut20)[leading];
-            if (isolation > iso_max) { continue; }
-
-            for (size_t j = 0; j < eta_min.size(); ++j) {
-                for (size_t k = 0; k < dhf.size()-1; ++k) {
-                    auto eta_x = static_cast<int64_t>(j);
-                    auto hf_x = static_cast<int64_t>(k);
-                    auto index = rho_data->index_for(x{eta_x,hf_x});
-                    auto avg_rho = get_avg_rho(pjt, eta_min[j], eta_max[j]);
-
-                    (*rho_mc)[index]->Fill(avg_rho, pjt->w);
-                }
-            }
-        }
-    }
-
-    for (size_t i = 0; i < eta_min.size(); ++i) {
-        for (size_t j = 0; j < dhf.size()-1; ++j) {
-            auto eta_x = static_cast<int64_t>(i);
-            auto hf_x = static_cast<int64_t>(j);
-            auto index = rho_data->index_for(x{eta_x,hf_x});
-
-            (*rho_data)[index]->Scale(1. / (*rho_data)[index]->Integral());
-            (*rho_mc)[index]->Scale(1. / (*rho_mc)[index]->Integral());
-
-            (*rho_data)[index]->SetMaximum(10);
-            (*rho_data)[index]->SetMinimum(1E-7);
-            (*rho_mc)[index]->SetMaximum(10);
-            (*rho_mc)[index]->SetMinimum(1E-7);
-
-            (*rho_ratio)[index]->Divide((*rho_data)[index], (*rho_mc)[index]);
-            (*rho_ratio)[index]->SetMaximum(100);
-            (*rho_ratio)[index]->SetMinimum(1E-3);
-        }
-    }
-
-    /* draw rho distributions */
-    auto system_tag = system + "  #sqrt{s_{NN}} = 5.02 TeV"s;
-    auto cms = "#bf{#scale[1.4]{CMS}} #it{#scale[1.2]{Preliminary}}"s;
-    cms += "         p_{T}^{#gamma} > "s + to_text(photon_pt_min) + " GeV"s;
-
-    auto hf_info = [&](int64_t index) {
-        info_text(index, 0.75, "Cent. %i - %i%%", dcent, true); };
-
-    for (size_t i = 0; i < eta_min.size(); ++i) {
-        auto hb = new pencil();
-        hb->category("type", "Data", "MC");
-        
-        auto c1 = new paper(set + "_" + tag + "_rho_distribution_" + bound_string[i], hb);
-        apply_style(c1, cms, system_tag);
-        c1->accessory(hf_info);
-        c1->divide(ihf->size()/2, -1);
-        c1->set(paper::flags::logy);
-
-        for (size_t j = 0; j < dhf.size()-1; ++j) {
-            auto eta_x = static_cast<int64_t>(i);
-            auto hf_x = static_cast<int64_t>(j);
-            auto index = rho_data->index_for(x{eta_x,hf_x});
-            
-            c1->add((*rho_mc)[index], "MC");
-            c1->stack((*rho_data)[index], "Data");
-        }
-
-        hb->sketch();
-        c1->draw("pdf");
-    }
-
-    for (size_t i = 0; i < eta_min.size(); ++i) {
-        auto hb = new pencil();
-        hb->category("type", "Data/MC");
-        
-        auto c1 = new paper(set + "_" + tag + "_rho_weight_" + bound_string[i], hb);
-        apply_style(c1, cms, system_tag);
-        c1->accessory(hf_info);
-        c1->divide(ihf->size()/2, -1);
-        c1->set(paper::flags::logy);
-
-        for (size_t j = 0; j < dhf.size()-1; ++j) {
-            auto eta_x = static_cast<int64_t>(i);
-            auto hf_x = static_cast<int64_t>(j);
-            auto index = rho_ratio->index_for(x{eta_x,hf_x});
-            
-            c1->add((*rho_ratio)[index], "Data/MC");
-        }
-
-        hb->sketch();
-        c1->draw("pdf");
-    }
-
-    /* save output */
-    in(output, [&]() {
-        rho_data->save(tag);
-        rho_mc->save(tag);
-        rho_ratio->save(tag);
-    });
+    p1->draw("pdf");
+    p2->draw("pdf");
 
     return 0;
 }
