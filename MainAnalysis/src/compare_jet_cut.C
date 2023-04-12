1b541489f1d64205e9ee0e8bb976b3babf4c8fbd
mitaylor <mitay@mit.edu>
2023-04-12 04:41:22 -0400

    jet_cut: test new plots

--- MainAnalysis/src/compare_jet_cut.C
+++ MainAnalysis/src/compare_jet_cut.C
@@ -44,26 +44,49 @@ int narrate(char const* config, char const* selections) {
     auto rptg = sel->get<std::vector<float>>("ptg_range");
 
     auto osg = sel->get<std::vector<int64_t>>("osg");
-std::cout << __LINE__ << std::endl;
+
     /* load history objects */
     auto ihf = new interval(dhf);
-std::cout << __LINE__ << std::endl;
+
     TFile* f_aa = new TFile((base + input_aa).data(), "read");
     TFile* f_pp = new TFile((base + input_pp).data(), "read"); 
-std::cout << __LINE__ << std::endl;
+
     std::vector<int64_t> cut = {1, 2, 3, 4, 5};
-std::cout << __LINE__ << std::endl;
+
     std::vector<history<TH1F>*> aa_unfolded_fold0(cut.size(), nullptr);
     std::vector<history<TH1F>*> aa_unfolded_fold1(cut.size(), nullptr);
-    std::vector<history<TH1F>*> pp_unfolded_fold0(cut.size(), nullptr);
-    std::vector<history<TH1F>*> pp_unfolded_fold1(cut.size(), nullptr);
-    std::cout << __LINE__ << std::endl;
+    std::vector<history<TH1F>*> pp_0_unfolded_fold0(cut.size(), nullptr);
+    std::vector<history<TH1F>*> pp_0_unfolded_fold1(cut.size(), nullptr);
+    std::vector<history<TH1F>*> pp_1_unfolded_fold0(cut.size(), nullptr);
+    std::vector<history<TH1F>*> pp_1_unfolded_fold1(cut.size(), nullptr);
+    std::vector<history<TH1F>*> pp_2_unfolded_fold0(cut.size(), nullptr);
+    std::vector<history<TH1F>*> pp_2_unfolded_fold1(cut.size(), nullptr);
+    std::vector<history<TH1F>*> pp_3_unfolded_fold0(cut.size(), nullptr);
+    std::vector<history<TH1F>*> pp_3_unfolded_fold1(cut.size(), nullptr);
+    
     for (int64_t i = 0; i < (int64_t) cut.size(); ++i) { 
-        std::cout << "aa_"s + label + std::to_string(i) + "_fold0"s << std::endl;
-        aa_unfolded_fold0[i] = new history<TH1F>(f_aa, "aa_"s + label + "_" + std::to_string(i) + "_fold0"s);std::cout << __LINE__ << std::endl;
-        aa_unfolded_fold1[i] = new history<TH1F>(f_aa, "aa_"s + label + "_" + std::to_string(i) + "_fold1"s);std::cout << __LINE__ << std::endl;
-        pp_unfolded_fold0[i] = new history<TH1F>(f_pp, "pp_"s + label + "_" + std::to_string(i) + "_fold0"s);std::cout << __LINE__ << std::endl;
-        pp_unfolded_fold1[i] = new history<TH1F>(f_pp, "pp_"s + label + "_" + std::to_string(i) + "_fold1"s);std::cout << __LINE__ << std::endl;
+        aa_unfolded_fold0[i] = new history<TH1F>(f_aa, "aa_"s + label + "_" + std::to_string(i) + "_fold0"s);
+        aa_unfolded_fold1[i] = new history<TH1F>(f_aa, "aa_"s + label + "_" + std::to_string(i) + "_fold1"s);
+
+        pp_0_unfolded_fold0[i] = new history<TH1F>(f_pp, "pp_"s + label + "_" + std::to_string(i) + "_fold0"s);
+        pp_0_unfolded_fold0[i]->rename(label + "_" + std::to_string(i) + "_" + std::to_string(0) + "_fold0"s);
+        pp_0_unfolded_fold1[i] = new history<TH1F>(f_pp, "pp_"s + label + "_" + std::to_string(i) + "_fold1"s);
+        pp_0_unfolded_fold1[i]->rename(label + "_" + std::to_string(i) + "_" + std::to_string(0) + "_fold1"s);
+
+        pp_1_unfolded_fold0[i] = new history<TH1F>(f_pp, "pp_"s + label + "_" + std::to_string(i) + "_fold0"s);
+        pp_1_unfolded_fold0[i]->rename(label + "_" + std::to_string(i) + "_" + std::to_string(1) + "_fold0"s);
+        pp_1_unfolded_fold1[i] = new history<TH1F>(f_pp, "pp_"s + label + "_" + std::to_string(i) + "_fold1"s);
+        pp_1_unfolded_fold1[i]->rename(label + "_" + std::to_string(i) + "_" + std::to_string(1) + "_fold1"s);
+
+        pp_2_unfolded_fold0[i] = new history<TH1F>(f_pp, "pp_"s + label + "_" + std::to_string(i) + "_fold0"s);
+        pp_2_unfolded_fold0[i]->rename(label + "_" + std::to_string(i) + "_" + std::to_string(2) + "_fold0"s);
+        pp_2_unfolded_fold1[i] = new history<TH1F>(f_pp, "pp_"s + label + "_" + std::to_string(i) + "_fold1"s);
+        pp_2_unfolded_fold1[i]->rename(label + "_" + std::to_string(i) + "_" + std::to_string(2) + "_fold1"s);
+
+        pp_3_unfolded_fold0[i] = new history<TH1F>(f_pp, "pp_"s + label + "_" + std::to_string(i) + "_fold0"s);
+        pp_3_unfolded_fold0[i]->rename(label + "_" + std::to_string(i) + "_" + std::to_string(3) + "_fold0"s);
+        pp_3_unfolded_fold1[i] = new history<TH1F>(f_pp, "pp_"s + label + "_" + std::to_string(i) + "_fold1"s);
+        pp_3_unfolded_fold1[i]->rename(label + "_" + std::to_string(i) + "_" + std::to_string(3) + "_fold1"s);
     }
 
     auto hb = new pencil();
@@ -82,8 +105,12 @@ std::cout << __LINE__ << std::endl;
     for (int64_t i = 0; i < (int64_t) cut.size(); ++i) { 
         aa_unfolded_fold0[i]->apply([&](TH1* h) { p1->add(h, "aa"); });
         aa_unfolded_fold1[i]->apply([&](TH1* h) { p2->add(h, "aa"); });
-        pp_unfolded_fold0[i]->apply([&](TH1* h) { p1->stack(h, "pp"); });
-        pp_unfolded_fold1[i]->apply([&](TH1* h) { p2->stack(h, "pp"); });
+
+        // pp_0_unfolded_fold0[i]->apply([&](TH1* h) { p1->stack(h, "pp"); });
+        // pp_0_unfolded_fold1[i]->apply([&](TH1* h) { p2->stack(h, "pp"); });
+
+        pp_0_unfolded_fold0[i]->apply([&](TH1* h, int64_t index) { p1->stack(i*4 + index + 1, h, "pp"); });
+        pp_0_unfolded_fold1[i]->apply([&](TH1* h, int64_t index) { p2->stack(i*4 + index + 1, h, "pp"); });
     }
     
     hb->sketch();
