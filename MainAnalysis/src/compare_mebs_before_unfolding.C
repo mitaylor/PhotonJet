aa988850b5995bb865a83ea31535af0c81c37c05
mitaylor <mitay@mit.edu>
2023-04-12 01:49:44 -0400

    mebs: update comparisons

--- MainAnalysis/src/compare_mebs_before_unfolding.C
+++ MainAnalysis/src/compare_mebs_before_unfolding.C
@@ -1,4 +1,5 @@
 #include "../include/lambdas.h"
+#include "../include/text.h"
 
 #include "../git/config/include/configurer.h"
 
@@ -78,7 +79,7 @@ int compare_before_unfolding(char const* config, char const* selections, const c
 
     auto kinematics = [&](int64_t index) {
         if (index > 0) {
-            auto photon_selections = to_text(bpho_pt[0]) + " < p_{T}^{#gamma} < "s + to_text(bpho_pt[1]) + " GeV, |#eta^{#gamma}| < "s + to_text(photon_eta_abs)  + 
+            auto photon_selections = to_text(photon_pt_min) + " < p_{T}^{#gamma} < 200 GeV, |#eta^{#gamma}| < "s + to_text(photon_eta_abs)  + 
                 ", #Delta#phi_{j#gamma} > " + to_text(dphi_min_numerator) + "#pi/"s + to_text(dphi_min_denominator);
             auto jet_selections = "anti-k_{T} R = 0.3, |#eta^{jet}| < "s + to_text(jet_eta_abs);
 
