9d3d391d383517c089c3a9c9f5cb0414fd06c491
mitaylor <mitay@mit.edu>
2023-05-02 07:50:17 -0400

    mean: add plots of means

--- MainAnalysis/src/obnubilate_mean.C
+++ MainAnalysis/src/obnubilate_mean.C
@@ -48,7 +48,7 @@ int obnubilate(char const* config, char const* selections, char const* output) {
     auto groups = conf->get<std::vector<int32_t>>("groups");
 
     auto dhf = conf->get<std::vector<float>>("hf_diff");
-    auto dcent = conf->get<std::vector<int32_t>>("cent_diff"); std::cout << __LINE__ << std::endl;
+    auto dcent = conf->get<std::vector<int32_t>>("cent_diff");
 
     auto is_paper = conf->get<bool>("paper");
 
@@ -65,7 +65,7 @@ int obnubilate(char const* config, char const* selections, char const* output) {
     auto const photon_eta_abs = sel->get<float>("photon_eta_abs");
 
     auto bpho_pt = sel->get<std::vector<float>>("photon_pt_bounds");
-    auto bjet_pt = sel->get<std::vector<float>>("jet_pt_bounds"); std::cout << __LINE__ << std::endl;
+    auto bjet_pt = sel->get<std::vector<float>>("jet_pt_bounds");
 
     /* manage memory manually */
     TH1::AddDirectory(false);
@@ -85,7 +85,7 @@ int obnubilate(char const* config, char const* selections, char const* output) {
 
     zip([&](auto const& label, auto const& legend) {
         hb->alias(label, legend); }, legend_keys, legends);
- std::cout << __LINE__ << std::endl;
+
     /* lambdas */
     std::function<void(TH1*)> square_ = [](TH1* h) {
         for_contents([](std::array<double, 1> v) {
@@ -93,7 +93,7 @@ int obnubilate(char const* config, char const* selections, char const* output) {
     std::function<void(TH1*)> sqrt_ = [](TH1* h) {
         for_contents([](std::array<double, 1> v) {
             return std::sqrt(v[0]); }, h); };
- std::cout << __LINE__ << std::endl;
+
     auto shader = [&](TH1* h, float max) {
         default_formatter(h, 0., max);
         auto col = h->GetLineColor();
@@ -118,7 +118,7 @@ int obnubilate(char const* config, char const* selections, char const* output) {
             l->DrawLatexNDC(0.865, 0.37, jet_selections.data());
         }
     };
- std::cout << __LINE__ << std::endl;
+
     /* prepare output */
     TFile* fout = new TFile(output, "recreate");
 
@@ -126,7 +126,7 @@ int obnubilate(char const* config, char const* selections, char const* output) {
     system_tag += (tag == "aa") ? ", 1.69 nb^{-1}"s : ", 302 pb^{-1}"s;
     auto cms = "#bf{#scale[1.4]{CMS}}"s;
     if (!is_paper) cms += " #it{#scale[1.2]{Preliminary}}"s;
- std::cout << __LINE__ << std::endl;
+
     /* calculate variations */
     zip([&](auto const& figure, auto range) {
         auto stub = "_"s + figure;
@@ -136,34 +136,35 @@ int obnubilate(char const* config, char const* selections, char const* output) {
 
         auto c2 = new paper(set + "_" + tag + "_mean_var_unfolding"s + stub, hb);
         apply_style(c2, cms, system_tag, std::bind(shader, _1, range));
- std::cout << __LINE__ << std::endl;
+
         auto base = new history<TH1F>(f, tag + "_"s + label + stub, "base_"s + tag + "_"s + label + stub);
 
         std::vector<history<TH1F>*> sets;
- std::cout << __LINE__ << std::endl;
+
         std::vector<history<TH1F>*> means(inputs.size(), nullptr);
         auto incl = new interval("Centrality"s, 4, 0.f, 4.f);
         auto fmean = std::bind(&interval::book<TH1F>, incl, _1, _2, _3);
- std::cout << __LINE__ << std::endl;
+
         auto base_mean = new history<TH1F>("mean_base"s, "", fmean, 1);
         title(std::bind(rename_axis, _1, "<#deltaj>"), base_mean);
- std::cout << __LINE__ << std::endl;
+
         for (int64_t i = 0; i < base->size(); ++i) {
             (*base_mean)[0]->SetBinContent(i + 1, (*base)[i]->GetMean());
             (*base_mean)[0]->SetBinError(i + 1, (*base)[i]->GetMeanError());
         }
- std::cout << __LINE__ << std::endl;
+
         std::vector<history<TH1F>*> batches(inputs.size(), nullptr);
         zip([&](auto& batch, auto file, auto const& label) {
-            batch = new history<TH1F>(file, tag + "_" + label + stub, "batch_"s + tag + "_"s + label + stub); std::cout << __LINE__ << std::endl;
+            batch = new history<TH1F>(file, tag + "_" + label + stub, "batch_"s + tag + "_"s + label + stub);
         }, batches, files, labels);
- std::cout << __LINE__ << std::endl;
+
         auto total = new history<TH1F>(*base_mean, "total");
         title(std::bind(rename_axis, _1, "<#deltaj>"), total);
         total->apply([](TH1* h) { h->Reset("MICES"); });
  std::cout << __LINE__ << std::endl;
         for (size_t k = 0; k < batches.size(); ++k) {
             means.push_back(new history<TH1F>("mean_"s + to_text(k), "", fmean, 1)); std::cout << __LINE__ << std::endl;
+            std::cout << means.size() << " " << means[k]->size() << std::endl;
             title(std::bind(rename_axis, _1, "<#deltaj>"), means[k]);
  std::cout << __LINE__ << std::endl;
             for (int64_t i = 0; i < batches[k]->size(); ++i) {
@@ -174,7 +175,7 @@ int obnubilate(char const* config, char const* selections, char const* output) {
                 printf("%.2f ", difference);
             }
             std::cout << std::endl;
- std::cout << __LINE__ << std::endl;
+
             means[k]->add(*base_mean, -1);
             means[k]->apply(square_);
         }
@@ -191,27 +192,27 @@ int obnubilate(char const* config, char const* selections, char const* output) {
 
             mean->apply(sqrt_);
         }, means, groups);
- std::cout << __LINE__ << std::endl;
+
         for (auto const& set : sets)
             total->add(*set, 1);
- std::cout << __LINE__ << std::endl;
+
         total->apply(sqrt_);
- std::cout << __LINE__ << std::endl;
+
         for (int64_t i = 0; i < total->size(); ++i) {
             float difference = (*total)[0]->GetBinContent(i + 1) - (*base_mean)[0]->GetBinContent(i + 1);
                 printf("%.2f ", difference);
         }
         std::cout << std::endl;
- std::cout << __LINE__ << std::endl;
+
         /* add plots */
         auto style1 = [&](TH1* h) { c1->adjust(h, "hist", "f"); };
         auto style2 = [&](TH1* h) { c2->adjust(h, "hist", "f"); };
- std::cout << __LINE__ << std::endl;
+
         total->apply([&](TH1* h) { 
             c1->add(h, "total"); style1(h); 
             c2->add(h, "total"); style2(h); 
         });
- std::cout << __LINE__ << std::endl;
+
         zip([&](auto& mean, auto const& label, auto const& plot) {
             if (plot == 1) {
                 mean->apply([&](TH1* h, int64_t index) {
@@ -226,26 +227,26 @@ int obnubilate(char const* config, char const* selections, char const* output) {
 
             mean->save(tag);
         }, means, legend_keys, plots);
- std::cout << __LINE__ << std::endl;
+
         /* add info text */
         c1->accessory(hf_info); 
         c1->accessory(kinematics);
         c2->accessory(hf_info); 
         c2->accessory(kinematics);
- std::cout << __LINE__ << std::endl;
+
         /* save histograms */
         for (auto const& set : sets)
             set->save(tag);
- std::cout << __LINE__ << std::endl;
+
         base->save(tag);
         total->save(tag);
- std::cout << __LINE__ << std::endl;
+
         hb->sketch();
 
         c1->draw("pdf");
         c2->draw("pdf");
     }, figures, ranges);
- std::cout << __LINE__ << std::endl;
+
     fout->Close();
 
     return 0;
