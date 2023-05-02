39c4a6005780fe9046250b961eea0714f2124da8
mitaylor <mitay@mit.edu>
2023-05-02 07:36:38 -0400

    mean: add plots of means

--- MainAnalysis/scripts/EndingMean.sh
+++ MainAnalysis/scripts/EndingMean.sh
@@ -5,8 +5,8 @@ set=$1
 
 run_pp() {
     tag=$1
-    ./bin/obnubilate configs/obnubilate/preapproval/obnubilate_${tag}.conf \
-        configs/analysis/${set}/analysis_pp.conf data/preapproval/${set}/obnubilate_${tag}.root
+    ./bin/obnubilate_mean configs/obnubilate/preapproval/obnubilate_mean_${tag}.conf \
+        configs/analysis/${set}/analysis_pp.conf data/preapproval/${set}/obnubilate_mean_${tag}.root
 }
 
 samples=(pp aa pp_smear_0_10 pp_smear_10_30 pp_smear_30_50 pp_smear_50_90)
