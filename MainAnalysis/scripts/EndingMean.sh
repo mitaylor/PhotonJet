6c149d3a94a79c9f649bc42987c94c174f460914
mitaylor <mitay@mit.edu>
2023-05-11 08:04:02 -0400

    obnubilate: improve plotting

--- MainAnalysis/scripts/EndingMean.sh
+++ MainAnalysis/scripts/EndingMean.sh
@@ -15,6 +15,6 @@ for sample in ${samples[@]}; do
     run_pp ${sample}
 done
 
-# ./bin/congratulate configs/congratulate/preapproval/congratulate.conf configs/analysis/${set}/analysis_pp.conf temp.root
+./bin/congratulate_mean configs/congratulate/preapproval/congratulate_mean.conf configs/analysis/${set}/analysis_pp.conf temp.root
 # ./bin/congratulate configs/congratulate/preapproval/congratulate_smeared.conf configs/analysis/${set}/analysis_pp.conf temp.root
 # ./bin/ratio configs/congratulate/preapproval/ratio.conf configs/analysis/${set}/analysis_pp.conf temp.root
--- MainAnalysis/src/congratulate_mean.C
+++ MainAnalysis/src/congratulate_mean.C
@@ -45,15 +45,6 @@ int congratulate(char const* config, char const* selections, char const* output)
 
     auto smeared = conf->get<bool>("smeared");
 
-    auto xmins = conf->get<std::vector<float>>("xmin");
-    auto xmaxs = conf->get<std::vector<float>>("xmax");
-    auto ymins = conf->get<std::vector<float>>("ymin");
-    auto ymaxs = conf->get<std::vector<float>>("ymax");
-    auto oflows = conf->get<std::vector<bool>>("oflow");
-
-    auto dhf = conf->get<std::vector<float>>("hf_diff");
-    auto dcent = conf->get<std::vector<int32_t>>("cent_diff");
-
     auto sel = new configurer(selections);
 
     auto set = sel->get<std::string>("set");
@@ -82,28 +73,12 @@ int congratulate(char const* config, char const* selections, char const* output)
         file = new TFile((base + input).data(), "read");
     }, files, inputs);
 
-    /* load histograms */
-    std::vector<std::string> base_stubs(6);
-    std::vector<std::string> syst_stubs(6);
-
-    zip([&](auto& base, auto& syst, auto const& tag) {
-        base = tag + "_base_" + tag + "_nominal_s_pure_raw_sub_";
-        syst = tag + "_total_base_" + tag + "_nominal_s_pure_raw_sub_";
-    }, base_stubs, syst_stubs, tags);
-
     /* prepare plots */
     auto hb = new pencil();
-    hb->category("system", "pp", "aa", "ss");
+    hb->category("system", "pp", "aa");
 
     hb->alias("aa", "PbPb");
-    if (smeared) hb->alias("ss", "pp (smeared)");
-    else hb->alias("ss", "pp");
-
-    std::function<void(int64_t, float)> hf_info = [&](int64_t x, float pos) {
-        info_text(x, pos, "Cent. %i - %i%%", dcent, true); };
-
-    auto aa_info = [&](int64_t index, history<TH1F>* h) {
-        stack_text(index, 0.73, 0.04, h, hf_info); };
+    if (smeared) hb->alias("pp", "pp (smeared)");
 
     auto kinematics = [&](int64_t index) {
         if (index > 0) {
@@ -120,24 +95,35 @@ int congratulate(char const* config, char const* selections, char const* output)
         }
     };
 
-    zip([&](auto const& figure, auto xmin, auto xmax, auto ymin, auto ymax,
-            auto integral) {
+    zip([&](auto const& figure) {
         /* get histograms */
-        std::vector<history<TH1F>*> hists(6, nullptr);
-        std::vector<history<TH1F>*> systs(6, nullptr);
+        std::vector<history<TH1F>*> hist_inputs(5, nullptr);
+        std::vector<history<TH1F>*> syst_inputs(5, nullptr);
 
-        zip([&](auto& hist, auto& syst, auto const file,
-                auto const& base_stub, auto const& syst_stub) {
-            hist = new history<TH1F>(file, base_stub + figure);
-            title(std::bind(rename_axis, _1, "1/N^{#gammaj}dN/d#deltaj"), hist);
-            syst = new history<TH1F>(file, syst_stub + figure);
-        }, hists, systs, files, base_stubs, syst_stubs);
+        zip([&](auto& hist_input, auto& syst_input, auto const file,
+                auto const& tag) {
+            hist_input = new history<TH1F>(file, tag + "_base_mean");
+            syst_input = new history<TH1F>(file, tag + "syst_mean");
+        }, hist_inputs, syst_inputs, files, tagss);
 
-        for (size_t i = 2; i < files.size(); ++i) {
-            std::string name1 = std::to_string(i) + std::string("happy");
-            std::string name2 = std::to_string(i) + std::string("sad");
-            hists[i]->rename(name1);
-            systs[i]->rename(name2);
+        std::vector<history<TH1F>*> hists(2, nullptr);
+        std::vector<history<TH1F>*> systs(2, nullptr);
+
+        auto incl = new interval(""s, 9, 0.f, 9.f);
+        auto fincl = std::bind(&interval::book<TH1F>, incl, _1, _2, _3);
+
+        for (size_t i = 0; i < hists.size(); ++i) {
+            auto hists[i] = new history<TH1F>("hist"s + to_text(i), "", fincl, 1);
+            auto systs[i] = new history<TH1F>("syst"s + to_text(i), "", fincl, 1);
+
+            title(std::bind(rename_axis, _1, "<#deltaj>"), hists[i]);
+
+            for (int64_t j = 0; j < (*hists[i])[0]->GetNbinsX(); ++j) {
+                (*hists[i])[0]->SetBinContent((j + 1)*2, (*hist_inputs[i])[j]->GetBinContent(1));
+                (*hists[i])[0]->SetBinError((j + 1)*2, (*hist_inputs[i])[j]->GetBinError(1));
+
+                (*systs[i])[0]->SetBinContent((j + 1)*2, (*syst_inputs[i])[j]->GetBinContent(1));
+            }
         }
 
         /* link histograms, uncertainties */
@@ -173,42 +159,15 @@ int congratulate(char const* config, char const* selections, char const* output)
             }
         };
 
-        /* minor adjustments */
-        if (integral) { xmin = convert_pi(xmin); xmax = convert_pi(xmax); }
-
         /* prepare papers */
-        auto p = new paper(set + "_" + prefix + "_results_pp_" + figure, hb);
-        apply_style(p, "#bf{#scale[1.4]{CMS}}     #sqrt{s} = 5.02 TeV"s, "pp 302 pb^{-1}"s, ymin, ymax);
-        p->accessory(std::bind(line_at, _1, 0.f, xmin, xmax));
-        p->accessory(kinematics);
-        p->jewellery(box);
-        p->divide(-1, 1);
-
-        auto a = new paper(set + "_" + prefix+ "_results_aa_" + figure, hb);
-        apply_style(a, "#bf{#scale[1.4]{CMS}}     #sqrt{s_{NN}} = 5.02 TeV"s, "PbPb 1.6 nb^{-1}"s, ymin, ymax);
-        a->accessory(std::bind(line_at, _1, 0.f, xmin, xmax));
-        a->accessory(std::bind(aa_info, _1, hists[0]));
-        a->accessory(kinematics);
-        a->jewellery(box);
-        a->divide(ihf->size()/2, -1);
-
         auto s = new paper(set + "_" + prefix + "_results_ss_" + figure, hb);
-        apply_style(s, "#bf{#scale[1.4]{CMS}}     #sqrt{s_{NN}} = 5.02 TeV"s, "PbPb 1.69 nb^{-1}, pp 302 pb^{-1}"s, ymin, ymax);
-        s->accessory(std::bind(line_at, _1, 0.f, xmin, xmax));
-        s->accessory(std::bind(aa_info, _1, hists[0]));
+        apply_style(s, "#bf{#scale[1.4]{CMS}}     #sqrt{s_{NN}} = 5.02 TeV"s, "PbPb 1.69 nb^{-1}, pp 302 pb^{-1}"s, 0, 0.1);
         s->accessory(kinematics);
         s->jewellery(box);
-        s->divide(ihf->size()/2, -1);
 
         /* draw histograms with uncertainties */
-        hists[0]->apply([&](TH1* h) { a->add(h, "aa"); s->add(h, "aa"); });
-        hists[1]->apply([&](TH1* h) { p->add(h, "pp"); });
-
-        for (int64_t i = 0; i < 4; ++i) {
-            hists[i + 2]->apply([&](TH1* h, int64_t index) {
-                s->stack(i + index + 1, h, "ss");
-            });
-        }
+        s->add((*hists[0])[0], "aa");
+        s->stack((*hists[1])[0], "pp");
 
         auto pp_style = [](TH1* h) {
             h->SetLineColor(1);
@@ -224,13 +183,10 @@ int congratulate(char const* config, char const* selections, char const* output)
 
         hb->style("pp", pp_style);
         hb->style("aa", aa_style);
-        hb->style("ss", pp_style);
         hb->sketch();
-
-        p->draw("pdf");
-        a->draw("pdf");
+        
         s->draw("pdf");
-    }, figures, xmins, xmaxs, ymins, ymaxs, oflows);
+    }, figures);
 
     in(output, []() {});
 
