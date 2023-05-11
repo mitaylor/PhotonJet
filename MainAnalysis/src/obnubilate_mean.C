8ae1f3387c6cf5fa2cccf12c961cd50a2348659c
mitaylor <mitay@mit.edu>
2023-05-11 06:51:15 -0400

    obnubilate: improve plotting

--- MainAnalysis/src/obnubilate_mean.C
+++ MainAnalysis/src/obnubilate_mean.C
@@ -160,7 +160,7 @@ int obnubilate(char const* config, char const* selections, char const* output) {
         auto incl = new interval(""s, inputs.size() + 1, 0.f, (float) inputs.size() + 1);
         auto fmean = std::bind(&interval::book<TH1F>, incl, _1, _2, _3);
 
-        auto means = new history<TH1F>("means"s, "", fmean, base->size());
+        auto means = new history<TH1F>("total_base_"s + tag + "_"s + label + "_" + figure, "", fmean, base->size());
         auto base_mean = new history<TH1F>("mean_base"s, "", fmean, base->size());
 
         for (int64_t i = 0; i < base->size(); ++i) {
