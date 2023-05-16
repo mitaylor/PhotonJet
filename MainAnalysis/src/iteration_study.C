b6b13cb6e3cb4e9374a201de7ba35346839c908e
mitaylor <mitay@mit.edu>
2023-05-16 07:47:33 -0400

    quantitate: compare ideal iteration to nonideal

--- MainAnalysis/src/iteration_study.C
+++ MainAnalysis/src/iteration_study.C
@@ -380,7 +380,7 @@ int quantitate(char const* config, char const* selections, char const* output) {
             auto photon_selections = to_text(bpho_pt[0]) + " < p_{T}^{#gamma} < "s + to_text(bpho_pt[1]) + " GeV, |#eta^{#gamma}| < "s + to_text(photon_eta_abs)  + 
                 ", #Delta#phi_{j#gamma} > " + to_text(dphi_min_numerator) + "#pi/"s + to_text(dphi_min_denominator);
             auto jet_selections = "anti-k_{T} R = 0.3, "s + to_text(bjet_pt[0]) + "p_{T}^{jet} < "s + to_text(bjet_pt[1]) + " GeV, |#eta^{jet}| < "s + to_text(jet_eta_abs);
-            auto iterations = to_text(choice_minus[index - 1]) + ", "s + to_text(choice_nominal[index - 1]) + ", "s + to_text(choice_plus[index - 1]);
+            auto iterations = "iterations: "s + to_text(choice_minus[index - 1]) + ", "s + to_text(choice_nominal[index - 1]) + ", "s + to_text(choice_plus[index - 1]);
 
             TLatex* l = new TLatex();
             l->SetTextAlign(31);
