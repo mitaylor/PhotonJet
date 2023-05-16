014c24faf0a1d46dfedb1aeec86d45f503d8d046
mitaylor <mitay@mit.edu>
2023-05-16 07:38:05 -0400

    quantitate: compare ideal iteration to nonideal

--- MainAnalysis/src/iteration_study.C
+++ MainAnalysis/src/iteration_study.C
@@ -172,15 +172,29 @@ int quantitate(char const* config, char const* selections, char const* output) {
     auto afters = conf->get<std::vector<std::string>>("afters");
     auto regularization = conf->get<std::string>("regularization");
 
+    auto dcent = conf->get<std::vector<int32_t>>("cent_diff");
+
     auto sel = new configurer(selections);
 
     auto set = sel->get<std::string>("set");
     auto base = sel->get<std::string>("base");
 
+    auto heavyion = sel->get<bool>("heavyion");
+
     auto rdrg = sel->get<std::vector<float>>("drg_range");
     auto rptg = sel->get<std::vector<float>>("ptg_range");
 
     auto osg = sel->get<std::vector<int64_t>>("osg");
+
+    auto const dphi_min_numerator = sel->get<float>("dphi_min_numerator");
+    auto const dphi_min_denominator = sel->get<float>("dphi_min_denominator");
+
+    auto const jet_eta_abs = sel->get<float>("jet_eta_abs");
+
+    auto const photon_eta_abs = sel->get<float>("photon_eta_abs");
+
+    auto bdr = sel->get<std::vector<float>>("dr_bounds");
+    auto bpho_pt = sel->get<std::vector<float>>("photon_pt_bounds");
 
     /* create intervals and multivals */
     auto idrg = new interval("#deltaj"s, rdrg);
@@ -204,9 +218,17 @@ int quantitate(char const* config, char const* selections, char const* output) {
     TFile* fout = new TFile(output, "recreate");
 
     /* prepare the post-unfolded data */
-    auto unfolded = new history<TH1F>("unfolded", "", null<TH1F>, (int64_t) afters.size());
-    auto unfolded_fold0 = new history<TH1F>("unfolded_fold0", "", null<TH1F>, (int64_t) afters.size());
-    auto unfolded_fold1 = new history<TH1F>("unfolded_fold1", "", null<TH1F>, (int64_t) afters.size());
+    auto unfolded_nominal = new history<TH1F>("unfolded_nominal", "", null<TH1F>, (int64_t) afters.size());
+    auto unfolded_nominal_fold0 = new history<TH1F>("unfolded_nominal_fold0", "", null<TH1F>, (int64_t) afters.size());
+    auto unfolded_nominal_fold1 = new history<TH1F>("unfolded_nominal_fold1", "", null<TH1F>, (int64_t) afters.size());
+
+    auto unfolded_plus = new history<TH1F>("unfolded_plus", "", null<TH1F>, (int64_t) afters.size());
+    auto unfolded_plus_fold0 = new history<TH1F>("unfolded_plus_fold0", "", null<TH1F>, (int64_t) afters.size());
+    auto unfolded_plus_fold1 = new history<TH1F>("unfolded_plus_fold1", "", null<TH1F>, (int64_t) afters.size());
+
+    auto unfolded_minus = new history<TH1F>("unfolded_minus", "", null<TH1F>, (int64_t) afters.size());
+    auto unfolded_minus_fold0 = new history<TH1F>("unfolded_minus_fold0", "", null<TH1F>, (int64_t) afters.size());
+    auto unfolded_minus_fold1 = new history<TH1F>("unfolded_minus_fold1", "", null<TH1F>, (int64_t) afters.size());
 
     /* determine the number of iterations to use */
     std::vector<float> min_nominal(chi_square->size(), 1);
@@ -291,22 +313,101 @@ int quantitate(char const* config, char const* selections, char const* output) {
         auto HUnfoldedBayes = (TH1F*) fafters[j]->Get(unfold_name.data());
         auto MUnfolded = (TMatrixT<double>*) fafters[j]->Get(matrix_name.data());
 
-        (*unfolded)[j] = HUnfoldedBayes;
-        (*unfolded_fold0)[j] = fold_mat((*unfolded)[j], MUnfolded, mg, 0, osg);
-        (*unfolded_fold1)[j] = fold_mat((*unfolded)[j], MUnfolded, mg, 1, osg);
+        (*unfolded_nominal)[j] = HUnfoldedBayes;
+        (*unfolded_nominal_fold0)[j] = fold_mat((*unfolded_nominal)[j], MUnfolded, mg, 0, osg);
+        (*unfolded_nominal_fold1)[j] = fold_mat((*unfolded_nominal)[j], MUnfolded, mg, 1, osg);
     }
 
-    normalise_to_unity(unfolded_fold0, unfolded_fold1);
+    for (size_t j = 0; j < fafters.size(); ++j) {
+        std::string unfold_name = "HUnfoldedBayes" + std::to_string(choice_plus[j]);
+        std::string matrix_name = "MUnfoldedBayes" + std::to_string(choice_plus[j]);
 
-    unfolded->rename(tag + "_"s + label + "_raw_sub_pjet_u_dr_sum0_unfolded"s);
-    unfolded_fold0->rename(tag + "_"s + label + "_raw_sub_pjet_u_dr_sum0_unfolded_fold0"s);
-    unfolded_fold1->rename(tag + "_"s + label + "_raw_sub_pjet_u_dr_sum0_unfolded_fold1"s);
+        auto HUnfoldedBayes = (TH1F*) fafters[j]->Get(unfold_name.data());
+        auto MUnfolded = (TMatrixT<double>*) fafters[j]->Get(matrix_name.data());
 
-    unfolded->save();
-    unfolded_fold0->save();
-    unfolded_fold1->save();
+        (*unfolded_plus)[j] = HUnfoldedBayes;
+        (*unfolded_plus_fold0)[j] = fold_mat((*unfolded_plus)[j], MUnfolded, mg, 0, osg);
+        (*unfolded_plus_fold1)[j] = fold_mat((*unfolded_plus)[j], MUnfolded, mg, 1, osg);
+    }
+
+    for (size_t j = 0; j < fafters.size(); ++j) {
+        std::string unfold_name = "HUnfoldedBayes" + std::to_string(choice_minus[j]);
+        std::string matrix_name = "MUnfoldedBayes" + std::to_string(choice_minus[j]);
+
+        auto HUnfoldedBayes = (TH1F*) fafters[j]->Get(unfold_name.data());
+        auto MUnfolded = (TMatrixT<double>*) fafters[j]->Get(matrix_name.data());
+
+        (*unfolded_minus)[j] = HUnfoldedBayes;
+        (*unfolded_minus_fold0)[j] = fold_mat((*unfolded_minus)[j], MUnfolded, mg, 0, osg);
+        (*unfolded_minus_fold1)[j] = fold_mat((*unfolded_minus)[j], MUnfolded, mg, 1, osg);
+    }
+
+    normalise_to_unity(unfolded_nominal_fold0, unfolded_nominal_fold1, unfolded_plus_fold0, unfolded_plus_fold1, unfolded_minus_fold0, unfolded_minus_fold1);
+
+    unfolded_nominal->rename(tag + "_"s + label + "_raw_sub_pjet_u_dr_sum0_unfolded_nominal"s);
+    unfolded_nominal_fold0->rename(tag + "_"s + label + "_raw_sub_pjet_u_dr_sum0_unfolded_nominal_fold0"s);
+    unfolded_nominal_fold1->rename(tag + "_"s + label + "_raw_sub_pjet_u_dr_sum0_unfolded_nominal_fold1"s);
+
+    unfolded_plus->rename(tag + "_"s + label + "_raw_sub_pjet_u_dr_sum0_unfolded_plus"s);
+    unfolded_plus_fold0->rename(tag + "_"s + label + "_raw_sub_pjet_u_dr_sum0_unfolded_plus_fold0"s);
+    unfolded_plus_fold1->rename(tag + "_"s + label + "_raw_sub_pjet_u_dr_sum0_unfolded_plus_fold1"s);
+
+    unfolded_minus->rename(tag + "_"s + label + "_raw_sub_pjet_u_dr_sum0_unfolded_minus"s);
+    unfolded_minus_fold0->rename(tag + "_"s + label + "_raw_sub_pjet_u_dr_sum0_unfolded_minus_fold0"s);
+    unfolded_minus_fold1->rename(tag + "_"s + label + "_raw_sub_pjet_u_dr_sum0_unfolded_minus_fold1"s);
+
+    unfolded_nominal->save();
+    unfolded_nominal_fold0->save();
+    unfolded_nominal_fold1->save();
 
     fout->Close();
+
+    /* iteration comparison plot */
+    auto system_tag = "  #sqrt{s_{NN}} = 5.02 TeV"s;
+    system_tag += (heavyion) ? ", 1.69 nb^{-1}"s : ", 302 pb^{-1}"s;
+    auto cms = "#bf{#scale[1.4]{CMS}} #it{#scale[1.2]{Preliminary}}"s;
+
+    auto hf_info = [&](int64_t index) {
+        info_text(index, 0.75, "Cent. %i - %i%%", dcent, true); };
+
+    auto kinematics = [&](int64_t index) {
+        if (index > 0) {
+            auto photon_selections = to_text(bpho_pt[0]) + " < p_{T}^{#gamma} < "s + to_text(bpho_pt[1]) + " GeV, |#eta^{#gamma}| < "s + to_text(photon_eta_abs)  + 
+                ", #Delta#phi_{j#gamma} > " + to_text(dphi_min_numerator) + "#pi/"s + to_text(dphi_min_denominator);
+            auto jet_selections = "anti-k_{T} R = 0.3, " + to_text(bjet_pt[0]) + "p_{T}^{jet} < "s + to_text(bjet_pt[1]) + " GeV, |#eta^{jet}| < "s + to_text(jet_eta_abs);
+
+            TLatex* l = new TLatex();
+            l->SetTextAlign(31);
+            l->SetTextFont(43);
+            l->SetTextSize(13);
+            l->DrawLatexNDC(0.865, 0.71, photon_selections.data());
+            l->DrawLatexNDC(0.865, 0.65, jet_selections.data());
+        }
+    };
+
+    auto hb = new pencil();
+    hb->category("type", "nominal", "plus", "minus");
+
+    hb->alias("plus", "increased iterations");
+    hb->alias("minus", "decreased iterations");
+
+    auto p1 = new paper(set + "_" + label + "_iteration_comparison", hb);
+    p1->divide(ihf->size(), -1);
+    p1->accessory(hf_info);
+    p1->accessory(kinematics);
+    apply_style(p1, cms, system_tag, -2., 27.);
+    p1->accessory(std::bind(line_at, _1, 0.f, bdr[0], bdr[1]));
+
+    unfolded_nominal_fold0->apply([&](TH1* h) { p1->add(h, "nominal"); });
+
+    for (int64_t i = 0; i < chi_square->size(); ++i) {
+        if (choice_plus[i] != choice_nominal[i]) p1->stack(i + 1, (*unfolded_plus_fold0)[i], "plus");
+        if (choice_minus[i] != choice_nominal[i]) p1->stack(i + 1, (*unfolded_minus_fold0)[i], "minus");
+    }
+
+    hb->sketch();
+
+    p1->draw("pdf");
 
     return 0;
 }
