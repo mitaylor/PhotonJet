edf210ee9af64ee4c7935deb2d5d62e52d93b940
mitaylor <mitay@mit.edu>
2023-06-08 16:45:04 -0400

    photon_pt_spectrum: fix bug in taking the ratio

--- MainAnalysis/src/compare_photon_pt_spectrum_accumulate.C
+++ MainAnalysis/src/compare_photon_pt_spectrum_accumulate.C
@@ -92,7 +92,7 @@ int compare_photon_pt_spectrum(char const* config, char const* selections, const
 
     auto photon_pt_ratio = new history<TH1F>("photon_pt_ratio"s, "", fpt, ihf->size());
     for (int64_t i = 0; i < ihf->size(); ++i) {
-        (*rho_ratio)[i]->Divide((*photon_pt_data)[i], (*photon_pt_mc)[i]);
+        (*photon_pt_ratio)[i]->Divide((*photon_pt_data)[i], (*photon_pt_mc)[i]);
     }
 
     photon_pt_ratio->apply([&](TH1* h) { h->Fit("expo"); });
