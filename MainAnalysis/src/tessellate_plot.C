f9c7d96ed3d6b7cabe34cb1b17b2f3038592d48f
mitaylor <mitay@mit.edu>
2023-06-19 17:06:14 -0400

    tessellate: plot signal and data

---
+++ MainAnalysis/src/tessellate_plot.C
@@ -0,0 +1,113 @@
+#include "../include/lambdas.h"
+#include "../include/specifics.h"
+#include "../include/text.h"
+
+#include "../git/config/include/configurer.h"
+
+#include "../git/history/include/interval.h"
+#include "../git/history/include/multival.h"
+#include "../git/history/include/history.h"
+
+#include "../git/paper-and-pencil/include/paper.h"
+#include "../git/paper-and-pencil/include/pencil.h"
+
+#include "../git/tricks-and-treats/include/overflow_angles.h"
+#include "../git/tricks-and-treats/include/trunk.h"
+
+#include "TFile.h"
+#include "TH1.h"
+
+using namespace std::literals::string_literals;
+using namespace std::placeholders;
+
+int jubilate(char const* config, char const* selections, char const* output) {
+    auto conf = new configurer(config);
+
+    auto input = conf->get<std::string>("input");
+    auto system = conf->get<std::string>("system");
+    auto tag = conf->get<std::string>("tag");
+
+    auto dhf = conf->get<std::vector<float>>("hf_diff");
+
+    auto dcent = conf->get<std::vector<int32_t>>("cent_diff");
+
+    /* selections */
+    auto sel = new configurer(selections);
+
+    auto set = sel->get<std::string>("set");
+    auto base = sel->get<std::string>("base");
+
+    auto const dphi_min_numerator = sel->get<float>("dphi_min_numerator");
+    auto const dphi_min_denominator = sel->get<float>("dphi_min_denominator");
+
+    auto const jet_pt_min = sel->get<float>("jet_pt_min");
+    auto const jet_eta_abs = sel->get<float>("jet_eta_abs");
+
+    auto const photon_eta_abs = sel->get<float>("photon_eta_abs");
+
+    auto dpt = sel->get<std::vector<float>>("photon_pt_diff");
+
+    auto ihf = new interval(dhf);
+
+    /* load history objects */
+    TFile* f = new TFile((base + input).data(), "read");
+
+    TH1::SetDefaultSumw2();
+
+    history<TH1F>* see_data;
+    history<TH1F>* see_sig;
+
+    see_data = new history<TH1F>(f, tag + "_see_data");
+    see_sig = new history<TH1F>(f, tag + "_see_sig");
+
+    /* draw figures */
+    std::function<void(int64_t, float)> pt_info = [&](int64_t x, float pos) {
+        info_text(x, pos, "%.0f < p_{T}^{#gamma} < %.0f", dpt, false); };
+
+    std::function<void(int64_t, float)> hf_info = [&](int64_t x, float pos) {
+        info_text(x, pos, "Cent. %i - %i%%", dcent, true); };
+
+    auto pthf_info = [&](int64_t index) {
+        stack_text(index, 0.75, 0.04, nevt, pt_info, hf_info); };
+
+    auto hb = new pencil();
+    hb->category("system", "pp", "PbPb");
+    hb->category("type", "MC", "data");
+
+    hb->set_binary("type");
+
+    auto system_tag = system + "  #sqrt{s_{NN}} = 5.02 TeV, 1.69 nb^{-1}"s;
+    auto cms = "#bf{#scale[1.4]{CMS}} #it{#scale[1.2]{Preliminary}}"s;
+    cms += "                  anti-k_{T} R = 0.3, p_{T}^{jet} > "s + to_text(jet_pt_min) + " GeV, |#eta^{jet}| < "s + to_text(jet_eta_abs) + ", ";
+    cms += "|#eta^{#gamma}| < "s + to_text(photon_eta_abs) + ", #Delta#phi_{j#gamma} > " + to_text(dphi_min_numerator) + "#pi/"s + to_text(dphi_min_denominator);
+
+    auto c1 = new paper(set + "_" + tag + "_tessellate_templates", hb);
+    apply_style(c1, cms, system_tag, -1., 24.);
+    c1->accessory(pthf_info);
+    c1->divide(-1 , ihf->size());
+
+    for (int64_t i = 0; i < see_data->size(); ++i) {
+        (*see_data)[i]->Scale(1. / (*see_data)[i]->Integral());
+        (*see_sig)[i]->Scale(1. / (*see_sig)[i]->Integral());
+
+        c1->add((*see_data)[i], system, "data");
+        c1->stack((*see_sig)[i], system, "MC");
+    }
+
+    hb->sketch();
+
+    c1->draw("pdf");
+
+    /* save output */
+    in(output, []() {});
+
+    return 0;
+}
+
+int main(int argc, char* argv[]) {
+    if (argc == 4)
+        return jubilate(argv[1], argv[2], argv[3]);
+
+    printf("usage: %s [config] [selections] [output]\n", argv[0]);
+    return 1;
+}
