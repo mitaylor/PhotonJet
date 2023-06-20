b23365aa9d7e8a66bfc8cbd0a44786eec5580783
mitaylor <mitay@mit.edu>
2023-06-20 01:01:06 -0400

    abcd: alternate purity calculation method

---
+++ MainAnalysis/scripts/ABCD.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+
+set=$1
+
+run_pp() {
+    tag=$1
+
+    echo "nohup ./bin/abcd configs/tessellate/preapproval/tessellate_${tag}.conf configs/analysis/${set}/analysis_pp.conf data/preapproval/${set}/abcd_${tag}.root > log/${set}_abcd_${tag}.txt 2>&1 &"
+}
+
+run_aa() {
+    tag=$1
+
+    echo "nohup ./bin/abcd configs/tessellate/preapproval/tessellate_${tag}.conf configs/analysis/${set}/analysis_aa.conf data/preapproval/${set}/abcd_${tag}.root > log/${set}_abcd_${tag}.txt 2>&1 &"
+}
+
+samples=(pp)
+
+for sample in ${samples[@]}; do
+    run_pp ${sample}
+
+    for syst in loose tight qcd; do
+        run_pp ${sample}_${syst}
+    done
+done
+
+samples=(aa)
+
+for sample in ${samples[@]}; do
+    run_aa ${sample}
+
+    for syst in loose tight qcd cent_up cent_down; do
+        run_aa ${sample}_${syst}
+    done
+done
