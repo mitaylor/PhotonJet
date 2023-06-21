013e3d614883ff5276985e2a8df80e1ccda22c98
mitaylor <mitay@mit.edu>
2023-06-21 16:41:35 -0400

    photon_pt_spectrum: calculate from data

---
+++ MainAnalysis/scripts/Spectrum.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+set=$1
+
+run_pp() {
+    tag=$1
+
+    ./bin/photon_pt_spectrum configs/populate/preapproval-parallel/populate_${tag}_raw.conf configs/analysis/${set}/analysis_pp.conf /data/submit/mitay/spectrum/${set}/populate_${tag}_raw.root > log/${set}_spectrum_populate_${tag}_raw.txt 2>&1 &
+}
+
+samples=(pp)
+
+for sample in ${samples[@]}; do
+    run_pp ${sample}
+
+    for syst in qcd; do
+        run_pp ${sample}_${syst}
+    done
+done
