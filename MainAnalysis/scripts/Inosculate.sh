4488861534d27f59a63375778bdd6ef46ae1b6d4
mitaylor <mitay@mit.edu>
2023-06-27 16:14:44 -0400

    inosculate_ele: debug

--- MainAnalysis/scripts/Inosculate.sh
+++ MainAnalysis/scripts/Inosculate.sh
@@ -5,14 +5,14 @@ set=$1
 run_pp() {
     tag=$1
 
-    ./bin/inosculate configs/inosculate/preapproval/inosculate_${tag}.conf configs/analysis/${set}/analysis_pp.conf data/preapproval/${set}/inosculate_${tag}.root > log/${set}_inosculate_${tag}.txt 2>&1 &
+    # ./bin/inosculate configs/inosculate/preapproval/inosculate_${tag}.conf configs/analysis/${set}/analysis_pp.conf data/preapproval/${set}/inosculate_${tag}.root > log/${set}_inosculate_${tag}.txt 2>&1 &
     ./bin/inosculate_ele configs/inosculate/preapproval/inosculate_${tag}.conf configs/analysis/${set}/analysis_pp.conf data/preapproval/${set}/inosculate_ele_${tag}.root > log/${set}_inosculate_ele_${tag}.txt 2>&1 &
 }
 
 run_aa() {
     tag=$1
 
-    ./bin/inosculate configs/inosculate/preapproval/inosculate_${tag}.conf configs/analysis/${set}/analysis_aa.conf data/preapproval/${set}/inosculate_${tag}.root > log/${set}_inosculate_${tag}.txt 2>&1 &
+    # ./bin/inosculate configs/inosculate/preapproval/inosculate_${tag}.conf configs/analysis/${set}/analysis_aa.conf data/preapproval/${set}/inosculate_${tag}.root > log/${set}_inosculate_${tag}.txt 2>&1 &
     ./bin/inosculate_ele configs/inosculate/preapproval/inosculate_${tag}.conf configs/analysis/${set}/analysis_aa.conf data/preapproval/${set}/inosculate_ele_${tag}.root > log/${set}_inosculate_ele_${tag}.txt 2>&1 &
 }
 
