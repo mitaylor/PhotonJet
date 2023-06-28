155fd0f39f18723a84a3f7d3a5a8d106139fa6df
mitaylor <mitay@mit.edu>
2023-06-28 05:53:02 -0400

    combine_populate: add photon es systematic

--- MainAnalysis/scripts/CombinePopulate.sh
+++ MainAnalysis/scripts/CombinePopulate.sh
@@ -16,7 +16,7 @@ run_aa() {
     ./bin/combine_populate configs/combine_populate/combine_${tag}.conf configs/analysis/${set}/analysis_aa.conf data/preapproval/${set}/populate_${tag}.root
 }
 
-samples=(pp_raw pp_bkg pp_qcd_raw pp_qcd_bkg pp_qcd_gen_iso_raw pp_qcd_gen_iso_bkg pp_wo_ele_rej_raw pp_wo_ele_rej_bkg pp_qcd_er_raw pp_qcd_er_bkg pp_er_raw pp_er_bkg pp_es_up_raw pp_es_up_bkg pp_es_down_raw pp_es_down_bkg)
+samples=(pp_raw pp_bkg pp_qcd_raw pp_qcd_bkg pp_qcd_gen_iso_raw pp_qcd_gen_iso_bkg pp_wo_ele_rej_raw pp_wo_ele_rej_bkg pp_qcd_er_raw pp_qcd_er_bkg pp_er_raw pp_er_bkg pp_es_raw pp_es_bkg)
 
 for sample in ${samples[@]}; do
     run_pp ${sample}
@@ -46,7 +46,7 @@ for sample in ${samples[@]}; do
     run_pp ${sample}
 done
 
-samples=(aa_raw aa_bkg aa_qcd_raw aa_qcd_bkg aa_qcd_gen_iso_raw aa_qcd_gen_iso_bkg aa_wo_ele_rej_raw aa_wo_ele_rej_bkg aa_cent_down_raw aa_cent_down_bkg aa_cent_up_raw aa_cent_up_bkg aa_no_eff_raw aa_no_eff_bkg aa_qcd_no_er_raw aa_qcd_no_er_bkg aa_no_er_raw aa_no_er_bkg aa_es_up_raw aa_es_up_bkg aa_es_down_raw aa_es_down_bkg)
+samples=(aa_raw aa_bkg aa_qcd_raw aa_qcd_bkg aa_qcd_gen_iso_raw aa_qcd_gen_iso_bkg aa_wo_ele_rej_raw aa_wo_ele_rej_bkg aa_cent_down_raw aa_cent_down_bkg aa_cent_up_raw aa_cent_up_bkg aa_no_eff_raw aa_no_eff_bkg aa_qcd_no_er_raw aa_qcd_no_er_bkg aa_no_er_raw aa_no_er_bkg aa_es_raw aa_es_bkg)
 
 for sample in ${samples[@]}; do
     run_aa ${sample}
