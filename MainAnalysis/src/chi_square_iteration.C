59f7bf10956979f141d26866c9ab622ffe4c4a38
mitaylor <mitay@mit.edu>
2023-07-28 18:43:49 -0400

    chi_square_iteration: try different method

--- MainAnalysis/src/chi_square_iteration.C
+++ MainAnalysis/src/chi_square_iteration.C
@@ -229,7 +229,7 @@ int sum_iteration(char const* config, char const* selections, char const* output
     p1->set(paper::flags::logy);
 
     for (int64_t i = 0; i < preunfold->size(); ++i) {
-        (*sum)[i]->SetMinimum((*sum_diff)[i]->GetMinimum());
+        // (*sum)[i]->SetMinimum((*sum_diff)[i]->GetMinimum());
 
         p1->add((*sum)[i], "total");
         p1->stack((*sum_stat)[i], "stat");
