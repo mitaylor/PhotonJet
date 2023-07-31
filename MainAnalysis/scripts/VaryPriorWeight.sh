ef51c0b3e8e631526293742c433dda0f3733ccbf
mitaylor <mitay@mit.edu>
2023-07-30 23:40:09 -0400

    vary_prior: add configs for processing weighted prior inputs

---
+++ MainAnalysis/scripts/VaryPriorWeight.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+set=$1
+
+samples=(pp pp_smear_0_10 pp_smear_10_30 pp_smear_30_50 pp_smear_50_90)
+
+for sample in ${samples[@]}; do
+    ./bin/vary_prior configs/vary_prior/vary_prior_${sample}_up_weighted.conf \
+        configs/analysis/${set}/analysis_pp.conf data/preapproval/${set}/vacillate_weight_${sample}_prior_up.root
+    ./bin/vary_prior configs/vary_prior/vary_prior_${sample}_down_weighted.conf \
+        configs/analysis/${set}/analysis_pp.conf data/preapproval/${set}/vacillate_weight_${sample}_prior_down.root
+done
+
+samples=(aa)
+
+for sample in ${samples[@]}; do
+    ./bin/vary_prior configs/vary_prior/vary_prior_${sample}_up_weighted.conf \
+        configs/analysis/${set}/analysis_aa.conf data/preapproval/${set}/vacillate_weight_${sample}_prior_up.root
+    ./bin/vary_prior configs/vary_prior/vary_prior_${sample}_down_weighted.conf \
+        configs/analysis/${set}/analysis_aa.conf data/preapproval/${set}/vacillate_weight_${sample}_prior_down.root
+done
