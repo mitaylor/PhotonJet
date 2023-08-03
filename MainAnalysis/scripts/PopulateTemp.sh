c60a880a123cc7c4f017a5b76a0998e03fbfec00
mitaylor <mitay@mit.edu>
2023-08-03 05:56:31 -0400

    rerun es systematic with correct values

---
+++ MainAnalysis/scripts/PopulateTemp.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+set=$1
+
+run_pp() {
+    tag=$1
+
+    ./bin/populate configs/populate/preapproval-parallel/populate_${tag}_raw.conf configs/analysis/${set}/analysis_pp.conf /data/submit/mitay/populate/${set}/populate_${tag}_raw.root > log/${set}_populate_${tag}_raw.txt 2>&1 &
+    ./bin/populate configs/populate/preapproval-parallel/populate_${tag}_bkg.conf configs/analysis/${set}/analysis_pp.conf /data/submit/mitay/populate/${set}/populate_${tag}_bkg.root > log/${set}_populate_${tag}_bkg.txt 2>&1 &
+}
+
+samples=(pp pp_smear_0_10 pp_smear_10_30 pp_smear_30_50 pp_smear_50_90)
+
+for sample in ${samples[@]}; do
+    run_pp ${sample}
+
+    for syst in es; do
+        run_pp ${sample}_${syst}
+    done
+done
