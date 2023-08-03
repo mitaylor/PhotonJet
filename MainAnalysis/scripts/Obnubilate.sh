1f4f4791acd30899617026c8f003e35a8b3133df
mitaylor <mitay@mit.edu>
2023-08-03 19:36:24 -0400

    obnubilate: investigate problem

---
+++ MainAnalysis/scripts/Obnubilate.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+
+
+set=$1
+
+run_pp() {
+    tag=$1
+
+    ./bin/obnubilate configs/obnubilate/preapproval/obnubilate_${tag}.conf \
+        configs/analysis/${set}/analysis_pp.conf data/preapproval/${set}/obnubilate_${tag}.root
+}
+
+run_aa() {
+    tag=$1
+
+    ./bin/obnubilate configs/obnubilate/preapproval/obnubilate_${tag}.conf \
+        configs/analysis/${set}/analysis_aa.conf data/preapproval/${set}/obnubilate_${tag}.root
+}
+
+samples=(pp pp_smear_0_10 pp_smear_10_30 pp_smear_30_50 pp_smear_50_90)
+
+for sample in ${samples[@]}; do
+    run_pp ${sample}
+done
+
+samples=(aa)
+
+for sample in ${samples[@]}; do
+    run_aa ${sample}
+done
---
+++ MainAnalysis/scripts/ObnubilateMean.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+
+set=$1
+
+run_pp() {
+    tag=$1
+    ./bin/obnubilate_mean configs/obnubilate/preapproval/obnubilate_${tag}.conf \
+        configs/analysis/${set}/analysis_pp.conf data/preapproval/${set}/obnubilate_mean_${tag}.root
+}
+
+samples=(pp aa pp_smear_0_10 pp_smear_10_30 pp_smear_30_50 pp_smear_50_90)
+
+for sample in ${samples[@]}; do
+    run_pp ${sample}
+done
