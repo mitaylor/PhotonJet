f84bdf814d98af58305c0d974246bcdd7c9334a4
mitaylor <mitay@mit.edu>
2023-08-02 20:55:34 -0400

    condor: create skims for zee

--- MainAnalysis/condor/SetInputs.sh
+++ MainAnalysis/condor/SetInputs.sh
@@ -17,13 +17,13 @@
 
 # regulate_aa_zee
 ./SubmitJobs.sh \
-"/eos/cms/store/group/phys_heavyions/chenyi/PbPb2018/Forest/DYJetsToLL_MLL-50_TuneCP5_HydjetDrumMB_5p02TeV-amcatnloFXFX-pythia8/HINPbPbSpring21MiniAOD-mva98_112X_upgrade2018_realistic_HI_v9-v1/MINIAODSIM/DYJetsToLL_MLL-50_TuneCP5_HydjetDrumMB_5p02TeV-amcatnloFXFX-pythia8/20230501_ZHadronMCDY/230501_155504/" \
+"/mnt/T2_US_MIT/hadoop/cms/store/user/mitaylor/PhotonJet/DYJetsToLL_MLL-50_TuneCP5_HydjetDrumMB_5p02TeV-amcatnloFXFX-pythia8/HINPbPbSpring21MiniAOD-mva98_112X_upgrade2018_realistic_HI_v9-v1/MINIAODSIM/DYJetsToLL_MLL-50_TuneCP5_HydjetDrumMB_5p02TeV-amcatnloFXFX-pythia8/20230802PbPbZee/230802_171856/" \
 "regulate_aa_zee.conf" \
 "regulate_aa_zee"
 
 # regulate_pp_zee
 ./SubmitJobs.sh \
-"/eos/cms/store/group/phys_heavyions/chenyi/PbPb2018/Forest/DYJetsToLL_MLL-50_TuneCP5_5020GeV-amcatnloFXFX-pythia8/RunIIpp5Spring18DR-94X_mc2017_realistic_forppRef5TeV-v2/AODSIM/DYJetsToLL_MLL-50_TuneCP5_5020GeV-amcatnloFXFX-pythia8/20230408_ZHadronMLLWithMuTreePP/230408_215932/" \
+"/mnt/T2_US_MIT/hadoop/cms/store/user/mitaylor/PhotonJet/DYJetsToLL_MLL-50_TuneCP5_5020GeV-amcatnloFXFX-pythia8/RunIIpp5Spring18DR-94X_mc2017_realistic_forppRef5TeV-v2/AODSIM/DYJetsToLL_MLL-50_TuneCP5_5020GeV-amcatnloFXFX-pythia8/20230802PPZee/230802_171642/" \
 "regulate_pp_zee.conf" \
 "regulate_pp_zee"
 
