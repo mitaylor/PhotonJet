a8eeb6e15dc06f0c4e94de08db98312896dacf05
mitaylor <mitay@mit.edu>
2023-08-06 21:09:07 -0400

    theory: update implementation

--- MainAnalysis/src/theory.C
+++ MainAnalysis/src/theory.C
@@ -193,7 +193,7 @@ int theory(char const* config, char const* selections, char const* output) {
     };
 
     /* prepare papers */
-    auto p = new paper(set + "_theory_comparison_" + type + "_" + tag, hb);
+    auto p = new paper(set + "_theory_comparison_ratio_" + type + "_" + tag, hb);
     apply_style(p, "#bf{#scale[1.4]{CMS}}"s, "#sqrt{s_{NN}} = 5.02 TeV"s, ymin, ymax);
     p->accessory(std::bind(line_at, _1, 0.f, xmin, xmax));
     p->accessory(kinematics);
