e93dd640e182a62bf12b7fe2a63ff4ef9671b469
mitaylor <mitay@mit.edu>
2023-08-07 16:11:39 -0400

    remove smeared comparisons

--- MainAnalysis/scripts/EndingMean.sh
+++ MainAnalysis/scripts/EndingMean.sh
@@ -9,7 +9,7 @@ run_pp() {
         configs/analysis/${set}/analysis_pp.conf data/preapproval/${set}/obnubilate_mean_${tag}.root
 }
 
-samples=(pp aa pp_smear_0_10 pp_smear_10_30 pp_smear_30_50 pp_smear_50_90)
+samples=(pp aa) # pp_smear_0_10 pp_smear_10_30 pp_smear_30_50 pp_smear_50_90
 
 for sample in ${samples[@]}; do
     run_pp ${sample}
@@ -17,5 +17,3 @@ done
 
 ./bin/congratulate_mean configs/congratulate/preapproval/congratulate_mean.conf configs/analysis/${set}/analysis_pp.conf temp.root
 ./bin/congratulate_mean configs/congratulate/preapproval/congratulate_mean_zoom.conf configs/analysis/${set}/analysis_pp.conf temp.root
-# ./bin/congratulate configs/congratulate/preapproval/congratulate_smeared.conf configs/analysis/${set}/analysis_pp.conf temp.root
-# ./bin/ratio configs/congratulate/preapproval/ratio.conf configs/analysis/${set}/analysis_pp.conf temp.root
