ec79ca7fbfa6506bce23019d8d985e6f04d553aa
mitaylor <mitay@mit.edu>
2023-08-06 21:19:48 -0400

    theory: update implementation

--- MainAnalysis/scripts/Theory.sh
+++ MainAnalysis/scripts/Theory.sh
@@ -6,6 +6,7 @@
 
 ./bin/theory configs/theory/pho_40/theory_aa_hybrid.conf configs/analysis/pho_40/analysis_aa.conf temp.root
 ./bin/theory configs/theory/pho_40/theory_pp_hybrid.conf configs/analysis/pho_40/analysis_pp.conf temp.root
+./bin/theory_ratio configs/theory/pho_40/theory_ratio_hybrid.conf configs/analysis/pho_40/analysis_aa.conf temp.root
 
 ./bin/theory configs/theory/theory_aa.conf configs/analysis/pho_40_60/analysis_aa.conf temp.root
 ./bin/theory configs/theory/theory_pp.conf configs/analysis/pho_40_60/analysis_pp.conf temp.root
