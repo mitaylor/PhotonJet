d240b1f028870a6655d7da941872aa10b471bf9d
mitaylor <mitay@mit.edu>
2023-08-07 16:38:27 -0400

    pencil: update colors

--- MainAnalysis/scripts/ObnubilateMean.sh
+++ MainAnalysis/scripts/ObnubilateMean.sh
@@ -9,7 +9,7 @@ run_pp() {
         configs/analysis/${set}/analysis_pp.conf data/preapproval/${set}/obnubilate_mean_${tag}.root
 }
 
-samples=(pp aa pp_smear_0_10 pp_smear_10_30 pp_smear_30_50 pp_smear_50_90)
+samples=(pp aa) # pp_smear_0_10 pp_smear_10_30 pp_smear_30_50 pp_smear_50_90
 
 for sample in ${samples[@]}; do
     run_pp ${sample}
