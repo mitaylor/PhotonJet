95ef731e9259572f7c56a1b8d5cffa8d1943b36f
mitaylor <mitay@mit.edu>
2023-08-07 16:23:24 -0400

    pencil: update colors

--- MainAnalysis/git/paper-and-pencil/src/pencil.C
+++ MainAnalysis/git/paper-and-pencil/src/pencil.C
@@ -10,22 +10,22 @@ using namespace std::literals::string_literals;
 
 static std::vector<int32_t> const colours = {
     TColor::GetColor("#515151"),
-    TColor::GetColor("#f2777a"),
-    TColor::GetColor("#f99157"),
-    TColor::GetColor("#ffcc66"),
-    TColor::GetColor("#99cc99"),
-    TColor::GetColor("#6699cc"),
-    TColor::GetColor("#f005ec"),
-    TColor::GetColor("#28b09c"),
-    TColor::GetColor("#1bfa02"),
-    TColor::GetColor("#736238"),
-    TColor::GetColor("#fa0505"),
-    TColor::GetColor("#ffb600"),
-    TColor::GetColor("#99cc99"),
-    TColor::GetColor("#ffff00"),
-    TColor::GetColor("#f2777a"),
-    TColor::GetColor("#f99157"),
-    TColor::GetColor("#ffcc66")
+    TColor::GetColor("#FF484E"),
+    TColor::GetColor("#FF9E48"),
+    TColor::GetColor("#FFFA48"),
+    TColor::GetColor("#A9FF48"),
+    TColor::GetColor("#48FF9E"),
+    TColor::GetColor("#48FFFA"),
+    TColor::GetColor("#48A9FF"),
+    TColor::GetColor("#484EFF"),
+    TColor::GetColor("#9E48FF"),
+    TColor::GetColor("#F948FF"),
+    TColor::GetColor("#FF48AA"),
+    TColor::GetColor("#FF484E"),
+    TColor::GetColor("#FF9E48"),
+    TColor::GetColor("#FFFA48"),
+    TColor::GetColor("#A9FF48"),
+    TColor::GetColor("#48FF9E")
 };
 
 static std::vector<int32_t> const msolid = { 20, 21, 22, 23, 29, 41 };
