9a41b74321249fc4653c81019be2d66f95ee4bc4
mitaylor <mitay@mit.edu>
2023-08-07 09:48:43 -0400

    tesselate: scan widths

--- MainAnalysis/scripts/TessellateScan.sh
+++ MainAnalysis/scripts/TessellateScan.sh
@@ -5,7 +5,7 @@ set=$1
 run_pp() {
     tag=$1
 
-    echo "nohup ./bin/tessellate configs/tessellate/preapproval/tessellate_${tag}.conf configs/analysis/${set}/analysis_pp.conf data/preapproval/${set}/tessellate_${tag}.root > log/${set}_tessellate_${tag}.txt 2>&1 &"
+    echo "nohup ./bin/tessellate_squeeze_scan configs/tessellate/preapproval/scan/tessellate_${tag}.conf configs/analysis/${set}/analysis_pp.conf data/preapproval/${set}/tessellate_${tag}.root > log/${set}_tessellate_${tag}.txt 2>&1 &"
 }
 
 samples=(pp)
@@ -13,7 +13,7 @@ samples=(pp)
 for sample in ${samples[@]}; do
     run_pp ${sample}
 
-    for syst in p1 p5 p10 p50 n1 n5 n10 n50; do
-        run_pp ${sample}_offset_${syst}
+    for syst in pp_fine pp_80 pp_85 pp_90 pp_93 pp_95 pp_97; do
+        run_pp ${syst}
     done
 done
