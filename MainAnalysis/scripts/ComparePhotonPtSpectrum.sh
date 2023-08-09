3e1cfd026fbb80f9273b90c289d2935e638d8f46
mitaylor <mitay@mit.edu>
2023-08-09 06:51:10 -0400

    spectra: compare pho 60

--- MainAnalysis/scripts/ComparePhotonPtSpectrum.sh
+++ MainAnalysis/scripts/ComparePhotonPtSpectrum.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-# set=$1
+set=$1
 
 # run_pp() {
 #     tag=$1
@@ -33,19 +33,19 @@
 run_pp() {
     tag=$1
 
-    ./bin/compare_photon_pt_spectrum configs/compare_photon_pt_spectrum/compare_photon_pt_spectrum_pp.conf configs/analysis/pho_40/analysis_pp.conf data/preapproval/photon_pt_weight_pp.root
-    ./bin/compare_photon_pt_spectrum configs/compare_photon_pt_spectrum/compare_photon_pt_spectrum_pp_loose_purity.conf configs/analysis/pho_40/analysis_pp.conf data/preapproval/photon_pt_weight_pp_loose_purity.root
-    ./bin/compare_photon_pt_spectrum configs/compare_photon_pt_spectrum/compare_photon_pt_spectrum_pp_tight_purity.conf configs/analysis/pho_40/analysis_pp.conf data/preapproval/photon_pt_weight_pp_tight_purity.root
-    ./bin/compare_photon_pt_spectrum configs/compare_photon_pt_spectrum/compare_photon_pt_spectrum_pp_er.conf configs/analysis/pho_40/analysis_pp.conf data/preapproval/photon_pt_weight_pp_er.root
+    ./bin/compare_photon_pt_spectrum configs/compare_photon_pt_spectrum/compare_photon_pt_spectrum_pp.conf configs/analysis/${set}/analysis_pp.conf data/preapproval/${set}/photon_pt_weight_pp.root
+    ./bin/compare_photon_pt_spectrum configs/compare_photon_pt_spectrum/compare_photon_pt_spectrum_pp_loose_purity.conf configs/analysis/${set}/analysis_pp.conf data/preapproval/${set}/photon_pt_weight_pp_loose_purity.root
+    ./bin/compare_photon_pt_spectrum configs/compare_photon_pt_spectrum/compare_photon_pt_spectrum_pp_tight_purity.conf configs/analysis/${set}/analysis_pp.conf data/preapproval/${set}/photon_pt_weight_pp_tight_purity.root
+    ./bin/compare_photon_pt_spectrum configs/compare_photon_pt_spectrum/compare_photon_pt_spectrum_pp_er.conf configs/analysis/${set}/analysis_pp.conf data/preapproval/${set}/photon_pt_weight_pp_er.root
 }
 
 run_aa() {
     tag=$1
 
-    ./bin/compare_photon_pt_spectrum configs/compare_photon_pt_spectrum/compare_photon_pt_spectrum_aa.conf configs/analysis/pho_40/analysis_aa.conf data/preapproval/photon_pt_weight_aa.root
-    ./bin/compare_photon_pt_spectrum configs/compare_photon_pt_spectrum/compare_photon_pt_spectrum_aa_loose_purity.conf configs/analysis/pho_40/analysis_aa.conf data/preapproval/photon_pt_weight_aa_loose_purity.root
-    ./bin/compare_photon_pt_spectrum configs/compare_photon_pt_spectrum/compare_photon_pt_spectrum_aa_tight_purity.conf configs/analysis/pho_40/analysis_aa.conf data/preapproval/photon_pt_weight_aa_tight_purity.root
-    ./bin/compare_photon_pt_spectrum configs/compare_photon_pt_spectrum/compare_photon_pt_spectrum_aa_no_er.conf configs/analysis/pho_40/analysis_aa.conf data/preapproval/photon_pt_weight_aa_no_er.root
+    ./bin/compare_photon_pt_spectrum configs/compare_photon_pt_spectrum/compare_photon_pt_spectrum_aa.conf configs/analysis/${set}/analysis_aa.conf data/preapproval/${set}/photon_pt_weight_aa.root
+    ./bin/compare_photon_pt_spectrum configs/compare_photon_pt_spectrum/compare_photon_pt_spectrum_aa_loose_purity.conf configs/analysis/${set}/analysis_aa.conf data/preapproval/${set}/photon_pt_weight_aa_loose_purity.root
+    ./bin/compare_photon_pt_spectrum configs/compare_photon_pt_spectrum/compare_photon_pt_spectrum_aa_tight_purity.conf configs/analysis/${set}/analysis_aa.conf data/preapproval/${set}/photon_pt_weight_aa_tight_purity.root
+    ./bin/compare_photon_pt_spectrum configs/compare_photon_pt_spectrum/compare_photon_pt_spectrum_aa_no_er.conf configs/analysis/${set}/analysis_aa.conf data/preapproval/${set}/photon_pt_weight_aa_no_er.root
 }
 
 samples=(pp)
@@ -60,4 +60,4 @@ for sample in ${samples[@]}; do
     run_aa ${sample}
 done
 
-./bin/compare_photon_pt_spectrum_mc configs/compare_photon_pt_spectrum/compare_photon_pt_spectrum_mc.conf configs/analysis/pho_40/analysis_pp.conf data/preapproval/photon_pt_weight_mc.root
+./bin/compare_photon_pt_spectrum_mc configs/compare_photon_pt_spectrum/compare_photon_pt_spectrum_mc.conf configs/analysis/${set}/analysis_pp.conf data/preapproval/photon_pt_weight_mc.root
