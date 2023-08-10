560e6bc13a3ecb14598b56f720bbabf808d5ee08
mitaylor <mitay@mit.edu>
2023-08-10 17:06:55 -0400

    vacillate: add configs for running without weighting

---
+++ MainAnalysis/scripts/VacillateNoWeight.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+
+set=$1
+
+run_pp() {
+    tag=$1
+
+    echo "nohup ./bin/vacillate_dr configs/vacillate_dr/preapproval/vacillate_${tag}.conf configs/analysis/${set}/analysis_pp.conf data/preapproval/${set}/vacillate_${tag}.root > log/${set}_vacillate_${tag}.txt 2>&1 &"
+}
+
+run_aa() {
+    tag=$1
+
+    echo "nohup ./bin/vacillate_dr configs/vacillate_dr/preapproval/vacillate_${tag}.conf configs/analysis/${set}/analysis_aa.conf data/preapproval/${set}/vacillate_${tag}.root > log/${set}_vacillate_${tag}.txt 2>&1 &"
+}
+
+samples=(pp)
+
+for sample in ${samples[@]}; do
+    for syst in no_weight; do
+        run_pp ${sample}_${syst}
+    done
+done
+
+samples=(aa)
+
+for sample in ${samples[@]}; do
+    for syst in no_weight; do
+        run_aa ${sample}_${syst}
+    done
+done
