9cdd087b87b4dee087e597dec8f5b0027edcfb8f
mitaylor <mitay@mit.edu>
2023-08-11 05:59:12 -0400

    RunAll: add temp configs'

---
+++ Unfolding/UnfoldingPreparation/RunAllTemp.sh
@@ -0,0 +1,144 @@
+#!/usr/bin/env bash
+
+set=$1
+
+# AANominalPtWeight: Execute
+./Execute --Data ../../MainAnalysis/data/preapproval/${set}/accumulate_aa.root \
+    --DataHistogram aa_nominal_s_pure_raw_sub_pjet_u_dr_sum0_0 \
+    --Response ../../MainAnalysis/data/preapproval/${set}/vacillate_aa_redo.root \
+    --ResponseHistogram aa_c_0 \
+    --ResponseTruth aa_g_0 \
+    --ResponseReco aa_r_0 \
+    --PrimaryRecoBins 0,0.01,0.02,0.03,0.04,0.05,0.06,0.07,0.08,0.09,0.105,0.12,0.135,0.15,0.18,0.21,0.3 \
+    --BinningRecoBins 20,30,35,40,45,50,55,60,70,80,90,100,120,150,200 \
+    --PrimaryGenBins 0,0.015,0.03,0.045,0.06,0.08,0.1,0.12,0.15,0.2,0.3 \
+    --BinningGenBins 20,30,40,50,60,70,80,100,120,200 \
+    --Output Output/${set}/AANominalPtWeight0.root
+./Execute --Data ../../MainAnalysis/data/preapproval/${set}/accumulate_aa.root \
+    --DataHistogram aa_nominal_s_pure_raw_sub_pjet_u_dr_sum0_1 \
+    --Response ../../MainAnalysis/data/preapproval/${set}/vacillate_aa_redo.root \
+    --ResponseHistogram aa_c_1 \
+    --ResponseTruth aa_g_1 \
+    --ResponseReco aa_r_1 \
+    --PrimaryRecoBins 0,0.01,0.02,0.03,0.04,0.05,0.06,0.07,0.08,0.09,0.105,0.12,0.135,0.15,0.18,0.21,0.3 \
+    --BinningRecoBins 20,30,35,40,45,50,55,60,70,80,90,100,120,150,200 \
+    --PrimaryGenBins 0,0.015,0.03,0.045,0.06,0.08,0.1,0.12,0.15,0.2,0.3 \
+    --BinningGenBins 20,30,40,50,60,70,80,100,120,200 \
+    --Output Output/${set}/AANominalPtWeight1.root
+./Execute --Data ../../MainAnalysis/data/preapproval/${set}/accumulate_aa.root \
+    --DataHistogram aa_nominal_s_pure_raw_sub_pjet_u_dr_sum0_2 \
+    --Response ../../MainAnalysis/data/preapproval/${set}/vacillate_aa_redo.root \
+    --ResponseHistogram aa_c_2 \
+    --ResponseTruth aa_g_2 \
+    --ResponseReco aa_r_2 \
+    --PrimaryRecoBins 0,0.01,0.02,0.03,0.04,0.05,0.06,0.07,0.08,0.09,0.105,0.12,0.135,0.15,0.18,0.21,0.3 \
+    --BinningRecoBins 20,30,35,40,45,50,55,60,70,80,90,100,120,150,200 \
+    --PrimaryGenBins 0,0.015,0.03,0.045,0.06,0.08,0.1,0.12,0.15,0.2,0.3 \
+    --BinningGenBins 20,30,40,50,60,70,80,100,120,200 \
+    --Output Output/${set}/AANominalPtWeight2.root
+./Execute --Data ../../MainAnalysis/data/preapproval/${set}/accumulate_aa.root \
+    --DataHistogram aa_nominal_s_pure_raw_sub_pjet_u_dr_sum0_3 \
+    --Response ../../MainAnalysis/data/preapproval/${set}/vacillate_aa_redo.root \
+    --ResponseHistogram aa_c_3 \
+    --ResponseTruth aa_g_3 \
+    --ResponseReco aa_r_3 \
+    --PrimaryRecoBins 0,0.01,0.02,0.03,0.04,0.05,0.06,0.07,0.08,0.09,0.105,0.12,0.135,0.15,0.18,0.21,0.3 \
+    --BinningRecoBins 20,30,35,40,45,50,55,60,70,80,90,100,120,150,200 \
+    --PrimaryGenBins 0,0.015,0.03,0.045,0.06,0.08,0.1,0.12,0.15,0.2,0.3 \
+    --BinningGenBins 20,30,40,50,60,70,80,100,120,200 \
+    --Output Output/${set}/AANominalPtWeight3.root
+./Execute --Data ../../MainAnalysis/data/preapproval/${set}/accumulate_aa.root \
+    --DataHistogram aa_nominal_s_pure_raw_sub_pjet_u_dr_merge_0 \
+    --Response ../../MainAnalysis/data/preapproval/${set}/vacillate_aa_redo.root \
+    --ResponseHistogram aa_c_merge_0 \
+    --ResponseTruth aa_g_merge_0 \
+    --ResponseReco aa_r_merge_0 \
+    --PrimaryRecoBins 0,0.01,0.02,0.03,0.04,0.05,0.06,0.07,0.08,0.09,0.105,0.12,0.135,0.15,0.18,0.21,0.3 \
+    --BinningRecoBins 20,30,35,40,45,50,55,60,70,80,90,100,120,150,200 \
+    --PrimaryGenBins 0,0.015,0.03,0.045,0.06,0.08,0.1,0.12,0.15,0.2,0.3 \
+    --BinningGenBins 20,30,40,50,60,70,80,100,120,200 \
+    --Output Output/${set}/AANominalPtWeightMerge.root
+
+# PPNominalPtWeight: Execute
+./Execute --Data ../../MainAnalysis/data/preapproval/${set}/accumulate_pp.root \
+    --DataHistogram pp_nominal_s_pure_raw_sub_pjet_u_dr_sum0_0 \
+    --Response ../../MainAnalysis/data/preapproval/${set}/vacillate_pp_redo.root \
+    --ResponseHistogram pp_c_0 \
+    --ResponseTruth pp_g_0 \
+    --ResponseReco pp_r_0 \
+    --PrimaryRecoBins 0,0.01,0.02,0.03,0.04,0.05,0.06,0.07,0.08,0.09,0.105,0.12,0.135,0.15,0.18,0.21,0.3 \
+    --BinningRecoBins 20,30,35,40,45,50,55,60,70,80,90,100,120,150,200 \
+    --PrimaryGenBins 0,0.015,0.03,0.045,0.06,0.08,0.1,0.12,0.15,0.2,0.3 \
+    --BinningGenBins 20,30,40,50,60,70,80,100,120,200 \
+    --Output Output/${set}/PPNominalPtWeight0.root
+
+
+# AANominalNoWeight: Execute
+./Execute --Data ../../MainAnalysis/data/preapproval/${set}/accumulate_aa.root \
+    --DataHistogram aa_nominal_s_pure_raw_sub_pjet_u_dr_sum0_0 \
+    --Response ../../MainAnalysis/data/preapproval/${set}/vacillate_aa_no_weight.root \
+    --ResponseHistogram aa_c_0 \
+    --ResponseTruth aa_g_0 \
+    --ResponseReco aa_r_0 \
+    --PrimaryRecoBins 0,0.01,0.02,0.03,0.04,0.05,0.06,0.07,0.08,0.09,0.105,0.12,0.135,0.15,0.18,0.21,0.3 \
+    --BinningRecoBins 20,30,35,40,45,50,55,60,70,80,90,100,120,150,200 \
+    --PrimaryGenBins 0,0.015,0.03,0.045,0.06,0.08,0.1,0.12,0.15,0.2,0.3 \
+    --BinningGenBins 20,30,40,50,60,70,80,100,120,200 \
+    --Output Output/${set}/AANominalNoWeight0.root
+./Execute --Data ../../MainAnalysis/data/preapproval/${set}/accumulate_aa.root \
+    --DataHistogram aa_nominal_s_pure_raw_sub_pjet_u_dr_sum0_1 \
+    --Response ../../MainAnalysis/data/preapproval/${set}/vacillate_aa_no_weight.root \
+    --ResponseHistogram aa_c_1 \
+    --ResponseTruth aa_g_1 \
+    --ResponseReco aa_r_1 \
+    --PrimaryRecoBins 0,0.01,0.02,0.03,0.04,0.05,0.06,0.07,0.08,0.09,0.105,0.12,0.135,0.15,0.18,0.21,0.3 \
+    --BinningRecoBins 20,30,35,40,45,50,55,60,70,80,90,100,120,150,200 \
+    --PrimaryGenBins 0,0.015,0.03,0.045,0.06,0.08,0.1,0.12,0.15,0.2,0.3 \
+    --BinningGenBins 20,30,40,50,60,70,80,100,120,200 \
+    --Output Output/${set}/AANominalNoWeight1.root
+./Execute --Data ../../MainAnalysis/data/preapproval/${set}/accumulate_aa.root \
+    --DataHistogram aa_nominal_s_pure_raw_sub_pjet_u_dr_sum0_2 \
+    --Response ../../MainAnalysis/data/preapproval/${set}/vacillate_aa_no_weight.root \
+    --ResponseHistogram aa_c_2 \
+    --ResponseTruth aa_g_2 \
+    --ResponseReco aa_r_2 \
+    --PrimaryRecoBins 0,0.01,0.02,0.03,0.04,0.05,0.06,0.07,0.08,0.09,0.105,0.12,0.135,0.15,0.18,0.21,0.3 \
+    --BinningRecoBins 20,30,35,40,45,50,55,60,70,80,90,100,120,150,200 \
+    --PrimaryGenBins 0,0.015,0.03,0.045,0.06,0.08,0.1,0.12,0.15,0.2,0.3 \
+    --BinningGenBins 20,30,40,50,60,70,80,100,120,200 \
+    --Output Output/${set}/AANominalNoWeight2.root
+./Execute --Data ../../MainAnalysis/data/preapproval/${set}/accumulate_aa.root \
+    --DataHistogram aa_nominal_s_pure_raw_sub_pjet_u_dr_sum0_3 \
+    --Response ../../MainAnalysis/data/preapproval/${set}/vacillate_aa_no_weight.root \
+    --ResponseHistogram aa_c_3 \
+    --ResponseTruth aa_g_3 \
+    --ResponseReco aa_r_3 \
+    --PrimaryRecoBins 0,0.01,0.02,0.03,0.04,0.05,0.06,0.07,0.08,0.09,0.105,0.12,0.135,0.15,0.18,0.21,0.3 \
+    --BinningRecoBins 20,30,35,40,45,50,55,60,70,80,90,100,120,150,200 \
+    --PrimaryGenBins 0,0.015,0.03,0.045,0.06,0.08,0.1,0.12,0.15,0.2,0.3 \
+    --BinningGenBins 20,30,40,50,60,70,80,100,120,200 \
+    --Output Output/${set}/AANominalNoWeight3.root
+./Execute --Data ../../MainAnalysis/data/preapproval/${set}/accumulate_aa.root \
+    --DataHistogram aa_nominal_s_pure_raw_sub_pjet_u_dr_merge_0 \
+    --Response ../../MainAnalysis/data/preapproval/${set}/vacillate_aa_no_weight.root \
+    --ResponseHistogram aa_c_merge_0 \
+    --ResponseTruth aa_g_merge_0 \
+    --ResponseReco aa_r_merge_0 \
+    --PrimaryRecoBins 0,0.01,0.02,0.03,0.04,0.05,0.06,0.07,0.08,0.09,0.105,0.12,0.135,0.15,0.18,0.21,0.3 \
+    --BinningRecoBins 20,30,35,40,45,50,55,60,70,80,90,100,120,150,200 \
+    --PrimaryGenBins 0,0.015,0.03,0.045,0.06,0.08,0.1,0.12,0.15,0.2,0.3 \
+    --BinningGenBins 20,30,40,50,60,70,80,100,120,200 \
+    --Output Output/${set}/AANominalNoWeightMerge.root
+
+# PPNominalNoWeight: Execute
+./Execute --Data ../../MainAnalysis/data/preapproval/${set}/accumulate_pp.root \
+    --DataHistogram pp_nominal_s_pure_raw_sub_pjet_u_dr_sum0_0 \
+    --Response ../../MainAnalysis/data/preapproval/${set}/vacillate_pp_no_weight.root \
+    --ResponseHistogram pp_c_0 \
+    --ResponseTruth pp_g_0 \
+    --ResponseReco pp_r_0 \
+    --PrimaryRecoBins 0,0.01,0.02,0.03,0.04,0.05,0.06,0.07,0.08,0.09,0.105,0.12,0.135,0.15,0.18,0.21,0.3 \
+    --BinningRecoBins 20,30,35,40,45,50,55,60,70,80,90,100,120,150,200 \
+    --PrimaryGenBins 0,0.015,0.03,0.045,0.06,0.08,0.1,0.12,0.15,0.2,0.3 \
+    --BinningGenBins 20,30,40,50,60,70,80,100,120,200 \
+    --Output Output/${set}/PPNominalNoWeight0.root
