5e6ddad8678b2b6143eef21ad668ce3ee0de1e68
mitaylor <mitay@mit.edu>
2023-08-11 17:16:20 -0400

    photon_sum_iso: update for pbpb

--- MainAnalysis/scripts/PhotonSumIso.sh
+++ MainAnalysis/scripts/PhotonSumIso.sh
@@ -9,6 +9,13 @@ run_pp() {
     ./bin/photon_isolation_study configs/populate/preapproval-parallel/populate_${tag}_bkg.conf configs/analysis/${set}/analysis_pp.conf data/preapproval/${set}/photon_isolation_study_${tag}_bkg.root > log/${set}_photon_isolation_study_${tag}_bkg.txt 2>&1 &
 }
 
+run_aa() {
+    tag=$1
+
+    ./bin/photon_isolation_study configs/populate/preapproval-parallel/populate_${tag}_raw.conf configs/analysis/${set}/analysis_aa.conf data/preapproval/${set}/photon_isolation_study_${tag}_raw.root > log/${set}_photon_isolation_study_${tag}_raw.txt 2>&1 &
+    ./bin/photon_isolation_study configs/populate/preapproval-parallel/populate_${tag}_bkg.conf configs/analysis/${set}/analysis_aa.conf data/preapproval/${set}/photon_isolation_study_${tag}_bkg.root > log/${set}_photon_isolation_study_${tag}_bkg.txt 2>&1 &
+}
+
 samples=(pp)
 
 for sample in ${samples[@]}; do
@@ -16,5 +23,15 @@ for sample in ${samples[@]}; do
 
     for syst in qcd; do
         run_pp ${sample}_${syst}
+    done
+done
+
+samples=(aa)
+
+for sample in ${samples[@]}; do
+    run_aa ${sample}
+
+    for syst in qcd; do
+        run_aa ${sample}_${syst}
     done
 done
