762c274b61914e80fae1efe0fada86c3649663ab
mitaylor <mitay@mit.edu>
2023-08-15 08:49:34 -0400

    Foresting: EM enriched dijet

---
+++ Foresting/211011_PPMC/crabConfig_forest_AOD_94X_MC_EM.py
@@ -0,0 +1,30 @@
+OutputBase = '/store/user/mitaylor/PhotonJet'
+# OutputBase = '/store/group/phys_heavyions/mitaylor/PhotonJet'
+DatasetName = '/QCD_pThat-30_EMEnrichedDijet_TuneCP5_5p02TeV_pythia8/RunIIpp5Spring18DR-94X_mc2017_realistic_forppRef5TeV_v1-v1/AODSIM'
+Tag = '20230802PPEM'
+
+from WMCore.Configuration import Configuration
+config = Configuration()
+
+config.section_("General")
+config.General.requestName = Tag
+
+config.section_("JobType")
+config.JobType.pluginName = 'Analysis'
+config.JobType.psetName = 'runConfig_forest_AOD_94X_MC.py'
+config.JobType.maxMemoryMB = 2500    # request high memory machines.
+config.JobType.maxJobRuntimeMin = 2750    # request longer runtime, ~48 hours.
+
+config.section_("Data")
+config.Data.inputDataset = DatasetName
+config.Data.inputDBS = 'global'
+config.Data.splitting = 'FileBased'
+config.Data.unitsPerJob = 1
+config.Data.publication = False
+config.Data.outputDatasetTag = Tag
+config.Data.outLFNDirBase = OutputBase + DatasetName
+
+config.section_("Site")
+config.Site.storageSite = 'T2_US_MIT'
+# config.Site.storageSite = 'T2_CH_CERN'
+config.Site.ignoreGlobalBlacklist = True
---
+++ Foresting/211011_PbPbMC/crabConfig_forest_miniAOD_112X_EM.py
@@ -0,0 +1,30 @@
+OutputBase = '/store/user/mitaylor/PhotonJet'
+# OutputBase = '/store/group/phys_heavyions/mitaylor/PhotonJet'
+DatasetName = '/QCD_pThat-30_EMEnrichedDijet_TuneCP5_caloParmStage2v3_HydjetDrumMB_5p02TeV_pythia8/HINPbPbSpring21MiniAOD-FixL1CaloGT_112X_upgrade2018_realistic_HI_v9-v1/MINIAODSIM'
+Tag = '20230815PbPbEM'
+
+from WMCore.Configuration import Configuration
+config = Configuration()
+
+config.section_("General")
+config.General.requestName = Tag
+
+config.section_("JobType")
+config.JobType.pluginName = 'Analysis'
+config.JobType.psetName = 'runConfig_forest_miniAOD_112X_MC.py'
+config.JobType.maxMemoryMB = 5000
+config.JobType.maxJobRuntimeMin = 1500
+
+config.section_("Data")
+config.Data.inputDataset = DatasetName
+config.Data.inputDBS = 'global'
+config.Data.splitting = 'FileBased'
+config.Data.unitsPerJob = 1
+config.Data.publication = False
+config.Data.outputDatasetTag = Tag
+config.Data.outLFNDirBase = OutputBase + DatasetName
+
+config.section_("Site")
+config.Site.storageSite = 'T2_US_MIT'
+# config.Site.storageSite = 'T2_CH_CERN'
+config.Site.ignoreGlobalBlacklist = True
