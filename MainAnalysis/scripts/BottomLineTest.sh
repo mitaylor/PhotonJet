94e991e80947b4b37e8e15febbf7a8dbc4a76129
mitaylor <mitay@mit.edu>
2023-08-15 21:56:10 -0400

    run script

--- MainAnalysis/scripts/BottomLineTest.sh
+++ MainAnalysis/scripts/BottomLineTest.sh
@@ -1,9 +1,5 @@
 #!/usr/bin/env bash
 
-./bin/bottom_line_test configs/test/bottom_line_test_aa.conf configs/analysis/pho_60/analysis_aa.conf test.root 2 10
-
-./bin/bottom_line_test configs/test/bottom_line_test_aa.conf configs/analysis/pho_60/analysis_aa.conf test.root 2 10
-
 for cent in 0 1 2 3
 do
     echo "PbPb Centrality $cent"
@@ -25,7 +21,7 @@ echo
 for iteration in 1 2 3 4 5 6 7 8 9 10 15 20 30 40
 do
     echo "Iteration $iteration"
-    ./bin/bottom_line_test configs/test/bottom_line_test_aa.conf configs/analysis/pho_60/analysis_aa.conf test.root 0 $iteration
+    ./bin/bottom_line_test configs/test/bottom_line_test_pp.conf configs/analysis/pho_60/analysis_pp.conf test.root 0 $iteration
 done
 
 echo
