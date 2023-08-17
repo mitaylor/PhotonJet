3c3d07094c0acd4c242061a791b28edcba094c96
mitaylor <mitay@mit.edu>
2023-08-16 23:47:32 -0400

    bottom_line_test_collapsed: fix the ndof

--- MainAnalysis/scripts/QuantitateUnfolding.sh
+++ MainAnalysis/scripts/QuantitateUnfolding.sh
@@ -22,7 +22,7 @@ run_aa() {
     ./bin/quantitate_merge configs/quantitate/quantitate_${tag}.conf configs/analysis/${set}/analysis_aa.conf data/preapproval/${set}/quantitate_${tag}.root
 }
 
-samples=(pp pp_smear_0_10 pp_smear_10_30 pp_smear_30_50 pp_smear_50_90)
+samples=(pp)
 
 for sample in ${samples[@]}; do
     run_pp ${sample}
