259b4c13f8d0d84b9c61f1cb0cecd20fb9351d9f
mitaylor <mitay@mit.edu>
2023-11-20 13:55:57 -0500

    binning: create scripts to try running

--- MainAnalysis/scripts/Binning.sh
+++ MainAnalysis/scripts/Binning.sh
@@ -5,7 +5,7 @@ set=$1
 run_pp() {
     tag=$1
 
-    ./bin/binning configs/populate/serial/populate_${tag}_raw.conf configs/analysis/${set}/analysis_pp.conf /data/submit/mitay/binning/arc/${set}/populate_${tag}_raw.root > log/${set}_spectrum_populate_${tag}_raw.txt 2>&1 &
+    ./bin/binning configs/populate/serial/populate_${tag}_raw.conf configs/analysis/${set}/analysis_pp.conf /data/submit/mitay/binning/arc/${set}/binning_${tag}_raw.root > log/${set}_binning_${tag}_raw.txt 2>&1 &
 }
 
 samples=(pp)
