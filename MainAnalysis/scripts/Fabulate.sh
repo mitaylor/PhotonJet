84cf5a2c85750f9fc9206de9b450ed1b7ca2bdaf
mitaylor <mitay@mit.edu>
2023-12-01 15:13:52 -0500

    fabulate: update to have correct dr matching and to get JER as a function of reco jet pt

--- MainAnalysis/scripts/Fabulate.sh
+++ MainAnalysis/scripts/Fabulate.sh
@@ -4,11 +4,11 @@ run() {
     tag=$1
     echo $tag
     ./bin/fabulate configs/fabulate/fabulate_${tag}.conf data/arc/fabulate_${tag}.root
-    ./bin/fabulate_angle configs/fabulate/fabulate_${tag}_angle.conf data/arc/fabulate_${tag}_angle.root
-    echo "./bin/fabulate_angle_smeared configs/fabulate/fabulate_${tag}_angle.conf data/arc/fabulate_${tag}_angle.root"
+    # ./bin/fabulate_angle configs/fabulate/fabulate_${tag}_angle.conf data/arc/fabulate_${tag}_angle.root
+    # echo "./bin/fabulate_angle_smeared configs/fabulate/fabulate_${tag}_angle.conf data/arc/fabulate_${tag}_angle.root"
 }
 
-samples=(pp_smear_0_10_test pp_smear_10_30_test pp_smear_30_50_test pp_smear_50_90_test)
+samples=(pp aa)
 
 for sample in ${samples[@]}; do
     run ${sample}
