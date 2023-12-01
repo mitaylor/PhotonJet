7d7e5900e40921b0355631c96731ff9548e53315
mitaylor <mitay@mit.edu>
2023-12-01 15:34:28 -0500

    fabulate: update to have correct dr matching and to get JER as a function of reco jet pt

--- MainAnalysis/scripts/Distillate.sh
+++ MainAnalysis/scripts/Distillate.sh
@@ -4,10 +4,10 @@ run() {
     tag=$1
     echo $tag
     ./bin/distillate configs/distillate/distillate_${tag}_scale.conf data/arc/distillate_${tag}_scale.root
-    ./bin/distillate_angle configs/distillate/distillate_${tag}_angle_dj.conf data/arc/distillate_${tag}_angle_dj.root
+    # ./bin/distillate_angle configs/distillate/distillate_${tag}_angle_dj.conf data/arc/distillate_${tag}_angle_dj.root
 }
 
-samples=(pp_smear_0_10_test pp_smear_10_30_test pp_smear_30_50_test pp_smear_50_90_test)
+samples=(pp aa)
 
 for sample in ${samples[@]}; do
     run ${sample}
