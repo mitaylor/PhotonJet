ce3e1bd6897717b5c16fd85daf739089784ea10f
mitaylor <mitay@mit.edu>
2023-12-14 17:59:48 -0500

    jubilate: update the plotting to have the right kinematic cuts

--- MEBS/src/jubilate.C
+++ MEBS/src/jubilate.C
@@ -41,9 +41,9 @@ int jubilate(char const* config, char const* selections, char const* output) {
     auto minimums = conf->get<std::vector<float>>("minimums");
     auto maximums = conf->get<std::vector<float>>("maximums");
 
-    // auto jpt = conf->get<bool>("jpt");
-    // auto eta = conf->get<bool>("eta");
-    // auto dphi = conf->get<bool>("dphi");
+    auto jpt = conf->get<bool>("jpt");
+    auto eta = conf->get<bool>("eta");
+    auto dphi = conf->get<bool>("dphi");
 
     /* selections */
     auto sel = new configurer(selections);
@@ -73,7 +73,6 @@ int jubilate(char const* config, char const* selections, char const* output) {
 
     auto system_tag = system + "  #sqrt{s_{NN}} = 5.02 TeV"s;
     auto cms = "#bf{#scale[1.4]{CMS}} #it{#scale[1.2]{Preliminary}}"s;
-    cms += "         p_{T}^{#gamma} > "s + to_text(photon_pt_min) + " GeV"s;
 
     zip([&](auto const& label, auto& min, auto& max) {
         std::cout << label << std::endl;
@@ -89,6 +88,21 @@ int jubilate(char const* config, char const* selections, char const* output) {
         auto shape = hist->shape(); // photon pt, scan (optional), centrality
         
         if (shape.size() == 2) {
+            auto cuts_info = [&](int64_t index) {
+                if (index > 0) {
+                    auto photon_selections = "p_{T}^{#gamma} > "s + to_text(photon_pt_min) + " GeV, |#eta^{#gamma}| < "s + to_text(photon_eta_abs);
+                    if (jpt || eta) photon_selections += ", #Delta#phi_{j#gamma} > " + to_text(dphi_min_numerator) + "#pi/"s + to_text(dphi_min_denominator);
+                    auto jet_selections = "anti-k_{T} R = 0.3, |#eta^{jet}| < "s + to_text(jet_eta_abs) + ", " + to_text(jet_pt_min) + " < p_{T}^{jet} < "s + to_text(jet_pt_max) + " GeV";
+
+                    TLatex* l = new TLatex();
+                    l->SetTextAlign(31);
+                    l->SetTextFont(43);
+                    l->SetTextSize(11);
+                    l->DrawLatexNDC(0.86, 0.65, photon_selections.data());
+                    l->DrawLatexNDC(0.86, 0.60, jet_selections.data());
+                }
+            };
+
             auto c = new paper(set + "_" + tag + "_" + label, hb);
             apply_style(c, cms, system_tag);
             c->accessory(std::bind(line_at, _1, 0.f, min, max));
@@ -107,20 +121,36 @@ int jubilate(char const* config, char const* selections, char const* output) {
 
         if (shape.size() == 3) {
             std::vector<paper*> cs(shape[1], nullptr);
-            // std::vector<float> drange;
 
-            // if (jpt) drange = rjpt;
-            // else if (eta) drange = reta;
-            // else if (dphi) drange = rdphi;
+            auto range_info = [&](int64_t index) {
+                if (jpt) { info_text(index, 0.71, "%d < p_{T}^{jet} < %d GeV", rjpt, false); }
+                if (eta) { info_text(index, 0.71, "%d < #eta^{jet} < %d", reta, false); }
+                if (dphi) { info_text(index, 0.71, "%d < #Delta#phi_{j#gamma} < %d", rdphi, false); }
+            };
 
-            // auto hf_info = [&](int64_t index) {
-            //     info_text(index, 0.75, "Range %i - %i", drange, true); };
+            auto cuts_info = [&](int64_t index) {
+                if (index > 0) {
+                    auto photon_selections = "p_{T}^{#gamma} > "s + to_text(photon_pt_min) + " GeV, |#eta^{#gamma}| < "s + to_text(photon_eta_abs);
+                    if (jpt || eta) photon_selections += ", #Delta#phi_{j#gamma} > " + to_text(dphi_min_numerator) + "#pi/"s + to_text(dphi_min_denominator);
+                    auto jet_selections = "anti-k_{T} R = 0.3, |#eta^{jet}| < "s + to_text(jet_eta_abs);
+                    if (dphi || eta) jet_selections += ", " + to_text(jet_pt_min) + " < p_{T}^{jet} < "s + to_text(jet_pt_max) + " GeV";
+
+                    TLatex* l = new TLatex();
+                    l->SetTextAlign(31);
+                    l->SetTextFont(43);
+                    l->SetTextSize(11);
+                    l->DrawLatexNDC(0.86, 0.65, photon_selections.data());
+                    l->DrawLatexNDC(0.86, 0.60, jet_selections.data());
+                }
+            };
 
             for (int64_t i = 0; i < shape[1]; ++i) {
                 cs[i] = new paper(set + "_" + tag + "_" + label + "_" + to_text(i), hb);
                 apply_style(cs[i], cms, system_tag);
                 cs[i]->accessory(std::bind(line_at, _1, 0.f, min, max));
+                cs[i]->accessory(range_info);
                 cs[i]->accessory(hf_info);
+                cs[i]->accessory(cuts_info);
                 cs[i]->divide(ihf->size() , -1);
 
                 for (int64_t j = 0; j < shape[2]; ++j) {
