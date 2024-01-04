da85dbd800b228eeb23d26aa3a893f362797eda7
mitaylor <mitay@mit.edu>
2024-01-04 03:41:08 -0500

    combine_populate: update config files

--- MEBS/scripts/CombinePopulate.sh
+++ MEBS/scripts/CombinePopulate.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 
-./bin/combine_populate configs/combine_populate/combine_extra_aa_qcd_raw.conf configs/analysis/pho_60/analysis_aa.conf data/populate_extra_aa_qcd_raw.root
+./bin/combine_populate configs/combine_populate/combine_aa_qcd_raw.conf configs/analysis/pho_60/analysis_aa.conf data/populate_aa_qcd_raw.root
+./bin/combine_populate configs/combine_populate/combine_fast_aa_qcd_raw.conf configs/analysis/pho_60/analysis_aa.conf data/populate_fast_aa_qcd_raw.root
