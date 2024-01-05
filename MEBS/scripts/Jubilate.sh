629586e414a5345b03c659bd83c989f0339bae20
mitaylor <mitay@mit.edu>
2024-01-05 12:33:23 -0500

    jubilate: remake plots

--- MEBS/scripts/Jubilate.sh
+++ MEBS/scripts/Jubilate.sh
@@ -1,8 +1,5 @@
 #!/usr/bin/env bash
 
-# ./bin/jubilate configs/jubilate/jubilate_nominal_eta_scan_aa_qcd_raw.conf configs/analysis/pho_60/analysis_aa.conf test.root
-# ./bin/jubilate configs/jubilate/jubilate_nominal_dphi_scan_aa_qcd_raw.conf configs/analysis/pho_60/analysis_aa.conf test.root
-# ./bin/jubilate configs/jubilate/jubilate_nominal_jpt_scan_aa_qcd_raw.conf configs/analysis/pho_60/analysis_aa.conf test.root
-./bin/jubilate configs/jubilate/jubilate_extra_eta_scan_aa_qcd_raw.conf configs/analysis/pho_60/analysis_aa.conf test.root
-./bin/jubilate configs/jubilate/jubilate_extra_dphi_scan_aa_qcd_raw.conf configs/analysis/pho_60/analysis_aa.conf test.root
-./bin/jubilate configs/jubilate/jubilate_extra_jpt_scan_aa_qcd_raw.conf configs/analysis/pho_60/analysis_aa.conf test.root
+./bin/jubilate configs/jubilate/jubilate_fast_eta_scan_aa_qcd_raw.conf configs/analysis/pho_60/analysis_aa.conf test.root
+./bin/jubilate configs/jubilate/jubilate_fast_dphi_scan_aa_qcd_raw.conf configs/analysis/pho_60/analysis_aa.conf test.root
+./bin/jubilate configs/jubilate/jubilate_fast_jpt_scan_aa_qcd_raw.conf configs/analysis/pho_60/analysis_aa.conf test.root
