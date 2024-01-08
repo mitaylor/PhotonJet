cec753ca8eeb4125831f870d469ac5a448805386
mitaylor <mitay@mit.edu>
2024-01-08 17:59:50 +0900

    populate: test ncoll matching within 1%

--- MEBS/scripts/RunPopulateFastNCollMC.sh
+++ MEBS/scripts/RunPopulateFastNCollMC.sh
@@ -6,22 +6,22 @@ n=$3
 
 ls /data/submit/mitay/photons/regulate_aa_qcd_extra* > files
 ls /data/submit/mitay/photons/aod/* >> files
-# split -n l/10 --numeric-suffixes=0 -a 1 files files_
-# awk -F "[/.]" '{print $(NF-1)}' files_${n} > index_${n}
+split -n l/4 --numeric-suffixes=0 -a 1 files files_
+awk -F "[/.]" '{print $(NF-1)}' files_${n} > index_${n}
 
-# mkdir -p /data/submit/mitay/mebs/${set}/
-# mkdir -p nohup/log/
+mkdir -p /data/submit/mitay/mebs/${set}/
+mkdir -p nohup/log/
 
-# files=($(cat files_${n}))
-# index=($(cat index_${n}))
+files=($(cat files_${n}))
+index=($(cat index_${n}))
 
-# rm files*
-# rm index*
+rm files*
+rm index*
 
-# for i in ${!files[@]}; do
-#     echo "std::vector<std::string> input = \\" > nohup/${tag}_${index[i]}.conf
-#     echo "${files[i]}" >> nohup/${tag}_${index[i]}.conf
-#     cat ../configs/populate/parallel/${tag}_template.conf >> nohup/${tag}_${index[i]}.conf
+for i in ${!files[@]}; do
+    echo "std::vector<std::string> input = \\" > nohup/${tag}_${index[i]}.conf
+    echo "${files[i]}" >> nohup/${tag}_${index[i]}.conf
+    cat ../configs/populate/parallel/${tag}_template.conf >> nohup/${tag}_${index[i]}.conf
 
-#     nohup ../bin/populate_fast_ncoll nohup/${tag}_${index[i]}.conf ../configs/analysis/${set}/analysis_aa.conf /data/submit/mitay/mebs/${set}/${tag}_fast_ncoll_${index[i]}.root  > nohup/log/${set}_${tag}_fast_ncoll_${index[i]}.txt 2>&1 &
-# done
+    nohup ../bin/populate_fast_ncoll nohup/${tag}_${index[i]}.conf ../configs/analysis/${set}/analysis_aa.conf /data/submit/mitay/mebs/${set}/${tag}_fast_ncoll_${index[i]}.root  > nohup/log/${set}_${tag}_fast_ncoll_${index[i]}.txt 2>&1 &
+done
