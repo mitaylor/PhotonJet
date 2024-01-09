2136b133164e0b06ce82a9170e016e54db56c2eb
mitaylor <mitay@mit.edu>
2024-01-09 21:02:19 +0900

    jubilate: update for ncoll test

--- MEBS/scripts/Jubilate.sh
+++ MEBS/scripts/Jubilate.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-./bin/jubilate configs/jubilate/jubilate_fast_eta_scan_aa_qcd_raw.conf configs/analysis/pho_60/analysis_aa.conf test.root
-./bin/jubilate configs/jubilate/jubilate_fast_dphi_scan_aa_qcd_raw.conf configs/analysis/pho_60/analysis_aa.conf test.root
-./bin/jubilate configs/jubilate/jubilate_fast_jpt_scan_aa_qcd_raw.conf configs/analysis/pho_60/analysis_aa.conf test.root
+./bin/jubilate configs/jubilate/jubilate_fast_ncoll_eta_scan_aa_qcd_raw.conf configs/analysis/pho_60/analysis_aa.conf test.root
+./bin/jubilate configs/jubilate/jubilate_fast_ncoll_dphi_scan_aa_qcd_raw.conf configs/analysis/pho_60/analysis_aa.conf test.root
+./bin/jubilate configs/jubilate/jubilate_fast_ncoll_jpt_scan_aa_qcd_raw.conf configs/analysis/pho_60/analysis_aa.conf test.root
