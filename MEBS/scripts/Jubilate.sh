9a3f0964be8ef6721f92238cebc0938a61071647
mitaylor <mitay@mit.edu>
2024-01-16 22:36:17 +0900

    populate: try advanced mixed event background subtraction

--- MEBS/scripts/Jubilate.sh
+++ MEBS/scripts/Jubilate.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-./bin/jubilate configs/jubilate/jubilate_fast_ncoll_eta_scan_aa_qcd_raw.conf configs/analysis/pho_60/analysis_aa.conf test.root
-./bin/jubilate configs/jubilate/jubilate_fast_ncoll_dphi_scan_aa_qcd_raw.conf configs/analysis/pho_60/analysis_aa.conf test.root
-./bin/jubilate configs/jubilate/jubilate_fast_ncoll_jpt_scan_aa_qcd_raw.conf configs/analysis/pho_60/analysis_aa.conf test.root
+./bin/jubilate configs/jubilate/jubilate_fast_mebs_eta_scan_aa_qcd_raw.conf configs/analysis/pho_60/analysis_aa.conf test.root
+./bin/jubilate configs/jubilate/jubilate_fast_mebs_dphi_scan_aa_qcd_raw.conf configs/analysis/pho_60/analysis_aa.conf test.root
+./bin/jubilate configs/jubilate/jubilate_fast_mebs_jpt_scan_aa_qcd_raw.conf configs/analysis/pho_60/analysis_aa.conf test.root
