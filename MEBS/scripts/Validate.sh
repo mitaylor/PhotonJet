b51a398daba58cdbae54bc21a6d5a77276b40af7
mitaylor <mitay@mit.edu>
2024-01-19 00:36:33 +0900

    populate: try vz matching

--- MEBS/scripts/Validate.sh
+++ MEBS/scripts/Validate.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-./bin/validate configs/validate/validate_mebs_jpt_scan_aa_qcd_raw.conf configs/analysis/pho_60/analysis_aa.conf temp.root
-./bin/validate configs/validate/validate_mebs_dphi_scan_aa_qcd_raw.conf configs/analysis/pho_60/analysis_aa.conf temp.root
-./bin/validate configs/validate/validate_mebs_eta_scan_aa_qcd_raw.conf configs/analysis/pho_60/analysis_aa.conf temp.root
+./bin/validate configs/validate/validate_vz_jpt_scan_aa_qcd_raw.conf configs/analysis/pho_60/analysis_aa.conf temp.root
+./bin/validate configs/validate/validate_vz_dphi_scan_aa_qcd_raw.conf configs/analysis/pho_60/analysis_aa.conf temp.root
+./bin/validate configs/validate/validate_vz_eta_scan_aa_qcd_raw.conf configs/analysis/pho_60/analysis_aa.conf temp.root
