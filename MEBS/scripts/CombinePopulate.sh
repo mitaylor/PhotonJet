af9c2665e5c72a8f333ec20a7dd7804eda9132e7
mitaylor <mitay@mit.edu>
2024-01-18 23:28:48 +0900

    populate: try vz matching

--- MEBS/scripts/CombinePopulate.sh
+++ MEBS/scripts/CombinePopulate.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 # ./bin/combine_populate configs/combine_populate/combine_fast_aa_qcd_raw.conf configs/analysis/pho_60/analysis_aa.conf data/populate_fast_aa_qcd_raw.root
-./bin/combine_populate configs/combine_populate/combine_fast_mebs_aa_qcd_raw.conf configs/analysis/pho_60/analysis_aa.conf data/populate_fast_mebs_aa_qcd_raw.root
+./bin/combine_populate configs/combine_populate/combine_fast_vz_aa_qcd_raw.conf configs/analysis/pho_60/analysis_aa.conf data/populate_fast_vz_aa_qcd_raw.root
--- MEBS/scripts/Jubilate.sh
+++ MEBS/scripts/Jubilate.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-./bin/jubilate configs/jubilate/jubilate_fast_mebs_angle_eta_scan_aa_qcd_raw.conf configs/analysis/pho_60/analysis_aa.conf test.root
-./bin/jubilate configs/jubilate/jubilate_fast_mebs_angle_dphi_scan_aa_qcd_raw.conf configs/analysis/pho_60/analysis_aa.conf test.root
-./bin/jubilate configs/jubilate/jubilate_fast_mebs_angle_jpt_scan_aa_qcd_raw.conf configs/analysis/pho_60/analysis_aa.conf test.root
+./bin/jubilate configs/jubilate/jubilate_fast_vz_eta_scan_aa_qcd_raw.conf configs/analysis/pho_60/analysis_aa.conf test.root
+./bin/jubilate configs/jubilate/jubilate_fast_vz_dphi_scan_aa_qcd_raw.conf configs/analysis/pho_60/analysis_aa.conf test.root
+./bin/jubilate configs/jubilate/jubilate_fast_vz_jpt_scan_aa_qcd_raw.conf configs/analysis/pho_60/analysis_aa.conf test.root
