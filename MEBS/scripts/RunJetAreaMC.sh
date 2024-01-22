cdabe5b20bea03fa38ce6a0f2b45d8661cd201e0
mitaylor <mitay@mit.edu>
2024-01-23 07:51:40 +0900

    jet_area: calculate the fractional area covered by jets

---
+++ MEBS/scripts/RunJetAreaMC.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+set=$1
+tag=$2
+n=$3
+
+ls /data/submit/mitay/photons/regulate_aa_qcd_* > files
+split -n l/10 --numeric-suffixes=0 -a 1 files files_
+awk -F "[/.]" '{print $(NF-1)}' files_${n} > index_${n}
+
+mkdir -p /data/submit/mitay/mebs/${set}/
+mkdir -p nohup/log/
+
+files=($(cat files_${n}))
+index=($(cat index_${n}))
+
+rm files*
+rm index*
+
+for i in ${!files[@]}; do
+    echo "std::vector<std::string> input = \\" > nohup/${tag}_${index[i]}.conf
+    echo "${files[i]}" >> nohup/${tag}_${index[i]}.conf
+    cat ../configs/populate/parallel/${tag}_template.conf >> nohup/${tag}_${index[i]}.conf
+
+    nohup ../bin/jet_area nohup/${tag}_${index[i]}.conf ../configs/analysis/${set}/analysis_aa.conf /data/submit/mitay/mebs/jet_area/${set}/${tag}_fast_${index[i]}.root  > nohup/log/${set}_${tag}_fast_${index[i]}.txt 2>&1 &
+done
