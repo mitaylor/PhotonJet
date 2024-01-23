de317fe170a85e3196a5fea22143852517c73a2f
mitaylor <mitay@mit.edu>
2024-01-23 22:20:05 +0900

    populate_original: debug behavior in pp

--- MainAnalysis/scripts/CombinePopulateTemp.sh
+++ MainAnalysis/scripts/CombinePopulateTemp.sh
@@ -16,12 +16,6 @@ run_aa() {
     ./bin/combine_populate configs/combine_populate/combine_${tag}.conf configs/analysis/${set}/analysis_aa.conf data/arc/${set}/populate_${tag}.root
 }
 
-samples=(pp_raw pp_bkg pp_qcd_raw pp_qcd_bkg)
-
-for sample in ${samples[@]}; do
-    run_pp ${sample}
-done
-
 samples=(aa_raw aa_bkg aa_qcd_raw aa_qcd_bkg)
 
 for sample in ${samples[@]}; do
