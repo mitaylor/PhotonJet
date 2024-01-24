62372d01a1d4d1a370b9abe34ed4bf01a1c5cf4f
mitaylor <mitay@mit.edu>
2024-01-24 19:53:17 +0900

    jet_area: combine and plot

--- MainAnalysis/src/plot_jet_area.C
+++ MainAnalysis/src/plot_jet_area.C
@@ -63,19 +63,19 @@ int combine_populate(char const* config, char const* selections, char const* out
     auto mixed = new TH2F("signal", "Fractional Jet Area: Mixed", 4, 0, 4, 6, 0, 6);
 
     for (int i = 0; i < 4; ++i) {
-        signal.SetBinContent(i + 1, 1, jet20->GetBinContent(1));
-        signal.SetBinContent(i + 1, 2, jet25->GetBinContent(1));
-        signal.SetBinContent(i + 1, 3, jet30->GetBinContent(1));
-        signal.SetBinContent(i + 1, 4, jet35->GetBinContent(1));
-        signal.SetBinContent(i + 1, 5, jet40->GetBinContent(1));
-        signal.SetBinContent(i + 1, 6, jet50->GetBinContent(1));
+        signal.SetBinContent(i + 1, 1, (*jet20)[i]->GetBinContent(1));
+        signal.SetBinContent(i + 1, 2, (*jet25)[i]->GetBinContent(1));
+        signal.SetBinContent(i + 1, 3, (*jet30)[i]->GetBinContent(1));
+        signal.SetBinContent(i + 1, 4, (*jet35)[i]->GetBinContent(1));
+        signal.SetBinContent(i + 1, 5, (*jet40)[i]->GetBinContent(1));
+        signal.SetBinContent(i + 1, 6, (*jet50)[i]->GetBinContent(1));
 
-        mixed.SetBinContent(i + 1, 1, mix_jet20->GetBinContent(1));
-        mixed.SetBinContent(i + 1, 2, mix_jet25->GetBinContent(1));
-        mixed.SetBinContent(i + 1, 3, mix_jet30->GetBinContent(1));
-        mixed.SetBinContent(i + 1, 4, mix_jet35->GetBinContent(1));
-        mixed.SetBinContent(i + 1, 5, mix_jet40->GetBinContent(1));
-        mixed.SetBinContent(i + 1, 6, mix_jet50->GetBinContent(1));
+        mixed.SetBinContent(i + 1, 1, (*mix_jet20)[i]->GetBinContent(1));
+        mixed.SetBinContent(i + 1, 2, (*mix_jet25)[i]->GetBinContent(1));
+        mixed.SetBinContent(i + 1, 3, (*mix_jet30)[i]->GetBinContent(1));
+        mixed.SetBinContent(i + 1, 4, (*mix_jet35)[i]->GetBinContent(1));
+        mixed.SetBinContent(i + 1, 5, (*mix_jet40)[i]->GetBinContent(1));
+        mixed.SetBinContent(i + 1, 6, (*mix_jet50)[i]->GetBinContent(1));
     }
 
     signal.Write();
