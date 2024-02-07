340fc005ecb4dca753a8be2b8998c1f6fa266410
mitaylor <mitay@mit.edu>
2024-02-07 12:35:43 -0500

    regulate: update condor framework

--- MainAnalysis/condor/regulate/SubmitJobs.sh
+++ MainAnalysis/condor/regulate/SubmitJobs.sh
@@ -9,12 +9,19 @@ cp /tmp/x509up_u168456 x509up_u168456
 mkdir -p ${output_tag}
 cd ${output_tag}
 rm -f *
-cp ../../configs/regulate/preapproval-parallel/${config_fragment} .
+cp ../../configs/regulate/${config_fragment} .
 
-# split the input files so there are no more than 30 files per job
-find ${folder} -type f > ${output_tag}
+# find ${folder} -type f > ${output_tag}
+
+# split the input files
+folders=`xrdfs xrootd.cmsaf.mit.edu ls ${folder}`
+
+for folder in ${folders}
+do
+    xrdfs xrootd.cmsaf.mit.edu ls ${folder} >> ${output_tag}
+done
+
 sed -i 's/^.*\/store/root:\/\/xrootd.cmsaf.mit.edu\/\/store/' ${output_tag}
-# sed -i 's/^.*\/store/root:\/\/eoscms.cern.ch\/\/store/' ${output_tag}
 
 ## split -l 1 --numeric-suffixes=0 -a 3 ${output_tag} ${output_tag}_
 ## files=`find . -type f -name "${output_tag}_[0-9][0-9][0-9]" -printf '%f\n'`
@@ -24,4 +31,4 @@ awk -F "[_.]" -v OFS=", " '{print $(NF-1), $0}' ${output_tag} > ${output_tag}.li
 cat ../SubmitCondor.condor | sed "s/__MASTER__/${output_tag}/g" > SubmitCondor.condor
 sed -i "s/__CONFIG__/${config_fragment}/g" SubmitCondor.condor
 
-condor_submit SubmitCondor.condor
+# condor_submit SubmitCondor.condor
