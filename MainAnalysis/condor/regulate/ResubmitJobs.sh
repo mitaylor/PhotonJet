d0f4bfa950b312a0b8b80067dadd82649e775a13
mitaylor <mitay@mit.edu>
2024-02-08 08:36:43 -0500

    regulate: get list of missing files

--- MainAnalysis/condor/regulate/ResubmitJobs.sh
+++ MainAnalysis/condor/regulate/ResubmitJobs.sh
@@ -5,14 +5,16 @@ tags=`ls */*.list | sed 's/\/.*$//'`
 for tag in ${tags}; do
     echo ${tag}
     if [ -f ${tag}/${tag}.txt ]; then
-        indices=`cat ${tag}/${tag}.txt`
+        cd ${tag}
+        mv ${tag}.list ${tag}_old.list
+
+        indices=`cat ${tag}.txt`
 
         for index in ${indices}; do
-            echo `grep "^${index}," ${tag}/${tag}.list` >> ${tag}/${tag}_temp.list
+            echo `grep "^${index}," ${tag}_old.list` >> ${tag}.list
+            rm ${tag}.*.${index}.*
         done
 
-        cd ${tag}
-        mv ${tag}_temp.list ${tag}.list
         # condor_submit SubmitCondor.condor
         cd ..
     fi
