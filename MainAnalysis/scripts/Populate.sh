f6d37069c813b415bbdd124675f40511a39c2f8f
mitaylor <mitay@mit.edu>
2024-02-08 18:09:23 -0500

    populate: run the qcd systematics serially

--- MainAnalysis/scripts/Populate.sh
+++ MainAnalysis/scripts/Populate.sh
@@ -18,3 +18,18 @@ for sample in ${samples[@]}; do
         run_pp ${sample}_${syst}
     done
 done
+
+run_aa() {
+    tag=$1
+
+    ./bin/populate_original configs/populate/serial/populate_${tag}_raw.conf configs/analysis/${set}/analysis_aa.conf /data/submit/mitay/populate/arc/${set}/populate_${tag}_raw.root > log/${set}_populate_${tag}_raw.txt 2>&1 &
+    ./bin/populate_original configs/populate/serial/populate_${tag}_bkg.conf configs/analysis/${set}/analysis_aa.conf /data/submit/mitay/populate/arc/${set}/populate_${tag}_bkg.root > log/${set}_populate_${tag}_bkg.txt 2>&1 &
+}
+
+samples=(aa)
+
+for sample in ${samples[@]}; do
+    for syst in qcd qcd_gen_iso closure; do
+        run_aa ${sample}_${syst}
+    done
+done
