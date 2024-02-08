fd62bc740f201cb91d0d8173e27b090d53c3b8f0
mitaylor <mitay@mit.edu>
2024-02-08 12:11:20 -0500

    narrate: change code

--- MainAnalysis/condor/regulate/ResubmitJobs.sh
+++ MainAnalysis/condor/regulate/ResubmitJobs.sh
@@ -3,19 +3,19 @@
 tags=`ls */*.txt | sed 's/\/.*$//'`
 
 for tag in ${tags}; do
-    echo ${tag}
     if [ -f ${tag}/${tag}.txt ]; then
+        echo ${tag}
         cd ${tag}
-        # mv ${tag}.list ${tag}_old.list
+        mv ${tag}.list ${tag}_old.list
 
-        # indices=`cat ${tag}.txt`
+        indices=`cat ${tag}.txt`
 
-        # for index in ${indices}; do
-        #     echo `grep "^${index}," ${tag}_old.list` >> ${tag}.list
-        #     rm -f ${tag}.*.${index}.*
-        # done
+        for index in ${indices}; do
+            echo `grep "^${index}," ${tag}_old.list` >> ${tag}.list
+            rm -f ${tag}.*.${index}.*
+        done
 
-        # condor_submit SubmitCondor.condor
+        condor_submit SubmitCondor.condor
         cd ..
     fi
 done
