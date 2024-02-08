90b6016a44b459e7d0d43db1e30cddd88e13e4a3
mitaylor <mitay@mit.edu>
2024-02-08 12:16:57 -0500

    narrate: change code

--- MainAnalysis/condor/regulate/CheckMissingJobs.sh
+++ MainAnalysis/condor/regulate/CheckMissingJobs.sh
@@ -1,14 +1,12 @@
 #!/usr/bin/env bash
 
-rm -f missing.txt
 rm -f existing.txt
 
 tags=`ls */*.list | sed 's/\/.*$//'`
 
 for tag in ${tags}; do
     indices=`cat ${tag}/${tag}.list | awk '{print $1}' | sed 's/\,.*$//'`
-    rm existing.txt
-    rm ${tag}/${tag}.txt
+    rm -f ${tag}/${tag}.txt
 
     for index in ${indices}; do
         if [ -f /data/submit/mitay/photons/condor/${tag}_${index}.root ]; then
