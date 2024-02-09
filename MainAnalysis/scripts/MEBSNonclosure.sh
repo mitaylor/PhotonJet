b06e51d6ef8589612cd00031ae5e2510765bfda2
mitaylor <mitay@mit.edu>
2024-02-09 14:13:20 -0500

    tessellate: update the script for running

--- MainAnalysis/scripts/MEBSNonclosure.sh
+++ MainAnalysis/scripts/MEBSNonclosure.sh
@@ -5,7 +5,7 @@ set=$1
 run_aa() {
     tag=$1
 
-    ./bin/mebs_nonclosure configs/populate/serial/populate_${tag}_raw.conf configs/analysis/${set}/analysis_aa.conf data/arc/${set}/${set}/populate_${tag}_mebs_nonclosure_raw.root > log/${set}_mebs_nonclosure_${tag}_raw.txt 2>&1 &
+    ./bin/mebs_nonclosure configs/populate/serial/populate_${tag}_raw.conf configs/analysis/${set}/analysis_aa.conf data/arc/${set}/populate_${tag}_mebs_nonclosure_raw.root > log/${set}_mebs_nonclosure_${tag}_raw.txt 2>&1 &
     ./bin/mebs_nonclosure configs/populate/serial/populate_${tag}_bkg.conf configs/analysis/${set}/analysis_aa.conf data/arc/${set}/populate_${tag}_mebs_nonclosure_bkg.root > log/${set}_mebs_nonclosure_${tag}_bkg.txt 2>&1 &
 }
 
