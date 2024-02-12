0559217040c1af9c08c8aa2741a6298022347381
mitaylor <mitay@mit.edu>
2024-02-12 17:15:23 -0500

    mebs_nonclosure: correct bug with filling dr histogram

--- MainAnalysis/scripts/Populate.sh
+++ MainAnalysis/scripts/Populate.sh
@@ -9,15 +9,15 @@ run_pp() {
     ./bin/populate_original configs/populate/serial/populate_${tag}_bkg.conf configs/analysis/${set}/analysis_pp.conf /data/submit/mitay/populate/arc/${set}/populate_${tag}_bkg.root > log/${set}_populate_${tag}_bkg.txt 2>&1 &
 }
 
-samples=(pp)
+# samples=(pp)
 
-for sample in ${samples[@]}; do
-    run_pp ${sample}
+# for sample in ${samples[@]}; do
+#     run_pp ${sample}
 
-    for syst in wo_ele_rej qcd qcd_gen_iso er es closure; do
-        run_pp ${sample}_${syst}
-    done
-done
+#     for syst in wo_ele_rej qcd qcd_gen_iso er es closure; do
+#         run_pp ${sample}_${syst}
+#     done
+# done
 
 run_aa() {
     tag=$1
