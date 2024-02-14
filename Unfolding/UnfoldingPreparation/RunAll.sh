a83dc94f8a4f7952953f9d1e8644055533d9a720
mitaylor <mitay@mit.edu>
2024-02-13 23:19:48 -0500

    Unfolding: update unfolding preparation to be one config

--- Unfolding/UnfoldingPreparation/RunAll.sh
+++ Unfolding/UnfoldingPreparation/RunAll.sh
@@ -6,10 +6,40 @@ nominal=$2
 unfolding=$3
 mebs=$4
 
-primary_reco_bins="0,0.01,0.015,0.02,0.025,0.03,0.04,0.05,0.06,0.07,0.08,0.1,0.15,0.3"
-binning_reco_bins="30,35,40,45,50,60,70,80,100,200"
-primary_gen_bins="0,0.01,0.016,0.024,0.032,0.04,0.05,0.06,0.08,0.1,0.15,0.3"
-binning_gen_bins="30,40,50,60,70,80,100,200"
+if [[ $set == "pho_60" ]]; then
+    primary_reco_bins="0,0.01,0.02,0.03,0.04,0.05,0.06,0.07,0.08,0.09,0.105,0.12,0.135,0.15,0.18,0.21,0.3"
+    binning_reco_bins="20,30,35,40,45,50,55,60,70,80,90,100,120,150,200"
+    primary_gen_bins="0,0.015,0.03,0.045,0.06,0.08,0.1,0.12,0.15,0.2,0.3"
+    binning_gen_bins="20,30,40,50,60,70,80,100,120,200"
+fi 
+
+if [[ $set == "pho_60_rebin1" ]]; then
+    primary_reco_bins="0,0.01,0.02,0.03,0.045,0.06,0.08,0.1,0.12,0.15,0.2"
+    binning_reco_bins="30,40,50,60,70,80,100,120,200"
+    primary_gen_bins="0,0.01,0.02,0.03,0.045,0.06,0.08,0.1,0.12,0.15,0.2"
+    binning_gen_bins="30,40,50,60,70,80,100,120,200"
+fi 
+
+if [[ $set == "pho_60_rebin2" ]]; then
+    primary_reco_bins="0,0.01,0.015,0.02,0.025,0.03,0.04,0.05,0.06,0.07,0.08,0.1,0.12,0.15,0.2"
+    binning_reco_bins="30,35,40,50,60,70,80,100,120,200"
+    primary_gen_bins="0,0.01,0.016,0.024,0.032,0.04,0.05,0.06,0.08,0.1,0.12,0.15,0.2"
+    binning_gen_bins="30,40,50,60,70,80,100,120,200"
+fi 
+
+if [[ $set == "pho_60_rebin3" ]]; then
+    primary_reco_bins="0,0.01,0.015,0.02,0.025,0.03,0.04,0.05,0.06,0.07,0.08,0.1,0.15,0.3"
+    binning_reco_bins="30,35,40,45,50,60,70,80,100,200"
+    primary_gen_bins="0,0.01,0.016,0.024,0.032,0.04,0.05,0.06,0.08,0.1,0.15,0.3"
+    binning_gen_bins="30,40,50,60,70,80,100,200"
+fi 
+
+if [[ $set == "pho_60_rebin4" ]]; then
+    primary_reco_bins="0,0.01,0.015,0.02,0.025,0.03,0.04,0.05,0.06,0.07,0.08,0.1,0.15"
+    binning_reco_bins="30,35,40,45,50,60,70,80,100,200"
+    primary_gen_bins="0,0.01,0.016,0.024,0.032,0.04,0.05,0.06,0.08,0.1,0.15"
+    binning_gen_bins="30,40,50,60,70,80,100,200"
+fi 
 
 if [ ${nominal} -eq 1 ]
     then
@@ -70,6 +100,63 @@ if [ ${nominal} -eq 1 ]
         --PrimaryGenBins ${primary_gen_bins} \
         --BinningGenBins ${binning_gen_bins} \
         --Output Output/${set}/AANominalMerge.root
+
+    # AAMEBSNonclosure: Execute
+    ./Execute --Data ../../MainAnalysis/data/arc/${set}/granulate_before_unfolding_aa_mebs_nonclosure.root \
+        --DataHistogram aa_mod_aa_qcd_mebs_nonclosure_s_pure_raw_sub_pjet_u_dr_jpt_sum0_0 \
+        --Response ../../MainAnalysis/data/arc/${set}/vacillate_aa.root \
+        --ResponseHistogram aa_c_0 \
+        --ResponseTruth aa_g_0 \
+        --ResponseReco aa_r_0 \
+        --PrimaryRecoBins ${primary_reco_bins} \
+        --BinningRecoBins ${binning_reco_bins} \
+        --PrimaryGenBins ${primary_gen_bins} \
+        --BinningGenBins ${binning_gen_bins} \
+        --Output Output/${set}/AAMEBSNonclosure0.root
+    ./Execute --Data ../../MainAnalysis/data/arc/${set}/granulate_before_unfolding_aa_mebs_nonclosure.root \
+        --DataHistogram aa_mod_aa_qcd_mebs_nonclosure_s_pure_raw_sub_pjet_u_dr_jpt_sum0_1 \
+        --Response ../../MainAnalysis/data/arc/${set}/vacillate_aa.root \
+        --ResponseHistogram aa_c_1 \
+        --ResponseTruth aa_g_1 \
+        --ResponseReco aa_r_1 \
+        --PrimaryRecoBins ${primary_reco_bins} \
+        --BinningRecoBins ${binning_reco_bins} \
+        --PrimaryGenBins ${primary_gen_bins} \
+        --BinningGenBins ${binning_gen_bins} \
+        --Output Output/${set}/AAMEBSNonclosure1.root
+    ./Execute --Data ../../MainAnalysis/data/arc/${set}/granulate_before_unfolding_aa_mebs_nonclosure.root \
+        --DataHistogram aa_mod_aa_qcd_mebs_nonclosure_s_pure_raw_sub_pjet_u_dr_jpt_sum0_2 \
+        --Response ../../MainAnalysis/data/arc/${set}/vacillate_aa.root \
+        --ResponseHistogram aa_c_2 \
+        --ResponseTruth aa_g_2 \
+        --ResponseReco aa_r_2 \
+        --PrimaryRecoBins ${primary_reco_bins} \
+        --BinningRecoBins ${binning_reco_bins} \
+        --PrimaryGenBins ${primary_gen_bins} \
+        --BinningGenBins ${binning_gen_bins} \
+        --Output Output/${set}/AAMEBSNonclosure2.root
+    ./Execute --Data ../../MainAnalysis/data/arc/${set}/granulate_before_unfolding_aa_mebs_nonclosure.root \
+        --DataHistogram aa_mod_aa_qcd_mebs_nonclosure_s_pure_raw_sub_pjet_u_dr_jpt_sum0_3 \
+        --Response ../../MainAnalysis/data/arc/${set}/vacillate_aa.root \
+        --ResponseHistogram aa_c_3 \
+        --ResponseTruth aa_g_3 \
+        --ResponseReco aa_r_3 \
+        --PrimaryRecoBins ${primary_reco_bins} \
+        --BinningRecoBins ${binning_reco_bins} \
+        --PrimaryGenBins ${primary_gen_bins} \
+        --BinningGenBins ${binning_gen_bins} \
+        --Output Output/${set}/AAMEBSNonclosure3.root
+    ./Execute --Data ../../MainAnalysis/data/arc/${set}/granulate_before_unfolding_aa_mebs_nonclosure.root \
+        --DataHistogram aa_mod_aa_qcd_mebs_nonclosure_s_pure_raw_sub_pjet_u_dr_jpt_merge_0 \
+        --Response ../../MainAnalysis/data/arc/${set}/vacillate_aa.root \
+        --ResponseHistogram aa_c_merge_0 \
+        --ResponseTruth aa_g_merge_0 \
+        --ResponseReco aa_r_merge_0 \
+        --PrimaryRecoBins ${primary_reco_bins} \
+        --BinningRecoBins ${binning_reco_bins} \
+        --PrimaryGenBins ${primary_gen_bins} \
+        --BinningGenBins ${binning_gen_bins} \
+        --Output Output/${set}/AAMEBSNonclosureMerge.root
 
     # AAES: Execute
     ./Execute --Data ../../MainAnalysis/data/arc/${set}/accumulate_aa_es.root \
@@ -355,6 +442,63 @@ if [ ${nominal} -eq 1 ]
         --PrimaryGenBins ${primary_gen_bins} \
         --BinningGenBins ${binning_gen_bins} \
         --Output Output/${set}/AAQCDMerge.root
+
+    # AAQCDMEBSNonclosure: Execute
+    ./Execute --Data ../../MainAnalysis/data/arc/${set}/accumulate_aa_qcd_mebs_nonclosure.root \
+        --DataHistogram aa_qcd_mebs_nonclosure_s_pure_raw_sub_pjet_u_dr_jpt_sum0_0 \
+        --Response ../../MainAnalysis/data/arc/${set}/vacillate_aa_qcd.root \
+        --ResponseHistogram aa_c_0 \
+        --ResponseTruth aa_g_0 \
+        --ResponseReco aa_r_0 \
+        --PrimaryRecoBins ${primary_reco_bins} \
+        --BinningRecoBins ${binning_reco_bins} \
+        --PrimaryGenBins ${primary_gen_bins} \
+        --BinningGenBins ${binning_gen_bins} \
+        --Output Output/${set}/AAQCDMEBSNonclosure0.root
+    ./Execute --Data ../../MainAnalysis/data/arc/${set}/accumulate_aa_qcd_mebs_nonclosure.root \
+        --DataHistogram aa_qcd_mebs_nonclosure_s_pure_raw_sub_pjet_u_dr_jpt_sum0_1 \
+        --Response ../../MainAnalysis/data/arc/${set}/vacillate_aa_qcd.root \
+        --ResponseHistogram aa_c_1 \
+        --ResponseTruth aa_g_1 \
+        --ResponseReco aa_r_1 \
+        --PrimaryRecoBins ${primary_reco_bins} \
+        --BinningRecoBins ${binning_reco_bins} \
+        --PrimaryGenBins ${primary_gen_bins} \
+        --BinningGenBins ${binning_gen_bins} \
+        --Output Output/${set}/AAQCDMEBSNonclosure1.root
+    ./Execute --Data ../../MainAnalysis/data/arc/${set}/accumulate_aa_qcd_mebs_nonclosure.root \
+        --DataHistogram aa_qcd_mebs_nonclosure_s_pure_raw_sub_pjet_u_dr_jpt_sum0_2 \
+        --Response ../../MainAnalysis/data/arc/${set}/vacillate_aa_qcd.root \
+        --ResponseHistogram aa_c_2 \
+        --ResponseTruth aa_g_2 \
+        --ResponseReco aa_r_2 \
+        --PrimaryRecoBins ${primary_reco_bins} \
+        --BinningRecoBins ${binning_reco_bins} \
+        --PrimaryGenBins ${primary_gen_bins} \
+        --BinningGenBins ${binning_gen_bins} \
+        --Output Output/${set}/AAQCDMEBSNonclosure2.root
+    ./Execute --Data ../../MainAnalysis/data/arc/${set}/accumulate_aa_qcd_mebs_nonclosure.root \
+        --DataHistogram aa_qcd_mebs_nonclosure_s_pure_raw_sub_pjet_u_dr_jpt_sum0_3 \
+        --Response ../../MainAnalysis/data/arc/${set}/vacillate_aa_qcd.root \
+        --ResponseHistogram aa_c_3 \
+        --ResponseTruth aa_g_3 \
+        --ResponseReco aa_r_3 \
+        --PrimaryRecoBins ${primary_reco_bins} \
+        --BinningRecoBins ${binning_reco_bins} \
+        --PrimaryGenBins ${primary_gen_bins} \
+        --BinningGenBins ${binning_gen_bins} \
+        --Output Output/${set}/AAQCDMEBSNonclosure3.root
+    ./Execute --Data ../../MainAnalysis/data/arc/${set}/accumulate_aa_qcd_mebs_nonclosure.root \
+        --DataHistogram aa_qcd_mebs_nonclosure_s_pure_raw_sub_pjet_u_dr_jpt_merge_0 \
+        --Response ../../MainAnalysis/data/arc/${set}/vacillate_aa_qcd.root \
+        --ResponseHistogram aa_c_merge_0 \
+        --ResponseTruth aa_g_merge_0 \
+        --ResponseReco aa_r_merge_0 \
+        --PrimaryRecoBins ${primary_reco_bins} \
+        --BinningRecoBins ${binning_reco_bins} \
+        --PrimaryGenBins ${primary_gen_bins} \
+        --BinningGenBins ${binning_gen_bins} \
+        --Output Output/${set}/AAQCDMEBSNonclosureMerge.root
 
     # AAClosure: Execute
     ./Execute --Data ../../MainAnalysis/data/arc/${set}/accumulate_aa_closure.root \
