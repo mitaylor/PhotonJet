b1485a74362460abb98b991f8e3eed7c30637bda
mitaylor <mitay@mit.edu>
2024-02-14 12:51:31 -0500

    compare_bayes: add versions that are normalized vs unnormalized

--- MainAnalysis/scripts/CompareUnfolding.sh
+++ MainAnalysis/scripts/CompareUnfolding.sh
@@ -8,9 +8,7 @@ run_pp() {
     echo ${tag}
 
     ./bin/compare_bayes_kerrors_normalized configs/quantitate/quantitate_${tag}.conf configs/analysis/${set}/analysis_pp.conf data/arc/${set}/compare_bayes_kerrors_normalized_${tag}.root
-    echo "./bin/compare_bayes_kerrors_normalized configs/quantitate/quantitate_${tag}.conf configs/analysis/${set}/analysis_pp.conf data/arc/${set}/compare_bayes_kerrors_normalized_${tag}.root"
     ./bin/compare_bayes_kerrors_unnormalized configs/quantitate/quantitate_${tag}.conf configs/analysis/${set}/analysis_pp.conf data/arc/${set}/compare_bayes_kerrors_unnormalized_${tag}.root
-    echo "./bin/compare_bayes_kerrors_unnormalized configs/quantitate/quantitate_${tag}.conf configs/analysis/${set}/analysis_pp.conf data/arc/${set}/compare_bayes_kerrors_unnormalized_${tag}.root"
 }
 
 run_aa() {
@@ -19,9 +17,7 @@ run_aa() {
     echo ${tag}
 
     ./bin/compare_bayes_kerrors_normalized configs/quantitate/quantitate_${tag}.conf configs/analysis/${set}/analysis_aa.conf data/arc/${set}/compare_bayes_kerrors_normalized_${tag}.root
-    echo "./bin/compare_bayes_kerrors_normalized configs/quantitate/quantitate_${tag}.conf configs/analysis/${set}/analysis_aa.conf data/arc/${set}/compare_bayes_kerrors_normalized_${tag}.root"
     ./bin/compare_bayes_kerrors_unnormalized configs/quantitate/quantitate_${tag}.conf configs/analysis/${set}/analysis_aa.conf data/arc/${set}/compare_bayes_kerrors_unnormalized_${tag}.root
-    echo "./bin/compare_bayes_kerrors_unnormalized configs/quantitate/quantitate_${tag}.conf configs/analysis/${set}/analysis_aa.conf data/arc/${set}/compare_bayes_kerrors_unnormalized_${tag}.root"
 }
 
 samples=(pp pp_qcd)
@@ -30,7 +26,7 @@ for sample in ${samples[@]}; do
     run_pp ${sample}
 done
 
-samples=(aa aa_qcd)
+samples=(aa aa_qcd aa_mebs_nonclosure aa_qcd_mebs_nonclosure)
 
 for sample in ${samples[@]}; do
     run_aa ${sample}
