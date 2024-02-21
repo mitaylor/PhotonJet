e8193da978bc80f54471b2e459be5a6db923ee8f
mitaylor <mitay@mit.edu>
2024-02-21 12:45:20 -0500

    compare_errors: compare the outputs of different error settings for unfolding

--- MainAnalysis/src/compare_errors.C
+++ MainAnalysis/src/compare_errors.C
@@ -236,7 +236,7 @@ int quantitate(char const* config, char const* selections, char const* output) {
     unfolded.push_back(new history<TH1F>("unfolded_regularization"s + regularization, "", null<TH1F>, (int64_t) afters.size()));
     unfolded_fold0.push_back(new history<TH1F>("unfolded_fold0_regularization"s + regularization, ""s, null<TH1F>, (int64_t) afters.size()));
     unfolded_fold1.push_back(new history<TH1F>("unfolded_fold1_regularization"s + regularization, ""s, null<TH1F>, (int64_t) afters.size()));
-    covariance.push_back(new history<TH2D>*>("covariance_regularization"s + regularization, ""s, (int64_t) afters.size()));
+    covariance.push_back(new history<TH2D>("covariance_regularization"s + regularization, ""s, null<TH2D>, (int64_t) afters.size()));
 
     /* extract histograms */
     for (size_t j = 0; j < afters.size(); ++j) {
