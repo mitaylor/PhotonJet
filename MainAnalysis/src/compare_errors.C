c451d5b4754e7182ddb1bb3e09742b3cf1483692
mitaylor <mitay@mit.edu>
2024-02-21 14:24:07 -0500

    compare_errors: compare the outputs of different error settings for unfolding

--- MainAnalysis/src/compare_errors.C
+++ MainAnalysis/src/compare_errors.C
@@ -326,7 +326,7 @@ int quantitate(char const* config, char const* selections, char const* output) {
 
     covariance[0]->apply([&](TH1* h) { p4->add(h, "data"s); });
     covariance[0]->apply([&](TH1* h) { 
-        h->GetZaxis()->SetLabelSize(0.35);
+        h->GetZaxis()->SetLabelSize(0.035);
         p4->adjust(h, "colz", ""); 
         });
 
