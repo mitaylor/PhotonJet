73cbb9b4e0cfe5f515ab1130687ebafa6e4e0097
mitaylor <mitay@mit.edu>
2024-02-22 10:26:47 -0500

    compare_errors: bug fix with hist names

--- MainAnalysis/scripts/CompareErrors.sh
+++ MainAnalysis/scripts/CompareErrors.sh
@@ -19,3 +19,21 @@ for sample in ${samples[@]}; do
         done
     done
 done
+
+run_aa() {
+    tag=$1
+
+    ./bin/compare_errors configs/compare_errors/quantitate_${tag}.conf configs/analysis/${set}/analysis_aa.conf data/arc/${set}/compare_errors_${tag}.root
+}
+
+samples=(aa)
+types=(bayes svd)
+errors=(knoerror kerrors kcovariance kcovtoys kerrorstoys)
+
+for sample in ${samples[@]}; do
+    for type in ${types[@]}; do
+        for error in ${errors[@]}; do
+            run_aa ${sample}_${type}_${error}
+        done
+    done
+done
