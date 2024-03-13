7860d950aa6c9f4493e14fb4dd9e13a216379f3f
mitaylor <mitay@mit.edu>
2024-03-13 07:14:04 -0400

    compare_unfolding_prior: first commit

--- MainAnalysis/src/compare_unfolding_prior.C
+++ MainAnalysis/src/compare_unfolding_prior.C
@@ -231,7 +231,7 @@ int quantitate(char const* config, char const* selections, char const* output) {
 
     auto minimum = [&](int64_t index) {
         if (index > -1) {
-            auto reg = (algorithm == "Bayes") ? "k_{reg}"s : "it"s;
+            auto reg = (algorithm == "SVD") ? "k_{reg}"s : "it"s;
             auto min = "Regularization: "s + reg + "^{mc} = "s + to_text(choice_mc[index-1]) + " and " + reg + "^{flat} = "s + to_text(choice_flat[index-1]);
             auto alg = "Algorithm: "s + algorithm;
             auto src = "Source: "s + label;
