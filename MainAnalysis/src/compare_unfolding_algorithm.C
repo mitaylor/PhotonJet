a653e05d9905d9c35f9118f14e3baf2ba2bc42b0
mitaylor <mitay@mit.edu>
2024-03-13 06:02:07 -0400

    compare_unfolding_algorithm: first try, no plotting

--- MainAnalysis/src/compare_unfolding_algorithm.C
+++ MainAnalysis/src/compare_unfolding_algorithm.C
@@ -41,22 +41,22 @@ TH2F* variance(TH1* flat, multival const* m) {
 
 TH1F* fold_mat(TH1* flat, TMatrixT<double>* covariance, multival const* m, int64_t axis,
            std::vector<int64_t>& offsets) {
-    auto name = std::string(flat->GetName()) + "_fold" + std::to_string(axis);std::cout << __LINE__ << std::endl;
+    auto name = std::string(flat->GetName()) + "_fold" + std::to_string(axis);
     auto hfold = m->axis(axis).book<TH1F, 2>(0, name, "",
         { offsets[axis << 1], offsets[(axis << 1) + 1] });
-std::cout << __LINE__ << std::endl;
-    auto shape = m->shape();std::cout << __LINE__ << std::endl;
 
-    auto front = std::vector<int64_t>(m->dims(), 0);std::cout << __LINE__ << std::endl;
-    auto back = std::vector<int64_t>(m->dims(), 0);std::cout << __LINE__ << std::endl;
+    auto shape = m->shape();
+
+    auto front = std::vector<int64_t>(m->dims(), 0);
+    auto back = std::vector<int64_t>(m->dims(), 0);
     for (int64_t i = 0; i < m->dims(); ++i) {
         front[i] = offsets[i << 1];
         back[i] = shape[i] - offsets[(i << 1) + 1];
     }
-std::cout << __LINE__ << std::endl;
-    auto size = back[axis] - front[axis];std::cout << __LINE__ << std::endl;
-    auto list = new std::vector<int64_t>[size];std::cout << __LINE__ << std::endl;
-std::cout << __LINE__ << std::endl;
+
+    auto size = back[axis] - front[axis];
+    auto list = new std::vector<int64_t>[size];
+
     for (int64_t i = 0; i < m->size(); ++i) {
         auto indices = m->indices_for(i);
 
@@ -72,29 +72,27 @@ std::cout << __LINE__ << std::endl;
 
         list[index].push_back(i);
     }
-std::cout << __LINE__ << std::endl;
+
     for (int64_t i = 0; i < size; ++i) {
         auto indices = list[i];
         int64_t count = indices.size();
-std::cout << __LINE__ << std::endl;
+
         auto error = 0.;
         for (int64_t j = 0; j < count; ++j) {
-            auto j_x = indices[j];std::cout << __LINE__ << std::endl;
+            auto j_x = indices[j];
             for (int64_t k = 0; k < count; ++k) {
-                auto k_x = indices[k];std::cout << __LINE__ << std::endl;
-                std::cout << error << std::endl; std::cout << (covariance == nullptr) << std::endl;
-                std::cout << (*covariance)(1, 1) << std::endl;
-                error = error + (*covariance)(j_x, k_x);std::cout << __LINE__ << std::endl;
+                auto k_x = indices[k];
+                error = error + (*covariance)(j_x, k_x);
             }
         }
-std::cout << __LINE__ << std::endl;
+
         hfold->SetBinError(i + 1, std::sqrt(error));
     }
-std::cout << __LINE__ << std::endl;
+
     delete [] list;
-std::cout << __LINE__ << std::endl;
+
     hfold->Scale(1., "width");
-std::cout << __LINE__ << std::endl;
+
     return hfold;
 }
 
@@ -111,6 +109,9 @@ int quantitate(char const* config, char const* selections, char const* output) {
     auto file_svd = conf->get<std::string>("file_svd");
     auto file_bayes = conf->get<std::string>("file_bayes");
 
+    auto dhf = conf->get<std::vector<float>>("hf_diff");
+    auto dcent = conf->get<std::vector<int32_t>>("cent_diff");
+
     auto sel = new configurer(selections);
 
     auto set = sel->get<std::string>("set");
@@ -121,11 +122,14 @@ int quantitate(char const* config, char const* selections, char const* output) {
 
     auto osg = sel->get<std::vector<int64_t>>("osg");
 
+    auto rpt = sel->get<std::vector<float>>("photon_pt_bounds");
+
     /* create intervals and multivals */
     auto idrg = new interval("#deltaj"s, rdrg);
     auto iptg = new interval("p_{T}^{j}"s, rptg);
 
     auto mg = new multival(*idrg, *iptg);
+    auto mpthf = new multival(rpt, dhf);
 
     /* manage memory manually */
     TH1::AddDirectory(false);
@@ -173,47 +177,112 @@ int quantitate(char const* config, char const* selections, char const* output) {
 
         std::string unfold_name_bayes = "HUnfoldedBayes" + std::to_string(choice_bayes[j]);
         std::string matrix_name_bayes = "MUnfoldedBayes" + std::to_string(choice_bayes[j]);
-
-        std::cout << unfold_name_svd << " " << matrix_name_svd << " " << unfold_name_bayes << " " << matrix_name_bayes << std::endl;
         
         auto HUnfoldedSVD = (TH1F*) fdata_svd[j]->Get(unfold_name_svd.data());
         auto MUnfoldedSVD = (TMatrixT<double>*) fdata_svd[j]->Get(matrix_name_svd.data());
 
-        auto HUnfoldedBayes = (TH1F*) fdata_bayes[j]->Get(unfold_name_bayes.data());std::cout << __LINE__ << std::endl;
-        auto MUnfoldedBayes = (TMatrixT<double>*) fdata_bayes[j]->Get(matrix_name_bayes.data());std::cout << __LINE__ << std::endl;
+        auto HUnfoldedBayes = (TH1F*) fdata_bayes[j]->Get(unfold_name_bayes.data());
+        auto MUnfoldedBayes = (TMatrixT<double>*) fdata_bayes[j]->Get(matrix_name_bayes.data());
 
-        std::cout << HUnfoldedBayes->GetNbinsX() << " " << HUnfoldedBayes->GetBinContent(1) << std::endl;
-        std::cout << HUnfoldedSVD->GetNbinsX() << " " << HUnfoldedSVD->GetBinContent(1) << std::endl;
-        std::cout << MUnfoldedBayes->GetNcols() << " " << MUnfoldedBayes->GetNrows() << std::endl;
-        std::cout << MUnfoldedSVD->GetNcols() << " " << MUnfoldedSVD->GetNrows() << std::endl;
+        (*unfolded_bayes)[j] = HUnfoldedBayes;
+        (*unfolded_bayes_fold0)[j] = fold_mat(HUnfoldedBayes, MUnfoldedBayes, mg, 0, osg);
+        (*unfolded_bayes_fold1)[j] = fold_mat(HUnfoldedBayes, MUnfoldedBayes, mg, 1, osg);
 
-        (*unfolded_bayes)[j] = HUnfoldedBayes;std::cout << __LINE__ << std::endl;
-        (*unfolded_bayes_fold0)[j] = fold_mat(HUnfoldedBayes, MUnfoldedBayes, mg, 0, osg);std::cout << __LINE__ << std::endl;
-        (*unfolded_bayes_fold1)[j] = fold_mat(HUnfoldedBayes, MUnfoldedBayes, mg, 1, osg);std::cout << __LINE__ << std::endl;
-
-        (*unfolded_svd)[j] = HUnfoldedSVD;std::cout << __LINE__ << std::endl;
-        (*unfolded_svd_fold0)[j] = fold_mat(HUnfoldedSVD, MUnfoldedSVD, mg, 0, osg);std::cout << __LINE__ << std::endl;
-        (*unfolded_svd_fold1)[j] = fold_mat(HUnfoldedSVD, MUnfoldedSVD, mg, 1, osg);std::cout << __LINE__ << std::endl;
+        (*unfolded_svd)[j] = HUnfoldedSVD;
+        (*unfolded_svd_fold0)[j] = fold_mat(HUnfoldedSVD, MUnfoldedSVD, mg, 0, osg);
+        (*unfolded_svd_fold1)[j] = fold_mat(HUnfoldedSVD, MUnfoldedSVD, mg, 1, osg);
     }
 
     /* rename histograms */
     unfolded_svd->rename("unfolded_svd");
     unfolded_svd_fold0->rename("unfolded_svd_fold0");
     unfolded_svd_fold1->rename("unfolded_svd_fold1");
-std::cout << __LINE__ << std::endl;
+
     unfolded_bayes->rename("unfolded_bayes");
     unfolded_bayes_fold0->rename("unfolded_bayes_fold0");
     unfolded_bayes_fold1->rename("unfolded_bayes_fold1");
-std::cout << __LINE__ << std::endl;
+
     /* save histograms */
     unfolded_svd->save();
     unfolded_svd_fold0->save();
     unfolded_svd_fold1->save();
-std::cout << __LINE__ << std::endl;
+
     unfolded_bayes->save();
     unfolded_bayes_fold0->save();
     unfolded_bayes_fold1->save();
-std::cout << __LINE__ << std::endl;
+
+    /* plotting setup */
+    auto system_tag = "  #sqrt{s_{NN}} = 5.02 TeV"s;
+    system_tag += (tag == "aa") ? ", 1.69 nb^{-1}"s : ", 302 pb^{-1}"s;
+    auto cms = "#bf{#scale[1.4]{CMS}} #it{#scale[1.2]{Preliminary}}"s;
+    auto title = (algorithm == "Bayes") ? "Iterations"s : "k_{reg}"s;
+
+    std::function<void(int64_t, float)> pt_info = [&](int64_t x, float pos) {
+        info_text(x, pos, "%.0f < p_{T}^{#gamma} < %.0f", rpt, false); };
+
+    std::function<void(int64_t, float)> hf_info = [&](int64_t x, float pos) {
+        info_text(x, pos, "Cent. %i - %i%%", dcent, true); };
+
+    auto pthf_info = [&](int64_t index) {
+        stack_text(index, 0.85, 0.04, mpthf, pt_info, hf_info); };
+
+    auto minimum = [&](int64_t index) {
+        auto min = "Regularization: k_{reg} = "s + to_text(choice_svd[index-1]) + " and "s to_text(choice_bayes[index-1]);
+        auto pri = "Prior: "s + prior;
+        auto src = "Source: "s + label;
+
+        TLatex* l = new TLatex();
+        l->SetTextFont(43);
+        l->SetTextAlign(11);
+        l->SetTextSize(11);
+        l->DrawLatexNDC(0.5, 0.65, min.data());
+        l->DrawLatexNDC(0.5, 0.60, pri.data());
+        l->DrawLatexNDC(0.5, 0.55, typ.data());
+    };
+
+    /* plot histograms */
+    auto hb = new pencil();
+
+    hb->category("type", "data");
+    hb->category("algorithm", "SVD", "Bayes");
+
+    hb->set_binary("algorithm");
+
+    auto p1 = new paper(set + "_unfolding_algorithm_" + tag + "_data_" + label + "_" + prior + "_dj", hb);
+
+    p1->divide(files.size(), -1);
+    p1->accessory(pthf_info);
+    p1->accessory(minimum);
+    apply_style(p1, cms, system_tag, -2, 20);
+    p1->set(paper::flags::logy);
+
+    for (size_t i = 0; i < files.size(); ++i) {
+        // (*mse)[i]->GetXaxis()->SetTitle(title.data());
+
+        p1->add((*unfolded_svd_fold0)[i], "data", "SVD");
+        p1->stack((*unfolded_bayes_fold0)[i], "data", "Bayes");
+    }
+
+    auto p2 = new paper(set + "_unfolding_algorithm_" + tag + "_data_" + label + "_" + prior + "_jpt", hb);
+
+    p2->divide(files.size(), -1);
+    p2->accessory(pthf_info);
+    p2->accessory(minimum);
+    apply_style(p2, cms, system_tag, -0.002, 0.02);
+    p2->set(paper::flags::logy);
+
+    for (size_t i = 0; i < files.size(); ++i) {
+        // (*mse)[i]->GetXaxis()->SetTitle(title.data());
+
+        p2->add((*unfolded_svd_fold1)[i], "data", "SVD");
+        p2->stack((*unfolded_bayes_fold1)[i], "data", "Bayes");
+    }
+
+    hb->sketch();
+
+    p1->draw("pdf");
+    p2->draw("pdf");
+
     fout->Close();
 
     return 0;
