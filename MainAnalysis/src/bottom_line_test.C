c817e239b10d3fe780493422dbf7a20bb0b221e5
mitaylor <mitay@mit.edu>
2024-03-20 08:25:30 -0400

    bottom_line_test: revive code for new setup

--- MainAnalysis/src/bottom_line_test.C
+++ MainAnalysis/src/bottom_line_test.C
@@ -257,12 +257,12 @@ int bottom_line_test(char const* config, char const* selections, char const* out
     std::cout << __LINE__ << std::endl;
     std::cout << base + theory_file << std::endl;
     std::cout << base + matrix_file << std::endl;
+    if (tag != "aa") {
     zip([&](auto& fafter, auto const& after) {
         fafter = new TFile(("unfolded/Data/"s + set + "/Bayes/MC/kErrors/"s + after).data(), "read");
         std::cout << "unfolded/Data/"s + set + "/Bayes/MC/kErrors/"s + after << std::endl;
     }, fafter, after_file);
     std::cout << __LINE__ << std::endl;
-    if (tag != "aa") {
     TFile* ftheory = new TFile((base + theory_file).data(), "read");
     TFile* fmatrix = new TFile((base + matrix_file).data(), "read");
     std::cout << __LINE__ << std::endl;
