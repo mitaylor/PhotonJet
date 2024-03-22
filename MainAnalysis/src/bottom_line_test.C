84f4d5635e141faa6ee8f68f7d6e2542ec9a0e90
mitaylor <mitay@mit.edu>
2024-03-22 11:10:59 -0400

    bottom_line_test: try scaling so there are the same number of entries in each

--- MainAnalysis/src/bottom_line_test.C
+++ MainAnalysis/src/bottom_line_test.C
@@ -117,7 +117,7 @@ TH1F* fold(TH1* flat, TH2* covariance, multival const* m, int64_t axis,
     delete [] list;
     delete cov;
 
-    hfold->Scale(1., "width");
+    // hfold->Scale(1., "width");
 
     return hfold;
 }
@@ -174,7 +174,7 @@ TH1F* fold_mat(TH1* flat, TMatrixT<double>* covariance, multival const* m, int64
 
     delete [] list;
 
-    hfold->Scale(1., "width");
+    // hfold->Scale(1., "width");
 
     return hfold;
 }
