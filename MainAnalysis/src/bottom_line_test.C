cf6378bcbd6714cb4e70961bec0631ef8d8a1a83
mitaylor <mitay@mit.edu>
2024-03-22 11:44:11 -0400

    bottom_line_test: try scaling so there are the same number of entries in each

--- MainAnalysis/src/bottom_line_test.C
+++ MainAnalysis/src/bottom_line_test.C
@@ -900,6 +900,7 @@ int bottom_line_test(char const* config, char const* selections, char const* out
 
     p2->divide(size, -1);
     p2->accessory(pthf_info);
+    p1->accessory(std::bind(pass, _1, choice_jpt));
     apply_style(p2, cms, system_tag);
 
     for (int64_t i = 0; i < size; ++i) {
@@ -912,6 +913,7 @@ int bottom_line_test(char const* config, char const* selections, char const* out
 
     p3->divide(size, -1);
     p3->accessory(pthf_info);
+    p1->accessory(std::bind(pass, _1, choice_simple_dj));
     apply_style(p3, cms, system_tag);
 
     for (int64_t i = 0; i < size; ++i) {
@@ -924,6 +926,7 @@ int bottom_line_test(char const* config, char const* selections, char const* out
 
     p4->divide(size, -1);
     p4->accessory(pthf_info);
+    p1->accessory(std::bind(pass, _1, choice_simple_jpt));
     apply_style(p4, cms, system_tag);
 
     for (int i = 0; i < size; ++i) {
