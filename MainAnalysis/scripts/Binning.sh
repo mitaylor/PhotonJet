20bd448aceb1ad260c4efa641eca84d68d76faa3
mitaylor <mitay@mit.edu>
2024-03-24 14:48:45 -0400

    inosculate: run pp with zee skim

--- MainAnalysis/scripts/Binning.sh
+++ MainAnalysis/scripts/Binning.sh
@@ -9,7 +9,7 @@ run_pp() {
     ./bin/binning configs/populate/serial/populate_${tag}_bkg.conf configs/analysis/${set}/analysis_pp.conf /data/submit/mitay/binning/arc/${set}/populate_${tag}_raw/populate_${tag}_bkg.root > log/${set}_binning_${tag}_bkg.txt 2>&1 &
 }
 
-samples=(pp)
+samples=(pp pp_qcd)
 
 for sample in ${samples[@]}; do
     run_pp ${sample}
