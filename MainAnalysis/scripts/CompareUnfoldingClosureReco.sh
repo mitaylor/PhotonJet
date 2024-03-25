b01cba42c974c19052093f0a738eacb8b34aeb7d
mitaylor <mitay@mit.edu>
2024-03-25 00:35:55 -0400

    compate_unfolding_closure_reco: make closure comparison before unfolding, excluding mebs

--- MainAnalysis/scripts/CompareUnfoldingClosureReco.sh
+++ MainAnalysis/scripts/CompareUnfoldingClosureReco.sh
@@ -5,13 +5,13 @@ set=$1
 run_pp() {
     tag=$1
 
-    ./bin/compare_unfolding_closure_reco configs/compare_unfolding_closure_reco/compare_unfolding_closure_reco_${tag}.conf configs/analysis/${set}/analysis_pp.conf data/arc/${set}/compare_unfolding_closure_reco_${tag}.root
+    ./bin/compare_unfolding_closure_reco configs/compare_unfolding_closure_reco/compare_unfolding_closure_${tag}.conf configs/analysis/${set}/analysis_pp.conf data/arc/${set}/compare_unfolding_closure_reco_${tag}.root
 }
 
 run_aa() {
     tag=$1
 
-    ./bin/compare_unfolding_closure_reco configs/compare_unfolding_closure_reco/compare_unfolding_closure_reco_${tag}.conf configs/analysis/${set}/analysis_aa.conf data/arc/${set}/compare_unfolding_closure_reco_${tag}.root
+    ./bin/compare_unfolding_closure_reco configs/compare_unfolding_closure_reco/compare_unfolding_closure_${tag}.conf configs/analysis/${set}/analysis_aa.conf data/arc/${set}/compare_unfolding_closure_reco_${tag}.root
 }
 
 samples=(pp)
