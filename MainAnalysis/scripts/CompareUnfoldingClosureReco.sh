63b2f266c6b719f1089b5afe53a3a1de2b6e0de8
mitaylor <mitay@mit.edu>
2024-03-25 00:35:17 -0400

    compate_unfolding_closure_reco: make closure comparison before unfolding, excluding mebs

---
+++ MainAnalysis/scripts/CompareUnfoldingClosureReco.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+
+set=$1
+
+run_pp() {
+    tag=$1
+
+    ./bin/compare_unfolding_closure_reco configs/compare_unfolding_closure_reco/compare_unfolding_closure_reco_${tag}.conf configs/analysis/${set}/analysis_pp.conf data/arc/${set}/compare_unfolding_closure_reco_${tag}.root
+}
+
+run_aa() {
+    tag=$1
+
+    ./bin/compare_unfolding_closure_reco configs/compare_unfolding_closure_reco/compare_unfolding_closure_reco_${tag}.conf configs/analysis/${set}/analysis_aa.conf data/arc/${set}/compare_unfolding_closure_reco_${tag}.root
+}
+
+samples=(pp)
+
+for sample in ${samples[@]}; do
+    run_pp ${sample}
+done
+
+samples=(aa)
+
+for sample in ${samples[@]}; do
+    run_aa ${sample}
+
+    for input in mebs_nonclosure; do
+        run_aa ${sample}_${input}
+    done
+done
