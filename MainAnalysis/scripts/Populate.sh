e34c2f84c30a161e1e74dd0b9d5f8724f4acc7f8
mitaylor <mitay@mit.edu>
2024-03-24 21:29:42 -0400

    populate: check the exclusion region and acceptance correction

--- MainAnalysis/scripts/Populate.sh
+++ MainAnalysis/scripts/Populate.sh
@@ -6,7 +6,7 @@ run_pp() {
     tag=$1
 
     ./bin/populate_original configs/populate/serial/populate_${tag}_raw.conf configs/analysis/${set}/analysis_pp.conf /data/submit/mitay/populate/arc/${set}/populate_${tag}_raw/populate_${tag}_raw.root > log/${set}_populate_${tag}_raw.txt 2>&1 &
-    ./bin/populate_original configs/populate/serial/populate_${tag}_bkg.conf configs/analysis/${set}/analysis_pp.conf /data/submit/mitay/populate/arc/${set}/populate_${tag}_raw/populate_${tag}_bkg.root > log/${set}_populate_${tag}_bkg.txt 2>&1 &
+    ./bin/populate_original configs/populate/serial/populate_${tag}_bkg.conf configs/analysis/${set}/analysis_pp.conf /data/submit/mitay/populate/arc/${set}/populate_${tag}_bkg/populate_${tag}_bkg.root > log/${set}_populate_${tag}_bkg.txt 2>&1 &
 }
 
 samples=(pp)
