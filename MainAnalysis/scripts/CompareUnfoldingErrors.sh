092129eada68c966dd6e21b4cd0494af39c262ed
mitaylor <mitay@mit.edu>
2024-04-04 12:41:25 -0400

    compare_unfolding_errors: update to have script

---
+++ MainAnalysis/scripts/CompareUnfoldingErrors.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+set=$1
+
+run_pp() {
+    tag=$1
+
+    ./bin/compare_unfolding_errors configs/compare_unfolding_errors/compare_unfolding_errors_${tag}.conf configs/analysis/${set}/analysis_pp.conf data/arc/${set}/compare_unfolding_errors_${tag}.root
+}
+
+run_aa() {
+    tag=$1
+
+    ./bin/compare_unfolding_errors configs/compare_unfolding_errors/compare_unfolding_errors_${tag}.conf configs/analysis/${set}/analysis_aa.conf data/arc/${set}/compare_unfolding_errors_${tag}.root
+}
+
+samples=(pp)
+
+for sample in ${samples[@]}; do
+    run_pp ${sample}_${input}
+done
+
+samples=(aa)
+
+for sample in ${samples[@]}; do
+    run_aa ${sample}_${input}
+done
