901971983e09951b897f05e070d23bd7c455d5ff
mitaylor <mitay@mit.edu>
2024-04-12 12:22:06 -0400

    quantitate: update for prior and stat matrix systematics

--- MainAnalysis/scripts/Quantitate.sh
+++ MainAnalysis/scripts/Quantitate.sh
@@ -18,41 +18,41 @@ run_aa() {
     ./bin/quantitate configs/quantitate/quantitate_${tag}.conf configs/analysis/${set}/analysis_aa.conf data/arc/${set}/quantitate_${tag}.root
 }
 
-samples=(pp)
-
-for sample in ${samples[@]}; do
-    run_pp ${sample}
-
-    for syst in wo_ele_rej qcd qcd_gen_iso loose_purity tight_purity jeu_up jeu_down jer_up es; do
-        run_pp ${sample}_${syst}
-    done
-done
-
 # samples=(pp)
 
 # for sample in ${samples[@]}; do
-#     for syst in stat_matrix prior_up prior_down; do
+#     run_pp ${sample}
+
+#     for syst in wo_ele_rej qcd qcd_gen_iso loose_purity tight_purity jeu_up jeu_down jer_up es; do
 #         run_pp ${sample}_${syst}
 #     done
 # done
 
-samples=(aa)
+samples=(pp)
 
 for sample in ${samples[@]}; do
-    run_aa ${sample}
-
-    for syst in wo_ele_rej qcd qcd_gen_iso loose_purity tight_purity jeu_up jeu_down jer_up cent_up cent_down es qcd_mebs_nonclosure; do
-        run_aa ${sample}_${syst}
+    for syst in stat_matrix prior; do
+        run_pp ${sample}_${syst}
     done
 done
 
 # samples=(aa)
 
 # for sample in ${samples[@]}; do
-#     for syst in stat_matrix prior_up prior_down; do
+#     run_aa ${sample}
+
+#     for syst in wo_ele_rej qcd qcd_gen_iso loose_purity tight_purity jeu_up jeu_down jer_up cent_up cent_down es qcd_mebs_nonclosure; do
 #         run_aa ${sample}_${syst}
 #     done
 # done
+
+samples=(aa)
+
+for sample in ${samples[@]}; do
+    for syst in stat_matrix prior; do
+        run_aa ${sample}_${syst}
+    done
+done
 
 # samples=(aa)
 
