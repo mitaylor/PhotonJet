633d4c07b67ee56466fd42703c0d7a7775381757
mitaylor <mitay@mit.edu>
2024-04-25 09:28:45 -0400

    theory: update theory comparisons

--- MainAnalysis/scripts/Theory.sh
+++ MainAnalysis/scripts/Theory.sh
@@ -1,21 +1,7 @@
 #!/usr/bin/env bash
 
-./bin/theory configs/theory/theory_aa.conf configs/analysis/pho_40/analysis_aa.conf temp.root
-./bin/theory configs/theory/theory_pp.conf configs/analysis/pho_40/analysis_pp.conf temp.root
-./bin/theory_ratio configs/theory/theory_ratio.conf configs/analysis/pho_40/analysis_aa.conf temp.root
+set=$1
 
-./bin/theory configs/theory/pho_40/theory_aa_hybrid.conf configs/analysis/pho_40/analysis_aa.conf temp.root
-./bin/theory configs/theory/pho_40/theory_pp_hybrid.conf configs/analysis/pho_40/analysis_pp.conf temp.root
-./bin/theory_ratio configs/theory/pho_40/theory_ratio_hybrid.conf configs/analysis/pho_40/analysis_aa.conf temp.root
-
-./bin/theory configs/theory/theory_aa.conf configs/analysis/pho_40_60/analysis_aa.conf temp.root
-./bin/theory configs/theory/theory_pp.conf configs/analysis/pho_40_60/analysis_pp.conf temp.root
-./bin/theory_ratio configs/theory/theory_ratio.conf configs/analysis/pho_40_60/analysis_aa.conf temp.root
-
-./bin/theory configs/theory/theory_aa.conf configs/analysis/pho_60/analysis_aa.conf temp.root
-./bin/theory configs/theory/theory_pp.conf configs/analysis/pho_60/analysis_pp.conf temp.root
-./bin/theory_ratio configs/theory/theory_ratio.conf configs/analysis/pho_60/analysis_aa.conf temp.root
-
-./bin/theory configs/theory/theory_aa.conf configs/analysis/pho_100/analysis_aa.conf temp.root
-./bin/theory configs/theory/theory_pp.conf configs/analysis/pho_100/analysis_pp.conf temp.root
-./bin/theory_ratio configs/theory/theory_ratio.conf configs/analysis/pho_100/analysis_aa.conf temp.root
+./bin/theory configs/theory/theory_aa.conf configs/analysis/${set}/analysis_aa.conf temp.root
+./bin/theory configs/theory/theory_pp.conf configs/analysis/${set}/analysis_pp.conf temp.root
+./bin/theory_ratio configs/theory/theory_ratio.conf configs/analysis/${set}/analysis_aa.conf temp.root
