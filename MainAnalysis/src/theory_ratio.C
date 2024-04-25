bdcde2546a791431e6e7fcc0605369832e5850ec
mitaylor <mitay@mit.edu>
2024-04-25 10:37:21 -0400

    theory_ratio: add additional figures

--- MainAnalysis/src/theory_ratio.C
+++ MainAnalysis/src/theory_ratio.C
@@ -170,7 +170,7 @@ int theory(char const* config, char const* selections, char const* output) {
             bjet_pt[0] = ptg_range[osg[2]];
             bjet_pt[1] = ptg_range[ptg_range.size() - 1 - osg[3]];
         }
-        std::cout << __LINE__ << std::endl;
+        std::cout << __LINE__ << std::endl; std::cout << "aa_base_aa_nominal_s_pure_raw_sub_"s + figure << std::endl;
         auto hist_aa = new history<TH1F>(file_aa, "aa_base_aa_nominal_s_pure_raw_sub_"s + figure);
         auto syst_aa = new history<TH1F>(file_aa, "aa_total_base_aa_nominal_s_pure_raw_sub_"s + figure);
 std::cout << __LINE__ << std::endl;
