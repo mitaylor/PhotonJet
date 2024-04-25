a1f3e62393014cf80c6d2828cc566c5e62dc6e9e
mitaylor <mitay@mit.edu>
2024-04-25 10:29:57 -0400

    theory_ratio: add additional figures

--- MainAnalysis/src/theory_ratio.C
+++ MainAnalysis/src/theory_ratio.C
@@ -238,12 +238,12 @@ int theory(char const* config, char const* selections, char const* output) {
 
         for (size_t i = 0; i < theory_tags.size(); ++i) {
             if (theory_inputs_ratio.size() == 0) {
+                std::cout << theory_figures_aa[i] + "_" + theory_figure << " " << theory_figures_pp[i] + "_" + theory_figure << std::endl;
                 theory_files_aa[i] = new TFile((base + theory_inputs_aa[i]).data(), "read");
                 theory_files_pp[i] = new TFile((base + theory_inputs_pp[i]).data(), "read");
 
                 theory_hists_aa[i] = new history<TH1F>(theory_files_aa[i], theory_figures_aa[i] + "_" + theory_figure);
                 theory_hists_pp[i] = new history<TH1F>(theory_files_pp[i], theory_figures_pp[i] + "_" + theory_figure);
-                std::cout << theory_figures_aa[i] + "_" + theory_figure << " " << theory_figures_pp[i] + "_" + theory_figure << std::endl;
                 theory_ratios[i] = new history<TH1F>(*theory_hists_aa[i], "ratio"s);
 
                 for (int64_t j = 1; j <= (*hist_ratio)[0]->GetNbinsX(); ++j) {
