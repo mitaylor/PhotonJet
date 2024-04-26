e8d136ded528587f1d0d14b6dc40c305d7a782c6
mitaylor <mitay@mit.edu>
2024-04-26 17:22:25 -0400

    gather_theory: update for new tree structure

--- MainAnalysis/src/gather_theory.C
+++ MainAnalysis/src/gather_theory.C
@@ -204,7 +204,9 @@ int gather_theory(char const* config, char const* selections, char const* output
 
         for (int64_t j = 0; j < nentries; ++j) {
             t->GetEntry(j);
-            // std::cout << (*photonPt)[0] << std::endl;
+            if (j < 100) std::cout << (*photonPt)[0] << std::endl;
+            if (j < 100) std::cout << (*photonEta)[0] << std::endl;
+            if (j < 100) std::cout << (*photonPhi)[0] << std::endl;
 
             if ((float) (*photonPt)[0] < photon_pt_min) { continue; }
             if ((float) (*photonPt)[0] > photon_pt_max) { continue; }
