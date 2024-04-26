f2f1f688b7f9d1ddd718cc8f1ebb6836a9937042
mitaylor <mitay@mit.edu>
2024-04-26 07:43:23 -0400

    final_results: add ratio plot

--- MainAnalysis/src/final_results_ratios.C
+++ MainAnalysis/src/final_results_ratios.C
@@ -277,7 +277,7 @@ int congratulate(char const* config, char const* selections, char const* output)
         line.SetLineStyle(kDashed);
 
         /* declare legend */
-        TLegend legend(0.565, 0.65, 0.95, 0.85);
+        TLegend legend(0.565, 0.75, 0.95, 0.85);
         legend.SetTextFont(42);
         legend.SetTextSize(0.07);
         legend.SetFillStyle(0);
