6bd843959bd0db896fe471037c395350518ef1e2
mitaylor <mitay@mit.edu>
2024-04-25 23:50:11 -0400

    final_results: add plot of final results matching previous style

--- MainAnalysis/src/final_results_spectra_ratios.C
+++ MainAnalysis/src/final_results_spectra_ratios.C
@@ -79,7 +79,7 @@ void set_axis(TGaxis &axis)
     axis.SetMaxDigits(6);
     axis.SetNoExponent();
     axis.SetTickLength(0);
-    axis.Draw();
+    // axis.Draw();
 }
 
 int congratulate(char const* config, char const* selections, char const* output) {
@@ -267,7 +267,7 @@ int congratulate(char const* config, char const* selections, char const* output)
         {
             pads[i]->cd();
 
-            // world.Draw("axis");
+            world.Draw("axis");
             (*syst_aa)[i]->Draw("same e2");
             (*syst_pp)[0]->Draw("same e2");
             (*hist_aa)[i]->Draw("same");
