8999e36a04805e54ae66a2675f1276b1d06e2bf9
mitaylor <mitay@mit.edu>
2024-04-29 17:33:47 -0400

    final_results: add jet pt plots as a stack

--- MainAnalysis/src/final_results_data_ratio_spectra.C
+++ MainAnalysis/src/final_results_data_ratio_spectra.C
@@ -133,8 +133,6 @@ int congratulate(char const* config, char const* selections, char const* output)
     auto ymins = conf->get<std::vector<float>>("ymins");
     auto ymaxs = conf->get<std::vector<float>>("ymaxs");
 
-    auto osg_dr = conf->get<std::vector<int64_t>>("osg");
-
     auto ratio = conf->get<bool>("ratio");
     auto spectra = conf->get<bool>("spectra");
 
@@ -158,7 +156,10 @@ int congratulate(char const* config, char const* selections, char const* output)
     auto osg_part1 = sel->get<std::vector<int64_t>>("osg_part1");
     auto osg_part2 = sel->get<std::vector<int64_t>>("osg_part2");
 
-    std::vector<float> bjet_pt = {1.0, 1.0};
+    const int nrows = figures.size();
+    const int npads = 4;
+
+    std::vector<std::vector<float>> bjet_pt(nrows, std::vector<float(2, 0));
 
     /* manage memory manually */
     TH1::AddDirectory(false);
@@ -168,195 +169,211 @@ int congratulate(char const* config, char const* selections, char const* output)
     auto file_aa = new TFile((base + input_aa).data(), "read");
     auto file_pp = new TFile((base + input_pp).data(), "read");
 
-    zip([&](auto const& figure, auto type, auto ymin, auto ymax) {
-        /* define kinematics and luminosity*/
-        switch (type) {
+    /* define kinematics and luminosity */
+    auto text_system = "#sqrt{s_{NN}} = 5.02 TeV, PbPb 1.69 nb^{-1}, pp 302 pb^{-1}"s;
+    auto text_cms = "CMS"s;
+    auto text_photon_pt = to_text(bpho_pt[0]) + " < p_{T}^{#gamma} < "s + to_text(bpho_pt[1]) + " GeV"s;
+    auto text_photon_eta = "|#eta^{#gamma}| < "s + to_text(photon_eta_abs);
+    auto text_dphi = "#Delta#phi_{j#gamma} > #frac{"s + to_text(dphi_min_numerator) + "#pi}{"s + to_text(dphi_min_denominator) + "}"s;
+    auto text_jet_alg = "anti-k_{T} R = 0.3"s;
+    auto text_jet_eta = "|#eta^{j}| < "s + to_text(jet_eta_abs);
+
+    std::vector<history<TH1F>*> hists_aa(nrows);
+    std::vector<history<TH1F>*> systs_aa(nrows);
+    std::vector<history<TH1F>*> hists_pp(nrows);
+    std::vector<history<TH1F>*> systs_pp(nrows);
+    std::vector<history<TH1F>*> hists_ratio(nrows);
+    std::vector<history<TH1F>*> systs_ratio(nrows);
+
+    for (int i = 0; i < nrows; ++i) {
+        /* define jet pT bounds */
+        switch (type[i]) {
         case 1:
-        
-            bjet_pt[0] = ptg_range[osg_part1[2]];
-            bjet_pt[1] = ptg_range[ptg_range.size() - 1 - osg_part1[3]];
+            bjet_pt[i][0] = ptg_range[osg_part1[2]];
+            bjet_pt[i][1] = ptg_range[ptg_range.size() - 1 - osg_part1[3]];
             break;
         case 2:
-        
-            bjet_pt[0] = ptg_range[osg_part2[2]];
-            bjet_pt[1] = ptg_range[ptg_range.size() - 1 - osg_part2[3]];
+            bjet_pt[i][0] = ptg_range[osg_part2[2]];
+            bjet_pt[i][1] = ptg_range[ptg_range.size() - 1 - osg_part2[3]];
             break;
         default:
-        
-            bjet_pt[0] = ptg_range[osg[2]];
-            bjet_pt[1] = ptg_range[ptg_range.size() - 1 - osg[3]];
+            bjet_pt[i][0] = ptg_range[osg[2]];
+            bjet_pt[i][1] = ptg_range[ptg_range.size() - 1 - osg[3]];
         }
 
-        auto text_system = "#sqrt{s_{NN}} = 5.02 TeV, PbPb 1.69 nb^{-1}, pp 302 pb^{-1}"s;
-        auto text_cms = "CMS"s;
-        auto text_photon_pt = to_text(bpho_pt[0]) + " < p_{T}^{#gamma} < "s + to_text(bpho_pt[1]) + " GeV"s;
-        auto text_photon_eta = "|#eta^{#gamma}| < "s + to_text(photon_eta_abs);
-        auto text_dphi = "#Delta#phi_{j#gamma} > #frac{"s + to_text(dphi_min_numerator) + "#pi}{"s + to_text(dphi_min_denominator) + "}"s;
-        auto text_jet_alg = "anti-k_{T} R = 0.3"s;
         auto text_jet_pt = to_text(bjet_pt[0]) + " < p_{T}^{j} < "s + to_text(bjet_pt[1]) + " GeV"s;
-        auto text_jet_eta = "|#eta^{j}| < "s + to_text(jet_eta_abs);
-
+    
         /* get histograms */
-        auto hist_aa = new history<TH1F>(file_aa, "aa_base_aa_nominal_s_pure_raw_sub_" + figure);
-        auto syst_aa = new history<TH1F>(file_aa, "aa_total_base_aa_nominal_s_pure_raw_sub_" + figure);
-        auto hist_pp = new history<TH1F>(file_pp, "pp_base_pp_nominal_s_pure_raw_sub_" + figure);
-        auto syst_pp = new history<TH1F>(file_pp, "pp_total_base_pp_nominal_s_pure_raw_sub_" + figure);
+        hists_aa[i] = new history<TH1F>(file_aa, "aa_base_aa_nominal_s_pure_raw_sub_" + figure);
+        systs_aa[i] = new history<TH1F>(file_aa, "aa_total_base_aa_nominal_s_pure_raw_sub_" + figure);
+        hists_pp[i] = new history<TH1F>(file_pp, "pp_base_pp_nominal_s_pure_raw_sub_" + figure);
+        systs_pp[i] = new history<TH1F>(file_pp, "pp_total_base_pp_nominal_s_pure_raw_sub_" + figure);
+        hists_ratio[i] = new history<TH1F>(*hists_aa[i], "hist");
+        systs_ratio[i] = new history<TH1F>(*systs_aa[i], "syst");
+
+        set_systematics(*hists_aa[i], *systs_aa[i]);
+        set_systematics(*hists_pp[i], *systs_pp[i]);
         
-        auto hist_ratio = new history<TH1F>(*hist_aa, "hist");
-        auto syst_ratio = new history<TH1F>(*syst_aa, "syst");
+        if (ratio)      set_values(*hist_ratio[i], *syst_ratio[i], *hists_aa[i], *systs_aa[i], *hists_pp[i], *systs_pp[i]);
+
+        format(*hists_aa[i], *systs_aa[i], 0);
+        format(*hists_pp[i], *systs_pp[i], 1);
+        format(*hist_ratio[i], *syst_ratio[i], 2);
+    }
+
+    /* size canvas */
+    double panel_size = 500;
+    double padding_width_left = 140;
+    double padding_width_right = 50;
+    double padding_height = 130;
+
+    double canvas_width = panel_size * 4 + padding_width_left + padding_width_right;
+    double canvas_height = panel_size * nrows + padding_height * 2;
+
+    double pad_x0 = padding_width_left / canvas_width;
+    double pad_y0 = padding_height / canvas_height;
+    double pad_dx = panel_size / canvas_width;
+    double pad_dy = panel_size / canvas_height;
+
+    double xmin = bdr[0];
+    double xmax = bdr[1];
+
+    /* declare canvas, pads, axes, and titles */
+    TCanvas canvas("canvas", "", canvas_width, canvas_height);
+
+    std::vector<TH2F*> worlds(nrows);
+    std::vector<std::vector<TPad*>> pads(nrows, std::vector<TPad*> npads);
+    std::vector<TGaxis*> axis_x(npads);
+    std::vector<TGaxis*> axis_y(nrows);
+
+    for (int i = 0; i < nrows; ++i) {
+        worlds[i] = new TH2F("world", ";;", 100, xmin, xmax, 100, ymins[i], ymaxs[i]);
+        worlds[i].SetStats(0);
+
+        pads[i][0] = new TPad("P1", "", pad_x0 + pad_dx * 0, pad_y0 + pad_dy * 0, pad_x0 + pad_dx * 1, pad_y0 + pad_dy * 1, 0);
+        pads[i][1] = new TPad("P2", "", pad_x0 + pad_dx * 1, pad_y0 + pad_dy * 0, pad_x0 + pad_dx * 2, pad_y0 + pad_dy * 1, 0);
+        pads[i][2] = new TPad("P3", "", pad_x0 + pad_dx * 2, pad_y0 + pad_dy * 0, pad_x0 + pad_dx * 3, pad_y0 + pad_dy * 1, 0);
+        pads[i][3] = new TPad("P4", "", pad_x0 + pad_dx * 3, pad_y0 + pad_dy * 0, pad_x0 + pad_dx * 4, pad_y0 + pad_dy * 1, 0);
         
-        set_systematics(hist_aa, syst_aa);
-        set_systematics(hist_pp, syst_pp);
-        
-        if (ratio)      set_values(hist_ratio, syst_ratio, hist_aa, syst_aa, hist_pp, syst_pp);
+        set_pad(*pads[i][0]);
+        set_pad(*pads[i][1]);
+        set_pad(*pads[i][2]);
+        set_pad(*pads[i][3]);
 
-        format(hist_aa, syst_aa, 0);
-        format(hist_pp, syst_pp, 1);
-        format(hist_ratio, syst_ratio, 2);
+        axis_y[i] = new TGaxis(pad_x0 + pad_dx * 0, pad_y0 + pad_dy * i, pad_x0 + pad_dx * 0, pad_y0 + pad_dy * (i + 1), ymins[i], ymaxs[i], 510, "S");
+    }
 
-        /* size canvas */
-        double panel_size = 500;
-        double padding_width_left = 140;
-        double padding_width_right = 50;
-        double padding_height = 130;
+    canvas.cd();
 
-        double canvas_width = panel_size * 4 + padding_width_left + padding_width_right;
-        double canvas_height = panel_size * 1 + padding_height * 2;
+    axis_x[0] = new TGaxis(pad_x0 + pad_dx * 0, pad_y0 + pad_dy * 0, pad_x0 + pad_dx * 1, pad_y0 + pad_dy * 0, xmin, xmax * 0.999, 510, "S");
+    axis_x[1] = new TGaxis(pad_x0 + pad_dx * 1, pad_y0 + pad_dy * 0, pad_x0 + pad_dx * 2, pad_y0 + pad_dy * 0, xmin, xmax * 0.999, 510, "S");
+    axis_x[2] = new TGaxis(pad_x0 + pad_dx * 2, pad_y0 + pad_dy * 0, pad_x0 + pad_dx * 3, pad_y0 + pad_dy * 0, xmin, xmax * 0.999, 510, "S");
+    axis_x[3] = new TGaxis(pad_x0 + pad_dx * 3, pad_y0 + pad_dy * 0, pad_x0 + pad_dx * 4, pad_y0 + pad_dy * 0, xmin, xmax * 0.999, 510, "S");
 
-        double pad_x0 = padding_width_left / canvas_width;
-        double pad_y0 = padding_height / canvas_height;
-        double pad_dx = panel_size / canvas_width;
-        double pad_dy = panel_size / canvas_height;
+    set_axis(axis_x[0]);
+    set_axis(axis_x[1]);
+    set_axis(axis_x[2]);
+    set_axis(axis_x[3]);
 
-        double xmin = bdr[0];
-        double xmax = bdr[1];
+    TLatex latex;
+    latex.SetNDC();
 
-        /* declare canvas, pads, axes, and titles */
-        TCanvas canvas("canvas", "", canvas_width, canvas_height);
+    latex.SetTextFont(42);
+    latex.SetTextSize(0.045);
+    latex.SetTextAlign(22);
+    latex.SetTextAngle(0);
+    latex.DrawLatex(pad_x0 + pad_dx * 0.5, pad_y0 * 0.5, "#deltaj");
+    latex.DrawLatex(pad_x0 + pad_dx * 1.5, pad_y0 * 0.5, "#deltaj");
+    latex.DrawLatex(pad_x0 + pad_dx * 2.5, pad_y0 * 0.5, "#deltaj");
+    latex.DrawLatex(pad_x0 + pad_dx * 3.5, pad_y0 * 0.5, "#deltaj");
 
-        TH2F world("world", ";;", 100, xmin, xmax, 100, ymin, ymax);
-        world.SetStats(0);
+    latex.SetTextFont(42);
+    latex.SetTextSize(0.045);
+    latex.SetTextAlign(22);
+    latex.SetTextAngle(90);
+    if (ratio)      latex.DrawLatex(pad_x0 * 0.4, pad_y0 + pad_dy * nrows * 0.5, "PbPb / pp");
+    if (spectra)    latex.DrawLatex(pad_x0 * 0.4, pad_y0 + pad_dy * nrows * 0.5, "#frac{1}{N_{#gamma}} #frac{dN_{j#gamma}}{d#deltaj}");
 
-        const int npads = 4;
-        TPad *pads[npads];
+    latex.SetTextFont(62);
+    latex.SetTextSize(0.055);
+    latex.SetTextAlign(11);
+    latex.SetTextAngle(0);
+    latex.DrawLatex(pad_x0, pad_y0 * 1.15 + pad_dy * 1, text_cms.c_str());
 
-        pads[0] = new TPad("P1", "", pad_x0 + pad_dx * 0, pad_y0 + pad_dy * 0, pad_x0 + pad_dx * 1, pad_y0 + pad_dy * 1, 0);
-        pads[1] = new TPad("P2", "", pad_x0 + pad_dx * 1, pad_y0 + pad_dy * 0, pad_x0 + pad_dx * 2, pad_y0 + pad_dy * 1, 0);
-        pads[2] = new TPad("P3", "", pad_x0 + pad_dx * 2, pad_y0 + pad_dy * 0, pad_x0 + pad_dx * 3, pad_y0 + pad_dy * 1, 0);
-        pads[3] = new TPad("P4", "", pad_x0 + pad_dx * 3, pad_y0 + pad_dy * 0, pad_x0 + pad_dx * 4, pad_y0 + pad_dy * 1, 0);
+    latex.SetTextFont(42);
+    latex.SetTextSize(0.045);
+    latex.SetTextAlign(31);
+    latex.SetTextAngle(0);
+    latex.DrawLatex(pad_x0 + pad_dx * 4, pad_y0 * 1.15 + pad_dy * 1, text_system.c_str());
 
-        set_pad(*pads[0]);
-        set_pad(*pads[1]);
-        set_pad(*pads[2]);
-        set_pad(*pads[3]);
+    TGraph line;
+    if (ratio)      line.SetPoint(0, xmin, 1);
+    if (ratio)      line.SetPoint(1, xmax, 1);
+    if (spectra)    line.SetPoint(0, xmin, 0);
+    if (spectra)    line.SetPoint(1, xmax, 0);
+    line.SetLineStyle(kDashed);
 
-        canvas.cd();
+    /* declare legend */
+    auto legend_x_min = (ratio) ? 0.75 : 0.65;
+    TLegend legend(0.565, legend_x_min, 0.95, 0.85);
+    legend.SetTextFont(42);
+    legend.SetTextSize(0.07);
+    legend.SetFillStyle(0);
+    legend.SetBorderSize(0);
+    if (ratio)      legend.AddEntry((*syst_ratio)[0], "PbPb/pp", "plf");
+    if (spectra)    legend.AddEntry((*syst_aa)[0], "PbPb", "plf");
+    if (spectra)    legend.AddEntry((*syst_pp)[0], "pp", "plf");
 
-        TGaxis axis_x1(pad_x0 + pad_dx * 0, pad_y0 + pad_dy * 0, pad_x0 + pad_dx * 1, pad_y0 + pad_dy * 0, xmin, xmax * 0.999, 510, "S");
-        TGaxis axis_x2(pad_x0 + pad_dx * 1, pad_y0 + pad_dy * 0, pad_x0 + pad_dx * 2, pad_y0 + pad_dy * 0, xmin, xmax * 0.999, 510, "S");
-        TGaxis axis_x3(pad_x0 + pad_dx * 2, pad_y0 + pad_dy * 0, pad_x0 + pad_dx * 3, pad_y0 + pad_dy * 0, xmin, xmax * 0.999, 510, "S");
-        TGaxis axis_x4(pad_x0 + pad_dx * 3, pad_y0 + pad_dy * 0, pad_x0 + pad_dx * 4, pad_y0 + pad_dy * 0, xmin, xmax * 0.999, 510, "S");
-        TGaxis axis_y1(pad_x0 + pad_dx * 0, pad_y0 + pad_dy * 0, pad_x0 + pad_dx * 0, pad_y0 + pad_dy * 1, ymin, ymax, 510, "S");
-
-        set_axis(axis_x1);
-        set_axis(axis_x2);
-        set_axis(axis_x3);
-        set_axis(axis_x4);
-        set_axis(axis_y1);
-
-        TLatex latex;
-        latex.SetNDC();
-
-        latex.SetTextFont(42);
-        latex.SetTextSize(0.045);
-        latex.SetTextAlign(22);
-        latex.SetTextAngle(0);
-        latex.DrawLatex(pad_x0 + pad_dx * 0.5, pad_y0 * 0.5, "#deltaj");
-        latex.DrawLatex(pad_x0 + pad_dx * 1.5, pad_y0 * 0.5, "#deltaj");
-        latex.DrawLatex(pad_x0 + pad_dx * 2.5, pad_y0 * 0.5, "#deltaj");
-        latex.DrawLatex(pad_x0 + pad_dx * 3.5, pad_y0 * 0.5, "#deltaj");
-
-        latex.SetTextFont(42);
-        latex.SetTextSize(0.045);
-        latex.SetTextAlign(22);
-        latex.SetTextAngle(90);
-        if (ratio)      latex.DrawLatex(pad_x0 * 0.4, pad_y0 + pad_dy * 0.5, "PbPb / pp");
-        if (spectra)    latex.DrawLatex(pad_x0 * 0.4, pad_y0 + pad_dy * 0.5, "#frac{1}{N_{#gamma}} #frac{dN_{j#gamma}}{d#deltaj}");
-
-        latex.SetTextFont(62);
-        latex.SetTextSize(0.055);
-        latex.SetTextAlign(11);
-        latex.SetTextAngle(0);
-        latex.DrawLatex(pad_x0, pad_y0 * 1.15 + pad_dy * 1, text_cms.c_str());
-
-        latex.SetTextFont(42);
-        latex.SetTextSize(0.045);
-        latex.SetTextAlign(31);
-        latex.SetTextAngle(0);
-        latex.DrawLatex(pad_x0 + pad_dx * 4, pad_y0 * 1.15 + pad_dy * 1, text_system.c_str());
-
-        TGraph line;
-        if (ratio)      line.SetPoint(0, xmin, 1);
-        if (ratio)      line.SetPoint(1, xmax, 1);
-        if (spectra)    line.SetPoint(0, xmin, 0);
-        if (spectra)    line.SetPoint(1, xmax, 0);
-        line.SetLineStyle(kDashed);
-
-        /* declare legend */
-        auto legend_x_min = (ratio) ? 0.75 : 0.65;
-        TLegend legend(0.565, legend_x_min, 0.95, 0.85);
-        legend.SetTextFont(42);
-        legend.SetTextSize(0.07);
-        legend.SetFillStyle(0);
-        legend.SetBorderSize(0);
-        if (ratio)      legend.AddEntry((*syst_ratio)[0], "PbPb/pp", "plf");
-        if (spectra)    legend.AddEntry((*syst_aa)[0], "PbPb", "plf");
-        if (spectra)    legend.AddEntry((*syst_pp)[0], "pp", "plf");
-
-        for(int i = 0; i < npads; i++)
+    for (int i = 0; i < nrows; i++) {
+        for(int j = 0; j < npads; j++)
         {
-            pads[i]->cd();
+            pads[i][j]->cd();
 
-            world.Draw("axis");
-            if (ratio)      (*syst_ratio)[i]->Draw("same e2");
-            if (ratio)      (*hist_ratio)[i]->Draw("same");
-            if (spectra)    (*syst_aa)[i]->Draw("same e2");
-            if (spectra)    (*syst_pp)[0]->Draw("same e2");
-            if (spectra)    (*hist_aa)[i]->Draw("same");
-            if (spectra)    (*hist_pp)[0]->Draw("same");
+            world[i]->Draw("axis");
+
+            if (ratio)      (*systs_ratio[i])[j]->Draw("same e2");
+            if (ratio)      (*hists_ratio[i])[j]->Draw("same");
+            if (spectra)    (*systs_aa[i])[j]->Draw("same e2");
+            if (spectra)    (*systs_pp[i])[0]->Draw("same e2");
+            if (spectra)    (*hists_aa[i])[j]->Draw("same");
+            if (spectra)    (*hists_pp[i])[0]->Draw("same");
+
             line.Draw("l");
         }
+    }
 
-        pads[0]->cd();
-        latex.SetTextAlign(31);
-        latex.SetTextSize(0.07);
-        latex.DrawLatex(0.95, 0.9, "Cent. 50-90%");
-        legend.Draw();
+    pads[0][0]->cd();
+    latex.SetTextAlign(31);
+    latex.SetTextSize(0.07);
+    latex.DrawLatex(0.95, 0.9, "Cent. 50-90%");
+    legend.Draw();
 
-        pads[1]->cd();
-        latex.SetTextAlign(31);
-        latex.SetTextSize(0.07);
-        latex.DrawLatex(0.95, 0.9, "Cent. 30-50%");
+    pads[0][1]->cd();
+    latex.SetTextAlign(31);
+    latex.SetTextSize(0.07);
+    latex.DrawLatex(0.95, 0.9, "Cent. 30-50%");
 
-        pads[2]->cd();
-        latex.SetTextAlign(31);
-        latex.SetTextSize(0.07);
-        latex.DrawLatex(0.95, 0.9, "Cent. 10-30%");
+    latex.SetTextAlign(31);
+    latex.SetTextSize(0.06);
+    latex.DrawLatex(0.95, 0.78, (text_jet_pt).c_str());
 
-        pads[3]->cd();
-        latex.SetTextAlign(31);
-        latex.SetTextSize(0.07);
-        latex.DrawLatex(0.95, 0.9, "Cent. 0-10%");
+    pads[0][2]->cd();
+    latex.SetTextAlign(31);
+    latex.SetTextSize(0.07);
+    latex.DrawLatex(0.95, 0.9, "Cent. 10-30%");
 
-        latex.SetTextAlign(31);
-        latex.SetTextSize(0.06);
-        latex.DrawLatex(0.95, 0.78, (text_photon_pt + ", " + text_photon_eta).c_str());
-        latex.DrawLatex(0.95, 0.68, (text_dphi + ", " + text_jet_alg).c_str());
-        latex.DrawLatex(0.95, 0.58, (text_jet_pt + ", " + text_jet_eta).c_str());
+    latex.SetTextAlign(31);
+    latex.SetTextSize(0.06);
+    latex.DrawLatex(0.95, 0.78, (text_photon_pt + ", " + text_photon_eta).c_str());
+    latex.DrawLatex(0.95, 0.68, (text_dphi + ", " + text_jet_alg + ", " + text_jet_eta).c_str());
 
-        if (ratio)      canvas.SaveAs((set + "_final_ratio_" + figure + ".pdf").c_str());
-        if (spectra)    canvas.SaveAs((set + "_final_spectra_" + figure + ".pdf").c_str());
-    }, figures, types, ymins, ymaxs);
+    pads[0][3]->cd();
+    latex.SetTextAlign(31);
+    latex.SetTextSize(0.07);
+    latex.DrawLatex(0.95, 0.9, "Cent. 0-10%");
+
+    if (ratio)      canvas.SaveAs((set + "_final_ratio_" + figure + ".pdf").c_str());
+    if (spectra)    canvas.SaveAs((set + "_final_spectra_" + figure + ".pdf").c_str());
 
     in(output, []() {});
 
