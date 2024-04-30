99a326cb4cf737e3d2f0df8f11fba3157a6fc351
mitaylor <mitay@mit.edu>
2024-04-30 12:47:50 -0400

    final_results: add plot of means

--- MainAnalysis/scripts/FinalResults.sh
+++ MainAnalysis/scripts/FinalResults.sh
@@ -6,3 +6,5 @@ set=$1
 ./bin/final_results_data_ratio_spectra configs/final/final_results_data_ratio_subsets.conf configs/analysis/${set}/analysis_aa.conf test.root
 ./bin/final_results_data_ratio_spectra configs/final/final_results_data_spectra_whole.conf configs/analysis/${set}/analysis_aa.conf test.root
 ./bin/final_results_data_ratio_spectra configs/final/final_results_data_spectra_subsets.conf configs/analysis/${set}/analysis_aa.conf test.root
+./bin/final_results_data_means configs/final/final_results_data_means_whole.conf configs/analysis/${set}/analysis_aa.conf test.root
+./bin/final_results_data_means configs/final/final_results_data_means_subsets.conf configs/analysis/${set}/analysis_aa.conf test.root
