2b85452429161a3ee17489a6643850e2dc416218
mitaylor <mitay@mit.edu>
2024-05-01 10:06:44 -0400

    final_results: add plot of means

--- MainAnalysis/src/final_results_theory_ratio_spectra.C
+++ MainAnalysis/src/final_results_theory_ratio_spectra.C
@@ -172,7 +172,7 @@ int congratulate(char const* config, char const* selections, char const* output)
     auto file_pp = new TFile((base + input_pp).data(), "read");
 
     /* define kinematics and luminosity */
-    suto text_system = "(5.02 TeV)"s;
+    auto text_system = "(5.02 TeV)"s;
     auto text_cms = "CMS"s;
     auto text_photon_pt = to_text(bpho_pt[0]) + " < p_{T}^{#gamma} < "s + to_text(bpho_pt[1]) + " GeV"s;
     auto text_photon_eta = "|#eta^{#gamma}| < "s + to_text(photon_eta_abs);
