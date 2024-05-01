52658fbcdd2aca78428dc76f3e2a19ec51e9dcea
mitaylor <mitay@mit.edu>
2024-05-01 17:16:09 -0400

    final_results: add theory prediction comparisons

--- MainAnalysis/src/final_results_theory_ratio_spectra.C
+++ MainAnalysis/src/final_results_theory_ratio_spectra.C
@@ -533,8 +533,8 @@ int congratulate(char const* config, char const* selections, char const* output)
         if (system == 2)    graphs_hists_ratio_jewel[i].Draw("same lX");
         if (system == 2)    graphs_hists_ratio_jewel_no_recoil[i].Draw("same 3");
         if (system == 2)    graphs_hists_ratio_jewel_no_recoil[i].Draw("same lX");
-        if (system == 2)    graphs_systs_ratio[i]->Draw("same e2");
-        if (system == 2)    graphs_hists_ratio[i]->Draw("same");
+        if (system == 2)    graphs_systs_ratio[i].Draw("same e2");
+        if (system == 2)    graphs_hists_ratio[i].Draw("same");
 
         if (system == 0)    graphs_hists_aa_jewel[i].Draw("same 3");
         if (system == 0)    graphs_hists_aa_jewel[i].Draw("same lX");
@@ -544,15 +544,15 @@ int congratulate(char const* config, char const* selections, char const* output)
         if (system == 0)    graphs_hists_aa_pyquen_no_wide[i].Draw("same lX");
         if (system == 0)    graphs_hists_aa_pyquen[i].Draw("same 3");
         if (system == 0)    graphs_hists_aa_pyquen[i].Draw("same lX");
-        if (system == 0)    graphs_systs_aa[i]->Draw("same e2");
-        if (system == 0)    graphs_hists_aa[i]->Draw("same");
+        if (system == 0)    graphs_systs_aa[i].Draw("same e2");
+        if (system == 0)    graphs_hists_aa[i].Draw("same");
 
         if (system == 1)    graphs_hists_pp_jewel[i].Draw("same 3");
         if (system == 1)    graphs_hists_pp_jewel[i].Draw("same lX");
         if (system == 1)    graphs_hists_pp_pyquen[i].Draw("same 3");
         if (system == 1)    graphs_hists_pp_pyquen[i].Draw("same lX");
-        if (system == 1)    graphs_systs_pp[i]->Draw("same e2");
-        if (system == 1)    graphs_hists_pp[i]->Draw("same");
+        if (system == 1)    graphs_systs_pp[i].Draw("same e2");
+        if (system == 1)    graphs_hists_pp[i].Draw("same");
 
         line.Draw("l");
 
