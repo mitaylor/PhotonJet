44ae85b025ac748013c66ed3427cbfb4a8e2c95b
mitaylor <mitay@mit.edu>
2024-05-15 16:47:50 -0400

    distillate: try to look at the angular resolution

--- MainAnalysis/scripts/Distillate.sh
+++ MainAnalysis/scripts/Distillate.sh
@@ -4,6 +4,7 @@ run() {
     tag=$1
     echo $tag
     ./bin/distillate configs/distillate/distillate_${tag}_scale.conf data/arc/distillate_${tag}_scale.root
+    ./bin/distillate configs/distillate/distillate_${tag}_angle.conf data/arc/distillate_${tag}_angle.root
     # ./bin/distillate_angle configs/distillate/distillate_${tag}_angle_dj.conf data/arc/distillate_${tag}_angle_dj.root
 }
 
