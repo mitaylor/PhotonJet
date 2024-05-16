231b7cfeaa0eec8e38a4f80789a67c15497ccd62
mitaylor <mitay@mit.edu>
2024-05-16 17:46:23 -0400

    gather_hybrid: update for many different options

--- MainAnalysis/src/gather_hybrid.C
+++ MainAnalysis/src/gather_hybrid.C
@@ -24,67 +24,149 @@
 using namespace std::literals::string_literals;
 using namespace std::placeholders;
 
-template <typename... T>
-void scale_bin_width(T*... args) {
-    (void)(int [sizeof...(T)]) { (args->apply([](TH1* obj) {
-        obj->Scale(1., "width"); }), 0)... };
-}
-
-template <typename... T>
-void normalise_to_unity(T*&... args) {
-    (void)(int [sizeof...(T)]) { (args->apply([](TH1* obj) {
-        obj->Scale(1. / obj->Integral("width")); }), 0)... };
-}
-
 int gather_theory(char const* config, char const* output) {
     auto conf = new configurer(config);
 
-    auto inputs = conf->get<std::vector<std::string>>("inputs");
+    auto inputs_selfnorm = conf->get<std::vector<std::string>>("inputs_selfnorm");
+    auto inputs_selfnorm_part1 = conf->get<std::vector<std::string>>("inputs_selfnorm_part1");
+    auto inputs_selfnorm_part2 = conf->get<std::vector<std::string>>("inputs_selfnorm_part2");
+    auto inputs_totnorm = conf->get<std::vector<std::string>>("inputs_totnorm");
+    auto inputs_totnorm_part1 = conf->get<std::vector<std::string>>("inputs_totnorm_part1");
+    auto inputs_totnorm_part2 = conf->get<std::vector<std::string>>("inputs_totnorm_part2");
+
     auto labels = conf->get<std::vector<std::string>>("labels");
 
-    std::vector<float> gdr = { 0.00, 0.02, 0.04, 0.06, 0.08, 0.10, 0.125, 0.15, 0.20, 0.30 };
+    std::vector<float> gdr = { 0.00, 0.01, 0.016, 0.024, 0.032, 0.04, 0.05, 0.06, 0.08, 0.1, 0.15, 0.3 };
 
     /* make histograms */
     auto idr = new interval("#deltaj"s, gdr);
     auto fdr = std::bind(&interval::book<TH1F>, idr, _1, _2, _3);
 
-    std::vector<history<TH1F>*> pp_hists(inputs.size(), nullptr);
-    std::vector<history<TH1F>*> aa_hists(inputs.size(), nullptr);
-    std::vector<history<TH1F>*> ratio_hists(inputs.size(), nullptr);
+    int size = labels.size();
+
+    std::vector<history<TH1F>*> hists_pp_selfnorm(size, nullptr);
+    std::vector<history<TH1F>*> hists_pp_selfnorm_part1(size, nullptr);
+    std::vector<history<TH1F>*> hists_pp_selfnorm_part2(size, nullptr);
+    std::vector<history<TH1F>*> hists_pp_totnorm(size, nullptr);
+    std::vector<history<TH1F>*> hists_pp_totnorm_part1(size, nullptr);
+    std::vector<history<TH1F>*> hists_pp_totnorm_part2(size, nullptr);
+
+    std::vector<history<TH1F>*> hists_aa_selfnorm(size, nullptr);
+    std::vector<history<TH1F>*> hists_aa_selfnorm_part1(size, nullptr);
+    std::vector<history<TH1F>*> hists_aa_selfnorm_part2(size, nullptr);
+    std::vector<history<TH1F>*> hists_aa_totnorm(size, nullptr);
+    std::vector<history<TH1F>*> hists_aa_totnorm_part1(size, nullptr);
+    std::vector<history<TH1F>*> hists_aa_totnorm_part2(size, nullptr);
+
+    std::vector<history<TH1F>*> hists_ratio_selfnorm(size, nullptr);
+    std::vector<history<TH1F>*> hists_ratio_selfnorm_part1(size, nullptr);
+    std::vector<history<TH1F>*> hists_ratio_selfnorm_part2(size, nullptr);
+    std::vector<history<TH1F>*> hists_ratio_totnorm(size, nullptr);
+    std::vector<history<TH1F>*> hists_ratio_totnorm_part1(size, nullptr);
+    std::vector<history<TH1F>*> hists_ratio_totnorm_part2(size, nullptr);
 
     /* manage memory manually */
     TH1::AddDirectory(false);
     TH1::SetDefaultSumw2();
 
     /* load input */
-    for (size_t i = 0; i < inputs.size(); ++i) {
-        std::cout << inputs[i] << std::endl;
-        std::ifstream f(inputs[i].data());
+    for (int i = 0; i < size; ++i) {
+        std::ifstream file_selfnorm(inputs_selfnorm[i].data());
+        std::ifstream file_selfnorm_part1(inputs_selfnorm_part1[i].data());
+        std::ifstream file_selfnorm_part2(inputs_selfnorm_part2[i].data());
+        std::ifstream file_totnorm(inputs_totnorm[i].data());
+        std::ifstream file_totnorm_part1(inputs_totnorm_part1[i].data());
+        std::ifstream file_totnorm_part2(inputs_totnorm_part2[i].data());
 
-        pp_hists[i] = new history<TH1F>("pp_dr_"s + labels[i], "", fdr, 1);
-        aa_hists[i] = new history<TH1F>("aa_dr_"s + labels[i], "", fdr, 1);
-        ratio_hists[i] = new history<TH1F>("ratio_dr_"s + labels[i], "", fdr, 1);
+        hists_pp_selfnorm[i] = new history<TH1F>("pp_selfnorm_"s + labels[i] + "_dr", "", fdr, 1);
+        hists_pp_selfnorm_part1[i] = new history<TH1F>("pp_selfnorm_"s + labels[i] + "_dr_part1", "", fdr, 1);
+        hists_pp_selfnorm_part2[i] = new history<TH1F>("pp_selfnorm_"s + labels[i] + "_dr_part2", "", fdr, 1);
+        hists_pp_totnorm[i] = new history<TH1F>("pp_totnorm_"s + labels[i] + "_dr", "", fdr, 1);
+        hists_pp_totnorm_part1[i] = new history<TH1F>("pp_totnorm_"s + labels[i] + "_dr_part1", "", fdr, 1);
+        hists_pp_totnorm_part2[i] = new history<TH1F>("pp_totnorm_"s + labels[i] + "_dr_part2", "", fdr, 1);
 
-        float bin_center;
-        float pp_center, pp_error;
-        float aa_down, aa_up;
-        float ratio_down, ratio_up;
+        hists_aa_selfnorm[i] = new history<TH1F>("aa_selfnorm_"s + labels[i] + "_dr", "", fdr, 1);
+        hists_aa_selfnorm_part1[i] = new history<TH1F>("aa_selfnorm_"s + labels[i] + "_dr_part1", "", fdr, 1);
+        hists_aa_selfnorm_part2[i] = new history<TH1F>("aa_selfnorm_"s + labels[i] + "_dr_part2", "", fdr, 1);
+        hists_aa_totnorm[i] = new history<TH1F>("aa_totnorm_"s + labels[i] + "_dr", "", fdr, 1);
+        hists_aa_totnorm_part1[i] = new history<TH1F>("aa_totnorm_"s + labels[i] + "_dr_part1", "", fdr, 1);
+        hists_aa_totnorm_part2[i] = new history<TH1F>("aa_totnorm_"s + labels[i] + "_dr_part2", "", fdr, 1);
 
-        for (int64_t j = 1; j <= (*aa_hists[i])[0]->GetNbinsX(); ++j) {  
-            f >> bin_center >> pp_center >> pp_error >> aa_up >> aa_down >> ratio_up >> ratio_down;
+        hists_ratio_selfnorm[i] = new history<TH1F>("ratio_selfnorm_"s + labels[i] + "_dr", "", fdr, 1);
+        hists_ratio_selfnorm_part1[i] = new history<TH1F>("ratio_selfnorm_"s + labels[i] + "_dr_part1", "", fdr, 1);
+        hists_ratio_selfnorm_part2[i] = new history<TH1F>("ratio_selfnorm_"s + labels[i] + "_dr_part2", "", fdr, 1);
+        hists_ratio_totnorm[i] = new history<TH1F>("ratio_totnorm_"s + labels[i] + "_dr", "", fdr, 1);
+        hists_ratio_totnorm_part1[i] = new history<TH1F>("ratio_totnorm_"s + labels[i] + "_dr_part1", "", fdr, 1);
+        hists_ratio_totnorm_part2[i] = new history<TH1F>("ratio_totnorm_"s + labels[i] + "_dr_part2", "", fdr, 1);
 
-            auto pp_hist = (*pp_hists[i])[0];
-            auto aa_hist = (*aa_hists[i])[0];
-            auto ratio_hist = (*ratio_hists[i])[0];
+        float bin_center, pp_center, pp_error, aa_down, aa_up, ratio_down, ratio_up;
 
-            pp_hist->SetBinContent(j, pp_center);
-            pp_hist->SetBinError(j, pp_error);
+        for (size_t j = 1; j < gdr.size(); ++j) {  
+            file_selfnorm >> bin_center >> pp_center >> pp_error >> aa_up >> aa_down >> ratio_up >> ratio_down;
 
-            aa_hist->SetBinContent(j, (aa_up + aa_down)/2);
-            aa_hist->SetBinError(j, (aa_up - aa_down)/2);
+            (*hists_pp_selfnorm[i])[0]->SetBinContent(j, pp_center);
+            (*hists_pp_selfnorm[i])[0]->SetBinError(j, pp_error);
 
-            ratio_hist->SetBinContent(j, (ratio_up + ratio_down)/2);
-            ratio_hist->SetBinError(j, (ratio_up - ratio_down)/2);
+            (*hists_aa_selfnorm[i])[0]->SetBinContent(j, (aa_up + aa_down)/2);
+            (*hists_aa_selfnorm[i])[0]->SetBinError(j, (aa_up - aa_down)/2);
+
+            (*hists_ratio_selfnorm[i])[0]->SetBinContent(j, (ratio_up + ratio_down)/2);
+            (*hists_ratio_selfnorm[i])[0]->SetBinError(j, (ratio_up - ratio_down)/2);
+
+            file_selfnorm_part1 >> bin_center >> pp_center >> pp_error >> aa_up >> aa_down >> ratio_up >> ratio_down;
+
+            (*hists_pp_selfnorm_part1[i])[0]->SetBinContent(j, pp_center);
+            (*hists_pp_selfnorm_part1[i])[0]->SetBinError(j, pp_error);
+
+            (*hists_aa_selfnorm_part1[i])[0]->SetBinContent(j, (aa_up + aa_down)/2);
+            (*hists_aa_selfnorm_part1[i])[0]->SetBinError(j, (aa_up - aa_down)/2);
+
+            (*hists_ratio_selfnorm_part1[i])[0]->SetBinContent(j, (ratio_up + ratio_down)/2);
+            (*hists_ratio_selfnorm_part1[i])[0]->SetBinError(j, (ratio_up - ratio_down)/2);
+
+            file_selfnorm_part2 >> bin_center >> pp_center >> pp_error >> aa_up >> aa_down >> ratio_up >> ratio_down;
+
+            (*hists_pp_selfnorm_part2[i])[0]->SetBinContent(j, pp_center);
+            (*hists_pp_selfnorm_part2[i])[0]->SetBinError(j, pp_error);
+
+            (*hists_aa_selfnorm_part2[i])[0]->SetBinContent(j, (aa_up + aa_down)/2);
+            (*hists_aa_selfnorm_part2[i])[0]->SetBinError(j, (aa_up - aa_down)/2);
+
+            (*hists_ratio_selfnorm_part2[i])[0]->SetBinContent(j, (ratio_up + ratio_down)/2);
+            (*hists_ratio_selfnorm_part2[i])[0]->SetBinError(j, (ratio_up - ratio_down)/2);
+
+            file_totnorm >> bin_center >> pp_center >> pp_error >> aa_up >> aa_down >> ratio_up >> ratio_down;
+
+            (*hists_pp_totnorm[i])[0]->SetBinContent(j, pp_center);
+            (*hists_pp_totnorm[i])[0]->SetBinError(j, pp_error);
+
+            (*hists_aa_totnorm[i])[0]->SetBinContent(j, (aa_up + aa_down)/2);
+            (*hists_aa_totnorm[i])[0]->SetBinError(j, (aa_up - aa_down)/2);
+
+            (*hists_ratio_totnorm[i])[0]->SetBinContent(j, (ratio_up + ratio_down)/2);
+            (*hists_ratio_totnorm[i])[0]->SetBinError(j, (ratio_up - ratio_down)/2);
+
+            file_totnorm_part1 >> bin_center >> pp_center >> pp_error >> aa_up >> aa_down >> ratio_up >> ratio_down;
+
+            (*hists_pp_totnorm_part1[i])[0]->SetBinContent(j, pp_center);
+            (*hists_pp_totnorm_part1[i])[0]->SetBinError(j, pp_error);
+
+            (*hists_aa_totnorm_part1[i])[0]->SetBinContent(j, (aa_up + aa_down)/2);
+            (*hists_aa_totnorm_part1[i])[0]->SetBinError(j, (aa_up - aa_down)/2);
+
+            (*hists_ratio_totnorm_part1[i])[0]->SetBinContent(j, (ratio_up + ratio_down)/2);
+            (*hists_ratio_totnorm_part1[i])[0]->SetBinError(j, (ratio_up - ratio_down)/2);
+
+            file_totnorm_part2 >> bin_center >> pp_center >> pp_error >> aa_up >> aa_down >> ratio_up >> ratio_down;
+
+            (*hists_pp_totnorm_part2[i])[0]->SetBinContent(j, pp_center);
+            (*hists_pp_totnorm_part2[i])[0]->SetBinError(j, pp_error);
+
+            (*hists_aa_totnorm_part2[i])[0]->SetBinContent(j, (aa_up + aa_down)/2);
+            (*hists_aa_totnorm_part2[i])[0]->SetBinError(j, (aa_up - aa_down)/2);
+
+            (*hists_ratio_totnorm_part2[i])[0]->SetBinContent(j, (ratio_up + ratio_down)/2);
+            (*hists_ratio_totnorm_part2[i])[0]->SetBinError(j, (ratio_up - ratio_down)/2);
         }
 
         f.close();
@@ -93,9 +175,26 @@ int gather_theory(char const* config, char const* output) {
     /* save histograms */
     in(output, [&]() {
         for (size_t i = 0; i < inputs.size(); ++i) {
-            pp_hists[i]->save();
-            aa_hists[i]->save();
-            ratio_hists[i]->save();
+            hists_pp_selfnorm[i]->save();
+            hists_pp_selfnorm_part1[i]->save();
+            hists_pp_selfnorm_part2[i]->save();
+            hists_pp_totnorm[i]->save();
+            hists_pp_totnorm_part1[i]->save();
+            hists_pp_totnorm_part2[i]->save();
+
+            hists_aa_selfnorm[i]->save();
+            hists_aa_selfnorm_part1[i]->save();
+            hists_aa_selfnorm_part2[i]->save();
+            hists_aa_totnorm[i]->save();
+            hists_aa_totnorm_part1[i]->save();
+            hists_aa_totnorm_part2[i]->save();
+
+            hists_ratio_selfnorm[i]->save();
+            hists_ratio_selfnorm_part1[i]->save();
+            hists_ratio_selfnorm_part2[i]->save();
+            hists_ratio_totnorm[i]->save();
+            hists_ratio_totnorm_part1[i]->save();
+            hists_ratio_totnorm_part2[i]->save();
         }
     });
 
