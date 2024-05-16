9849464784a2a0f57608eeb013a766dac21c50ea
mitaylor <mitay@mit.edu>
2024-05-16 09:36:02 -0400

    fabulate: try to look at the angular resolution

--- MainAnalysis/scripts/Fabulate.sh
+++ MainAnalysis/scripts/Fabulate.sh
@@ -3,8 +3,8 @@
 run() {
     tag=$1
     echo $tag
-    ./bin/fabulate configs/fabulate/fabulate_${tag}.conf data/arc/fabulate_${tag}.root
-    # ./bin/fabulate_angle configs/fabulate/fabulate_${tag}_angle.conf data/arc/fabulate_${tag}_angle.root
+    # ./bin/fabulate configs/fabulate/fabulate_${tag}.conf data/arc/fabulate_${tag}.root
+    ./bin/fabulate_angle configs/fabulate/fabulate_${tag}_angle.conf data/arc/fabulate_${tag}_angle.root
     # echo "./bin/fabulate_angle_smeared configs/fabulate/fabulate_${tag}_angle.conf data/arc/fabulate_${tag}_angle.root"
 }
 
