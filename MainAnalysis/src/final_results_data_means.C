3726652ac459c12434306d422870ae409341bbfa
mitaylor <mitay@mit.edu>
2024-05-18 11:01:42 -0400

    final: add preliminary label

--- MainAnalysis/src/final_results_data_means.C
+++ MainAnalysis/src/final_results_data_means.C
@@ -185,7 +185,7 @@ int congratulate(char const* config, char const* selections, char const* output)
 
     /* define kinematics and luminosity */
     auto text_system = "PbPb 1.69 nb^{-1}, pp 302 pb^{-1} (5.02 TeV)"s;
-    auto text_cms = "CMS"s;
+    auto text_cms = "CMS #scale[0.8]{#font[52]{Preliminary}}"s;
     auto text_photon_pt = to_text(bpho_pt[0]) + " < p_{T}^{#gamma} < "s + to_text(bpho_pt[1]) + " GeV"s;
     auto text_photon_eta = "|#eta^{#gamma}| < "s + to_text(photon_eta_abs);
     auto text_dphi = "#Delta#phi_{j#gamma} > #frac{"s + to_text(dphi_min_numerator) + "#pi}{"s + to_text(dphi_min_denominator) + "}"s;
