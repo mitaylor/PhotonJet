23d3abd8d8c87c906985a8f529bbc6b1a33454e6
mitaylor <mitay@mit.edu>
2024-06-24 21:29:42 -0400

    final: increase size of axes labels

--- MainAnalysis/src/final_results_theory_ratio_spectra.C
+++ MainAnalysis/src/final_results_theory_ratio_spectra.C
@@ -609,8 +609,8 @@ int congratulate(char const* config, char const* selections, char const* output)
 
     if (system == 4 && !subsets)    legend_y_min = legend_y_max;
     if (system == 4 && !subsets)    legend_y_max = legend_y_min + 0.07;
-    if (system == 4 && !subsets)    legend_x_min = legend_x_min - 0.5;
-    if (system == 4 && !subsets)    legend_x_max = legend_x_max - 0.5;
+    if (system == 4 && !subsets)    legend_x_min = legend_x_min - 0.3;
+    if (system == 4 && !subsets)    legend_x_max = legend_x_max - 0.3;
 
     TLegend legend_part2(legend_x_min, legend_y_min, legend_x_max, legend_y_max);
     legend_part2.SetTextFont(42);
