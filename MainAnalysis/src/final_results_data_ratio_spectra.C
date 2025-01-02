96296f5a3eb5fb0bf220db3e152c575b9bc78f96
mitaylor <mitay@mit.edu>
2025-01-02 13:45:53 -0500

    final: minor plotting updates

--- MainAnalysis/src/final_results_data_ratio_spectra.C
+++ MainAnalysis/src/final_results_data_ratio_spectra.C
@@ -627,7 +627,7 @@ int congratulate(char const* config, char const* selections, char const* output)
         boxes[i]->AddText((text_jet_pt).c_str());
     
         pads[i][0]->cd();
-        text[i]->Draw("same");
+        boxes[i]->Draw("same");
         // latex.SetTextAlign(22);
         // latex.SetTextSize(0.06 / factor_y * factor_x);
         // latex.DrawLatex(0.5, 1 - 0.2 / factor_y * factor_x, (text_jet_pt).c_str());
